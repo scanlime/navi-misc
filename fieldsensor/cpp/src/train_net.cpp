7cd23d2624c4ac7a714e29e15855b3a1bb837ff8
Micah Elizabeth Scott <micah@scanlime.org>
2003-02-28 12:50:17 +0000

    More utilities- should be able to train a net now

---
+++ fieldsensor/cpp/src/train_net.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <annie.h>
+using namespace std;
+
+int main(void) {
+  try {
+    annie::TwoLayerNetwork net("data/current.net");
+    annie::TrainingSet pattern("data/4x4cube.pattern");
+    
+    while (1) {
+      net.train(pattern, 100, 0.8, 0.0, true);
+      net.save("data/current.net");
+      cout << "Saved." << endl;
+    }
+  }
+  catch (annie::Exception &e) {
+    cout << e.what() << endl;
+  }
+  
+  return 0;
+}
