52fa925efc0ac5c89272f1d26c86dc42fd66b894
Micah Elizabeth Scott <micah@scanlime.org>
2003-07-04 04:19:22 +0000

    Adding a beautifully untested spiffy little module to make wrapping C/C++ data types really easy

--- pybzflag/test.py
+++ pybzflag/test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
-import BZFlag.ListServer 
-for server in BZFlag.ListServer.getDefault().list():
-    print server.title
+
+#import BZFlag.ListServer 
+#for server in BZFlag.ListServer.getDefault().list():
+#    print server.title
+
+import BZFlag.Protocol
+print BZFlag.Protocol.Struct('boing')
