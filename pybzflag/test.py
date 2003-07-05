117037a2ccae65f9df97c88d8bdacca48f361085
Micah Elizabeth Scott <micah@scanlime.org>
2003-07-05 07:42:19 +0000

    Better event handling architecture for messages

--- pybzflag/test.py
+++ pybzflag/test.py
@@ -16,21 +16,9 @@ else:
     serverName = server.name
     print server.info()
 
-class TestClient(BZFlag.Client.BaseClient):
-    def onConnect(self):
-        print "Connected, got client id %d" % self.id
-        print "Trying to join game..."
-        self.enterGame(BZFlag.Player.Identity("Bob the Avenger"))
+class TestClient(BZFlag.Client.PlayerClient):
+    pass
 
-    def expectMessage(self, socket, eventLoop):
-        msg = socket.readMessage()
-        print msg.__class__.__name__,
-        if isinstance(msg, BZFlag.Protocol.FromServer.MsgPlayerUpdate):
-            print "- Player %d at %s" % (msg.id, msg.position),
-        if isinstance(msg, BZFlag.Protocol.FromServer.MsgMessage):
-            message = msg.message[:msg.message.find(chr(0))]
-            print "- From %d, To %d: '%s'" % (msg.fromId, msg.toId, message),
-        print
-
-
-TestClient(serverName).run()
+playerIdent = BZFlag.Player.Identity("Bob the Avenger")
+client = TestClient(serverName, playerIdent)
+client.run()
