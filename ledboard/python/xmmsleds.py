c0f2bcf9830ea169ed652df63253fe015075d1d9
Micah Elizabeth Scott <micah@scanlime.org>
2003-08-27 20:49:36 +0000

    revert an accidental commit that blew away lots of code in the ledboard python xmms monitor

--- ledboard/python/xmmsleds.py
+++ ledboard/python/xmmsleds.py
@@ -1,60 +1,422 @@
 #!/usr/bin/env python
 #
-# A copy of the VFD code from the LED Board v2 project, hacked up a bit
-# to work with the USB VFD controller as a real-world test application.
+# XMMS visualization and information display on the LED Board and VFD.
+# Requires the pyxmms, pyesdm ledboard, and Numeric modules!
 #
+import ledboard, xmms, time, esd, os, socket, Numeric, FFT, math, random
 
-import xmms, time, os
-
-class CenturyVFD:
-    width = 20
-    lines = 2
-
+class ESDmonitor:
+    "Monitors EsounD's output stream, returning a NumPy array"
     def __init__(self):
-        self.dev = open("/dev/usb/widget", "w")
+        self.rate = 44100
+        fd = esd.esd_monitor_stream(esd.ESD_MONO,self.rate,'localhost','')
+        self.socket = socket.fromfd(fd, socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(0)
+        
+    def read(self):
+        data = ''
+        while True:
+            try:
+                data += self.socket.recv(4096)
+            except socket.error:
+                break
+        return Numeric.fromstring(data, Numeric.Int16)
 
-    def write(self, data):
-        "Write a string, handling newlines"
-        lines = data.split('\n')
-        lastLine = lines[-1]
-        for l in lines[:-1]:
-            self.writeVFD(l)
-            self.nl()
-        self.writeVFD(lastLine)
 
-    def writeVFD(self, data):
-        self.dev.write(data)
+class AudioAnalyzer:
+    def __init__(self):
+        self.monitor = ESDmonitor()
+        self.vuMeterSampler = NormalizedFreqRange(self, 20, 10000)
+        self.spectrumSamplers = []
+        self.oldSpectrum = Numeric.zeros(8)
+        fundamental = 30
+        octavesPerLED = 1
+        for i in range(8):
+            f1 = fundamental*(1<<(i*octavesPerLED))
+            f2 = fundamental*(1<<((i+1)*octavesPerLED))
+            self.spectrumSamplers.append(NormalizedFreqRange(self,f1,f2))
 
-    def cr(self):
-        self.dev.write("\r")
+    def freqIndex(self, hz):
+        return int(hz / (self.monitor.rate/1000.0))
 
-    def lf(self):
-        self.dev.write("\n")
+    def freqRange(self, fromHz, toHz):
+        idx1 = self.freqIndex(fromHz)
+        idx2 = self.freqIndex(toHz)
+        numBands = idx2 - idx1 + 1
+        return Numeric.add.reduce(self.freqDomain[idx1:idx2])
 
-    def nl(self):
-        self.cr()
-        self.lf()
+    def sample(self):
+        newData = self.monitor.read()
+        if len(newData) == 0:
+            return
+        self.timeDomain = newData
 
-    def clear(self):
-        self.writeVFD(chr(0x15))
+        # Round our actual sample size down to the nearest power of two for the FFT,
+        # and only save the absolute value of the first half of the real part.
+        self.numSamples = len(self.timeDomain)
+        f = 1
+        while f <= self.numSamples:
+            self.fftSamples = f
+            f *= 2
+        self.freqDomain = FFT.real_fft(self.timeDomain, self.fftSamples).real
+        self.freqDomain = Numeric.fabs(self.freqDomain[:len(self.freqDomain)/2])
 
-    def home(self):
-        self.writeVFD(chr(0x16))
+        self.spectrum = []
+        for s in self.spectrumSamplers:
+            self.spectrum.append(s.get())
+        self.spectrum = Numeric.array(self.spectrum)
 
-    def writeScreen(self, data):
-        "Write a complete screen of data, without clearing the screen"
-        lines = data.split('\n')
-        while len(lines)<self.lines:
-            lines.append("")
-        self.home()
-        for l in lines:
-            if len(l) < self.width:
-                l += " " * (self.width - len(l))
-            elif len(l) > self.width:
-                l = l[:self.width]
-            self.writeVFD(l)
-        self.dev.flush()
-        print "writeScreen"
+        self.beat = self.spectrum - self.oldSpectrum
+        self.oldSpectrum = self.spectrum
+
+        self.vuMeter = self.vuMeterSampler.get()
+
+
+class NormalizedFreqRange:
+    def __init__(self, signal, fromHz, toHz):
+        self.max = 0
+        self.signal = signal
+        self.fromHz = fromHz
+        self.toHz = toHz
+
+    def get(self):
+        # Decay our max slowly to account for changes in peak volume between songs
+        self.max *= 0.999                                            
+
+        raw = self.signal.freqRange(self.fromHz, self.toHz)
+        if raw > self.max:
+            self.max = raw
+        if self.max > 0:
+            return float(raw) / self.max
+        else:
+            return 0
+
+
+class VisualizerState:
+    def __init__(self, vis):
+        self.vis = vis
+        self.led = vis.led
+
+    def init(self):
+        pass
+    def slowUpdate(self):
+        pass
+    def fastUpdate(self):
+        pass
+
+    def transition(self, nextClass):
+        self.vis.state = nextClass(self.vis)
+        self.vis.state.init()
+
+
+class IdleState(VisualizerState):
+    def init(self):
+        self.frame = 0
+
+    def fastUpdate(self):
+        self.frame += 1
+        if self.frame == 2:
+            self.led.fade()
+            self.led.blit()
+            self.frame = 0
+
+    def slowUpdate(self):
+        if xmms.is_paused():
+            self.transition(PausedState)
+        elif xmms.is_playing():
+            self.transition(BeginPlayingState)
+
+
+class StoppedState(VisualizerState):
+    def init(self):
+        self.led.clear()
+        ledboard.GridPattern([
+            '     ',
+            '     ',
+            'ooooo',
+            'o   o',
+            'o   o',
+            'o   o',
+            'ooooo',
+            '     ',
+            ]).set(self.led)
+        self.led.blit()
+        self.timeout = 10
+
+    def slowUpdate(self):
+        self.timeout -= 1
+        if self.timeout < 0:
+            self.transition(IdleState)
+        if xmms.is_paused():
+            self.transition(PausedState)
+        elif xmms.is_playing():
+            self.transition(BeginPlayingState)
+
+
+class PausedState(VisualizerState):
+    def init(self):
+        self.theta = 0
+        self.led.clear()
+        ledboard.GridPattern([
+            '     ',
+            '     ',
+            ' o o ',
+            ' o o ',
+            ' o o ',
+            ' o o ',
+            '     ',
+            '     ',
+            ]).set(self.led)
+        self.led.blit()
+
+    def wavy(self, x):
+        return math.sin(self.theta + x*0.8)*7+8
+
+    def upperWavyFunc(self, led):
+        return self.wavy(led['x'])
+
+    def lowerWavyFunc(self, led):
+        return self.wavy(7-led['x'])
+
+    def fastUpdate(self):
+        self.theta += 0.15
+        self.theta %= 2*3.1415926
+        led.set(led.select({'group': 'bars', 'color': 'yellow'}), self.upperWavyFunc)
+        led.set(led.select({'group': 'bars', 'color': 'green' }), self.lowerWavyFunc)
+        led.blit()
+
+    def slowUpdate(self):
+        if not xmms.is_paused():
+            if xmms.is_playing():
+                self.transition(BeginPlayingState)
+            else:
+                self.transition(StoppedState)
+
+
+class BeginPlayingState(VisualizerState):
+    def init(self):
+        self.led.clear()
+        self.pattern = ledboard.GridPattern([
+            '      ',
+            'o     ',
+            'oo    ',
+            'o.o   ',
+            'o..o  ',
+            'o.o   ',
+            'oo    ',
+            'o     ',
+            ])
+        self.pattern.set(self.led)
+        self.led.blit()
+        self.timeout = 5
+
+    def slowUpdate(self):
+        self.pattern.scrollRight()
+        self.pattern.set(self.led)
+        self.led.blit()
+        self.timeout -= 1
+        if xmms.is_paused():
+            self.transition(PausedState)
+        elif not xmms.is_playing():
+            self.transition(StoppedState)
+        if self.timeout < 0:
+            self.transition(BeginPlayingFaderState)
+
+                            
+class BeginPlayingFaderState(VisualizerState):
+    def init(self):
+        self.frame = 0
+        self.timeout = 2
+
+    def fastUpdate(self):
+        self.frame += 1
+        if self.frame == 2:
+            self.led.fade()
+            self.led.blit()
+            self.frame = 0
+
+    def slowUpdate(self):
+        self.timeout -= 1
+        if self.timeout < 0:
+            self.transition(PlayingState)
+
+
+class VisualizerEffect:
+    def __init__(self, vis):
+        self.led = vis.led
+        self.vis = vis
+
+    def init(self):
+        pass
+
+    def fastUpdate(self):
+        pass
+
+    def slowUpdate(self):
+        pass
+        
+
+class SpectrogramEffect(VisualizerEffect):
+    def __init__(self,vis):
+        VisualizerEffect.__init__(self,vis)
+        self.grid = self.led.select({'group': 'grid'})
+        self.spectrogram = [[0]*8]*5
+
+    def spectrogramFunc(self, led):
+        return pow(self.spectrogram[led['x']][led['y']],1.8)*15.99
+
+    def slowUpdate(self):
+        self.spectrogram = [Numeric.fabs(self.vis.signal.spectrum)] + self.spectrogram[:-1]
+        self.led.set(self.grid, self.spectrogramFunc)
+
+
+class BeatMapEffect(VisualizerEffect):
+    def __init__(self, vis, criteria, axis='x', axisScale=1, axisBias=0, threshold=0.5, fadeSpeed=0.3):
+        VisualizerEffect.__init__(self,vis)
+        self.group = self.led.select(criteria)
+        self.axis = axis
+        self.axisScale = axisScale
+        self.axisBias = axisBias
+        self.threshold = threshold
+        self.fadeSpeed = fadeSpeed
+
+    def func(self, led):
+        if self.vis.signal.beat[led[self.axis]*self.axisScale+self.axisBias] > self.threshold:
+            return 15
+        
+    def fastUpdate(self):
+        self.led.fade(self.group, self.fadeSpeed)
+        self.led.set(self.group, self.func)
+        
+
+class VUMeterEffect(VisualizerEffect):
+    def __init__(self, vis, criteria, axis='x'):
+        VisualizerEffect.__init__(self,vis)
+        self.group = self.led.select(criteria)
+        self.axis = axis
+
+    def vuFunc(self, led):
+        if led[self.axis] <= self.vis.signal.vuMeter*8:
+            return 15
+
+    def fastUpdate(self):
+        self.led.fade(self.group, 3)
+        self.led.set(self.group, self.vuFunc)
+
+
+class VUBrightnessEffect(VisualizerEffect):
+    def __init__(self, vis, criteria, fadeGroup=None, fadeSpeed=0.2):
+        VisualizerEffect.__init__(self,vis)
+        self.group = self.led.select(criteria)
+        self.fadeGroup = self.led.select(fadeGroup)
+        self.fadeSpeed = fadeSpeed
+        
+    def fastUpdate(self):
+        self.led.fade(self.fadeGroup, self.fadeSpeed)
+        self.led.set(self.group, self.vis.signal.vuMeter * 15.9)
+
+
+class DotAnalyzerEffect(VisualizerEffect):
+    def __init__(self, vis, fadeSpeed=0.9):
+        VisualizerEffect.__init__(self,vis)
+        self.grid = self.led.select({'group': 'grid'})
+        self.fadeSpeed = fadeSpeed
+
+    def analyzerFunc(self, led):
+        if int(self.vis.signal.spectrum[led['y']]*5.9) == led['x']:
+            return 15
+
+    def fastUpdate(self):
+        self.led.fade(self.grid, self.fadeSpeed)
+        self.led.set(self.grid, self.analyzerFunc)
+
+        
+class FadeAnalyzerEffect(VisualizerEffect):
+    def __init__(self, vis, fadeSpeed=1.2):
+        VisualizerEffect.__init__(self,vis)
+        self.grid = self.led.select({'group': 'grid'})
+        self.fadeSpeed = fadeSpeed
+
+    def analyzerFunc(self, led):
+        if self.vis.signal.spectrum[led['y']]*5.9 >= led['x']:
+            return 15
+
+    def fastUpdate(self):
+        self.led.fade(self.grid, self.fadeSpeed)
+        self.led.set(self.grid, self.analyzerFunc)
+        
+
+class PlayingState(VisualizerState):
+    def init(self):
+
+        # Exactly one effect from each group will be active at once
+        self.effectdb = [
+            # LED grid effects
+            [ [SpectrogramEffect(self.vis)],
+              [DotAnalyzerEffect(self.vis)],
+              [FadeAnalyzerEffect(self.vis)],
+              [FadeAnalyzerEffect(self.vis, 5)],
+              ],
+            # Horizontal bar effects
+            [ [VUMeterEffect(self.vis, {'group': 'bars'})],
+              [BeatMapEffect(self.vis, {'group': 'bars'})],
+              ],
+            # Dot effects
+            [ [VUBrightnessEffect(self.vis, {'group': 'dot'})],
+              ],
+            # Pacman effects
+            [ [BeatMapEffect(self.vis, {'group': 'pacman'}, 'x', 1, 0)],
+              [VUBrightnessEffect(self.vis, {'group': 'pacman', 'color': 'blue'}, {'group': 'pacman'})],
+              [VUBrightnessEffect(self.vis, {'group': 'pacman', 'color': 'yellow'}, {'group': 'pacman'})],
+              [VUBrightnessEffect(self.vis, {'group': 'pacman', 'color': 'red'}, {'group': 'pacman'})],
+              ],               
+            # Corner effects
+            [ [BeatMapEffect(self.vis, {'group': 'corners', 'color': 'blue'},  'x', 0, 0, 0.3),
+               BeatMapEffect(self.vis, {'group': 'corners', 'color': 'white'}, 'x', 0, 4)],
+	      ],
+            ]
+        
+        self.resetEffect()
+
+    def resetEffect(self):
+        # Time limit, in slowUpdate units (nominally deciseconds) for one set of effects to run
+        self.timeout = 50
+
+        self.effects = []
+        for group in self.effectdb:
+            self.effects += random.choice(group)
+
+    def fastUpdate(self):
+        for e in self.effects:
+            e.fastUpdate()
+        self.led.blit()
+
+    def slowUpdate(self):
+        for e in self.effects:
+            e.slowUpdate()
+        self.timeout -= 1
+        if xmms.is_paused():
+            self.transition(PausedState)
+        elif not xmms.is_playing():
+            self.transition(StoppedState)
+        if self.timeout < 0:
+            self.resetEffect()
+
+
+class Visualizer:
+    def __init__(self, led):
+        self.led = led
+        self.signal = AudioAnalyzer()
+        self.state = IdleState(self)
+        self.state.init()
+
+    def fastUpdate(self):
+        self.signal.sample()
+        self.state.fastUpdate()
+
+    def slowUpdate(self):
+        self.state.slowUpdate()
+
 
 class VFDupdater:
     "Collects information from XMMS and displays it on the VFD panel"
@@ -87,7 +449,7 @@ class VFDupdater:
                 else:
                     title = ("%s%s%s" % (title, " " * self.scrollerGap, title))[self.scrollerIndex:]
             self.scrollerIndex += 1
-
+    
         # get the current time index
         if xmms.is_playing():
             millisec = xmms.get_output_time()
@@ -113,8 +475,10 @@ class VFDupdater:
 
 
 if __name__ == '__main__':
-    vfdupdater = VFDupdater(CenturyVFD())
-    components = [vfdupdater]
+    led = ledboard.Server()
+    vfdupdater = VFDupdater(led.vfd)
+    visualizer = Visualizer(led)
+    components = [vfdupdater, visualizer]
     then = time.time()
 
     while True:
