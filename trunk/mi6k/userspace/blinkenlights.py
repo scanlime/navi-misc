bb92703c9c78ec5048a6cefe9b807b7ba26993e4
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-08-29 18:49:29 +0000

    A more interesting test- one LED brightness is set by a sine wave, the other one flashes with exponential decay

--- trunk/mi6k/userspace/blinkenlights.py
+++ trunk/mi6k/userspace/blinkenlights.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import time
-from math import sin
+from math import *
 from fcntl import ioctl
 
 f = open("/dev/usb/widget", "w")
@@ -8,7 +8,11 @@ f = open("/dev/usb/widget", "w")
 startTime = time.time()
 while True:
     t = time.time() - startTime
-    blue  = sin(t * 5) * 0.4 + 0.6
-    white = sin(t * 40) * 0.4 + 0.6
-    ioctl(f, 2, (int(blue * 0x3FF) << 16) | int(white * 0x3FF))
+
+    blue  = exp((t % 2) * -10)
+    white = sin(t * 20) * 0.4 + 0.6
+
+    ioctl(f, 2,
+          (int(min(1, max(0, blue)) * 0x3FF) << 16) |
+          int(min(1, max(0, white)) * 0x3FF))
     time.sleep(0.01)
