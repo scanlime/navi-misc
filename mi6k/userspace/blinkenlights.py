edf7387af0a2ee6aa5a53d78fa885a040de29e96
Micah Elizabeth Scott <micah@scanlime.org>
2003-08-29 08:50:15 +0000

    Yay, fadey-widget

---
+++ mi6k/userspace/blinkenlights.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import time
+from math import sin
+from fcntl import ioctl
+
+f = open("/dev/usb/widget", "w")
+
+t = 0
+while True:
+    l = sin(t) * 0.4 + 0.6
+    ioctl(f, 2, int(l * 0x3FF) << 16)
+    t += 0.015
+    time.sleep(0.01)
