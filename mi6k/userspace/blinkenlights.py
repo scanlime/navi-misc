8c4df4f2470173011fc94be5df184cbc7c989d73
Micah Elizabeth Scott <micah@scanlime.org>
2003-08-29 18:40:48 +0000

    Swappify LEDs

--- mi6k/userspace/blinkenlights.py
+++ mi6k/userspace/blinkenlights.py
@@ -5,9 +5,10 @@ from fcntl import ioctl
 
 f = open("/dev/usb/widget", "w")
 
-t = 0
+startTime = time.time()
 while True:
-    l = sin(t) * 0.4 + 0.6
-    ioctl(f, 2, int(l * 0x3FF) << 16)
-    t += 0.05
+    t = time.time() - startTime
+    blue  = sin(t * 5) * 0.4 + 0.6
+    white = sin(t * 40) * 0.4 + 0.6
+    ioctl(f, 2, (int(blue * 0x3FF) << 16) | int(white * 0x3FF))
     time.sleep(0.01)
