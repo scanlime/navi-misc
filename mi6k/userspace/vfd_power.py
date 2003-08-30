c32cebd560bd51b34ab23c1bdc022e987f5b5183
Micah Elizabeth Scott <micah@scanlime.org>
2003-08-30 20:41:58 +0000

    New and improved xmmsvfd demo to show off the speed our new hardware has

--- mi6k/userspace/vfd_power.py
+++ mi6k/userspace/vfd_power.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import sys
 from fcntl import ioctl
-f = open("/dev/usb/widget", "w")
+f = open("/dev/usb/mi6k0", "w")
 ioctl(f, 0x3600, int(sys.argv[1]))
--- mi6k/userspace/xmmsvfd.py
+++ mi6k/userspace/xmmsvfd.py
@@ -6,10 +6,12 @@
 
 import xmms, time, os
 from fcntl import ioctl
+from Numeric import *
 
 class CenturyVFD:
     width = 20
     lines = 2
+    userDefinedCharacters = "\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF"
 
     def __init__(self):
         self.dev = open("/dev/usb/mi6k0", "w")
@@ -53,7 +55,7 @@ class CenturyVFD:
         self.writeVFD(chr(0x0E))
 
     def writeScreen(self, data):
-        "Write a complete screen of data, without clearing the screen"
+        """Write a complete screen of data, without clearing the screen"""
         lines = data.split('\n')
         while len(lines)<self.lines:
             lines.append("")
@@ -67,22 +69,125 @@ class CenturyVFD:
             self.writeVFD(l)
         self.dev.flush()
 
+    def defineCharacter(self, char, data):
+        """Set a user defined character from a 5x7 Numeric array"""
+
+        # The VFD has a strange dot ordering, we swizzle our original
+        # bitmap into an array ordered first by bit position then by byte
+        bits = take(data.flat, ( (17, 13,  9,  5,  1),
+                                 ( 0, 31, 27, 23, 19),
+                                 (16, 12,  8,  4,  0),
+                                 (34, 30, 26, 22, 18),
+                                 (15, 11,  7,  3,  0),
+                                 (33, 29, 25, 21,  0),
+                                 (14, 10,  6,  2,  0),
+                                 (32, 28, 24, 20,  0) ))
+
+        # Pack this resulting array into bytes
+        bytes = bits[0] + bits[1]*2 + bits[2]*4 + bits[3]*8 + bits[4]*16 + bits[5]*32 + bits[6]*64 + bits[7]*128
+
+        # Convert to a command string and send it
+        self.writeVFD(chr(0x18) + char + bytes.astype(UInt8).tostring())
+
 
 class VFDupdater:
     "Collects information from XMMS and displays it on the VFD panel"
     scrollerDelay = 6
     scrollerIndex = 0
     scrollerGap = 5
+    scrollSpeed = 12
     lastTitle = ''
-    spinnerState = '/-\\|'
+    spinnerStates = '/-\\|'
+    spinnerIndex = 0
+    spinnerSpeed = 18
+
+    spinnerIdle = array(
+        [ [ 0, 0, 0, 0, 0 ],
+          [ 0, 0, 0, 0, 0 ],
+          [ 0, 1, 1, 1, 0 ],
+          [ 0, 1, 1, 1, 0 ],
+          [ 0, 1, 1, 1, 0 ],
+          [ 0, 0, 0, 0, 0 ],
+          [ 0, 0, 0, 0, 0 ] ])
+
+    spinnerPaused = array(
+        [ [ 0, 0, 0, 0, 0 ],
+          [ 0, 0, 0, 0, 0 ],
+          [ 0, 1, 1, 1, 0 ],
+          [ 0, 0, 0, 0, 0 ],
+          [ 0, 1, 1, 1, 0 ],
+          [ 0, 0, 0, 0, 0 ],
+          [ 0, 0, 0, 0, 0 ] ])
+
+    spinnerPlaying = (
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 1, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 1, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 1, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 1, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 1 ],
+                [ 0, 1, 1, 1, 0 ],
+                [ 1, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 1, 1, 1, 1, 1 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 1, 0, 0, 0, 0 ],
+                [ 0, 1, 1, 1, 0 ],
+                [ 0, 0, 0, 0, 1 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 1, 0, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 0, 1, 0 ],
+                [ 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        array([ [ 0, 0, 0, 0, 0 ],
+                [ 0, 1, 0, 0, 0 ],
+                [ 0, 1, 0, 0, 0 ],
+                [ 0, 0, 1, 0, 0 ],
+                [ 0, 0, 0, 1, 0 ],
+                [ 0, 0, 0, 1, 0 ],
+                [ 0, 0, 0, 0, 0 ] ]),
+        )
 
     def __init__(self, vfd):
         self.vfd = vfd
+        self.lastTime = time.time()
 
-    def fastUpdate(self):
-        pass
+    def update(self):
+        # Calculate the time step
+        now = time.time()
+        dt = now - self.lastTime
+        self.lastTime = now
 
-    def slowUpdate(self):
         # Get the current song title
         title = str(xmms.get_playlist_title(xmms.get_playlist_pos()))
         if len(title) > self.vfd.width:
@@ -92,55 +197,43 @@ class VFDupdater:
             if title != self.lastTitle:
                 self.scrollerIndex = -self.scrollerDelay
                 self.lastTitle = title
-            if self.scrollerIndex > 0:
+            if self.scrollerIndex >= 1:
                 self.scrollerIndex %= self.scrollerGap + len(title)
-                if self.scrollerIndex == 0:
+                if self.scrollerIndex < 1:
                     self.scrollerIndex = -self.scrollerDelay
                 else:
-                    title = ("%s%s%s" % (title, " " * self.scrollerGap, title))[self.scrollerIndex:]
-            self.scrollerIndex += 1
+                    title = ("%s%s%s" % (title, " " * self.scrollerGap, title))[int(self.scrollerIndex):]
+            self.scrollerIndex += dt * self.scrollSpeed
 
         # get the current time index
         if xmms.is_playing():
             millisec = xmms.get_output_time()
             minutes = millisec / 60000
             seconds = (millisec % 60000) / 1000.0
-            playTime = "%3d:%02d" % (minutes, seconds)
+            playTime = "%3d:%05.2f" % (minutes, seconds)
         else:
-            playTime = " --:--"
+            playTime = " --:--.--"
 
         # Status string
         if xmms.is_paused():
             status = 'Paused'
-            spinner = '-'
+            spinner = self.spinnerPaused
         elif xmms.is_playing():
-            status = 'Playing...'
-            self.spinnerState = self.spinnerState[1:] + self.spinnerState[0]
-            spinner = self.spinnerState[0]
+            status = 'Playing'
+            self.spinnerIndex %= len(self.spinnerPlaying)
+            spinner = self.spinnerPlaying[int(self.spinnerIndex)]
+            self.spinnerIndex += dt * self.spinnerSpeed
         else:
             status = 'Idle'
-            spinner = ' '
+            spinner = self.spinnerIdle
 
-        self.vfd.writeScreen("%s\n%-12s%s %s" % (title, status, playTime, spinner))
+        spinnerChar = self.vfd.userDefinedCharacters[0]
+        self.vfd.defineCharacter(spinnerChar, spinner)
+        self.vfd.writeScreen("%s\n%-9s%s %s" % (title, status, playTime, spinnerChar))
 
 
 if __name__ == '__main__':
     vfdupdater = VFDupdater(CenturyVFD())
-    components = [vfdupdater]
-    then = time.time()
 
-    try:
-        while True:
-            # slowUpdates every 0.1 seconds, fastUpdates every 0.01 seconds nominally
-            for c in components:
-                c.slowUpdate()
-            for i in range(10):
-                for c in components:
-                    c.fastUpdate()
-                now = time.time()
-                target = then + 0.01
-                if now < target:
-                    time.sleep(target - now)
-                then = now
-    finally:
-        vfdupdater.vfd.powerOff()
+    while True:
+        vfdupdater.update()
