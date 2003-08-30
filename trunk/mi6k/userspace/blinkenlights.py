206d69b2d260bdb000fc96adaf76dae9075acd94
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-08-30 20:48:04 +0000

    Turn off the LEDs on exit from blinkenlights.py

--- trunk/mi6k/userspace/blinkenlights.py
+++ trunk/mi6k/userspace/blinkenlights.py
@@ -5,14 +5,17 @@ from fcntl import ioctl
 
 f = open("/dev/usb/mi6k0", "w")
 
-startTime = time.time()
-while True:
-    t = time.time() - startTime
-
-    blue  = exp((t % 2) * -10)
-    white = sin(t * 20) * 0.4 + 0.6
-
+def setLEDs(white, blue):
     ioctl(f, 0x3601, struct.pack("HH",
                                  min(1, max(0, white)) * 0xFFFF,
                                  min(1, max(0, blue )) * 0xFFFF))
-    time.sleep(0.01)
+
+startTime = time.time()
+try:
+    while True:
+        t = time.time() - startTime
+        setLEDs(sin(t * 20) * 0.4 + 0.6,
+                blue  = exp((t % 2) * -10))
+        time.sleep(0.01)
+finally:
+    setLEDs(0,0)
