6c24a5ddd029a8206ffa1b2600b8260c94902b9b
Micah Elizabeth Scott <micah@scanlime.org>
2003-08-31 23:32:27 +0000

    userspace code reorganization- created a python module containing all the VFD control code

--- mi6k/kernel/mi6k.c
+++ mi6k/kernel/mi6k.c
@@ -280,10 +280,12 @@ static int mi6k_open(struct inode *inode, struct file *file)
 			     dev->ir_rx_tbuffer, IR_URB_BUFFER_SIZE,
 			     mi6k_ir_rx_irq, dev, endpoint->bInterval);
 
+		/*
 		dbg("Submitting ir_rx_urb");
 		if (usb_submit_urb(&dev->ir_rx_urb)) {
 			dbg("Error submitting URB");
 		}
+		*/
 	}
 
 	/* unlock this device */
