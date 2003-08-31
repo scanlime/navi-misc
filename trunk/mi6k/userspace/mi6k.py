373835ba43a6990da76637b099bfe52694ff973a
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-08-31 23:32:27 +0000

    userspace code reorganization- created a python module containing all the VFD control code

---
+++ trunk/mi6k/userspace/mi6k.py
@@ -0,0 +1,109 @@
+""" mi6k
+
+Python module for interacting with the Media Infrawidget 6000
+"""
+
+from fcntl import ioctl
+from Numeric import *
+
+class CenturyVFD:
+    width = 20
+    lines = 2
+    userDefinedCharacters = "\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF"
+
+    def __init__(self, dev):
+        self.dev = dev
+        self.powerOn()
+
+    def powerOn(self):
+        ioctl(self.dev, 0x3600, 1)
+
+    def powerOff(self):
+        ioctl(self.dev, 0x3600, 0)
+
+    def write(self, data):
+        "Write a string, handling newlines"
+        lines = data.split('\n')
+        lastLine = lines[-1]
+        for l in lines[:-1]:
+            self.writeVFD(l)
+            self.nl()
+        self.writeVFD(lastLine)
+
+    def writeVFD(self, data):
+        self.dev.write(data)
+
+    def cr(self):
+        self.dev.write("\r")
+
+    def lf(self):
+        self.dev.write("\n")
+
+    def nl(self):
+        self.cr()
+        self.lf()
+
+    def clear(self):
+        self.writeVFD(chr(0x15))
+
+    def home(self):
+        self.writeVFD(chr(0x16))
+
+    def cursorOff(self):
+        self.writeVFD(chr(0x0E))
+
+    def writeScreen(self, data):
+        """Write a complete screen of data, without clearing the screen"""
+        lines = data.split('\n')
+        while len(lines)<self.lines:
+            lines.append("")
+        self.home()
+        self.cursorOff()
+        for l in lines:
+            if len(l) < self.width:
+                l += " " * (self.width - len(l))
+            elif len(l) > self.width:
+                l = l[:self.width]
+            self.writeVFD(l)
+        self.dev.flush()
+
+    def writeArray(self, data):
+        """write a complete screen of data from a Numeric array"""
+        data = asarray(data).astype(UInt8)
+        self.home()
+        self.cursorOff()
+        self.writeVFD(data.tostring())
+        self.dev.flush()
+
+    def defineCharacter(self, char, data):
+        """Set a user defined character from a 5x7 Numeric array"""
+
+        # The VFD has a strange dot ordering, we swizzle our original
+        # bitmap into an array ordered first by bit position then by byte
+        bits = take(data.flat, ( (17, 13,  9,  5,  1),
+                                 ( 0, 31, 27, 23, 19),
+                                 (16, 12,  8,  4,  0),
+                                 (34, 30, 26, 22, 18),
+                                 (15, 11,  7,  3,  0),
+                                 (33, 29, 25, 21,  0),
+                                 (14, 10,  6,  2,  0),
+                                 (32, 28, 24, 20,  0) ))
+
+        # Pack this resulting array into bytes
+        bytes = bits[0] + bits[1]*2 + bits[2]*4 + bits[3]*8 + bits[4]*16 + bits[5]*32 + bits[6]*64 + bits[7]*128
+
+        # Convert to a command string and send it
+        self.writeVFD(chr(0x18) + char + bytes.astype(UInt8).tostring())
+
+    def setBrightness(self, l, column=0xFF):
+        """Set the brightness of one column, or by default the entire display. l should be in the range [0,1]"""
+        self.writeVFD("\x19\x30" + chr(column) + chr(int((1-l) * 0x07)))
+
+    def flush(self):
+        self.dev.flush()
+        
+
+class MI6K:
+    def __init__(self):
+        self.dev = open("/dev/usb/mi6k0", "w")
+        self.vfd = CenturyVFD(self.dev)
--- trunk/mi6k/userspace/xmmsvfd.py
+++ trunk/mi6k/userspace/xmmsvfd.py
@@ -4,90 +4,8 @@
 # to work with the USB VFD controller as a real-world test application.
 #
 
-import xmms, time, os
-from fcntl import ioctl
+import xmms, time, os, mi6k
 from Numeric import *
-
-class CenturyVFD:
-    width = 20
-    lines = 2
-    userDefinedCharacters = "\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF"
-
-    def __init__(self):
-        self.dev = open("/dev/usb/mi6k0", "w")
-        self.powerOn()
-
-    def powerOn(self):
-        ioctl(self.dev, 0x3600, 1)
-
-    def powerOff(self):
-        ioctl(self.dev, 0x3600, 0)
-
-    def write(self, data):
-        "Write a string, handling newlines"
-        lines = data.split('\n')
-        lastLine = lines[-1]
-        for l in lines[:-1]:
-            self.writeVFD(l)
-            self.nl()
-        self.writeVFD(lastLine)
-
-    def writeVFD(self, data):
-        self.dev.write(data)
-
-    def cr(self):
-        self.dev.write("\r")
-
-    def lf(self):
-        self.dev.write("\n")
-
-    def nl(self):
-        self.cr()
-        self.lf()
-
-    def clear(self):
-        self.writeVFD(chr(0x15))
-
-    def home(self):
-        self.writeVFD(chr(0x16))
-
-    def cursorOff(self):
-        self.writeVFD(chr(0x0E))
-
-    def writeScreen(self, data):
-        """Write a complete screen of data, without clearing the screen"""
-        lines = data.split('\n')
-        while len(lines)<self.lines:
-            lines.append("")
-        self.home()
-        self.cursorOff()
-        for l in lines:
-            if len(l) < self.width:
-                l += " " * (self.width - len(l))
-            elif len(l) > self.width:
-                l = l[:self.width]
-            self.writeVFD(l)
-        self.dev.flush()
-
-    def defineCharacter(self, char, data):
-        """Set a user defined character from a 5x7 Numeric array"""
-
-        # The VFD has a strange dot ordering, we swizzle our original
-        # bitmap into an array ordered first by bit position then by byte
-        bits = take(data.flat, ( (17, 13,  9,  5,  1),
-                                 ( 0, 31, 27, 23, 19),
-                                 (16, 12,  8,  4,  0),
-                                 (34, 30, 26, 22, 18),
-                                 (15, 11,  7,  3,  0),
-                                 (33, 29, 25, 21,  0),
-                                 (14, 10,  6,  2,  0),
-                                 (32, 28, 24, 20,  0) ))
-
-        # Pack this resulting array into bytes
-        bytes = bits[0] + bits[1]*2 + bits[2]*4 + bits[3]*8 + bits[4]*16 + bits[5]*32 + bits[6]*64 + bits[7]*128
-
-        # Convert to a command string and send it
-        self.writeVFD(chr(0x18) + char + bytes.astype(UInt8).tostring())
 
 
 class VFDupdater:
@@ -233,7 +151,7 @@ class VFDupdater:
 
 
 if __name__ == '__main__':
-    vfdupdater = VFDupdater(CenturyVFD())
+    vfdupdater = VFDupdater(mi6k.MI6K().vfd)
 
     while True:
         vfdupdater.update()
