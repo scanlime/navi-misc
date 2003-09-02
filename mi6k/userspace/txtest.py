abd20914cd2053a51764a1442038ae909e3ec44e
Micah Elizabeth Scott <micah@scanlime.org>
2003-09-02 02:30:42 +0000

    Yoink some code from pybzflag to make a GUIfied transmitter testing proggie

--- mi6k/userspace/txtest.py
+++ mi6k/userspace/txtest.py
@@ -2,12 +2,27 @@
 #
 # Tiny IR transmitter test
 #
-f = open("/dev/lirc", "w")
+import time, Tweak
 
-def t(*a):
-    for v in a:
-        f.write(chr(v & 0xFF) + chr(v >> 8) + chr(0) + chr(0))
+class test:
+    def __init__(self):
+        self.f = open("/dev/lirc", "w")
+        self.pulse = 100
+        self.space = 100
 
-while True:
-    t(1,1, 1000,1000, 3000,2000, 1000,1000,)
+    def run(self):
+        while True:
+            #self.transmit(10000,5000, 500,500, 500,500, 100,500)
+            self.transmit(self.pulse, self.space)
 
+    def transmit(self, *a):
+        for i in xrange(16):
+            for v in a:
+                self.f.write(chr(int(v) & 0xFF) + chr(int(v) >> 8) + chr(0) + chr(0))
+        self.f.flush()
+        #time.sleep(0.05)
+
+t = test()
+Tweak.Window( Tweak.Quantity(t, 'pulse', (0,10000)),
+              Tweak.Quantity(t, 'space', (0,10000)) )
+Tweak.run(t)
