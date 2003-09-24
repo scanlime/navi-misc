57c371f46b66b9e9155759cdf2de91ab6ca87f28
Micah Elizabeth Scott <micah@scanlime.org>
2003-09-24 08:35:52 +0000

    The power of wasabi, on a galactic scale

--- pybzengine/wasabi.py
+++ pybzengine/wasabi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from BZEngine.UI import Viewport, ThreeDRender, ThreeDControl, HUD
-from BZEngine import Event, Animated, Geometry, Noise
+from BZEngine import Event, Animated, Geometry, Util
 import cPickle, copy
 from Numeric import *
 
@@ -27,26 +27,34 @@ title = HUD.Text(viewport.region(viewport.rect), "wasabi",
 
 class OrbitingParticles:
     """A particle system orbiting around the origin"""
-    def __init__(self, radius=2, speed=2, randomness=3):
-        self.particle = cPickle.load(open("data/green_nebula.particle"))
-        (self.center, j) = Noise.randomVectors((2,3), magnitude=radius)
-        self.velocity = Geometry.cross(self.center, j)
-        self.speed = speed
-        self.randomness = randomness
+    def __init__(self, particle, radius=3, rotationPeriod=1, noisePeriod=10, seed=0):
+        self.particle = particle
+        self.plane = Animated.Vector(Animated.PerlinNoise(frequency = 1.0 / noisePeriod))
+        self.angle = Animated.Value(Animated.RampFunction(rotationPeriod, (0, 2*math.pi)))
+        self.radius = radius
 
     def getDrawables(self):
         return [self.particle]
 
     def integrate(self, dt):
-        self.center += self.velocity * dt * self.speed
-        self.velocity -= self.center * dt * self.speed
-        self.velocity += Noise.randomVectors((1,3), magnitude=self.randomness * dt)[0]
-        self.particle.emitter.position = self.center
+        self.angle.integrate(dt)
+        self.plane.integrate(dt)
+
+        # Create two perpendicular vectors on our orbit plane, and use those
+        # and our angle to calculate the emitter position
+        plane = self.plane.get()
+        v1 = Geometry.normalize(Geometry.cross(plane, (1,0,0)))
+        v2 = Geometry.normalize(Geometry.cross(plane, v1))
+        self.particle.emitter.position = (cos(self.angle.value)*v1 + sin(self.angle.value)*v2) * self.radius
 
 
-orbits = [OrbitingParticles() for i in xrange(2)]
+orbitParticles = ("green_flame.particle", "green_nebula.particle")
+orbits = [OrbitingParticles(cPickle.load(open(Util.dataFile(name)))) for name in orbitParticles]
 for orbit in orbits:
     view.scene.add(orbit)
+
+view.scene.add(cPickle.load(open(Util.dataFile("galaxy.particle"))))
+
 
 time = Animated.Timekeeper()
 def setupFrame():
