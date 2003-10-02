dac0d1fffdbd78c506a173e985b143829ab5bbb5
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-02 01:22:56 +0000

    Add a distutils setup.py

---
+++ pybzengine/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup (name = "PyBZEngine",
+       version = "0.1",
+       description = "Simple 3D rendering engine and game framework",
+       maintainer = "Micah Dowty",
+       maintainer_email = "micah@picogui.org",
+       license = "LGPL",
+       packages = ['BZEngine'],
+       )
+
