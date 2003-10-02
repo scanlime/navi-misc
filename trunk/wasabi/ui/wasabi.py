0821a4536bd1c256e511745e10cd1c11b1ed1e43
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-10-02 06:19:00 +0000

    A book, a page, and a monkey. What more could it need?

--- trunk/wasabi/ui/wasabi.py
+++ trunk/wasabi/ui/wasabi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from BZEngine.UI import Viewport, ThreeDRender, ThreeDControl, HUD
+from BZEngine.UI import Viewport, ThreeDRender, ThreeDControl, HUD, Drawable
 from BZEngine import Event, Animated, Geometry, Util
 import cPickle, copy
 from Numeric import *
@@ -34,14 +34,105 @@ class OrbitingParticles:
         self.particle.emitter.position = (cos(self.angle.value)*v1 + sin(self.angle.value)*v2) * self.radius
 
 
-class Scene:
-    """A collection of scene objects and stuff"""
+class Page:
+    """A collection of scene objects, event handlers, and other objects forming an
+       'interactive video clip' with a definite beginning and end. These objects
+       are put into a Book along with other Pages.
 
-class WasabiTitle:
+       A page is active as long as it exists in memory- between when its constructor
+       finishes and when all references to it are gone. Because all communication
+       occurs via Events, cleanup happens automagically.
+
+       The Book manages instantiating and deleting Pages. A page signals that it's
+       job is finished by triggering its onFinish event.
+       """
     def __init__(self, view):
+        Event.attach(self, "onFinish")
         self.viewport = view.viewport
         self.view = view
         self.time = Animated.Timekeeper()
+
+
+class Book(Page):
+    """A collection of Pages. A Book is responsible for controlling the overal flow
+       of control in a program built from multiple Pages. The page at the beginning
+       of the Book is always the active page. Pages are removed from
+       the Book when their onFinish event is triggered. Pages can be added either at
+       the beginning or at the end of the book- pages added at the beginning are made
+       active immediately, and the formerly active page resumes when that page finishes.
+       Pages added at the end are activated after all other pages have finished.
+
+       Note that since pages are only instantiated when active, pages specified for
+       insertion into the Book are given as a (class, parameters) tuple. The 'view'
+       parameter is assumed, and is not given in the parameters list. If no parameters
+       are needed, the class can be given bare rather than as part of a tuple.
+
+       Also note that a book is, itself, a page. This is a convenient way to group
+       pages hierarchially.
+       """
+    def __init__(self, view, initialPages):
+        Page.__init__(self, view)
+        self.pages = initialPages
+        self.activeInstance = None
+        self.activeSpec = None
+        self.evaluatePages()
+
+    def pushFront(self, pages):
+        """Add the given list of pages to the front of the book. The first page
+           given will become active.
+           """
+        self.pages = pages + self.pages
+        self.evaluatePages()
+
+    def pushBack(self, pages):
+        """Add the given list of pages to the back of the book"""
+        self.pages = self.pages + pages
+        self.evaluatePages()
+
+    def popFront(self):
+        """Remove the currently active page. This is called automatically
+           by a page's onFinish handler.
+           """
+        del self.pages[0]
+        self.evaluatePages()
+
+    def evaluatePages(self):
+        """Used internally, this looks at the list of pages and determines if the active
+           page needs to be changed. If so, the new active page is instantiated and the
+           reference to the old active page is lost, terminating it.
+           """
+        if self.pages:
+            # If the currently active page wasn't created from the spec at the front of the page list,
+            # it should no longer be active. Make it so.
+            if self.activeSpec is not self.pages[0]:
+                self.activeSpec = None
+                self.activeInstance = None
+
+                # Transform the item at the front of self.pages into a pageClass and pageParameters.
+                # This accounts for the format described in this class's doc string- a page specification
+                # may be a bare class or it may be a (class, parameters) tuple.
+                try:
+                    pageClass, pageParameters = self.pages[0]
+                except TypeError:
+                    pageClass = self.pages[0]
+                    pageParameters = ()
+
+                # Create the new active page instance
+                self.activeSpec = self.pages[0]
+                self.activeInstance = pageClass(self.view, *pageParameters)
+
+                # Set up an observer on the page's onFinish handler that removes it from the book
+                self.activeInstance.onFinish.observe(self.popFront)
+        else:
+            # No more pages, disable any currently active page and call our onFinish event
+            self.activeSpec = None
+            self.activeInstance = None
+            self.onFinish()
+
+
+class WasabiLogo(Page):
+    def __init__(self, view):
+        Page.__init__(self, view)
 
         self.viewport.mode = Viewport.GL.ClearedMode(clearColor=(0, 0, 0, 1))
 
@@ -70,5 +161,27 @@ class WasabiTitle:
             orbit.integrate(dt)
 
 
-foo = WasabiTitle(view)
+class Monkey(Page):
+    def __init__(self, view):
+        Page.__init__(self, view)
+
+        self.viewport.mode = Viewport.GL.ClearedMode(clearColor=(0, 0, 0, 1))
+
+        monkey = Drawable.VRML.load("monkey.wrl")
+        view.scene.add(tuple(monkey.values()))
+
+        self.view.camera.position = (0,0,0)
+        self.view.camera.distance = 73
+        self.view.camera.azimuth = 210
+        self.view.camera.elevation = -2
+        self.view.camera.jump()
+
+        self.viewport.onSetupFrame.observe(self.setupFrame)
+
+    def setupFrame(self):
+        dt = self.time.step()
+        self.view.camera.azimuth += dt * 80
+
+
+book = Book(view, [WasabiLogo, Monkey])
 loop.run()
