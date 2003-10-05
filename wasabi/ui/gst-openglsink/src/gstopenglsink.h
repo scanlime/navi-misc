677350d0e8b65d1e826f80387ad085e2ab38eed0
David Trowbridge <trowbrds@gmail.com>
2003-10-05 21:28:56 +0000

    lots of refactoring, now works like the fakesink, but as a plugin

--- wasabi/ui/gst-openglsink/src/gstopenglsink.h
+++ wasabi/ui/gst-openglsink/src/gstopenglsink.h
@@ -1,6 +1,25 @@
-/* 
- * gstopenglsink.h: video sink that draws to an opengl texture
+/* GStreamer
+ * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
+ *                    2000 Wim Taymans <wtay@chello.be>
+ *
+ * gstopenglsink.h: 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
+
 
 #ifndef __GST_OPENGLSINK_H__
 #define __GST_OPENGLSINK_H__
@@ -11,39 +30,44 @@
 extern "C" {
 #endif /* __cplusplus */
 
-/* #define's don't like whitespacey bits */
 #define GST_TYPE_OPENGLSINK \
-  (gst_gst_openglsink_get_type())
+  (gst_openglsink_get_type())
 #define GST_OPENGLSINK(obj) \
   (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_OPENGLSINK,GstOpenGLSink))
 #define GST_OPENGLSINK_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_OPENGLSINK,GstOpenGLSink))
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_OPENGLSINK,GstOpenGLSinkClass))
 #define GST_IS_OPENGLSINK(obj) \
   (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_OPENGLSINK))
 #define GST_IS_OPENGLSINK_CLASS(obj) \
   (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_OPENGLSINK))
 
-typedef struct _GstOpenGLSink      GstOpenGLSink;
+typedef struct _GstOpenGLSink GstOpenGLSink;
 typedef struct _GstOpenGLSinkClass GstOpenGLSinkClass;
 
-struct _GstOpenGLSink
-{
-  GstElement element;
+struct _GstOpenGLSink {
+  GstElement 	 element;
 
-  GstPad *sinkpad;
+  gboolean 	 silent;
+  gboolean 	 dump;
+  gboolean 	 sync;
+  GstClock 	*clock;
 
-  gboolean silent;
+  gchar 	*last_message;
 };
 
-struct _GstOpenGLSinkClass
-{
+struct _GstOpenGLSinkClass {
   GstElementClass parent_class;
+
+  /* signals */
+  void (*handoff) (GstElement *element, GstBuffer *buf, GstPad *pad);
 };
 
-GType gst_gst_openglsink_get_type (void);
+GType gst_openglsink_get_type(void);
+
+gboolean gst_openglsink_factory_init (GstElementFactory *factory);
 
 #ifdef __cplusplus
 }
-#endif /* __cplusplus */
+#endif
 
 #endif /* __GST_OPENGLSINK_H__ */
