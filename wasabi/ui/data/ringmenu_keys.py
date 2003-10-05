f07b88f63cb1c43f3ff9562077c1c662e22ba7b9
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-05 00:34:20 +0000

    LIRC integration

--- wasabi/ui/data/ringmenu_keys.py
+++ wasabi/ui/data/ringmenu_keys.py
@@ -11,3 +11,8 @@ bindings.add(MousePress, 5).observe(command.spinRight)
 bindings.add(MousePress, 1).observe(command.selectCurrent)
 bindings.add(KeyPress, pygame.K_SPACE).observe(command.selectCurrent)
 bindings.add(KeyPress, pygame.K_RETURN).observe(command.selectCurrent)
+bindings.add(KeyPress, pygame.K_UP).observe(command.selectCurrent)
+
+from Wasabi import IR
+bindings.add(IR.ButtonPress, 'left', repeat=0).observe(command.spinLeft)
+bindings.add(IR.ButtonPress, 'right', repeat=0).observe(command.spinRight)
