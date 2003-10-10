9bb1cf5b9e4304c60d36f278581d789b900174c3
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-10-10 04:12:56 +0000

    Make the kernel module actually work, add a simple python test that dumps the PIC's address space

---
+++ trunk/rcpod/userspace/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup (name = "rcpod",
+       version = "0.1",
+       description = "Remote Controlled PIC of Doom",
+       maintainer = "Micah Dowty",
+       maintainer_email = "micah@picogui.org",
+       license = "LGPL",
+       py_modules = ['rcpod'],
+       )
+
