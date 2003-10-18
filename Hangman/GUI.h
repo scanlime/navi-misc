0917341b505453e16082444c768b3147c6cf5b96
Evan Sheehan <darthmall@users.noreply.github.com>
2003-10-18 09:37:08 +0000

    Quick backup before bed. Night guys

--- Hangman/GUI.h
+++ Hangman/GUI.h
@@ -4,13 +4,14 @@
  * W. Evan Sheehan
  * evan@navi.picogui.org
  *
- * A simple library of GUI tools using SDL.
+ * A simple library of GUI tools using SDL.  Used to create a layout tree, each node
+ * containing a rectangle.  Rectangles can be divided horizontally or vertically and can
+ * contain buttons, text fields or drawing fields.
  *
  */
 
 #include <SDL/SDL.h>
-/* Division type of the rectangle. */
-enum div_t{UNSET, HORIZONTAL, VERTICAL, MARGIN};
+
 
 typedef struct GUI_Node {
 	/* x and y are the coordinates of the top left corner of the rectangle.
@@ -20,22 +21,26 @@ typedef struct GUI_Node {
 
 	/* Percentage to split the node by. */
 	int percent; 
-	
-	/* Division of the rectangle: HORIZONTAL, VERTICAL, MARGIN or UNSET. */
-	enum div_t division;
 
+	/* Contains the text to be printed in the case of either a text field or a button.
+	 * In case of a drawing field can contain the path to a .png file to blit to the
+	 * screen.
+	 */
+	char* text;
+	
 	/* Pointer to the split function for the node. */
-	void (*split)(void);
+	int (*split)(struct GUI_Node *node);
 
 	/* Pointer to the draw function for the node. */
-	struct GUI_Node* (*draw)(void);
+	int (*draw)(struct GUI_Node* node, SDL_Surface* dest);
 
 	/* Controls the behaviour of the node (e.g. text field, button, etc.) */
-	void (*action)(void);
+	void (*action)(struct GUI_Node *node);
 
 	/* Child nodes. */
 	struct GUI_Node *left;
 	struct GUI_Node *right;
+
 } GUI_Node;
 
 /* Initialize the node. */
@@ -45,17 +50,17 @@ void GUI_Node_Init(GUI_Node *node);
 /* Split the rectangle horizontally where the top rectangle is 'percent' the size of
  * the initial rectangle and the bottom is the remaining part.
  */
-void split_horizontal(GUI_Node *node);
+int split_horizontal(GUI_Node *node);
 
 /* Split the rectangle vertically where the left rectangle is 'percent' the size of the
  * initial rectangle and the right rectangle is the remaining space.
  */
-void split_vertical(GUI_Node *node);
+int split_vertical(GUI_Node *node);
 
 /* Doesn't split the rectangle into two rectangles, but creates a rectangle smaller then
  * node by percent and puts it in the left child of node; the right child remains NULL.
  */
-void margin(GUI_Node *node);
+int margin(GUI_Node *node);
 
 /**************************** Window Elements *****************************/
 /* Produces a button node.
@@ -75,25 +80,31 @@ GUI_Node* make_drawfield();
 /* Draw the image of the button to the rectangle.  node is the rectangle to draw the button
  * to, text is printed in the button.
  */
-void draw_button(GUI_Node *node, SDL_Surface *dest, char* text);
+int draw_button(GUI_Node *node, SDL_Surface *dest);
 
 /* Draw the image of the text field to the rectangle. Write text to the screen,
  * leave blank if text is NULL.
  */
-void draw_textfield(GUI_Node *node, SDL_Surface *dest, char* text);
+int draw_textfield(GUI_Node *node, SDL_Surface *dest);
 
 /* Draw the image of the drawing field to the rectangle. Draw the image to the 
  * rectangle.  Draw nothing if image is NULL.
  */
-void draw_drawfield(GUI_Node *node, SDL_Surface* image);
+int draw_drawfield(GUI_Node *node, SDL_Surface *dest);
 
 /* Resize the entire layout tree and re-blit everything.
  */
-void resize(GUI_Node* node);
+int resize(GUI_Node* node);
 
 /********************************* Controls **********************************/
-void button_control();
+/* Control button behaviour.
+ */
+void button_control(GUI_Node *node);
 
-void textfield_control();
+/* Control textfield.
+ */
+void textfield_control(GUI_Node *node);
 
-void drawfield_control();
+/* Control draw spaces.
+ */
+void drawfield_control(GUI_Node *node);
