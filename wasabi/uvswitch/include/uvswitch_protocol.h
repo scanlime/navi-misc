16cf92b162a2220c6d531caee811c674564620b2
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-18 20:01:37 +0000

    Start uvswitch firmware

---
+++ wasabi/uvswitch/include/uvswitch_protocol.h
@@ -0,0 +1,52 @@
+;//################################################################################
+;//
+;// uvswitch_protocol.h - Definitions describing the host to device protocol used by the
+;//                       uvswitch This file is both valid C and assembly source, so it can be
+;//                       shared by host and device code.
+;//
+;// This file is part of the uvswitch project.
+;// Micah Dowty <micah@picogui.org>
+;//
+;//###############################################################################
+
+#ifndef __UVSWITCH_PROTOCOL_H
+#define __UVSWITCH_PROTOCOL_H
+
+;//************************************************** Idenfification
+
+;// The protocol version, stored in binary coded decimal.
+;// This is available from the device in the bcdDevice field
+;// of its DEVICE descriptor.
+#define UVSWITCH_PROTOCOL_VERSION  0x0101
+
+;// Device vendor and product IDs.
+;// The uvswitch's device class and protocol are both set to 'vendor-specific'.
+#define UVSWITCH_VENDOR_ID   0xE461
+#define UVSWITCH_PRODUCT_ID  0x0003
+
+;//************************************************** Control requests
+
+;// Set the current inputs. The current video input is the low byte of wValue,
+;// the white audio input is the high byte of wIndex, and the red audio input
+;// is the low byte of wIndex. Channels start at 1, a setting of 0 indicates
+;// the output should be disconnected. If the high byte of wValue is nonzero,
+;// the bypass input is active, otherwise the bypass output is connected to the
+;// switch's video output.
+#define UVSWITCH_CTRL_SWITCH	0x01
+
+;// Set the values used to detect video inputs. The low byte of wIndex is the
+;// number of precharge reads done to ensure the A/D converter's capacitor has
+;// stabilized with these low-impedance inputs. The high byte of wIndex is the
+;// number of integration reads. The low byte of wValue is the threshold value
+;// to check after integration.
+#define UVSWITCH_CTRL_CALIBRATE 0x02
+
+;//************************************************** Endpoints
+
+;// The EP1 IN endpoint transmits 1-byte packets containing a bitfield
+;// of active video input channels. The LSB corresponds to port 1.
+
+#endif
+
+;//### The End ###
+
