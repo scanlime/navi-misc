a2e81b6da2444e40361e32b2ce3afaa12d5b92d1
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-20 06:08:34 +0000

    Add a bit of userspace code to test poll()

---
+++ wasabi/uvswitch/userspace/poll_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# Test the driver's poll() function by waiting for input changes
+# using select() and acting on them by switching channels.
+#
+
+from select import select
+
+class VideoDetector:
+    def __init__(self, device="/dev/usb/uvswitch0"):
+        self.dev = open(device, "r");
+        self.activeList = []
+        self.read()
+
+    def read(self):
+        newActiveList = map(int, self.dev.readline().split(" "))
+        for channel in newActiveList:
+            if not channel in self.activeList:
+                self.channelOn(channel)
+        for channel in self.activeList:
+            if not channel in newActiveList:
+                self.channelOff(channel)
+        self.activeList = newActiveList
+
+    def channelOn(self, channel):
+        print "Channel %d activated" % channel
+
+    def channelOff(self, channel):
+        print "Channel %d deactivated" % channel
+
+adc  = VideoDetector()
+
+while True:
+    i, o, x = select([adc.dev], [], [], 1)
+    if i:
+        print i
+        adc.read()
+    else:
+        print "poing"
+
