1699b09020c77b491a0a5fc2d0ca93124f6d5f99
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-21 06:23:12 +0000

    Renumber ports in the little example doodads

--- wasabi/uvswitch/userspace/select_nes.sh
+++ wasabi/uvswitch/userspace/select_nes.sh
@@ -2,5 +2,5 @@
 #
 # Select channel 7 (the nes) and disable the bypass switch
 #
-echo 7 0 > /dev/usb/uvswitch0
+echo 4 0 > /dev/usb/uvswitch0
 
--- wasabi/uvswitch/userspace/select_ps2.sh
+++ wasabi/uvswitch/userspace/select_ps2.sh
@@ -2,5 +2,6 @@
 #
 # Select channel 1 (the playstation) and disable the bypass switch
 #
-echo 1 0 > /dev/usb/uvswitch0
+echo 2 0 > /dev/usb/uvswitch0
+
 
