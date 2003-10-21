aaa7521da9e17e786693c59f7c0b70afdd8d1d5d
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-21 06:20:19 +0000

    Make things fullscreen and IR happy

--- wasabi/ui/test.py
+++ wasabi/ui/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from BZEngine.UI import Viewport, ThreeDRender, Sequencer, Layout, HUD, ThreeDControl
+from BZEngine.UI import Viewport, ThreeDRender, Sequencer, Layout, HUD, ThreeDControl, Input
 from BZEngine import Event
 from Wasabi import Logos, Menu, Icon, IR
 from math import *
@@ -8,15 +8,13 @@ import pygame
 # Set up the usual pybzflag event loop, viewport, and view
 loop = Event.EventLoop()
 viewport = Viewport.OpenGLViewport(loop)
-viewport.setCaption("Wasabi Test")
+viewport.setCaption("Wasabi UI")
 view = ThreeDRender.View(viewport)
-control = ThreeDControl.Viewing(view, viewport)
+
+viewport.display.toggle_fullscreen()
+pygame.mouse.set_visible(0)
 
 # Set up an LIRC client
-#try:
-#    IR.getDefaultClient().attachToLoop(loop)
-#except:
-#    pass
 IR.getDefaultClient().attachToLoop(loop)
 
 # A sequencer page that displays a piece of text in the center of the screen, over a background image
@@ -41,9 +39,15 @@ class TestMenu(Menu.RingMenu):
         decoratedText = Sequencer.FadeIn(0.25, (0,0,0), Sequencer.FadeOut(1, (0,0,0), Sequencer.UserPageInterrupter(text)))
         self.book.pushBack(decoratedText)
 
+def logoInterrupter(book):
+    events = [
+        Input.KeyPress(book.viewport, pygame.K_SPACE),
+	IR.ButtonPress(book.viewport),
+	]
+    return Sequencer.PageInterrupter(events, Logos.getLogoSubBook())(book)
 
 mainBook = Sequencer.CyclicBook(view, [
-    Sequencer.FadeOut(0.2, (1,1,1), Sequencer.UserPageInterrupter(Logos.getLogoSubBook())),
+    Sequencer.FadeOut(0.2, (1,1,1), logoInterrupter),
     Sequencer.FadeIn(0.5, (1,1,1), Sequencer.FadeOut(0.25, (0,0,0), TestMenu))
     ])
 
