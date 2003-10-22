36d2d0e82071bfa600e56599a7510abfd5f2a8c7
Micah Elizabeth Scott <micah@scanlime.org>
2003-10-22 08:34:35 +0000

    Start moving hardware wrangling code into a separate module, make the IR module use the event loop correctly rather than polling with a continuous timer.

--- wasabi/ui/Wasabi/IR.py
+++ wasabi/ui/Wasabi/IR.py
@@ -44,7 +44,7 @@ class Code:
 
 class Client:
     """An lircd client. Polls /dev/lircd for new IR codes to dispatch"""
-    def __init__(self, socketName="/dev/lircd"):
+    def __init__(self, eventLoop, socketName="/dev/lircd"):
         # The onReceivedCode event is called with a Code instance
         # for every IR code received from the lircd socket
         Event.attach(self, 'onReceivedCode')
@@ -55,15 +55,24 @@ class Client:
         self.socketf = os.fdopen(self.socket.fileno())
         self.socket.setblocking(False)
 
-    def attachToLoop(self, eventLoop):
-        """Attach this LIRC client to the given event loop, polling at each
-           loop iteration for more available IR codes.
-           """
-        eventLoop.add(Event.ContinuousTimer(self.poll))
+        # Add ourselves to the main loop. It uses our getSelectable method.
+        eventLoop.add(self)
 
-    def poll(self):
-        # Receive all the codes available now. When there is no more,
-        # instead of blocking we'll get an IOError.
+    def getSelectable(self):
+        """Called by the main loop to see what items we have for its select()"""
+        return self.socket
+
+    def needsWrite(self):
+        """Called by the main loop to see if we need to poll for writing.
+           We don't, since we only read from lircd.
+           """
+        return False
+
+    def pollRead(self, eventLoop):
+        """Receive all the codes available now. When there is no more,
+           instead of blocking we'll get an IOError. This is called by
+           the main loop when it detects activity on our socket.
+           """
         try:
             while True:
                 self.onReceivedCode(self.readCode())
@@ -77,28 +86,27 @@ class Client:
 
 defaultClient = None
 
-def getDefaultClient():
-    """The Client is generally a singleton. This returns the default instance,
-       creating it if necessary.
+def initDefaultClient(eventLoop):
+    """Initialize the singleton default client. Requires an event loop.
        """
     global defaultClient
-    if not defaultClient:
-        defaultClient = Client()
-    return defaultClient
+    defaultClient = Client(eventLoop)
 
 
 class ButtonPress(Event.Event):
     """An event that fires when a specified IR remote button
        is pressed. The button name, remote, and repeat number
        are all optional. None is treated as a wildcard.
-       This uses the default IR client.
+       This uses the default IR client. If an IR device has not
+       been successfully initialized, this event never fires.
        """
     def __init__(self, viewport, name=None, remote=None, repeat=0):
+        global defaultClient
         Event.Event.__init__(self)
         self.name = name
         self.remote = remote
         self.repeat = repeat
-        getDefaultClient().onReceivedCode.observe(self.handleCode)
+        defaultClient.onReceivedCode.observe(self.handleCode)
 
     def handleCode(self, code):
         if self.name is not None and self.name != code.name:
