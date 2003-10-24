df83f683bcfdb0d467848da93f282441d1df9ad8
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-10-24 16:25:22 +0000

    Rename 'Video Input' icon to 'Unknown Input'

--- trunk/wasabi/data/icons.py
+++ trunk/wasabi/data/icons.py
@@ -4,12 +4,12 @@
 # by Icon.load()
 #
 
-navi      = Icon('icon_navi.png',      'Navi',          imageAspect = 1.623 )
-n64       = Icon('icon_n64.png',       'Nintendo 64',   imageAspect = 1.04  )
-atari     = Icon('icon_atari.png',     'Atari',         imageAspect = 1.52  )
-ps2       = Icon('icon_ps2.png',       'Playstation 2', imageAspect = 0.983 )
-dreamcast = Icon('icon_dreamcast.png', 'Dreamcast',     imageAspect = 1.66  )
-nintendo  = Icon('icon_nes.png',       'Nintendo',      imageAspect = 1.594 )
-genesis   = Icon('icon_sega.png',      'Genesis',       imageAspect = 1.679 )
-vidInput  = Icon('icon_rca_cables.png','Video Input',   imageAspect = 0.732 )
+navi         = Icon('icon_navi.png',       'Navi',          imageAspect = 1.623 )
+n64          = Icon('icon_n64.png',        'Nintendo 64',   imageAspect = 1.04  )
+atari        = Icon('icon_atari.png',      'Atari',         imageAspect = 1.52  )
+ps2          = Icon('icon_ps2.png',        'Playstation 2', imageAspect = 0.983 )
+dreamcast    = Icon('icon_dreamcast.png',  'Dreamcast',     imageAspect = 1.66  )
+nintendo     = Icon('icon_nes.png',        'Nintendo',      imageAspect = 1.594 )
+genesis      = Icon('icon_sega.png',       'Genesis',       imageAspect = 1.679 )
+unknownInput = Icon('icon_rca_cables.png', 'Unknown Input', imageAspect = 0.732 )
 
