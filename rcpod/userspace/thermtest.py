64e2656fa639dd03104088942fdb58432a18d4fe
Micah Elizabeth Scott <micah@scanlime.org>
2003-11-01 23:32:35 +0000

    Read data while clock is high (data valid) rather than after pulling clock low when the data is undefined.

--- rcpod/userspace/thermtest.py
+++ rcpod/userspace/thermtest.py
@@ -4,7 +4,7 @@ import rcpod, therm
 pic = rcpod.Device()
 eeprom = therm.eeprom(pic)
 
-eeprom.write_byte(0x0000, 0x5a)
+eeprom.write_byte(0x0000, 0x42)
 blah = eeprom.random_read(0x0000)
 for i in range(8):
     print '0x%x' % blah[i]
