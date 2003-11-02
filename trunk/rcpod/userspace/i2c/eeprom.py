29175232847409c5f881218b44d17f65273ad67f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-11-02 01:13:42 +0000

    implement the rest of the eeprom commands, because, why not?

--- trunk/rcpod/userspace/i2c/eeprom.py
+++ trunk/rcpod/userspace/i2c/eeprom.py
@@ -13,6 +13,15 @@ class eeprom:
 	self.i2c.writebyte(byte)					# data
 	self.i2c.stop()
 
+    def page_write(self, address, data*):
+        self.i2c.start()
+	self.i2c.writebyte(0xa0)					# device address + write
+	self.i2c.writebyte((address >> 8) & 0xff)			# high byte
+	self.i2c.writebyte(address & 0xff)				# low byte
+	for byte in data:
+	    self.i2c.writebyte(byte)
+	self.i2c.stop()
+
     def random_read(self, address):
         self.i2c.start()
 	self.i2c.writebyte(0xa0)					# device address + write
@@ -21,5 +30,22 @@ class eeprom:
 	self.i2c.start()
 	self.i2c.writebyte(0xa1)					# device address + read
 	blah = self.i2c.readbyte()
+	self.i2c.stop()
+	return blah
+
+    def current_read(self):
+        self.i2c.start()
+	self.i2c.writebyte(0xa1)					# device address + read
+	blah = self.i2c.readbyte()
+	self.i2c.stop()
+	return blah
+
+    def sequential_read(self, n):
+        blah = []
+        self.i2c.start()
+	self.i2c.writebyte(0xa1)					# device address + read
+	for i in range(n - 1):
+	    blah.append(self.i2c.readbyte_ack())
+	blah.append(self.i2c.readbyte())
 	self.i2c.stop()
 	return blah
