06ea1fce5ff28909e7c95d9b7442c53974d04746
Micah Elizabeth Scott <micah@scanlime.org>
2003-11-03 05:24:13 +0000

    Adding a test for RCPOD_CTRL_ANALOG_ALL

---
+++ rcpod/test/analogtest.c
@@ -0,0 +1,20 @@
+/* Read all A/D converters, print their values to stdout */
+
+#include <rcpod.h>
+#include <stdio.h>
+
+int main() {
+  unsigned char buffer[8];
+  int i;
+  rcpod_dev *rcpod = rcpod_InitSimple();
+
+  while (1) {
+    rcpod_AnalogSampleAll(rcpod, buffer);
+
+    for (i=0; i < sizeof(buffer); i++)
+      printf("%d ", buffer[i]);
+    printf("\n");
+  }
+
+  return 0;
+}
