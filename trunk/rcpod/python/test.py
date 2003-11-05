ba0c4f947251f5e289e37c69a83bef51f03889b5
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-11-05 08:56:13 +0000

    Just for fun, measure sample rate

--- trunk/rcpod/python/test.py
+++ trunk/rcpod/python/test.py
@@ -2,14 +2,22 @@
 #
 # Really simple test, so far only for the C wrappers in rcpod.lib
 
-import rcpod
+import rcpod, time
 
 dev = rcpod.lib.rcpod_InitSimple()
 
-while True:
-    # Perform an analog reading, using the array helpers
-    results = rcpod.lib.uchar_array(8)
-    rcpod.lib.rcpod_AnalogReadAll(dev, results)
-    l = [rcpod.lib.uchar_array_get(results, i) for i in range(8)]
+start = time.time()
+nsamples = 0
+try:
+    while True:
+        # Perform an analog reading, using the array helpers
+        results = rcpod.lib.uchar_array(8)
+        rcpod.lib.rcpod_AnalogReadAll(dev, results)
+        l = [rcpod.lib.uchar_array_get(results, i) for i in range(8)]
+        nsamples += 1
 
-    print "*" * l[0]
+        print "*" * l[0]
+
+finally:
+    end = time.time()
+    print "%f samples/second" % (nsamples / (end-start))
