ee655c40247ba895a786dd399ae018067ffa49a3
Micah Elizabeth Scott <micah@scanlime.org>
2003-11-08 09:39:13 +0000

    Config file support

--- therm/thermtest.py
+++ therm/thermtest.py
@@ -4,16 +4,8 @@ import pyrcpod, i2c
 dev = pyrcpod.devices[0].open()
 mbus = i2c.MultiBus(dev)
 sampler = i2c.ThermSampler(mbus)
-
-therms = [
-    i2c.Therm(0, 0, "Living room"),
-    i2c.Therm(1, 0, "Outside (living room window)"),
-    ]    
+therms = i2c.getTherms()
 
 while 1:
     sampler.update(therms)
     print therms
-
-#    print therm.readTemp(0x48)
-#	temp = temp * 9.0 / 5 + 32
-#        print temp
