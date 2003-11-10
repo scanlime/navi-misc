bae06bd784cf73922f09a3de54e8018794d7f857
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-11-10 21:33:10 +0000

    A better blinkytest- blinks both LEDs on the rcpod-485, demonstrates GpioAssertBuffer

--- trunk/rcpod/examples/blinkytest.c
+++ trunk/rcpod/examples/blinkytest.c
@@ -1,4 +1,6 @@
-/* Flashes an LED connected to RC2 (pin 13).
+/* Flashes LEDs connected to RC1 and RC2
+ * (such as the LEDs installed on the rcpod-485 board)
+ *
  * An example for GPIOs and pin descriptors.
  */
 
@@ -7,21 +9,27 @@
 
 int main() {
   rcpod_dev *rcpod = rcpod_InitSimple();
-  rcpod_pin led;
+  rcpod_pin leds[2];
 
   /* Define the LED pin once, then demonstrate pin descriptor manipulation
    * to turn its port into an output and toggle it on and off.
    */
-  led = RCPOD_PIN_RC2;
+  leds[0] = RCPOD_PIN_RC1;
+  leds[1] = RCPOD_NEGATE(RCPOD_PIN_RC2);
 
-  /* Make the pin an output */
-  rcpod_GpioAssert(rcpod, RCPOD_OUTPUT(led));
+  /* Make the pins outputs */
+  rcpod_GpioAssert(rcpod, RCPOD_OUTPUT(leds[0]));
+  rcpod_GpioAssert(rcpod, RCPOD_OUTPUT(leds[1]));
 
   while (1) {
-    /* In every iteration, negate the LED's current value and assert the new pin descriptor */
-    led = RCPOD_NEGATE(led);
-    rcpod_GpioAssert(rcpod, led);
-    usleep(100000);
+    /* In every iteration, negate the LED's current value and assert the new pin descriptors */
+    leds[0] = RCPOD_NEGATE(leds[0]);
+    leds[1] = RCPOD_NEGATE(leds[1]);
+
+    /* Multiple pin descriptors can be asserted at once, up to 4x faster */
+    rcpod_GpioAssertBuffer(rcpod, leds, 2);
+
+    usleep(200000);
   }
 
   return 0;
