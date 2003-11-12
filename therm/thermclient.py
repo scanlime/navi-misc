b71d946676ff9e4afce0189dbff5e8ca336dbcf2
Micah Elizabeth Scott <micah@scanlime.org>
2003-11-12 10:17:28 +0000

    Display information about the current averaging period, use the new protocol

--- therm/thermclient.py
+++ therm/thermclient.py
@@ -3,5 +3,18 @@ import xmlrpclib, time
 
 s = xmlrpclib.ServerProxy("http://navi.picogui.org:4510")
 
-for therm, reading in s.getAverages().items():
-    print "%30s : %.01f F" % (therm, reading)
+# Get information about the current averaging period
+times = s.getTimes()
+print "%0.2f seconds until next averaging period\n" % (
+    times['averagePeriod'] + times['periodStart'] - times['time'])
+
+# Get dictionaries mapping sensor IDs to their descriptions and their averages.
+# Sensors with no data available don't show up in the averages dict.
+desc = s.getDescriptions()
+avg = s.getAverages()
+for id in desc.keys():
+    if avg.has_key(id):
+    	reading = "%.01f F" % avg[id]
+    else:
+        reading = "No data"
+    print "%30s : %s" % (desc[id], reading)
