bbf6fb80b493d7b5c3cccac3d23c35ee4230b8b3
Micah Elizabeth Scott <micah@scanlime.org>
2003-11-12 08:24:22 +0000

    Spiff up the client a bit

--- therm/thermclient.py
+++ therm/thermclient.py
@@ -3,9 +3,5 @@ import xmlrpclib, time
 
 s = xmlrpclib.ServerProxy("http://navi.picogui.org:4510")
 
-try:
-    s.setAveragePeriod(1)
-except:
-    pass
-time.sleep(2)
-print s.getAverages()
+for therm, reading in s.getAverages().items():
+    print "%30s : %s F" % (therm, reading)
