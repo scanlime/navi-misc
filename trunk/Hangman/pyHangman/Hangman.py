2800a96d26ed2179acbbf560930d3aa6939c0a53
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-11-17 03:35:24 +0000

    It's coming along.

--- trunk/Hangman/pyHangman/Hangman.py
+++ trunk/Hangman/pyHangman/Hangman.py
@@ -18,6 +18,7 @@ class Hangman:
 		self.words = []
 		self.guesses = []
 
+
 	def FilePlay(self, fileName):
 		""" Load information from a file.  If file is not specified in 
 		argument list get one from the user.
@@ -27,10 +28,13 @@ class Hangman:
 		self.answer = randint(0, len(self.words)-1)
 		
 		print self.words[self.answer]
+		self.Play()
+
 
 	def NetPlay(self):
 		""" Load information from the net. """
 		return None
+
 
 	def DisplayFile(self, fileName = None):
 		""" Display the contents in the file. """
@@ -40,11 +44,40 @@ class Hangman:
 		for word in self.words:
 			print word
 
+
 	def Play(self):
 		""" Once game data is all set this runs the game flow through the course of
 		1 round.
 		"""
-	
+		correct = []
+		
+		for i in range(len(self.words[self.answer])):
+			correct.append('_')
+		
+		while len(self.guesses) < 8:
+			print "Enter a guess: "
+			temp = sys.stdin.readline().strip()
+
+			# Test for a correct answer.
+			if self.words[self.answer].find(temp) != -1:
+				# If the answer is correct insert it into the correct places in the
+				# 'correct' list.
+				correct = [self.TestEntry(i, temp) for i in range(len(correct))]
+			# If not correct append to guesses.
+			else:
+				self.guesses.append(temp)
+
+			print correct
+
+	def TestEntry(self, index, entry):
+		if self.words[self.answer][index] != '_':
+			return self.words[self.answer][index]
+		elif self.words[self.answer][index] != entry:
+			return '_'
+		else:
+			return entry
+
+
 	def GetFileName(self):
 		""" Prompt for a file name and return it. """
 		print "Enter a file name:"
