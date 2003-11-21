931a990381951f9ce79d0cce0c4cca5ad42717a6
Micah Elizabeth Scott <micah@scanlime.org>
2003-11-21 23:13:37 +0000

    A little clarification, remove the notes about ascii and decimal since at this point all I need is hex

--- rcpod/rs485dump.py
+++ rcpod/rs485dump.py
@@ -7,12 +7,46 @@
 #
 import pyrcpod, time
 
-dev = pyrcpod.devices[0].open(pyrcpod.Rcpod485)
-dev.serialInit(9600)
+baudrate = 9600
+bytesPerLine = 16
+timeout  = 0.5   # Flush the line buffer and insert a blank line to
+                 # separate blocks of incoming data when a pause of
+                 # this many seconds occurs.
 
-dev.serialRxStart()
 
-while True:
-    packet = dev.serialRxRead()
-    print " * " + " ".join(["%02X" % byte for byte in packet])
-    time.sleep(1)
+def dumpLine(line):
+    """Given a list of byte values, display them in hex, decimal, and ASCII"""
+    for byte in line:
+        print "%02X" % byte,
+    print
+
+
+def main():
+    dev = pyrcpod.devices[0].open(pyrcpod.Rcpod485)
+    dev.serialInit(baudrate)
+    dev.serialRxStart()
+    line = []
+    timestamp = None
+    print "Listening at %d baud..." % baudrate
+
+    while True:
+        newData = dev.serialRxRead()
+
+        if newData:
+            timestamp = time.time()
+            line.extend(newData)
+            if len(line) >= bytesPerLine:
+                # We have a complete line, display it
+                dumpLine(line[:bytesPerLine])
+                line = line[bytesPerLine:]
+
+        elif timestamp and time.time() > timestamp + timeout:
+            # It's been a while since the last data received, flush
+            # any partial line we may have and print a blank line
+            timestamp = None
+            dumpLine(line)
+            line = []
+            print
+
+if __name__ == "__main__":
+    main()
