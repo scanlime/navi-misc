1a3ac5c29a574d11d59ceac84064533093e486ed
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-11-24 15:37:26 +0000

    Removing all code related to the CX-1 micronet. It's going into the spacegrant repository at cx/ground/mnet_tools

--- trunk/rcpod/pyrcpod/board.py
+++ trunk/rcpod/pyrcpod/board.py
@@ -28,12 +28,7 @@ import device, time
 
 # These are the symbols that will be pulled from this module
 # into the 'pyrcpod' package.
-__all__ = ['Rcpod485', 'MnetError']
-
-
-class MnetError(Exception):
-    """A communications error on the RS-485 'micronet'"""
-    pass
+__all__ = ['Rcpod485']
 
 
 class Rcpod485(device.OpenedRcpod):
@@ -52,60 +47,6 @@ class Rcpod485(device.OpenedRcpod):
         # Set up transmit enable pin
         self.txe = self.rd4
         self.serialSetTxEnable(self.txe)
-
-    def mnetSend(self, data, source=10, destination=2, timeout=0.2):
-        """Sends a packet in the Citizen Explorer 'micronet' format,
-           receives and validates a response, and returns the data.
-           All data is expressed as a list of byte values.
-
-           This will wait 'timeout' seconds for the response.
-
-           Anatomy of a micronet packet:
-            - First byte is a destination device ID
-            - Second byte is a source device ID
-            - Third byte is the number of following data bytes
-            - Data bytes
-            - An 8-bit checksum byte such that all bytes in the packet
-              add up to 255, modulo 256
-           """
-        # Assemble the packet, calculating a checksum
-        packet = [destination, source, len(data)] + list(data)
-        csum = 0xFF
-        for byte in packet:
-            csum = (csum - byte) & 0xFF
-        packet.append(csum)
-
-        # Set the baud rate, send the packet, and wait for a response
-        self.serialInit(9600)
-        self.serialTxRxStart(packet)
-        time.sleep(timeout)
-        retPacket = self.serialRxRead()
-
-        # Make a string describing both the sent and the received packets,
-        # to be used in case of error.
-        pktInfo = "[%s] -> [%s]" % (
-            " ".join(["%02X" % byte for byte in packet]),
-            " ".join(["%02X" % byte for byte in retPacket]))
-
-        # Validate the received packet
-        if not retPacket:
-            raise MnetError("No response: %s" % pktInfo)
-        if len(retPacket) < 4:
-            raise MnetError("Packet too short: %s" % pktInfo)
-        if retPacket[0] != source or retPacket[1] != destination:
-            raise MnetError("Incorrect source/destination: %s" % pktInfo)
-        if len(retPacket) > retPacket[2]+4:
-            raise MnetError("Received too many bytes: %s" % pktInfo)
-        if len(retPacket) < retPacket[2]+4:
-            raise MnetError("Received too few bytes: %s" % pktInfo)
-        csum = 0
-        for byte in retPacket:
-            csum += byte
-        if csum != 255:
-            raise MnetError("Incorrect checksum: %s" % pktInfo)
-
-        # Return only the data portion of the packet
-        return retPacket[3:-1]
 
 ### The End ###
 
