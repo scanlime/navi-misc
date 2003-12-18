bc090ec3712be4dc209bec4cc308b35581aabe90
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-12-18 09:07:56 +0000

    fixed the spacing. ^^

--- trunk/Hangman/pyHangman/Hangman.py
+++ trunk/Hangman/pyHangman/Hangman.py
@@ -21,126 +21,126 @@ __version__ = "0.2"
 #__all__ = ["Hangman"]
 
 class Hangman:
-	""" Functions and data for running a game of Hangman. """
-	def __init__(self):
-		seed()
-		# List of words to select an answer from.
-		self.words = {}
-		# Answer for the current game.
-		self.answer = None
-		# List of incorrectly guessed characters.
-		self.guesses = []
-		# Number of incorrect guesses.
-		self.numMissed = 0
-		# List of correctly guessed characters.
-		self.correct = []
+  """ Functions and data for running a game of Hangman. """
+  def __init__(self):
+    seed()
+    # List of words to select an answer from.
+    self.words = {}
+    # Answer for the current game.
+    self.answer = None
+    # List of incorrectly guessed characters.
+    self.guesses = []
+    # Number of incorrect guesses.
+    self.numMissed = 0
+    # List of correctly guessed characters.
+    self.correct = []
 
-	def Guess(self, guess):
-		""" Checks the guess against the correct answer.  If it's correct guess
-				gets added to self.correct in all of the appropriate spots in the word.
-				If it's incorrect it gets appended to self.guesses.  correct and guesses
-				are returned in a tuple, correct first.
-				"""
-		# If the answer is correct insert it into the correct places in the
-		# 'correct' list.
-		if self.answer.find(guess) != -1:
-			self.correct = [self.TestEntry(i, guess) for i in range(len(self.correct))]
+  def Guess(self, guess):
+    """ Checks the guess against the correct answer.  If it's correct guess
+        gets added to self.correct in all of the appropriate spots in the word.
+        If it's incorrect it gets appended to self.guesses.  correct and guesses
+        are returned in a tuple, correct first.
+        """
+    # If the answer is correct insert it into the correct places in the
+    # 'correct' list.
+    if self.answer.find(guess) != -1:
+      self.correct = [self.TestEntry(i, guess) for i in range(len(self.correct))]
 
-		# If not correct and the letter hasn't been guessed yet, append to guesses.
-		# Increment Hangman.numMissed.
-		else:
-			self.numMissed += 1
-			if self.guesses.count(guess) == 0:
-				self.guesses.append(guess)
+    # If not correct and the letter hasn't been guessed yet, append to guesses.
+    # Increment Hangman.numMissed.
+    else:
+      self.numMissed += 1
+      if self.guesses.count(guess) == 0:
+        self.guesses.append(guess)
 
-		return (self.correct, self.guesses)
+    return (self.correct, self.guesses)
 
-	def NewGame(self):
-		""" Create a new game. """
-		# Clear the guesses and correct lists.
-		self.guesses = []
-		self.correct = []
-		# Set a new word.
-		wordList = self.words.keys()
-		self.answer = choice(wordList)
+  def NewGame(self):
+    """ Create a new game. """
+    # Clear the guesses and correct lists.
+    self.guesses = []
+    self.correct = []
+    # Set a new word.
+    wordList = self.words.keys()
+    self.answer = choice(wordList)
 
-		# Fill the correct list with '_'.
-		for i in range(len(self.answer)):
-			self.correct.append('_')
+    # Fill the correct list with '_'.
+    for i in range(len(self.answer)):
+      self.correct.append('_')
 
-		# Reset the number of misses
-		self.numMissed = 0
-		# For debugging ease.  Should be removed.
-		print self.answer
+    # Reset the number of misses
+    self.numMissed = 0
+    # For debugging ease.  Should be removed.
+    print self.answer
 
-	def gameStat(self):
-		""" Check the status of the current game. Return strings created from the
-				correct and guesses lists in a tuple for the purpose of printing.
-				In the event that the game has been won or lost an appropriate string
-				is appeneded to the correct string before returning.
-				"""
-		correctStr = " ".join(self.correct)
-		guessedStr = " ".join(self.guesses)
+  def gameStat(self):
+    """ Check the status of the current game. Return strings created from the
+        correct and guesses lists in a tuple for the purpose of printing.
+        In the event that the game has been won or lost an appropriate string
+        is appeneded to the correct string before returning.
+        """
+    correctStr = " ".join(self.correct)
+    guessedStr = " ".join(self.guesses)
 
-		if self.correct.count('_') == 0:
-			correctStr += "\nYou Win!!"
-		elif self.numMissed == 6:
-			correctStr += "\nYou Lose!"
+    if self.correct.count('_') == 0:
+      correctStr += "\nYou Win!!"
+    elif self.numMissed == 6:
+      correctStr += "\nYou Lose!"
 
-		return (correctStr, guessedStr)
+    return (correctStr, guessedStr)
 
-	def TestEntry(self, index, entry):
-		""" Provides a way to set the values in Hangman.correct with a list
-				comprehension when the user guesses correct.  TestEntry() returns
-				the appropriate character for that index of Hangman.correct.  If the
-				letter there has been guessed correctly before, it is returned.  If
-				guess does not go there a '_' is returned.  If the guess belongs in
-				the list at index return the guess.
-				For Example:
+  def TestEntry(self, index, entry):
+    """ Provides a way to set the values in Hangman.correct with a list
+        comprehension when the user guesses correct.  TestEntry() returns
+        the appropriate character for that index of Hangman.correct.  If the
+        letter there has been guessed correctly before, it is returned.  If
+        guess does not go there a '_' is returned.  If the guess belongs in
+        the list at index return the guess.
+        For Example:
 
-					self.correct = [self.TestEntry(i, guess)
-													for i in range(len(self.correct))]
-				"""
-		# If the item at correct[index] has been guessed, return the already correct
-		# letter.
-		if self.correct[index] != '_':
-			return self.correct[index]
-		elif self.answer[index] != entry:
-			return '_'
-		# If the guess is right, return it.
-		else:
-			return entry
+          self.correct = [self.TestEntry(i, guess)
+                          for i in range(len(self.correct))]
+        """
+    # If the item at correct[index] has been guessed, return the already correct
+    # letter.
+    if self.correct[index] != '_':
+      return self.correct[index]
+    elif self.answer[index] != entry:
+      return '_'
+    # If the guess is right, return it.
+    else:
+      return entry
 
-	def openFile(self, filename):
-		""" Open file filename and read it into a list.  Separate the list into
-				a dictionary wher the key is the word and the value is a clue.  Store
-				the dictionary in self.words.
-				"""
-		# Get the strings from the file.
-		wordList = open(filename).readlines()
-		wordList = [word.strip() for word in wordList]
+  def openFile(self, filename):
+    """ Open file filename and read it into a list.  Separate the list into
+        a dictionary wher the key is the word and the value is a clue.  Store
+        the dictionary in self.words.
+        """
+    # Get the strings from the file.
+    wordList = open(filename).readlines()
+    wordList = [word.strip() for word in wordList]
 
-		# Make the dictionary.
-		self.words = dict([self.getClues(wordList, i) for i in range(len(wordList))])
+    # Make the dictionary.
+    self.words = dict([self.getClues(wordList, i) for i in range(len(wordList))])
 
-	def getClues(self, wordList, i):
-		""" Allows the use of a list comprehension to create key value pairs
-				of the strings from a file.  If no clue is provided after a word then
-				the value returned with the word is "".
-				"""
-		# If there is no clue along with the word.
-		if wordList[i].count(' ') == 0:
-			return (wordList[i],"")
-		# Otherwise split the word from the clue.
-		else:
-			index = wordList[i].find(' ')
-			return (wordList[i][:index].strip(),wordList[i][index:].strip())
+  def getClues(self, wordList, i):
+    """ Allows the use of a list comprehension to create key value pairs
+        of the strings from a file.  If no clue is provided after a word then
+        the value returned with the word is "".
+        """
+    # If there is no clue along with the word.
+    if wordList[i].count(' ') == 0:
+      return (wordList[i],"")
+    # Otherwise split the word from the clue.
+    else:
+      index = wordList[i].find(' ')
+      return (wordList[i][:index].strip(),wordList[i][index:].strip())
 
-	def NetGame(self):
-		""" Start a new game using the word of the day from dictionary.com.
-				"""
-		page = urlopen("http://www.dictionary.com").read()
-		parser = wordoftheday()
-		parser.feed(page)
-		self.words[parser.word] = ""
-		self.NewGame()
+  def NetGame(self):
+    """ Start a new game using the word of the day from dictionary.com.
+        """
+    page = urlopen("http://www.dictionary.com").read()
+    parser = wordoftheday()
+    parser.feed(page)
+    self.words[parser.word] = ""
+    self.NewGame()
--- trunk/Hangman/pyHangman/wordoftheday.py
+++ trunk/Hangman/pyHangman/wordoftheday.py
@@ -11,21 +11,21 @@ from HTMLParser import HTMLParser
 import string
 
 class wordoftheday(HTMLParser):
-	""" Small subclass of HTMLParser that can search an html document
-			for specific tags, and retrieve the data contained by the tags
-			or that is surrounded by the tags.
-			"""
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.word = ""
-		self.isword = False
+  """ Small subclass of HTMLParser that can search an html document
+      for specific tags, and retrieve the data contained by the tags
+      or that is surrounded by the tags.
+      """
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.word = ""
+    self.isword = False
 
-	def handle_data(self, data):
-		""" Overridden from HTMLParser.  If the last tag grabbed is the tag
-				for dictionary.com's word of the day.  Store it in self.word if
-				the tags match and the data is longer than 1 character.
-				"""
-		# Test the last tag touched and make sure the data is longer than a character
-		# to insure it isn't garbage data.
-		if self.get_starttag_text() == '<a href="/wordoftheday/">' and len(data) > 1:
-			self.word = data
+  def handle_data(self, data):
+    """ Overridden from HTMLParser.  If the last tag grabbed is the tag
+        for dictionary.com's word of the day.  Store it in self.word if
+        the tags match and the data is longer than 1 character.
+        """
+    # Test the last tag touched and make sure the data is longer than a character
+    # to insure it isn't garbage data.
+    if self.get_starttag_text() == '<a href="/wordoftheday/">' and len(data) > 1:
+      self.word = data
