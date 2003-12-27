bad5007e387f72fa0eaa8e989183c5d880529ca6
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-12-27 09:13:06 +0000

    Fixed up the drop down menu object.

--- trunk/irc_rpg/CharacterSheet/GTKsheet.py
+++ trunk/irc_rpg/CharacterSheet/GTKsheet.py
@@ -1,10 +1,10 @@
-""" GTKsheet.py
+''' GTKsheet.py
 
 This module provides classes for turning an XML character sheet into gtk
 widgets for a usable interface.
 
 Copyright (C) 2003 W. Evan Sheehan <evan@navi.picogui.org>
-"""
+'''
 
 import xml.dom, xml.dom.minidom, GTKsheetElements
 from Character import Character
@@ -18,10 +18,11 @@ class GTKsheet:
     self.character = Character()
     self.character.readCharacter(dataFile)
     self.readSheet(self.character.getData('/character/layoutSheet'))
+    self.root = []
 
   def readSheet(self, layoutFile):
-    """ Read the XML file specified by layoutFile into a layout tree.
-        """
+    ''' Read the XML file specified by layoutFile into a layout tree.
+        '''
     self.dom = xml.dom.minidom.parse(layoutFile)
 
     # All character sheets must have atleast one <character_sheet> tag.
@@ -33,7 +34,7 @@ class GTKsheet:
         self.makeObjects(element, None).show()
 
   def makeObjects(self, newNode, parent):
-    """ Make objects out of the node passed. """
+    ''' Make objects out of the node passed. '''
     # Tag represents a layout object.
     try:
       newObject = GTKsheetElements.__dict__[newNode.tagName](newNode, self.character)
--- trunk/irc_rpg/CharacterSheet/GTKsheetElements.py
+++ trunk/irc_rpg/CharacterSheet/GTKsheetElements.py
@@ -1,17 +1,17 @@
-""" GTKsheetElements.py
+''' GTKsheetElements.py
 
 Contains classes for making XML tags in the character sheets into gtk
 widgets.
 
 Copyright (C) 2003 W. Evan Sheehan <evan@navi.picogui.org>
-"""
+'''
 
 import gtk, re, traceback
 from Character import Character
 from random import randint
 
 class sheetElement:
-  """ Superclass for all the tags. """
+  ''' Superclass for all the tags. '''
   def __init__(self, node):
     self.attributes = self.getAttrs(node)
 
@@ -24,7 +24,7 @@ class sheetElement:
 	for i in range(node.attributes.length)])
 
 class character_sheet(gtk.Window, sheetElement):
-  """ Basic tag that's treated as a gtk.Window. """
+  ''' Basic tag that's treated as a gtk.Window. '''
   def __init__(self, node, data):
     gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
     sheetElement.__init__(self, node)
@@ -38,9 +38,9 @@ class character_sheet(gtk.Window, sheetElement):
     child.show()
 
 class tab_view(gtk.Notebook, sheetElement):
-  """ gtk.Notebook subclassed for tabbed elements in the character
+  ''' gtk.Notebook subclassed for tabbed elements in the character
       sheet.
-      """
+      '''
   def __init__(self, node, data):
     sheetElement.__init__(self, node)
     gtk.Notebook.__init__(self)
@@ -55,7 +55,7 @@ class tab_view(gtk.Notebook, sheetElement):
     child.show()
 
 class vbox(gtk.VBox, sheetElement):
-  """ Just a normal vertical box for packing. """
+  ''' Just a normal vertical box for packing. '''
   def __init__(self, node, data):
     sheetElement.__init__(self, node)
     gtk.VBox.__init__(self, homogeneous=gtk.TRUE)
@@ -65,29 +65,29 @@ class vbox(gtk.VBox, sheetElement):
     ''' Just pack children into the vbox using pack_start because this is just
         a gtk.VBox.
 	'''
-    gtk.VBox.pack_start(self, child, padding=5)
+    self.pack_start(child, expand=gtk.FALSE, padding=5)
     child.show()
 
 class tab(vbox, sheetElement):
-  """ Treat tabs just like a vbox. """
+  ''' Treat tabs just like a vbox. '''
   def __init__(self, node, data):
     vbox.__init__(self, node, data)
 
 class hbox(gtk.HBox, sheetElement):
-  """ Just a normal horizontal box for packing. """
+  ''' Just a normal horizontal box for packing. '''
   def __init__(self, node, data):
     sheetElement.__init__(self, node)
     gtk.HBox.__init__(self)
 
   def packChild(self, child):
     ''' Pack children using pack_start, hbox is just a gtk.HBox. '''
-    gtk.HBox.pack_start(self, child, padding=5)
+    self.pack_start(child, padding=5)
     child.show()
 
 class text_field(gtk.HBox, sheetElement):
-  """ The text_field widget is a combination of a gtk.Label and a
+  ''' The text_field widget is a combination of a gtk.Label and a
       gtk.Entry packed into a gtk.HBox.
-      """
+      '''
   def __init__(self, node, characterData):
     sheetElement.__init__(self, node)
     gtk.HBox.__init__(self, spacing=5)
@@ -95,6 +95,7 @@ class text_field(gtk.HBox, sheetElement):
     self.label = gtk.Label(self.attributes.get('label', ''))
     self.text = gtk.Entry()
     self.text.set_text(characterData.getData(self.attributes.get('path', '')))
+    self.text.set_editable(gtk.FALSE)
     self.pack_start(self.label)
     self.pack_start(self.text)
 
@@ -107,18 +108,18 @@ class text_field(gtk.HBox, sheetElement):
     self.characterData.writeOut()
 
   def show(self):
-    """ Provide a way to show this object identical to showing normal
+    ''' Provide a way to show this object identical to showing normal
         gtk widgets.
-	"""
+	'''
     self.text.show()
     self.label.show()
     gtk.HBox.show(self)
 
 class dice(gtk.Button, sheetElement):
-  """ gtk.Button subclass to add some additional information about
+  ''' gtk.Button subclass to add some additional information about
       the widget, such as a reference to the dom node that created
       it.
-      """
+      '''
   def __init__(self, node, data, callback=lambda self: dice.roll(self)):
     sheetElement.__init__(self, node)
     gtk.Button.__init__(self, self.attributes.get('label', ''))
@@ -127,7 +128,7 @@ class dice(gtk.Button, sheetElement):
     self.data = {'times':[], 'mods':[]}
 
   def packChild(self, child):
-    """ Children of a dice node will always be data for that node. """
+    ''' Children of a dice node will always be data for that node. '''
     self.data[child.name].append(child.data)
 
   def copyWithCallback(self, callback):
@@ -138,15 +139,15 @@ class dice(gtk.Button, sheetElement):
     newDie.data = self.data
     return newDie
 
-  def roll(self, widget=None):
-    """ Roll the dice. """
+  def roll(self, widget=None, data=None): 
+    ''' Roll the dice. '''
     rolls = []
     times = 0
 
     # Calculate the total number of times to roll the dice.
     for time in self.data['times']:
       times += time
-    
+
     for i in range(times):
       roll = randint(1, int(self.attributes['sides']))
       # Add the modifiers to the roll.
@@ -158,7 +159,7 @@ class dice(gtk.Button, sheetElement):
 
 
 class times:
-  """ Number of times to roll the dice. """
+  ''' Number of times to roll the dice. '''
   def __init__(self, node, character):
     self.name = node.tagName
     if node.childNodes[0].data.count('/') > 0:
@@ -167,29 +168,25 @@ class times:
       self.data = int(node.childNodes[0].data)
 
 class mods(times):
-  """ Modifiers to results, modifiers are exactly the same as times, just
+  ''' Modifiers to results, modifiers are exactly the same as times, just
       with a different name.
-      """
+      '''
   def __init__(self, node, character):
     times.__init__(self, node, character)
 
-class drop_down(gtk.HBox, sheetElement):
+class drop_down(hbox):
   ''' Drop down menu in the character sheet, can contain one dice button to roll dice determined
       by the menu.
       '''
   def __init__(self, node, character):
-    sheetElement.__init__(self, node)
-    gtk.HBox.__init__(self)
+    hbox.__init__(self, node, character)
     #self.menu = []
 
     self.items = []
     self.button = None
     self.menu = gtk.Combo()
+    self.menu.entry.set_editable(gtk.FALSE)
     self.pack_start(self.menu)
-
-    # To determine whether the value in the menu should be appended to the list in
-    # the dice object, or should replace the last value.
-    self.beenPressed = False
 
     #for i in range(int(self.attributes.get('quantity', "1"))):
       #self.menu.append(gtk.Combo())
@@ -202,7 +199,7 @@ class drop_down(gtk.HBox, sheetElement):
 	of menu items. '''
     if isinstance(child, dice) and self.button is None:
       self.button = child.copyWithCallback(self.roll)
-      self.pack_end(self.button)
+      self.pack_end(self.button, padding=5)
     elif isinstance(child, drop_down_item):
       for item in child.data:
         self.items.append(item)
@@ -219,30 +216,22 @@ class drop_down(gtk.HBox, sheetElement):
     ''' Roll the dice object. '''
     # Get the data from the menu: number of times, number of sides and modifiers.
     values = re.search('\[([1-9]+)d([1-9]+)\+?([1-9]*)\]', self.menu.entry.get_text())
-    if values is not None:
-      # If this is the first time the die has been pressed append the value.
-      if not self.beenPressed:
-	self.beenPressed = True
-	self.button.data['times'].append(int(values.group(1)))
-	if values.group(3):
-	  self.button.data['mods'].append(int(values.group(3)))
-      # If the button has been pressed before, replace the last value.
-      else:
-	self.button.data['times'][len(self.button.data['times']) - 1] = int(values.group(1))
-	if values.group(3):
-	  self.button.data['mods'][len(self.button.data['mods']) - 1] = int(values.group(3))
-      # Set the number of sides and roll.
+    if values:
       self.button.attributes['sides'] = values.group(2)
-      dice.roll(self.button)
+      self.button.data['times'] = [int(values.group(1))]
+      if values.group(3):
+	self.button.data['mods'] = [int(values.group(3))]
+      self.button.roll()
 
 class drop_down_item(sheetElement):
   ''' Items in a drop down menu. '''
   def __init__(self, node, character):
     sheetElement.__init__(self, node)
 
+    # Get the data from the data sheet, split it on new lines and strip whitespace.
     if node.childNodes[0].data.count('/') > 0:
       self.data = character.getData(node.childNodes[0].data).split('\n')
-      for i in range(len(self.data)):
-	self.data[i] = self.data[i].strip()
+      self.data = [item.strip() for item in self.data]
+    # The data is constant in the layout sheet, so just save it.
     else:
       self.data = [node.childNodes[0].data]
