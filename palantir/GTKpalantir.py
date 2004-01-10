324c3faad27051173d6f64d370f6b9e95e53cfa1
Evan Sheehan <darthmall@users.noreply.github.com>
2004-01-10 10:43:48 +0000

    Alright, got the character sheet loading implemented in the client (piece of cake).  So much cleaning to be done, and so much new stuff to be written.  It didn't seem this big when I started. ^^

--- palantir/GTKpalantir.py
+++ palantir/GTKpalantir.py
@@ -1,6 +1,9 @@
 import gtk, gtk.glade
+from CharacterSheet.Character import Character
+from CharacterSheet.GTKsheet import GTKsheet
 
 def ShowMain():
+  ''' Show the main window. '''
   global tree
   tree = gtk.glade.XML('palantirMain.glade')
   for func in globals().iterkeys():
@@ -8,8 +11,29 @@ def ShowMain():
       tree.signal_connect(func, globals()[func])
 
 def on_character_sheet_activate(widget, data=None):
-  global tree
   if tree.get_widget('character_sheet').get_active():
     tree.get_widget('CharacterSheetPane').show()
   else:
     tree.get_widget('CharacterSheetPane').hide()
+
+def on_open_sheet_activate(widget, data=None):
+  tree.dialog = gtk.glade.XML('CharacterSheet/sheetselection.glade')
+  tree.dialog.get_widget('SheetSelection').set_filename('CharacterSheet/data/')
+  tree.dialog.signal_autoconnect({ 'on_ok_button_clicked':OpenSheet,
+   'on_cancel_button_clicked':lambda w: tree.dialog.get_widget('SheetSelection').destroy()})
+
+def OpenSheet(widget, data=None):
+  data = Character(tree.dialog.get_widget('SheetSelection').get_filename())
+  if hasattr(tree, 'sheet'):
+    tree.get_widget('CharacterViewPort').remove(tree.sheet.root)
+  tree.sheet = GTKsheet(data)
+  tree.sheet.filename = tree.dialog.get_widget('SheetSelection').get_filename()
+  tree.get_widget('CharacterViewPort').add(tree.sheet.root)
+  tree.sheet.root.show()
+  tree.dialog.get_widget('SheetSelection').destroy()
+
+def on_Main_destroy(widget, data=None):
+  gtk.main_quit()
+
+def on_quit_activate(widget, data=None):
+  gtk.main_quit()
