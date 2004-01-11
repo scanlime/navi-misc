7bc6948afe66db770ea2096f72274367934a2c5a
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-11 02:01:59 +0000

    Break time again

---
+++ trunk/palantir/CharacterSheet/__init__.py
@@ -0,0 +1,9 @@
+''' __init__.py
+
+The CharacterSheet module handles the storing, creation and viewing of
+character sheets for the Palantir RPG client.
+
+  Copyright (C) 2003 W. Evan Sheehan <evan@navi.cx>
+'''
+
+import xml.dom, xml.dom.minidom, string
