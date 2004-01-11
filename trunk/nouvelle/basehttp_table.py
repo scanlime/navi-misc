91523ca8fb84cb5c034ee5853cd1a48e2a5e9406
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-11 20:14:28 +0000

    Add a setup.py, improve the basehttp_table example a bit with a second view for the same data, add a version number

--- trunk/nouvelle/basehttp_table.py
+++ trunk/nouvelle/basehttp_table.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python
 #
 # A simple Nouvelle example using BaseHTTPServer and Nouvelle.Table
-# This runs a web server at http://localhost:8080, and presents a table
+# This runs a web server at http://localhost:8080, and presents tables
 # that you can resort by clicking the headings.
 #
-
-# So we can find Nouvelle even if it isn't installed...
-import sys, os; sys.path[0] = os.path.join(sys.path[0], '..', '..')
 
 import Nouvelle
 from Nouvelle import tag, place, BaseHTTP
 
-
 class MyTable(Nouvelle.ResortableTable):
     tableTag = tag('table', border='1')
-
 
 class Hello(BaseHTTP.Page):
     document = tag('html')[
                    tag('body') [
                        tag('h3')[ "Hello World!" ],
-                       place("dataTable"),
+                       tag('p') [ place("dataTable") ],
+                       tag('hr'),
+                       tag('p') [ place("foodTable") ],
                    ],
                ]
 
     data = [
-        ['Llama',   3, 6, 2],
-        ['Newt',    2, 7, 1],
-        ['Banana',  5, 6, 0],
-        ['Oatmeal', 3, 8, 2],
-        ['Pine',    2, 9, 4],
+        ['Llama',   3, 6, 2, 'Oatmeal'],
+        ['Newt',    2, 7, 1, 'Pine'],
+        ['Banana',  5, 6, 0, 'Banana'],
+        ['Oatmeal', 3, 8, 2, 'Newt'],
+        ['Pine',    2, 9, 4, 'Llama'],
         ]
 
     def render_dataTable(self, context):
@@ -38,7 +35,13 @@ class Hello(BaseHTTP.Page):
             Nouvelle.IndexedColumn('absorbency', 1),
             Nouvelle.IndexedColumn('conductivity', 2),
             Nouvelle.IndexedColumn('luminance', 3),
-            ])
+            ], id='widgets')
+
+    def render_foodTable(self, context):
+        return MyTable(self.data, [
+            Nouvelle.IndexedColumn('name', 0),
+            Nouvelle.IndexedColumn('eats', 4),
+            ], id='food')
 
 if __name__ == "__main__":
     BaseHTTP.main(Hello())
