dea7182b882e98f2bcf3c0725c968fbea20a35a4
Micah Elizabeth Scott <micah@scanlime.org>
2004-01-22 12:21:49 +0000

    Tagging 0.91 for release. Really the only thing that's happened since 0.90 are some very minor bugfixes, but these need to be released for CIA :)

--- xchat-gnome/fe-gnome/palette.h
+++
@@ -1,6 +0,0 @@
-extern GdkColor colors[];
-/*
-void palette_alloc (GtkWidget * widget);
-void palette_load (void);
-void palette_save (void);
-*/
--- xchat-gnome/fe-gnome/preferences_dialog.h
+++
@@ -1,8 +0,0 @@
-#include "gui.h"
-
-#ifndef XCHAT_GNOME_PREFERENCES_DIALOG_H
-#define XCHAT_GNOME_PREFERENCES_DIALOG_H
-
-void initialize_preferences_dialog();
-
-#endif
