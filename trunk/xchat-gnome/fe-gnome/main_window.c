d2b2d2267000b168b53f360fe8b41f60fd28f49f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-22 02:45:27 +0000

    make it pop up the prefs dialog

--- trunk/xchat-gnome/fe-gnome/main_window.c
+++ trunk/xchat-gnome/fe-gnome/main_window.c
@@ -1,6 +1,14 @@
 #include "main_window.h"
 
+void on_preferences_menu_activate(GtkWidget *widget, gpointer data);
+
 void initialize_main_window() {
   gui.main_window = GNOME_APP(glade_xml_get_widget(gui.xml, "xchat-gnome"));
+  g_signal_connect(G_OBJECT(glade_xml_get_widget(gui.xml, "preferences menu")), "activate", (GCallback) on_preferences_menu_activate, NULL);
   gtk_widget_show_all(GTK_WIDGET(gui.main_window));
+}
+
+void on_preferences_menu_activate(GtkWidget *widget, gpointer data) {
+  g_print("activating preferences\n");
+  gtk_widget_show_all(GTK_WIDGET(gui.preferences_dialog));
 }
