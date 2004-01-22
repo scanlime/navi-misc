7651520064097a6781a6b697b1274fbc2e765f38
David Trowbridge <trowbrds@gmail.com>
2004-01-22 02:20:07 +0000

    rearrangify things a bit

--- xchat-gnome/test.c
+++ xchat-gnome/test.c
@@ -11,13 +11,16 @@ int main(int argc, char **argv) {
   GtkWidget *topic_bar, *user_list, *server_list, *text_box;
 
   gnome_program_init("xchat test", "0.1", LIBGNOMEUI_MODULE, argc, argv, NULL);
-  xml = glade_xml_new("xchat-gnome.glade", NULL, NULL);
+  xml = glade_xml_new("fe-gnome/xchat-gnome.glade", NULL, NULL);
 
   init_settings();
   init_userlist();
 
   topic_bar = glade_xml_get_widget(xml, "topic entry");
   gtk_entry_set_text(GTK_ENTRY(topic_bar), "this is a topic");
+
+  gtk_widget_show_all(glade_xml_get_widget(xml, "xchat-gnome"));
+  gtk_widget_show_all(glade_xml_get_widget(xml, "preferences"));
 
   gtk_main();
   return 0;
