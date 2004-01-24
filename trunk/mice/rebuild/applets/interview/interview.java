1f08d9c76f114a1cc2239a53e026b1f3ebb2e1f5
brandon <brandon@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-24 10:54:45 +0000

    I think thats good enough for now, it is sorta behaving as I want it to

--- trunk/mice/rebuild/applets/interview/interview.java
+++ trunk/mice/rebuild/applets/interview/interview.java
@@ -9,22 +9,25 @@ import java.io.*;
 
 public class interview extends java.applet.Applet implements ActionListener
 {
+	TextField username, password;
 	TextArea question, answer;
-	Button button1, button2;
+	Button button1, button2, button3;
 
 	public void init()
 	{
 		//set up the various fields from the information class
+		username = new TextField("username",20);
+		password = new TextField("password",20);
 		question = new TextArea("",6,50,TextArea.SCROLLBARS_VERTICAL_ONLY);
 		answer = new TextArea("",10,50,TextArea.SCROLLBARS_VERTICAL_ONLY);
-		button1 = new Button("one");
-		button2 = new Button("two");
+		button1 = new Button("Sign In");
+		button2 = new Button("Reject");
+		button3 = new Button("Quit");
         
 		//add the various objects to the applet
-		add(question);
-		add(answer);
+		add(username);
+		add(password);
 		add(button1);
-		add(button2);
 		button1.addActionListener(this);
 	}
 	
@@ -32,7 +35,14 @@ public class interview extends java.applet.Applet implements ActionListener
 	{
 		remove(button1);
 		remove(button2);
-		button1.setLabel("Door Knob");
+		remove(username);
+		remove(password);
+		button1.removeActionListener(this);
+		button1.setLabel("Answer");
+		add(question);
+		add(answer);
 		add(button1);
+		add(button2);
+		add(button3);
 	}
 }
