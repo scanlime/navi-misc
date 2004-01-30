70f23b86ef2b787051ac92413a164a3b6584d9f6
brandon <gonkulator@gmail.com>
2004-01-30 20:59:44 +0000

    added some stuff, and documented what needed to be documented.

--- mice/rebuild/applets/admin/authenticate.java
+++ mice/rebuild/applets/admin/authenticate.java
@@ -18,6 +18,12 @@ public class authenticate implements ActionListener
 {
 	private admin myadmin;
 
+	/**
+	 * This method creates a new authentication object.
+	 * @param my The admin applet reference for adding and removing stuff to it.
+	 * @author Brandon Smith
+	 * @version 2.0
+	 */
 	public authenticate(admin my)
 	{
 		myadmin = my;
@@ -30,6 +36,13 @@ public class authenticate implements ActionListener
 		myadmin.ad(info.submit);
 	}
 	
+	/**
+	 * This method is what handles the press of the button to submit info to the
+	 * server.
+	 * @param e The ActionEvent object associated with this event
+	 * @author Brandon Smith
+	 * @version 2.0
+	 */
 	public void actionPerformed(ActionEvent e)
 	{
 		myadmin.rm(info.username);
