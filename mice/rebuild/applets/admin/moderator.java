4b61b6b97b73f4f471e365438e80ffe2bd424e5a
brandon <gonkulator@gmail.com>
2004-01-30 02:18:07 +0000

    Ok, here is a start on administration for the server.  Very little actual code, but lots of new files.

---
+++ mice/rebuild/applets/admin/moderator.java
@@ -0,0 +1,10 @@
+/**
+ * This class handles the moderation stuff, in that it sets its information in 
+ * the server, by telling it how many moderators, what their usernames/passwords
+ * are, and informing the moderator through all of it.
+ * @author Brandon Smith
+ * @version 2.0
+ */
+public class moderator
+{
+}
---
+++ mice/rebuild/server/aadmin.java
@@ -0,0 +1,12 @@
+/**
+ * This is the class that handles administration.  It keeps static information
+ * on the state of the server, so if the administrator gets disconnected, life
+ * will be fine with a reconnect.
+ * @author Brandon Smith
+ * @version 2.0
+ */
+
+public class aadmin
+{
+		
+}
