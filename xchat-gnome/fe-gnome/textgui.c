b9d0602e44b90879943786a20c38140e9009b43e
David Trowbridge <trowbrds@gmail.com>
2004-01-31 08:59:34 +0000

    finish up copyright notices

--- xchat-gnome/fe-gnome/textgui.c
+++ xchat-gnome/fe-gnome/textgui.c
@@ -1,3 +1,24 @@
+/*
+ * textgui.c - helpers for the main text gui
+ *
+ * Copyright (C) 2004 David Trowbridge and Dan Kuester
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
 #include "textgui.h"
 #include "palette.h"
 #include "../common/text.h"
@@ -43,7 +64,7 @@ void text_gui_add_text_buffer(struct session *sess) {
 
 void text_gui_remove_text_buffer(struct session *sess) {
 	session_gui *tgui;
-	
+
 	tgui = sess->gui;
 	gtk_xtext_buffer_free(tgui->buffer);
 	free(tgui);
