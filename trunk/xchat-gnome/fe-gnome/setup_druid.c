cf68aacc005fd1819057a262914f6eeb17da9360
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-02 02:06:19 +0000

    give the nickname field focus, hide the druid when its done

--- trunk/xchat-gnome/fe-gnome/setup_druid.c
+++ trunk/xchat-gnome/fe-gnome/setup_druid.c
@@ -21,8 +21,10 @@
 
 #include "setup_druid.h"
 #include "gui.h"
+#include "../common/servlist.h"
 
 static gboolean druid_finished;
+void setup_druid_servers_populate(GtkWidget *treeview);
 void setup_druid_nicknames_prepare(GnomeDruidPage *p, GtkWidget *w, gpointer d);
 void setup_druid_finish_prepare(GnomeDruidPage *p, GtkWidget *w, gpointer d);
 void setup_druid_finish(GnomeDruidPage *p, GtkWidget *w, gpointer d);
@@ -30,12 +32,16 @@ void setup_druid_nickname_changed(GtkEditable *entry, gpointer d);
 void setup_druid_realname_changed(GtkEditable *entry, gpointer d);
 
 void initialize_setup_druid() {
-	GtkWidget *widget;
+	GtkWidget *widget, *treeview;
 	GtkSizeGroup *group;
 	GtkWidget *nickname_page, *finish_page;
 	GtkWidget *nickname_entry, *realname_entry;
+	GtkListStore *store;
+	GtkCellRenderer *text_renderer, *autoconnect_renderer;
+	GtkTreeViewColumn *text_column, *autoconnect_column;
+	GtkTreeSelection *select;
 
-	gui.setup_druid = glade_xml_get_widget(gui.xml, "setup druid");
+	gui.setup_druid = GNOME_DRUID(glade_xml_get_widget(gui.xml, "setup druid"));
 
 	group = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
 	widget = glade_xml_get_widget(gui.xml, "setup druid nickname");
@@ -55,6 +61,23 @@ void initialize_setup_druid() {
 	g_signal_connect(G_OBJECT(nickname_entry), "realize", G_CALLBACK(setup_druid_nickname_changed), NULL);
 	realname_entry = glade_xml_get_widget(gui.xml, "setup druid realname");
 	g_signal_connect(G_OBJECT(realname_entry), "changed", G_CALLBACK(setup_druid_realname_changed), NULL);
+
+	treeview = glade_xml_get_widget(gui.xml, "setup druid server list");
+
+	store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_BOOLEAN);
+	gtk_tree_view_set_model(GTK_TREE_VIEW(treeview), GTK_TREE_MODEL(store));
+
+	text_renderer = gtk_cell_renderer_text_new();
+	text_column = gtk_tree_view_column_new_with_attributes("name", text_renderer, "text", 0, NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), text_column);
+	autoconnect_renderer = gtk_cell_renderer_toggle_new();
+	autoconnect_column = gtk_tree_view_column_new_with_attributes("auto-connect", autoconnect_renderer, "active", 1, NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), autoconnect_column);
+
+	select = gtk_tree_view_get_selection(GTK_TREE_VIEW(treeview));
+	gtk_tree_selection_set_mode(select, GTK_SELECTION_NONE);
+
+	setup_druid_servers_populate(treeview);
 }
 
 void run_setup_druid() {
@@ -65,18 +88,42 @@ void run_setup_druid() {
 	}
 }
 
-void setup_druid_nicknames_prepare(GnomeDruidPage *p, GtkWidget *w, gpointer d) {
-	GtkWidget *realname_entry;
+void setup_druid_servers_populate(GtkWidget *treeview) {
+	GtkListStore *store;
+	GtkTreeIter iter;
+	ircnet *net;
+	GSList *netlist;
 
-	realname_entry = glade_xml_get_widget(gui.xml, "setup druid realname");
-	gtk_entry_set_text(GTK_ENTRY(realname_entry), g_get_real_name());
+	netlist = network_list;
+	store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(treeview)));
+	gtk_list_store_clear(store);
+
+	while(netlist) {
+		net = netlist->data;
+		gtk_list_store_append(store, &iter);
+		gtk_list_store_set(store, &iter, 0, net->name, 1, FALSE, -1);
+		netlist = netlist->next;
+	}
+}
+
+void setup_druid_nicknames_prepare(GnomeDruidPage *p, GtkWidget *w, gpointer d) {
+	GtkWidget *nick, *real;
+
+	real = glade_xml_get_widget(gui.xml, "setup druid realname");
+	nick = glade_xml_get_widget(gui.xml, "setup druid nickname");
+	gtk_entry_set_text(GTK_ENTRY(real), g_get_real_name());
+	gtk_widget_grab_focus(nick);
 }
 
 void setup_druid_finish_prepare(GnomeDruidPage *p, GtkWidget *w, gpointer d) {
-	gnome_druid_set_show_finish(GNOME_DRUID(gui.setup_druid), TRUE);
+	gnome_druid_set_show_finish(gui.setup_druid, TRUE);
 }
 
 void setup_druid_finish(GnomeDruidPage *p, GtkWidget *w, gpointer d) {
+	GtkWidget *druid_window;
+
+	druid_window = glade_xml_get_widget(gui.xml, "setup druid window");
+	gtk_widget_hide_all(druid_window);
 	druid_finished = TRUE;
 }
 
@@ -87,9 +134,9 @@ void setup_druid_nickname_changed(GtkEditable *entry, gpointer d) {
 	real = glade_xml_get_widget(gui.xml, "setup druid realname");
 	if(strlen(gtk_entry_get_text(GTK_ENTRY(nick))) == 0 ||
 	   strlen(gtk_entry_get_text(GTK_ENTRY(real))) == 0)
-		gnome_druid_set_buttons_sensitive(GNOME_DRUID(gui.setup_druid), TRUE, FALSE, TRUE, TRUE);
+		gnome_druid_set_buttons_sensitive(gui.setup_druid, TRUE, FALSE, TRUE, TRUE);
 	else
-		gnome_druid_set_buttons_sensitive(GNOME_DRUID(gui.setup_druid), TRUE, TRUE, TRUE, TRUE);
+		gnome_druid_set_buttons_sensitive(gui.setup_druid, TRUE, TRUE, TRUE, TRUE);
 }
 
 void setup_druid_realname_changed(GtkEditable *entry, gpointer d) {
@@ -99,7 +146,7 @@ void setup_druid_realname_changed(GtkEditable *entry, gpointer d) {
 	real = glade_xml_get_widget(gui.xml, "setup druid realname");
 	if(strlen(gtk_entry_get_text(GTK_ENTRY(nick))) == 0 ||
 	   strlen(gtk_entry_get_text(GTK_ENTRY(real))) == 0)
-		gnome_druid_set_buttons_sensitive(GNOME_DRUID(gui.setup_druid), TRUE, FALSE, TRUE, TRUE);
+		gnome_druid_set_buttons_sensitive(gui.setup_druid, TRUE, FALSE, TRUE, TRUE);
 	else
-		gnome_druid_set_buttons_sensitive(GNOME_DRUID(gui.setup_druid), TRUE, TRUE, TRUE, TRUE);
+		gnome_druid_set_buttons_sensitive(gui.setup_druid, TRUE, TRUE, TRUE, TRUE);
 }
