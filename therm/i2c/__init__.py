d4addef466c8d6334155a85372d3aac70b8711fb
David Trowbridge <trowbrds@gmail.com>
2004-02-03 02:04:40 +0000

    more copyright - funny how micah owns copyright on code that i think i wrote, but oh well, he's a nice guy

--- therm/i2c/__init__.py
+++ therm/i2c/__init__.py
@@ -4,7 +4,7 @@ A python package for bit-banging i2c devices
 """
 #
 # Remote Controlled PIC of Doom
-# Copyright (C) 2003 Micah Dowty <micahjd@users.sourceforge.net>
+# Copyright (C) 2003-2004 Micah Dowty <micahjd@users.sourceforge.net>
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
