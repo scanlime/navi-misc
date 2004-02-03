aa2d0ace3af7913ce0037845cbd0d7fe84756f1a
David Trowbridge <trowbrds@gmail.com>
2004-02-03 02:01:43 +0000

    even more copyright

--- wasabi/Wasabi/IR.py
+++ wasabi/Wasabi/IR.py
@@ -4,7 +4,7 @@ An lircd client that handles dispatching IR events to input handlers.
 """
 #
 # Wasabi Project
-# Copyright (C) 2003 Micah Dowty <micahjd@users.sourceforge.net>
+# Copyright (C) 2003-2004 Micah Dowty <micahjd@users.sourceforge.net>
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
