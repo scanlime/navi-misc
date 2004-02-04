8a489ca036708f8e12725bbe4cfe0cbeaa50351f
brandon <brandon@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-04 03:31:48 +0000

    added stuff, coded stuff, punched stuff, ate some stuff, watched some tv stuff, stuff, stuff, stuff......

--- trunk/mice/rebuild/applets/moderate/accept.java
+++ trunk/mice/rebuild/applets/moderate/accept.java
@@ -11,19 +11,36 @@ import java.io.*;
  * @author Brandon Smith
  * @version 2.0
  */
-
 public class accept implements ActionListener
 {
 	private int ID;
 	
-	public accept(int ident)
+	private TextArea question.
+	
+	/**
+	 * This method sets the ID as its the constructor.
+	 * @param ident The ID of the person that this will be approved for
+	 * @param quest The TextArea that has the question in it.
+	 * @author Brandon Smith
+	 * @version 2.0
+	 */
+	public accept(int ident, TextArea quest)
 	{
 		ID = ident;
+		question = quest;
 	}
 	
+	/**
+	 * This method handles the click of the button.  It informs the server which
+	 * person the question is approved for.
+	 * @param e An ununsed event information object
+	 * @author Brandon Smith
+	 * @version 2.0
+	 */
 	public void actionPerformed(ActionEvent e)
 	{
 		net.write("accept");
 		net.write("" + ID);
+		quest.setText("");
 	}
 }
---
+++ trunk/mice/rebuild/applets/moderate/quit.java
@@ -0,0 +1,36 @@
+/**
+ * This class handles the sending of a quit message to the server when the quit
+ * button is clicked.
+ * @author Brandon Smith
+ * @version 2.0
+ */
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.*;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class quit implements ActionListener
+{
+	/**
+	 * This constructs the quit object handler
+	 * @author Brandon Smith
+	 * @version 2.0
+	 */
+	public quit(){}
+	
+	/**
+	 * This just sends the quit message when called
+	 * @param e The unused Action Event
+	 * @author Brandon Smith
+	 * @version 2.0
+	 */
+	public void actionListener(ActionEvent e)
+	{
+		net.write("quit");
+		net.closeConnection();
+	}
+}
