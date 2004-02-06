702b1388a4c20d42db26b7a3024c11d1c5aa146d
Micah Elizabeth Scott <micah@scanlime.org>
2004-02-06 07:29:53 +0000

    Yay, it's more complicated and does less... but the graph is now stored in C arrays via pyrex, so when this actually tries to do graph layout it should be way faster.

--- flibble/layout-test.py
+++ flibble/layout-test.py
@@ -1,50 +1,42 @@
 #!/usr/bin/env python
 
-import RandomArray, Numeric
+import force_directed
 
 
 class Edge:
     """Base class for graph edges. If edges need to include
        more information, this can be subclassed.
        """
-    def __init__(self, nodeA, nodeB):
-        self.a = nodeA
-        self.b = nodeB
+    def __init__(self, vertexA, vertexB):
+        self.a = vertexA
+        self.b = vertexB
 
 
 class Graph:
-    """Represents a graph, where nodes and edges are represented by flexible Python
-       objects and by an efficient Numeric array.
-       The Graph must be constructed with a sequence of Edge instances. Nodes can
-       be any hashable python object, and they will be inferred from the set of edges.
+    """A higher level graph abstraction in which edges are instances
+       of the Edge class and vertices can be any hashable python object.
+       This creates an edge list suitable for passing to a low-level graph.
        """
     def __init__(self, edges):
-        self.edgeList = edges
+        # A unique list of vertices, and a mapping from each vertex to its index in the list
+        self.vertexMap = {}
+        self.vertexList = []
 
-        # A unique list of nodes, and a mapping from each node to its index in the list
-        self.nodeMap = {}
-        self.nodeList = []
+        # A list of edges, as 2-tuples of vertex indices
+        self.edgeTuples = []
 
-        # Create a Numeric array with indices into the nodeList for each edge
-        self.edgeArray = Numeric.zeros((len(self.edgeList), 2), Numeric.Int)
-
-        # Populate egeArray, nodeMap, and nodeList
+        # Populate edgeList, vertexMap, and vertexList
         i = 0
-        for edge in self.edgeList:
-            # Add each node to the map and list
-            for node in edge.a, edge.b:
-                if node not in self.nodeMap:
-                    id = len(self.nodeList)
-                    self.nodeMap[node] = id
-                    self.nodeList.append(node)
+        for edge in edges:
+            # Add each vertex to the map and list
+            for vertex in edge.a, edge.b:
+                if vertex not in self.vertexMap:
+                    id = len(self.vertexList)
+                    self.vertexMap[vertex] = id
+                    self.vertexList.append(vertex)
 
             # Add this edge to the array
-            self.edgeArray[i] = self.nodeMap[edge.a], self.nodeMap[edge.b]
-            i += 1
-
-    def __repr__(self):
-        return "<%s with %d nodes and %d edges>" % (self.__class__.__name__,
-                                                    len(self.nodeList), len(self.edgeList))
+            self.edgeTuples.append((self.vertexMap[edge.a], self.vertexMap[edge.b]))
 
 
 if __name__ == '__main__':
@@ -52,30 +44,35 @@ if __name__ == '__main__':
     import cPickle
     rows = cPickle.load(open('cia-dataset.p'))
 
-    # Load the edges into a Graph object
-    graph = Graph([Edge(a, b) for a, b, s, f in rows])
-    print graph
+    # Load the edges into a high-level graph object
+    hlGraph = Graph([Edge(a, b) for a, b, s, f in rows])
 
-    # Randomize the initial positions
-    positions = RandomArray.uniform(0, 100, (len(graph.nodeList), 2))
+    # Make a force_directed graph from that
+    fdGraph = force_directed.Graph(hlGraph.edgeTuples, len(hlGraph.vertexList))
+    print fdGraph
 
-    # Solve for a local minimum in the energy of our spring system
-    springLen = 5
-    try:
-        while 1:
-            energy = 0
-            for a, b in graph.edgeArray:
-                ab = positions[a] - positions[b]
-                magnitude = Numeric.dot(ab, ab)
-                unitv = ab / magnitude
-                forceMag = (magnitude - springLen) * 0.2
-                energy += abs(forceMag)
-                force = unitv * forceMag
-                positions[a] -= force
-                positions[b] += force
-            print energy
-    except KeyboardInterrupt:
-        pass
+    fdGraph.randomize(0, 0, 100, 100)
+
+
+    vectors = fdGraph.getVectors()
+
+#     # Solve for a local minimum in the energy of our spring system
+#     springLen = 5
+#     try:
+#         while 1:
+#             energy = 0
+#             for a, b in graph.edgeArray:
+#                 ab = positions[a] - positions[b]
+#                 magnitude = Numeric.dot(ab, ab)
+#                 unitv = ab / magnitude
+#                 forceMag = (magnitude - springLen) * 0.2
+#                 energy += abs(forceMag)
+#                 force = unitv * forceMag
+#                 positions[a] -= force
+#                 positions[b] += force
+#             print energy
+#     except KeyboardInterrupt:
+#         pass
 
     # Begin the SVG
     svg = open('graph.svg', 'w')
@@ -87,14 +84,14 @@ if __name__ == '__main__':
               '  x="0" y="0" width="100" height="100">\n')
 
     # Write all edges
-    for a, b in graph.edgeArray:
-        va = positions[a]
-        vb = positions[b]
+    for a, b in hlGraph.edgeTuples:
+        va = vectors[a]
+        vb = vectors[b]
         svg.write('<line x1="%f" y1="%f" x2="%f" y2="%f" stroke="black" stroke-width="0.1"/>\n' %
                   (va[0], va[1], vb[0], vb[1]))
 
     # Write all nodes
-    for x, y in positions:
+    for x, y in vectors:
         svg.write('<circle cx="%f" cy="%f" r="0.2" stroke="black" fill="yellow" stroke-width="0.05"/>\n' %
                   (x, y))
 
