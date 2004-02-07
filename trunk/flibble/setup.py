707acedd465ca9d2606981d14e056095805e1401
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-07 01:21:44 +0000

    Move things around and fix the build system so we can have a Flibble package

--- trunk/flibble/setup.py
+++ trunk/flibble/setup.py
@@ -4,8 +4,14 @@ from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
 setup(
   name = "Flibble",
+  version = "pre-0.10",
+  description = "A simple python+pyrex graph vizualization package",
+  maintainer = "Micah Dowty",
+  maintainer_email = "micah@navi.cx",
+  license = "LGPL",
+  packages = ['Flibble'],
   ext_modules=[
-    Extension("force_directed", ["force_directed.pyx"])
+    Extension("Flibble.ForceDirected", ["Flibble/ForceDirected.pyx"])
     ],
   cmdclass = {'build_ext': build_ext}
 )
