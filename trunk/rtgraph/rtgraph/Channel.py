68fac3a1ed1da41267879b7355324907556e06ca
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-09 06:28:18 +0000

    add a loudmouth channel class that bothers everyone when it has new data

--- trunk/rtgraph/rtgraph/Channel.py
+++ trunk/rtgraph/rtgraph/Channel.py
@@ -124,6 +124,14 @@ class Channel(object):
            """
         return limitFloatPrecision(self.value, 4)
 
+class NotifiyingChannel(Channel):
+    """A channel that notifies its listeners when it's updated"""
+    listeners = None
+    def setValue(self, value, timestamp=None):
+        Channel.setValue(self, value, timestamp)
+        for listener in listeners:
+            listener.Notify(self)
+
 class FunctionChannel(Channel):
     """A channel that evaluates a function, specified as a Python expression
 
