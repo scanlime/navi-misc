9ff4abf4b31853da102a10edece3b267ade29955
David Trowbridge <trowbrds@gmail.com>
2004-02-11 06:47:28 +0000

    add a helpful warning

--- chaos/de_jong_explorer.c
+++ chaos/de_jong_explorer.c
@@ -295,5 +295,7 @@ void saveclick(GtkWidget *widget, gpointer user_data) {
   }
   g_object_unref(filter);
   gtk_widget_destroy(dialog);
+#else
+#warning "If you got gtk 2.3, you'd be able to save images"
 #endif
 }
