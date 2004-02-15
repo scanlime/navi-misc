b0562433b297a394be5e03219426e86b8c7cb3ee
brandon <gonkulator@gmail.com>
2004-02-15 07:58:23 +0000

    I updated the documentation stuff, and started work on the AIM update bot connections.

--- mice/rebuild/server/nchatup.java
+++ mice/rebuild/server/nchatup.java
@@ -9,14 +9,18 @@ import java.net.*;
 import java.util.*;
 import java.io.*;
 
-public class nchatup extends nbase
+public class nchatup extends nbase implements aupdatei
 {
 	/**
 	 * This method initializes this thread with the new connection information
-	 * @param host The host that the chat bot is running on
-	 * @param port The port number that the server is running on
+	 * @author Brandon Smith
+	 * @version 2.0
 	 */
 	public nchatup(String host, int port)
+	{
+	}
+	
+	public boolean connect(String host, int port)
 	{
 		try//connect to the AIM update server
 		{
@@ -25,15 +29,9 @@ public class nchatup extends nbase
 			out = new OutputStreamWriter(link.getOutputStream());
 		}
 		catch(IOException e){}
-		//get the number
 	}
 	
-	/**
-	 * Since this is a subclass of Thread, it has to have a run() method
-	 * @author Brandon Smith
-	 * @version 2.0
-	 */
-	public void run()
-	{
-	}
+	public void update(question tosend){}
+	public void notice(String tosend){}
+	public void quit(){}
 }
