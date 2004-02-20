233e873dd70ac0841a9598ba6564414a1578b19b
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-20 04:57:56 +0000

    This was an experiment hoping to speed up de jong explorer drastically by exploiting the fact that fairly large numerical truncation can be concealed by replacing the unknown digits with random noise. I admit defeat. Even with a lot of tweaking, I could only get the trig table based sine function to go about 50% faster than the built-in sine function. On top of that, to avoid the big speed hit of generating a random variate at each iteration, this used something like the blur table but worse. This turned out to be very finicky and really not worth a 50% speed increase.

---
+++ branches/de-jong-explorer-crappy-trig/src/de-jong.h
@@ -0,0 +1,122 @@
+/*
+ * de-jong.h - Shared declarations
+ *
+ * de Jong Explorer - interactive exploration of the Peter de Jong attractor
+ * Copyright (C) 2004 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+#ifndef __DE_JONG_H_
+#define __DE_JONG_H__
+
+struct vector2 {
+  float x,y;
+};
+
+struct computation_params {
+  float a, b, c, d;
+  float zoom, xoffset, yoffset, rotation;
+  float blur_radius, blur_ratio;
+  gboolean tileable;
+};
+
+struct render_params {
+  guint width, height;
+  guint oversample;
+  guint *counts;
+  GdkPixbuf *pixbuf;
+
+  guint color_table_size;
+  guint32 *color_table;
+
+  float iterations;
+  guint current_density;
+  guint target_density;
+
+  float exposure, gamma;
+  GdkColor fgcolor, bgcolor;
+  guint16 fgalpha, bgalpha;
+  gboolean clamped;
+
+  gboolean dirty_flag;
+};
+
+
+struct gui_state {
+  GladeXML *xml;
+  GtkWidget *window;
+
+  GtkWidget *drawing_area;
+  GdkGC *gc;
+
+  GtkStatusbar *statusbar;
+  guint render_status_message_id;
+  guint render_status_context;
+
+  guint idler;
+  gboolean writing_params;
+  gboolean just_resized;
+
+  gboolean update_calc_params_when_convenient;
+  gboolean update_render_params_when_convenient;
+
+  gchar* current_tool;
+  float last_mouse_x, last_mouse_y;
+
+  struct {
+    GtkListStore *keyframe_list;
+    GtkTreeView *keyframe_view;
+  } anim;
+};
+
+
+extern struct computation_params params;
+extern struct render_params render;
+extern struct gui_state gui;
+
+
+/* main.c */
+void set_defaults();
+gchar* save_parameters();
+gboolean set_parameter(const char *key, const char *value);
+void load_parameters(const gchar *paramstring);
+void load_parameters_from_file(const char *name);
+void save_to_file(const char *name);
+
+/* render.c */
+float uniform_variate();
+float normal_variate();
+void resize(int w, int h, int oversample);
+void update_pixels();
+void clear();
+void run_iterations(int count);
+
+/* ui-main.c */
+void interactive_main(int argc, char **argv);
+void restart_rendering();
+void write_gui_params();
+
+/* ui-animation.c */
+void animation_ui_init();
+
+
+#endif /* __DE_JONG_MAIN_H__ */
+
+/* The End */
