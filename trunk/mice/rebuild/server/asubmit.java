7019f3b251a0a41210c5c4dd933014376b0c076e
brandon <brandon@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-23 23:13:07 +0000

    So for what it does, this version certainly seems to be unbreakable.

--- trunk/mice/rebuild/server/asubmit.java
+++ trunk/mice/rebuild/server/asubmit.java
@@ -10,8 +10,11 @@ public class asubmit
 	/** Holds the question queue that is for unwashed questions. */
 	public static qqueue putin;
 	
+	/** For unwashed questions that come before everything is inited. */
+	public static qqueue store;
+	
 	/** 
-	 * This method initializes the queue
+	 * This method initializes the queue, and empties the temp into the dbase;
 	 * @param root The question database this pulls from
 	 * @author Brandon Smith
 	 * @version 2.0
@@ -19,6 +22,16 @@ public class asubmit
 	public static void init(qbase root)
 	{
 		putin = root.input;
+		if(store != null)
+		{
+			question temp;
+			temp = store.pop();
+			while(temp != null)
+			{
+				putin.add(temp);
+				temp = store.pop();
+			}
+		}
 	}
 	
 	/**
@@ -35,6 +48,34 @@ public class asubmit
 		//create a new question
 		question in = new question(Name,Question);
 		//add it to the input queue
-		putin.add(in);
+		if(putin != null) putin.add(in);
+		else
+		{
+			if(store == null) store = new qqueue();
+			store.add(in);
+		}
+		//log(in);
 	}
+	
+	/**
+	 * This method handles the logging of the question
+	 * @param it The question that got submitted
+	 * @author Brandon Smith
+	 * @version 2.1
+	 */ /*
+	private static void log(question it)
+	{
+		OutputStreamWriter out;
+		try
+		{
+			out = new OutputStreamWriter(new FileOutputStream("logfile.txt",true));
+			out.println("SUBMIT");
+			out.println(it.toLog());
+			out.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("Logging error\n"+e);
+		}
+	}*/
 }
