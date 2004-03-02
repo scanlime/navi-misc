ac35f7d0a3ac056c992a1c15ba2a65588f127ab7
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-02 04:04:32 +0000

    set a whole bunch of unimplemented widgets unsensitive, set max lengths on stuff, and try to load the ./ glade files before trying to load the globals

--- trunk/xchat-gnome/fe-gnome/gui.c
+++ trunk/xchat-gnome/fe-gnome/gui.c
@@ -34,12 +34,13 @@
 XChatGUI gui;
 
 gboolean initialize_gui_1() {
-	gui.xml = glade_xml_new(XCHATSHAREDIR"/xchat-gnome.glade", NULL, NULL);
+	gui.xml = glade_xml_new("xchat-gnome.glade", NULL, NULL);
 	if(!gui.xml)
-		gui.xml = glade_xml_new("xchat-gnome.glade", NULL, NULL);
+		gui.xml = glade_xml_new(XCHATSHAREDIR"/xchat-gnome.glade", NULL, NULL);
 	if(!gui.xml)
 		return FALSE;
 	initialize_setup_druid();
+	return TRUE;
 }
 
 gboolean initialize_gui_2() {
