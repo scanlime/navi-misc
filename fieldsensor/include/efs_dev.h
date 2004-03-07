87aa610a5e87aaf48474abbb3990d2545621f85c
Micah Elizabeth Scott <micah@scanlime.org>
2004-03-07 08:49:44 +0000

    Starting work on a fieldsensor module for Linux 2.6. This is completely untested, I'm committing it first since it will probably eat my computer.

---
+++ fieldsensor/include/efs_dev.h
@@ -0,0 +1,30 @@
+/*
+ * USB Electric Field Sensor userspace interface
+ *
+ * This header describes the interface between the fieldsensor kernel module and
+ * any userspace applications wishing to use the device.
+ *
+ * Copyright(c) 2003 Micah Dowty <micah@picogui.org>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ */
+
+#ifndef __EFS_DEV_H
+#define __EFS_DEV_H
+
+#include <asm/types.h>
+#include "efs_protocol.h"
+
+
+/************************************************** Device node ***************/
+
+/* Information about the efs's device node in /dev */
+#define EFS_DEV_NAMEFORMAT   "usb/efs%d"
+#define EFS_MINOR_BASE       215
+
+#endif /* __EFS_DEV_H */
+
+/* The End */
