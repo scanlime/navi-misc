a9478ec65cb5d504b6ce1c564c6969e57abd31d1
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-11 05:34:04 +0000

    Oddly enough, rtgraph works quite nicely on the readings from /dev/usb/efs0

--- trunk/fieldsensor/userspace/test.py
+++ trunk/fieldsensor/userspace/test.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
-import time, struct
+import time, struct, gtk, rtgraph
 
 efs = open("/dev/usb/efs0")
+channels = [rtgraph.Channel(name="Parameter block %d" % i) for i in xrange(8)]
 
 def read_averages(efs):
     packet = struct.unpack("i" * 9, efs.read(4*9))
     if packet[8]:
         return [packet[i] / float(packet[8]) for i in xrange(8)]
 
-while 1:
-    print read_averages(efs)
-    time.sleep(0.01)
+def poll_handler():
+    averages = read_averages(efs)
+    for i in xrange(8):
+        channels[i].value = averages[i]
+    gtk.timeout_add(10, poll_handler)
+
+win = rtgraph.GraphUIWindow(channels,
+                            rtgraph.HScrollLineGraph(range=(0,255)),
+                            valueUpdateInterval = 500)
+win.connect("destroy", gtk.mainquit)
+gtk.timeout_add(10, poll_handler)
+gtk.main()
