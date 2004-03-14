c327c28bc2c46406bfe39a0eab7e34f97678ac21
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-14 10:52:29 +0000

    Fix some slightly lysdexic calculations in cv_sdl_show_yuv_tiles()

--- trunk/vision/src/cam_test.c
+++ trunk/vision/src/cam_test.c
@@ -5,6 +5,7 @@ int main() {
   IplImage** images;
   cv_dc1394_init();
   while (cv_sdl_process_events()) {
+    /* Capture images from two cameras, display them in a 2x1 grid */
     images = cv_dc1394_capture_yuv(2);
     cv_sdl_show_yuv_tiles(images, 2, 2);
   }
