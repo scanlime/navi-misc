7d5888f6e92a4658b9601d71cab7c8dabc9d4612
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-14 04:34:56 +0000

    beginnings of a glarea widget, because i think that way

---
+++ trunk/bzedit/src/gldrawingarea.h
@@ -0,0 +1,36 @@
+#ifndef __GL_DRAWING_AREA_H__
+#define __GL_DRAWING_AREA_H__
+
+#include <gtk/gtkdrawingarea.h>
+#include <gtk/gtkgl.h>
+
+#define GL_DRAWING_AREA_TYPE		(gl_drawing_area_get_type ())
+#define GL_DRAWING_AREA(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GL_DRAWING_AREA_TYPE, GLDrawingArea))
+#define GL_DRAWING_AREA_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GL_DRAWING_AREA_TYPE, GLDrawingAreaClass))
+#define IS_GL_DRAWING_AREA(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GL_DRAWING_AREA_TYPE))
+#define IS_GL_DRAWING_AREA_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GL_DRAWING_AREA_TYPE))
+#define GL_DRAWING_AREA_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GL_DRAWING_AREA_TYPE, GLDrawingAreaClass))
+
+typedef struct _GLDrawingArea		GLDrawingArea;
+typedef struct _GLDrawingAreaClass	GLDrawingAreaClass;
+
+struct _GLDrawingArea
+{
+  GtkDrawingArea widget;
+
+  GdkGLConfig *config;
+  GdkGLContext *context;
+  GdkGLDrawable *drawable;
+};
+
+struct _GLDrawingAreaClass
+{
+  GtkDrawingAreaClass parent_class;
+};
+
+GType      gl_drawing_area_get_type     (void) G_GNUC_CONST;
+GtkWidget* gl_drawing_area_new          (GdkGLConfig *config);
+GtkWidget* gl_drawing_area_new_shared   (GLDrawingArea *base);
+void       gl_drawing_area_make_current (GLDrawingArea *glarea);
+
+#endif /* __GL_DRAWING_AREA_H__ */
