476f1a40d6ea8bef3f5ce36d812d9ecad2ad4f04
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-14 05:24:48 +0000

    hey look, the glarea is working

--- trunk/bzedit/src/main.c
+++ trunk/bzedit/src/main.c
@@ -1,14 +1,12 @@
 #include <gtk/gtk.h>
-#include <gtk/gtkgl.h>
 #include <glade/glade.h>
+#include "gldrawingarea.h"
 #include <GL/gl.h>
 
 int main(int argc, char **argv) {
   GladeXML *xml;
-  GtkWidget *widget;
+  GtkWidget *widget, *darea;
   GdkGLConfig *config;
-  GdkGLContext *context;
-  GdkGLDrawable *drawable;
 
   gtk_init(&argc, &argv);
   gtk_gl_init(&argc, &argv);
@@ -17,28 +15,24 @@ int main(int argc, char **argv) {
   if(!xml)
     return 1;
 
-  widget = glade_xml_get_widget(xml, "drawing area");
-
   config = gdk_gl_config_new_by_mode(GDK_GL_MODE_RGBA | GDK_GL_MODE_DEPTH | GDK_GL_MODE_DOUBLE);
-  gtk_widget_set_gl_capability(widget, config, NULL, TRUE, GDK_GL_RGBA_TYPE);
-  gtk_widget_add_events(widget, GDK_VISIBILITY_NOTIFY_MASK);
 
-  if(gtk_widget_is_gl_capable(widget))
-    g_print("gl config succeeded\n");
+  widget = glade_xml_get_widget (xml, "event box");
+  darea = gl_drawing_area_new (config);
+  gtk_container_add(GTK_CONTAINER(widget), darea);
 
-  widget = glade_xml_get_widget(xml, "window1");
-  gtk_widget_show_all(widget);
+  widget = glade_xml_get_widget (xml, "window1");
+  gtk_widget_show_all (widget);
 
-  widget = glade_xml_get_widget(xml, "drawing area");
+  while(g_main_context_pending(NULL))
+    g_main_context_iteration(NULL, TRUE);
 
-  context = gtk_widget_get_gl_context(widget);
-  drawable = gtk_widget_get_gl_drawable(widget);
-  gdk_gl_drawable_make_current(drawable, context);
+  gl_drawing_area_make_current(GL_DRAWING_AREA(darea));
 
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  gdk_gl_drawable_swap_buffers(drawable);
+  gl_drawing_area_swap_buffers(GL_DRAWING_AREA(darea));
 
   gtk_main();
 }
