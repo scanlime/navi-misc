d51868c4e1a6b337a68c477717f05ee8608108f0
Micah Elizabeth Scott <micah@scanlime.org>
2004-03-15 22:34:54 +0000

    Add a command line option to mirror the video

--- vision/src/camshift.cpp
+++ vision/src/camshift.cpp
@@ -87,7 +87,9 @@ void map_mouse_position(int *x, int *y, int *camera, IplImage *image, int grid_w
   *y %= image->height;
 }
 
-void interactive_camshift(int n_cameras, bool show_backprojections) {
+void interactive_camshift(int n_cameras = 1,
+			  bool show_backprojections = true,
+			  bool mirror = false) {
   IplImage **images;
   IplImage *yuv_backprojections[n_cameras];
   IplImage *planes[3];
@@ -143,6 +145,9 @@ void interactive_camshift(int n_cameras, bool show_backprojections) {
     images = cv_dc1394_capture_yuv(n_cameras);
 
     for (i=0; i<n_cameras; i++) {
+      if (mirror)
+	cvFlip(images[i], images[i], 1);
+
       /* Calculate the backprojection, in YUV space */
       cvCvtPixToPlane(images[i], planes[0], planes[1], planes[2], 0);
       cvCalcBackProject(planes, backprojection, object_hist[i]);
@@ -272,7 +277,8 @@ static void usage(char **argv) {
 	 "Interactive frontend for OpenCV's CAMSHIFT implementation\n"
 	 "\n"
 	 "  -b, --hide-backprojection  Hide the backprojection images shown below the cameras\n"
-	 "  -c. --cameras N            Set the number of cameras to use\n",
+	 "  -c. --cameras N            Set the number of cameras to use\n"
+	 "  -m, --mirror               Mirror mode, flips the video horizontally\n",
 	 argv[0]);
 }
 
@@ -280,15 +286,17 @@ int main(int argc, char **argv) {
   int c, option_index=0;
   int n_cameras = 1;
   bool show_backprojections = true;
+  bool mirror = false;
 
   while (1) {
     static struct option long_options[] = {
       {"help",                0, NULL, 'h'},
       {"hide-backprojection", 0, NULL, 'b'},
       {"cameras",             1, NULL, 'c'},
+      {"mirror",              0, NULL, 'm'},
       NULL,
     };
-    c = getopt_long(argc, argv, "hbc:",
+    c = getopt_long(argc, argv, "hbc:m",
 		    long_options, &option_index);
     if (c == -1)
       break;
@@ -301,6 +309,10 @@ int main(int argc, char **argv) {
 
     case 'c':
       n_cameras = atoi(optarg);
+      break;
+
+    case 'm':
+      mirror = true;
       break;
 
     case 'h':
@@ -316,7 +328,7 @@ int main(int argc, char **argv) {
   }
 
   cv_dc1394_init();
-  interactive_camshift(n_cameras, show_backprojections);
+  interactive_camshift(n_cameras, show_backprojections, mirror);
   return 0;
 }
 
