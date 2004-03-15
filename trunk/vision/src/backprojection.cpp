8747633896f3ff350081d22befb3301e929e43ff
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-15 07:08:35 +0000

    Better interactive object histogram sampling

--- trunk/vision/src/backprojection.cpp
+++ trunk/vision/src/backprojection.cpp
@@ -2,9 +2,9 @@
 #include <cv_sdl.h>
 #include <cv.hpp>
 #include <cvaux.h>
+#include <SDL.h>
 
 #define N_CAMERAS    1
-#define SAMPLE_SIZE  64
 
 
 void plot_crosshairs(IplImage *image, CvPoint *point,
@@ -41,6 +41,27 @@ void gray_to_yuv(IplImage *src, IplImage *dest) {
   }
 }
 
+void map_mouse_position(int *x, int *y, int *camera, IplImage *image) {
+  /* Given a mouse position in window coordinates, return
+   * a mouse position in image coordiantes and a camera number.
+   * A sample image must be provided so this knows our camera resolution.
+   */
+  SDL_Surface *surface = SDL_GetVideoSurface();
+
+  /* Convert the mouse coordinates to be relative to the YUV surface,
+   * undoing any resizing the user may have performed on our window.
+   */
+  *x = *x * image->width * N_CAMERAS / surface->w;
+  *y = *y * image->height * 2 / surface->h;
+
+  /* Assuming the cameras are horizontally tiled, figure out
+   * the camera we're clicking in and the position within that camera's frame
+   */
+  *camera = *x / image->width;
+  *x %= image->width;
+  *y %= image->height;
+}
+
 int main() {
   IplImage **images;
   IplImage *back_projections[N_CAMERAS];
@@ -49,10 +70,11 @@ int main() {
   IplImage *channel_temp;
   CvHistogram* object_hist[N_CAMERAS];
   int i, j;
-  int hist_size[] = {16, 32, 32};
+  int hist_size[] = {64, 64, 64};
   float channel_range[] = {0, 255};
   float* ranges[] = {channel_range, channel_range, channel_range};
-  int mouse_x, mouse_y;
+  int mouse_x, mouse_y, mouse_camera, mouse_buttons;
+  int sample_square_size = 32;
 
   cv_dc1394_init();
 
@@ -86,49 +108,67 @@ int main() {
   while (cv_sdl_process_events()) {
     images = cv_dc1394_capture_yuv(N_CAMERAS);
 
-    /* If the first mouse button is down, use this image to make a reference histogram
-     * for the object we're trying to track. The reference histogram is made from a small
-     * square around the mouse position.
-     */
-    if (SDL_GetMouseState(&mouse_x, &mouse_y) & SDL_BUTTON(1)) {
-      CvRect roi;
-
-      /* Assuming the cameras are horizontally tiled, figure out
-       * the camera we're clicking in and the position within that camera's frame
-       */
-      i = mouse_x / images[0]->width;
-      mouse_x %= images[0]->width;
-      mouse_y %= images[0]->height;
-
-      roi.x = mouse_x - SAMPLE_SIZE/2;
-      roi.y = mouse_y - SAMPLE_SIZE/2;
-      roi.width = SAMPLE_SIZE;
-      roi.height = SAMPLE_SIZE;
-
-      cvSetImageROI(images[i], roi);
-      cvSetImageROI(planes[0], roi);
-      cvSetImageROI(planes[1], roi);
-      cvSetImageROI(planes[2], roi);
-
-      object_hist[i] = cvCreateHist(3, hist_size, CV_HIST_ARRAY, ranges, 1);
-      cvCvtPixToPlane(images[i], planes[0], planes[1], planes[2], 0);
-      cvCalcHist(planes, object_hist[i], 0, 0);
-
-      cvResetImageROI(images[i]);
-      cvResetImageROI(planes[0]);
-      cvResetImageROI(planes[1]);
-      cvResetImageROI(planes[2]);
-
-      /* Draw a box around the area we just sampled */
-      cvRectangle(images[i], cvPoint(roi.x-1, roi.y-1),
-		  cvPoint(roi.x + roi.width + 1, roi.y + roi.width + 1),
-		  CV_RGB(128,128,255), 1);
-    }
-
+    /* Calculate the backprojection */
     for (i=0; i<N_CAMERAS; i++) {
       cvCvtPixToPlane(images[i], planes[0], planes[1], planes[2], 0);
       cvCalcBackProject(planes, channel_temp, object_hist[i]);
       gray_to_yuv(channel_temp, back_projections[i]);
+    }
+
+    if (SDL_GetVideoSurface()) {
+      /* Interactive interface for modifying the histogram,
+       * only works after we've initialized our video by displaying
+       * the first frame.
+       */
+
+      Uint8 *keystate = SDL_GetKeyState(NULL);
+      CvRect sample_rect;
+
+      /* The < and > keys change the size of the sample square */
+      if (keystate[',']) {
+	sample_square_size--;
+	if (sample_square_size < 1)
+	  sample_square_size = 1;
+      }
+      if (keystate['.']) {
+	sample_square_size++;
+      }
+
+      /* Get the current sampling rect, centered on the mouse cursor */
+      mouse_buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+      map_mouse_position(&mouse_x, &mouse_y, &mouse_camera, images[0]);
+      sample_rect.x = mouse_x - sample_square_size/2;
+      sample_rect.y = mouse_y - sample_square_size/2;
+      sample_rect.width = sample_square_size;
+      sample_rect.height = sample_square_size;
+
+      /* The right mouse button clears our histogram */
+      if (mouse_buttons & SDL_BUTTON(3)) {
+	cvClearHist(object_hist[mouse_camera]);
+      }
+
+      /* If the left mouse button is down, use this image to collect samples towards a
+       * reference histogram for the object we're trying to track.
+       */
+      if (mouse_buttons & SDL_BUTTON(1)) {
+	cvSetImageROI(images[mouse_camera], sample_rect);
+	cvSetImageROI(planes[0], sample_rect);
+	cvSetImageROI(planes[1], sample_rect);
+	cvSetImageROI(planes[2], sample_rect);
+
+	cvCvtPixToPlane(images[mouse_camera], planes[0], planes[1], planes[2], 0);
+	cvCalcHist(planes, object_hist[mouse_camera], 1);
+
+	cvResetImageROI(images[mouse_camera]);
+	cvResetImageROI(planes[0]);
+	cvResetImageROI(planes[1]);
+	cvResetImageROI(planes[2]);
+      }
+
+      /* Draw a box around the current sampling rectangle */
+      cvRectangle(images[mouse_camera], cvPoint(sample_rect.x-1, sample_rect.y-1),
+		  cvPoint(sample_rect.x + sample_rect.width + 1, sample_rect.y + sample_rect.width + 1),
+		  CV_RGB(128,128,255), 1);
     }
 
     cv_sdl_show_yuv_tiles(view_grid, N_CAMERAS*2, N_CAMERAS);
