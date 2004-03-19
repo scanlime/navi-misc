0f479de4fc8021646fbd5b586209469f7874a983
David Trowbridge <trowbrds@gmail.com>
2004-03-19 23:28:19 +0000

    minor cosmetic changes, implement a base Drawable class, and begin a texture manager

---
+++ bzedit/src/drawable.c
@@ -0,0 +1,65 @@
+/*
+ * drawable.h - Abstract base class for an object that can be drawn to an OpenGL
+ *              context. This is the basic unit used by the view to sort object by
+ *              texture, so its required that the drawable have at most one texture.
+ *
+ * BZEdit
+ * Copyright (C) 2004 David Trowbridge
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#include "drawable.h"
+
+static void drawable_class_init (DrawableClass *klass);
+static void drawable_init       (Drawable *drawable);
+
+GType
+drawable_get_type (void)
+{
+  static GType drawable_type = 0;
+
+  if (!drawable_type)
+    {
+      static const GTypeInfo drawable_info =
+      {
+	sizeof (DrawableClass),
+	NULL,               /* base init */
+	NULL,               /* base finalize */
+	(GClassInitFunc)    drawable_class_init,
+	NULL,               /* class finalize */
+	NULL,               /* class data */
+	sizeof (Drawable),
+	0,                  /* n preallocs */
+	(GInstanceInitFunc) drawable_init,
+      };
+
+      drawable_type = g_type_register_static (G_TYPE_OBJECT, "Drawable", &drawable_info, 0);
+    }
+
+  return drawable_type;
+}
+
+static void
+drawable_class_init (DrawableClass *klass)
+{
+}
+
+static void
+drawable_init (Drawable *drawable)
+{
+  drawable->texture = NULL;
+}
