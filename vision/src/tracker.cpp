ca5381c557a2f950d31605adab172022f4608b7c
Micah Elizabeth Scott <micah@scanlime.org>
2004-03-23 03:48:30 +0000

    Indicate with a red X or green check whether each camera can see the checkerboard

--- vision/src/tracker.cpp
+++ vision/src/tracker.cpp
@@ -9,6 +9,7 @@
 
 void tracker(int n_cameras, CvCalibFilter *calib) {
   IplImage **images;
+  IplImage *rgb_images[n_cameras];
   MultiCamshiftUI *tracker = NULL;
   SDL_Event event;
   Cv3dTrackerCameraInfo cam_info[n_cameras];
@@ -17,7 +18,9 @@ void tracker(int n_cameras, CvCalibFilter *calib) {
   Cv3dTrackerTrackedObject object;
   Cv3dTrackerCameraIntrinsics cam_intrinsics[n_cameras];
 
-  /* Copy camera intrinsics and reset calibration info */
+  memset(rgb_images, 0, sizeof(rgb_images));
+
+  /* Copy camera intrinsics from the saved calibration */
   for (int i=0; i<n_cameras; i++) {
     const CvCamera *cvcam = calib->GetCameraParams(i);
     cam_intrinsics[i].principal_point.x = cvcam->matrix[2];
@@ -29,19 +32,46 @@ void tracker(int n_cameras, CvCalibFilter *calib) {
     cam_intrinsics[i].distortion[2] = cvcam->distortion[2];
     cam_intrinsics[i].distortion[3] = cvcam->distortion[3];
   }
-  memset(&cam_info, 0, sizeof(cam_info));
 
   printf("Calibrating 3D tracker, 7x9 checkerboard must be in view of both cameras\n");
-  do {
+  while (1) {
     if (!cv_sdl_process_events())
       return;
     images = cv_dc1394_capture_yuv(n_cameras);
+
+    /* Draw an X or a check on each camera to indicate current calibration status */
+    for (int i=0; i<n_cameras; i++) {
+      IplImage *image = images[i];
+      if (cam_info[i].valid) {
+	/* Green check */
+	const double yuv_color = CV_RGB(0, 0, 255);
+	cvLineAA(image, cvPoint(10, 64), cvPoint(50, 128), yuv_color);
+	cvLineAA(image, cvPoint(50, 128), cvPoint(100, 10), yuv_color);
+      }
+      else {
+	/* Red X */
+	const double yuv_color = CV_RGB(255, 0, 192);
+	cvLineAA(image, cvPoint(10, 10), cvPoint(128, 128), yuv_color);
+	cvLineAA(image, cvPoint(10, 128), cvPoint(128, 10), yuv_color);
+      }
+    }
+
     cv_sdl_show_yuv_tiles(images, n_cameras, n_cameras);
 
-    for (int i=0; i<n_cameras; i++)
-      cvCvtColor(images[i], images[i], CV_YCrCb2RGB);
-  } while (!cv3dTrackerCalibrateCameras(n_cameras, cam_intrinsics, cvSize(6,8), 1, images, cam_info));
-  printf("Finished calibration\n");
+    /* Calibration should be done in RGB space */
+    for (int i=0; i<n_cameras; i++) {
+      if (!rgb_images[i])
+	rgb_images[i] = cvCreateImage(cvGetSize(images[i]), 8, 3);
+      cvCvtColor(images[i], rgb_images[i], CV_YCrCb2RGB);
+    }
+
+    /* Try to calibrate. This updates cam_info, and returns true if we're done */
+    memset(&cam_info, 0, sizeof(cam_info));
+    if (cv3dTrackerCalibrateCameras(n_cameras, cam_intrinsics, cvSize(6,8), 1, rgb_images, cam_info)) {
+      printf("Finished calibration\n");
+      break;
+    }
+  }
 
   while (1) {
     images = cv_dc1394_capture_yuv(n_cameras);
