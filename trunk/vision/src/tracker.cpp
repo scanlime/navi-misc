00f935172f0e9f1b160e5a9832a4fe669f7eb426
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-23 03:32:48 +0000

    Use camera intrinsics (focal length, principal point, and distortion matrix) from camera_calibrator, but calibrate camera orientation using cv3dTrackerCalibrateCameras() on startup. This requires holding up a checkerboard before starting to use the tracker, but it actually seems to be working properly now. yay.

--- trunk/vision/src/tracker.cpp
+++ trunk/vision/src/tracker.cpp
@@ -15,46 +15,37 @@ void tracker(int n_cameras, CvCalibFilter *calib) {
   CvBox2D *boxen;
   Cv3dTracker2dTrackedObject object2d[n_cameras];
   Cv3dTrackerTrackedObject object;
+  Cv3dTrackerCameraIntrinsics cam_intrinsics[n_cameras];
 
+  /* Copy camera intrinsics and reset calibration info */
   for (int i=0; i<n_cameras; i++) {
     const CvCamera *cvcam = calib->GetCameraParams(i);
-    cam_info[i].valid = true;
-    cam_info[i].principal_point.x = cvcam->matrix[2];
-    cam_info[i].principal_point.y = cvcam->matrix[5];
-    cam_info[i].mat[0][0] = cvcam->rotMatr[0];
-    cam_info[i].mat[1][0] = cvcam->rotMatr[1];
-    cam_info[i].mat[2][0] = cvcam->rotMatr[2];
-    cam_info[i].mat[3][0] = cvcam->transVect[0];
-    cam_info[i].mat[0][1] = cvcam->rotMatr[3];
-    cam_info[i].mat[1][1] = cvcam->rotMatr[4];
-    cam_info[i].mat[2][1] = cvcam->rotMatr[5];
-    cam_info[i].mat[3][1] = cvcam->transVect[1];
-    cam_info[i].mat[0][2] = cvcam->rotMatr[6];
-    cam_info[i].mat[1][2] = cvcam->rotMatr[7];
-    cam_info[i].mat[2][2] = cvcam->rotMatr[8];
-    cam_info[i].mat[3][2] = cvcam->transVect[2];
-    cam_info[i].mat[0][3] = 0;
-    cam_info[i].mat[1][3] = 0;
-    cam_info[i].mat[2][3] = 0;
-    cam_info[i].mat[3][3] = 1;
-
-    printf("Camera %d:\n"
-	   "  principal point (%f,%f)\n"
-	   "  matrix:\n"
-	   "\t %f %f %f %f\n"
-	   "\t %f %f %f %f\n"
-	   "\t %f %f %f %f\n"
-	   "\t %f %f %f %f\n\n", i,
-	   cam_info[i].principal_point.x, cam_info[i].principal_point.y,
-	   cam_info[i].mat[0][0], cam_info[i].mat[1][0], cam_info[i].mat[2][0], cam_info[i].mat[3][0],
-	   cam_info[i].mat[0][1], cam_info[i].mat[1][1], cam_info[i].mat[2][1], cam_info[i].mat[3][1],
-	   cam_info[i].mat[0][2], cam_info[i].mat[1][2], cam_info[i].mat[2][2], cam_info[i].mat[3][2],
-	   cam_info[i].mat[0][3], cam_info[i].mat[1][3], cam_info[i].mat[2][3], cam_info[i].mat[3][3]);
+    cam_intrinsics[i].principal_point.x = cvcam->matrix[2];
+    cam_intrinsics[i].principal_point.y = cvcam->matrix[5];
+    cam_intrinsics[i].focal_length[0] = cvcam->matrix[0];
+    cam_intrinsics[i].focal_length[1] = cvcam->matrix[4];
+    cam_intrinsics[i].distortion[0] = cvcam->distortion[0];
+    cam_intrinsics[i].distortion[1] = cvcam->distortion[1];
+    cam_intrinsics[i].distortion[2] = cvcam->distortion[2];
+    cam_intrinsics[i].distortion[3] = cvcam->distortion[3];
   }
+  memset(&cam_info, 0, sizeof(cam_info));
+
+  printf("Calibrating 3D tracker, 7x9 checkerboard must be in view of both cameras\n");
+  do {
+    if (!cv_sdl_process_events())
+      return;
+    images = cv_dc1394_capture_yuv(n_cameras);
+    cv_sdl_show_yuv_tiles(images, n_cameras, n_cameras);
+
+    for (int i=0; i<n_cameras; i++)
+      cvCvtColor(images[i], images[i], CV_YCrCb2RGB);
+  } while (!cv3dTrackerCalibrateCameras(n_cameras, cam_intrinsics, cvSize(6,8), 1, images, cam_info));
+  printf("Finished calibration\n");
 
   while (1) {
     images = cv_dc1394_capture_yuv(n_cameras);
-    calib->Undistort(images, images);
+    // calib->Undistort(images, images);
 
     if (!tracker)
       tracker = new MultiCamshiftUI(n_cameras, cvGetSize(images[0]));
