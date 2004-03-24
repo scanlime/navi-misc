a4e42dae38250c0d674bee37efcab4187a7a196b
lurgyman <lurgyman@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-24 02:21:10 +0000

    eep, it was broken, but no more

--- trough/PlayList.py
+++ trough/PlayList.py
@@ -11,14 +11,18 @@ DefaultPath = os.path.join(os.environ['HOME'],'.trough-playlist')
 
 class PlayList:
 	def __init__ (self, path=DefaultPath):
-		self.path=path
+		if path is None:
+		    self.path=DefaultPath
+		else:
+		    self.path=path
 		
 		try:
-			self.playlist = pickle.load(open(path,'r'))
+			self.playlist = pickle.load(open(self.path,'r'))
 		except:
 			self.playlist = Pieces.Pieces()
 
 	def dump (self):
+		print 'self.path=',self.path
 		pickle.dump(self.playlist,open(self.path,'w'),1)
 
 	def getPieces (self):
