45ece555ac9bde8e8172123af6f49936ffea2bc0
lurgyman <lurgyman@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-24 03:55:11 +0000

    hmm, so it turns out that there were some issues there, but fear not, they have been eradicated like tomorrows muffins

--- trunk/trough/Trough/FSTree.py
+++ trunk/trough/Trough/FSTree.py
@@ -9,72 +9,72 @@ suffices = ['mp3','ogg']    # TODO: Make this less silly
 
 class File:
     ''' FSTree.File
-    A single filesystem leaf, generally (but not necessarily) attached to a 
+    A single filesystem leaf, generally (but not necessarily) attached to a
     directory node.
-    '''
+	'''
     def __init__(self, name, parent=None):
-    ''' FSTree.File.__init__
-        name (string)
-            Deepest-level name of the entry in it's parent directory
-        parent (Directory):
-        Optional parent Directory class (to be used by derived classes)
-        '''
+    	''' FSTree.File.__init__
+            name (string)
+		Deepest-level name of the entry in it's parent directory
+            parent (Directory):
+		Optional parent Directory class (to be used by derived classes)
+	'''
         self.parent = parent
         self.name    = name
         self.callback = []
 
     def getName (self):
-    ''' FSTree.File.getName
-        Returns the filename
-        '''
-    return self.name
+	''' FSTree.File.getName
+            Returns the filename
+	'''
+        return self.name
 
     def getPath (self):
-    ''' FSTree.File.getPath        
-        Returns the full path to the File
-        '''
-    if self.parent:
-        return os.path.join (self.parent.getPath(),self.name)
-    return self.name
+	''' FSTree.File.getPath        
+            Returns the full path to the File
+       	'''
+	if self.parent:
+            return os.path.join (self.parent.getPath(),self.name)
+	return self.name
 
     def getParent (self):
-    ''' FSTree.File.getParent
-        Returns the File's parent node.
-        '''
-    return self.parent
+	''' FSTree.File.getParent
+	    Returns the File's parent node.
+    	'''
+        return self.parent
 
     def destroy(self):
-    ''' FSTree.File.destroy
-        '''
-    if self.callback:
-        self.callback.removed(self)
+	''' FSTree.File.destroy
+    	'''
+        if self.callback:
+            self.callback.removed(self)
 
     def _register_del_callback(self,target):
-    self.callback = target
+        self.callback = target
 
 class Directory (File):
     ''' FSTree.Directory
-        '''
+    '''
     def __init__(self, name, parent=None):
-        ''' FSTree.Directory.__init__
+    	''' FSTree.Directory.__init__
 
             name:    Deepest-level name of the entry in it's parent directory, 
             parent:    Optional parent Directory class
-            '''
-        
+      	'''
+
         File.__init__(self, name, parent)
-        
+
         self.subdirs = []
         self.files = []
 
         self.recurse()
 
     def recurse(self):
-        ''' FSTree.Directory.recurse
+    	''' FSTree.Directory.recurse
 
             Recurses through all of the files and subdirectories in this
             Directory, storing the heirarchial structure along the way.
-            '''
+       	'''
 
         path = File.getPath(self)
         try:
@@ -86,8 +86,7 @@ class Directory (File):
         entries.sort()
 
         for i in xrange(0,len(entries)):
-#
-#            print entries[i]
+#           print entries[i]
             if os.path.isdir(os.path.join(path,entries[i])):
                 self.subdirs.append(Directory(entries[i], self))
             else:
@@ -103,14 +102,14 @@ class Directory (File):
         return self.files
 
     def find(self,path):
-        ''' FSTree.Directory.find
-        
+    	''' FSTree.Directory.find
+
             Returns a Directory structure for a file or directory with
             the given path, if found inside anywhere recursively within
             the current directory.
-            '''
+       	'''
         path = os.path.abspath(path)
-        
+
         if self.getPath() == path:
             return self
 
@@ -122,23 +121,23 @@ class Directory (File):
                 return self.subdirs[i].find(path)
 
         return []
-        
+
 
     def inPath(self, path):
-        ''' FSTree.Directory.inPath
-        
+    	''' FSTree.Directory.inPath
+
             Returns True if path is somewhere inside the directory
             heirarchy.
-            '''
+       	'''
         return (self.getPath().find(os.path.abspath(path)) is 0)
 
     def destroy(self):
-        ''' FSTree.Directory.destroy
-        
+    	''' FSTree.Directory.destroy
+
             Annihilates the tree politely, making sure any files
             with callbacks associated with them are given fair
             warning.
-            '''
+	'''
         for i in xrange(0,len(self.subdirs)):
             self.subdirs[i].destroy()
         while len(self.files):
