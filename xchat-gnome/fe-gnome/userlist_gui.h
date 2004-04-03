e3c7467328ef96d1474e5ecf7956dcd147232d86
David Trowbridge <trowbrds@gmail.com>
2004-04-03 10:30:29 +0000

    create a user list class to manage list stores and completions

--- xchat-gnome/fe-gnome/userlist_gui.h
+++ xchat-gnome/fe-gnome/userlist_gui.h
@@ -27,8 +27,8 @@
 
 void initialize_userlist();
 void create_userlist(session *sess);
-void userlist_insert(session *sess, struct User *newuser, int row, int sel);
-gboolean userlist_remove(session *sess, struct User *user);
+void userlist_insertg(session *sess, struct User *newuser, int row, int sel);
+gboolean userlist_removeg(session *sess, struct User *user);
 void userlist_change(session *sess, struct User *user);
 void userlist_display(session_gui *sess);
 GCompletion *userlist_get_nick_completion();
