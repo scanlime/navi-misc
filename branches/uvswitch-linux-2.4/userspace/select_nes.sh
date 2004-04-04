3a595e9ea781e8b84b034acacc0c78e35ab62a6f
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-04 07:06:28 +0000

    Make a Linux 2.4 branch, since soon trunk will get ported to 2.6

---
+++ branches/uvswitch-linux-2.4/userspace/select_nes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Select channel 7 (the nes) and disable the bypass switch
+#
+echo 4 0 > /dev/usb/uvswitch0
+
---
+++ branches/uvswitch-linux-2.4/userspace/select_ps2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Select channel 1 (the playstation) and disable the bypass switch
+#
+echo 2 0 > /dev/usb/uvswitch0
+
+
