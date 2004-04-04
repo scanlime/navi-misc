88b15798d971ec99675b26c3e102d29a485fed7c
Micah Elizabeth Scott <micah@scanlime.org>
2004-04-04 07:06:49 +0000

    Make a Linux 2.4 branch, since soon trunk will get ported to 2.6

--- palantir/CharacterSheet/__init__.py
+++
@@ -1,9 +0,0 @@
-''' __init__.py
-
-The CharacterSheet module handles the storing, creation and viewing of
-character sheets for the Palantir RPG client.
-
-  Copyright (C) 2003 W. Evan Sheehan <evan@navi.cx>
-'''
-
-import xml.dom, xml.dom.minidom, string
