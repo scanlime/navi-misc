42ac2e72ef7988d761e0671ab44918cc047b9921
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-06 02:06:34 +0000

    clean up inital menu, slow down spin, make ground texture no suck

--- trunk/bznext/client/ui/src/mainMenu.cpp
+++ trunk/bznext/client/ui/src/mainMenu.cpp
@@ -33,6 +33,8 @@ void CMainMenu::Attach ( void )
 	Overlay* o = (Overlay*)OverlayManager::getSingleton().getByName("menu/mainMenu");
 	o->show();
 
+	gameLoop->GetCamera()->moveRelative(Vector3(0,1.5f,0));
+
 	// set up a very very simple scene
 	gameLoop->GetSceneManager()->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
 	Light* sun = gameLoop->GetSceneManager()->createLight("Sun");
@@ -42,7 +44,7 @@ void CMainMenu::Attach ( void )
 
 	gameLoop->GetSceneManager()->setSkyBox(true, "grassland_skybox",5000,true,Quaternion(1.57079632f,Vector3(1,0,0)));
 
-	Mesh* mesh = MeshManager::getSingleton().createPlane("GroundPlane", Plane (Vector3(0,1,0),Vector3(0,0,0),Vector3(1,0,0)),800, 800,1,1,true,1,20,20);
+	Mesh* mesh = MeshManager::getSingleton().createPlane("GroundPlane", Plane (Vector3(0,1,0),Vector3(0,0,0),Vector3(1,0,0)),800, 800,1,1,true,1,60,60);
 	mesh->getSubMeshIterator().getNext()->setMaterialName("ground_mat");
 	Entity *mGroundEntity = gameLoop->GetSceneManager()->createEntity("Ground","GroundPlane");
 	SceneNode * mGroundNode = static_cast<SceneNode*>(gameLoop->GetSceneManager()->getRootSceneNode()->createChild());
@@ -57,7 +59,7 @@ tePanelReturn CMainMenu::Process ( std::string &next )
 {
 	Vector3 YawAxis(0,0,1);
 	Vector3 PitchAxis(1,0,0);
-	float MoveFactor = 45.0f *gameLoop->GetTimer().GetFrameTime();
+	float MoveFactor = 15.0f *gameLoop->GetTimer().GetFrameTime();
 	gameLoop->GetCamera()->rotate(YawAxis,-MoveFactor);
 	return ePanelContinue;
 }
