b55f0587228dd1638918accf71829e78c4c0c723
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-12 06:07:15 +0000

    start in on a buffer object

---
+++ branches/xchat-gnome-xtext-rewrite/fe-gnome/xtextbuffer.h
@@ -0,0 +1,41 @@
+#ifndef __XTEXT_BUFFER_H__
+#define __XTEXT_BUFFER_H__
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define XTEXT_BUFFER_TYPE            (xtext_buffer_get_type ())
+#define XTEXT_BUFFER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), XTEXT_BUFFER_TYPE, XTextBuffer))
+#define XTEXT_BUFFER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), XTEXT_BUFFER_TYPE, XTextBufferClass))
+#define IS_XTEXT_BUFFER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), XTEXT_BUFFER_TYPE))
+#define IS_XTEXT_BUFFER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), XTEXT_BUFFER_TYPE))
+
+typedef struct _XTextBuffer      XTextBuffer;
+typedef struct _XTextBufferClass XTextBufferClass;
+typedef struct _textentry        textentry;
+
+struct _XTextBuffer
+{
+  GObject parent;
+
+  textentry *text_first;
+  textentry *text_last;
+};
+
+struct _XTextBufferClass
+{
+  GObjectClass parent_class;
+};
+
+GType        xtext_buffer_get_type (void) G_GNUC_CONST;
+XTextBuffer* xtext_buffer_new      (void);
+
+void         xtext_buffer_append        (XTextBuffer *buffer, unsigned char *text, int len);
+void         xtext_buffer_append_indent (XTextBuffer *buffer, unsigned char *left, int llen,
+                                                              unsigned char *right, int rlen);
+void         xtext_buffer_clear         (XTextBuffer *buffer);
+
+G_END_DECLS
+
+#endif
