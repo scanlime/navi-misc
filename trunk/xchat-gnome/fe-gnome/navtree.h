e968d4425ac9c6401d36529e59a28b70e8bebd0b
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-12 00:20:49 +0000

    Think I've got most of this outlined in navtree.h now, now it's homework time. =P

--- trunk/xchat-gnome/fe-gnome/navtree.h
+++ trunk/xchat-gnome/fe-gnome/navtree.h
@@ -27,20 +27,23 @@
 
 G_BEGIN_DECLS
 
+typedef struct _NavTree			NavTree;
+typedef struct _NavTreeClass NavTreeClass;
+typedef struct _NavModel NavModel;
+typedef struct _NavModelClass NavModelClass;
+
+/***** NavTree *****/
 #define NAVTREE_TYPE 						(navigation_tree_get_type())
 #define NAVTREE(obj)						(G_TYPE_CHECK_INSTANCE_CAST((obj), NAVTREE_TYPE, NavTree))
 #define NAVTREE_CLASS(klass)					(G_TYPE_CHECK_CLASS_CAST((klass), NAVTREE_TYPE, NavTreeClass))
 #define IS_NAVTREE(obj)					(G_TYPE_CHECK_INSTANCE_TYPE((obj), NAVTREE_TYPE))
 #define IS_NAVTREE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), NAVTREE_TYPE))
 
-/***** NavTree *****/
-typedef struct _NavTree			NavTree;
-typedef struct _NavTreeClass NavTreeClass;
-
 struct _NavTree
 {
 	GtkTreeView parent;
 	GtkTreePath *current_path;
+	NavModel *model;
 };
 
 struct _NavTreeClass
@@ -51,15 +54,30 @@ struct _NavTreeClass
 GType navigation_tree_get_type (void) G_GNUC_CONST;
 NavTree* navigation_tree_new (void);
 
+/* Add/remove server/channel functions. */
+void navigation_tree_create_new_network_entry (NavTree *navtree, struct session *sess);
+void navigation_tree_create_new_channel_entry (NavTree *navtree, struct session *sess);
+void navigation_tree_remove_channel_entry			(NavTree *navtree, struct session *sess);
+void navigation_tree_remove_network_entry			(NavTree *navtree, struct session *sess);
+
+/* Channel/server selection functions. */
+void navigation_tree_select_nth_channel				(NavTree *navtree, gint chan_num);
+void navigation_tree_select_next_channel			(NavTree *navtree);
+void navigation_tree_select_prev_channel			(NavTree *navtree);
+void navigation_tree_select_next_network			(NavTree *navtree);
+void navigation_tree_select_prev_network			(NavTree *navtree);
+
+/* Misc. functions. */
+void navigation_tree_set_channel_name					(NavTree *navtree, struct session *sess);
+void navigation_tree_set_disconn							(NavTree *navtree, struct session *sess);
+void navigation_tree_set_hilight							(NavTree *navtree, struct session *sess);
+
 /***** NavModel *****/
 #define NAVMODEL_TYPE						 (navigation_model_get_type())
 #define NAVMODEL(obj)						 (G_TYPE_CHECK_INSTANCE_CAST((obj), NAVMODEL_TYPE, NavModel))
 #define NAVMODEL_CLASS(klass)		 (G_TYPE_CHECK_CLASS_CAST((klass), NAVMODEL_TYPE, NavModelClass))
 #define IS_NAVMODEL(obj)				 (G_TYPE_CHECK_INSTANCE_TYPE((obj), NAVMODEL_TYPE))
 #define IS_NAVMODEL_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), NAVMODEL_TYPE))
-
-typedef struct _NavModel NavModel;
-typedef struct _NavModelClass NavModelClass;
 
 struct _NavModel
 {
@@ -74,6 +92,13 @@ struct _NavModelClass
 
 GType navigation_model_get_type (void) G_GNUC_CONST;
 NavModel* navigation_model_new (void); /* XXX: Do we want this here or in the .c? */
+
+/* Add/remove server/channel functions. */
+void navigation_model_add_new_network (NavModel *model, struct session *sess);
+void navigation_model_add_new_channel (NavModel *model, struct session *sess);
+void navigation_model_remove_network	(NavModel *model, struct session *sess);
+void navigation_model_remove_channel	(NavModel *model, struct session *sess);
+
 G_END_DECLS
 
 #endif
