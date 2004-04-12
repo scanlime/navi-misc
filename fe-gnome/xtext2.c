0983dc654e5bc9be48daa85e5a5386d150bc9da7
David Trowbridge <trowbrds@gmail.com>
2004-04-12 22:28:35 +0000

    rip out a bunch of stuff

--- fe-gnome/xtext2.c
+++ fe-gnome/xtext2.c
@@ -13,28 +13,6 @@ static void     xtext2_realize         (GtkWidget *widget);
 static void     xtext2_unrealize       (GtkWidget *widget);
 static void     xtext2_size_request    (GtkWidget *widget, GtkRequisition *requisition);
 static void     xtext2_size_allocate   (GtkWidget *widget, GtkAllocation *allocation);
-static gboolean xtext2_button_press    (GtkWidget *widget, GdkEventButton *event);
-static gboolean xtext2_button_release  (GtkWidget *widget, GdkEventButton *event);
-static gboolean xtext2_motion_notify   (GtkWidget *widget, GdkEventMotion *event);
-static gboolean xtext2_selection_clear (GtkWidget *widget, GdkEventSelection *event);
-static void     xtext2_selection_get   (GtkWidget *widget, GtkSelectionData *sel_data, guint info, guint time);
-static gboolean xtext2_expose          (GtkWidget *widget, GdkEventExpose *event);
-static gboolean xtext2_scroll          (GtkWidget *widget, GdkEventScroll *event);
-
-static void     backend_init           (XText2 *xtext);
-static void     backend_deinit         (XText2 *xtext);
-static void     backend_font_open      (XText2 *xtext, char *name);
-static int      backend_get_char_width (XText2 *xtext, unsigned char *str, int *mbl_ret);
-static int      backend_get_text_width (XText2 *xtext, char *str, int len, gboolean multibyte);
-static int      backend_draw_text      (XText2 *xtext, gboolean fill, GdkGC *gc, int x, int y, char *str, int len, int width, gboolean multibyte);
-
-static void     paint                  (GtkWidget *widget, GdkRectangle *area);
-static void     render_page            (XText2 *xtext);
-static char*    selection_get_text     (XText2 *xtext, int *len);
-static void     set_indent             (XText2 *xtext, gboolean indent);
-static void     set_show_separator     (XText2 *xtext, gboolean show);
-static void     set_tint_color         (XText2 *xtext, GdkColor *color);
-static void     set_word_wrap          (XText2 *xtext, gboolean wrap);
 
 static gpointer parent_class;
 
@@ -58,23 +36,13 @@ enum
 
 static gint xtext_signals[LAST_SIGNAL];
 
-/* selection values */
-enum
-{
-  TARGET_UTF8_STRING,
-  TARGET_STRING,
-  TARGET_TEXT,
-  TARGET_COMPOUND_TEXT,
-};
-
 struct _XText2Private
 {
   /* view settings */
-  GdkColor     tint;                 /* color to tint the background */
-  gboolean     indent:         TRUE; /* whether the widget can indent */
-  gboolean     show_separator: TRUE; /* whether the separator bar is drawn */
-  gboolean     word_wrap:      TRUE; /* wrap words or not */
-  gboolean     avoid_trans:    TRUE; /* avoid transparency? */
+  gboolean     indent;               /* indent text? */
+  gboolean     show_separator;       /* show the separator bar? */
+  GdkColor     tint;                 /* tint color */
+  gboolean     word_wrap;            /* wrap words? */
 
   /* drawing data */
   gint         depth;                /* gdk window depth */
@@ -86,40 +54,6 @@ struct _XText2Private
   GdkDrawable *draw_buffer;          /* buffer to draw into */
   GdkPixmap   *background_pm;        /* background image */
   GdkCursor   *hand_cursor;          /* hand cursor (for urls, etc) */
-
-#ifdef USE_XFT
-  /* XFT backend */
-  XftColor  color[20];
-  XftColor *color_fg;
-  XftColor *color_bg;
-  XftDraw  *xftdraw;
-  XftFont  *font;
-#else
-  /* Pango backend */
-  struct
-  {
-    PangoFontDescription *font;
-    int ascent, descent;
-  } *font, pango_font;
-  PangoLayout *layout;
-#endif
-
-  /* selection information */
-  gboolean button_down:        TRUE;
-  int      select_start_x;
-  int      select_start_y;
-  int      select_start_adj;         /* adjustment value when the selection started */
-  int      select_end_x;
-  int      select_end_y;
-
-  /* general state */
-  gboolean moving_separator:   TRUE; /* currently moving the separator bar? */
-  gboolean word_or_line_select:TRUE; /* selecting a word or line? */
-
-  int clip_x;                        /* clip rect left   */
-  int clip_x2;                       /* clip rect right  */
-  int clip_y;                        /* clip rect top    */
-  int clip_y2;                       /* clip rect bottom */
 };
 
 GType
@@ -168,13 +102,6 @@ xtext2_class_init (XText2Class *klass)
   widget_class->unrealize             = xtext2_unrealize;
   widget_class->size_request          = xtext2_size_request;
   widget_class->size_allocate         = xtext2_size_allocate;
-  widget_class->button_press_event    = xtext2_button_press;
-  widget_class->button_release_event  = xtext2_button_release;
-  widget_class->motion_notify_event   = xtext2_motion_notify;
-  widget_class->selection_clear_event = xtext2_selection_clear;
-  widget_class->selection_get         = xtext2_selection_get;
-  widget_class->expose_event          = xtext2_expose;
-  widget_class->scroll_event          = xtext2_scroll;
 
   pspec = g_param_spec_string ("font",
                                "Font",
@@ -242,25 +169,25 @@ xtext2_set_property (GObject *object, guint param_id, const GValue *value, GPara
     case PROP_INDENT:
     {
       gboolean indent = g_value_get_boolean (value);
-      set_indent (xtext, indent);
+      xtext->priv->indent = indent;
       break;
     }
     case PROP_SHOW_SEPARATOR:
     {
       gboolean show = g_value_get_boolean (value);
-      set_show_separator (xtext, show);
+      xtext->priv->show_separator = show;
       break;
     }
     case PROP_TINT_COLOR:
     {
       GdkColor *color = g_value_get_boxed (value);
-      set_tint_color (xtext, color);
+      xtext->priv->tint = *color;
       break;
     }
     case PROP_WORD_WRAP:
     {
       gboolean wrap = g_value_get_boolean (value);
-      set_word_wrap (xtext, wrap);
+      xtext->priv->word_wrap = wrap;
       break;
     }
     default:
@@ -375,33 +302,13 @@ xtext2_realize (GtkWidget *widget)
   /* draw_directly to window */
   xtext->priv->draw_buffer = widget->window;
 
-#if defined(USE_XLIB) || defined(WIN32)
-  /* FIXME: transparency */
-#endif
-  if (xtext->priv->background_pm)
-  {
-    gdk_gc_set_tile (xtext->priv->bgc, xtext->priv->background_pm);
-    gdk_gc_set_ts_origin (xtext->priv->bgc, 0, 0);
-    /* FIXME: ts? */
-    gdk_gc_set_fill (xtext->priv->bgc, GDK_TILED);
-  }
-
-#if (GTK_MAJOR_VERSION == 2) && (GTK_MINOR_VERSION == 0)
-  xtext->priv->hand_cursor = gdk_cursor_new (GDK_HAND1);
-#else
-  xtext->priv->hand_cursor = gdk_cursor_new_for_display (gdk_drawable_get_display (widget->window), GDK_HAND1);
-#endif
-
   gdk_window_set_back_pixmap (widget->window, NULL, FALSE);
-
-  backend_init (xtext);
 }
 
 static void
 xtext2_unrealize (GtkWidget *widget)
 {
   XText2 *xtext = XTEXT2 (widget);
-  backend_deinit (xtext);
   if (((GtkWidgetClass*)parent_class)->unrealize)
     ((GtkWidgetClass*)parent_class)->unrealize (widget);
 }
@@ -421,12 +328,6 @@ xtext2_size_allocate (GtkWidget *widget, GtkAllocation *allocation)
   gboolean transparent = TRUE;
 
   /* FIXME: buffer madness */
-  if (allocation->x == widget->allocation.x &&
-      allocation->y == widget->allocation.y &&
-      xtext->priv->avoid_trans)
-    transparent = FALSE;
-
-  xtext->priv->avoid_trans = FALSE;
 
   widget->allocation = *allocation;
   if (GTK_WIDGET_REALIZED (widget))
@@ -434,339 +335,4 @@ xtext2_size_allocate (GtkWidget *widget, GtkAllocation *allocation)
     /* FIXME: stuff! */
     gdk_window_move_resize (widget->window, allocation->x, allocation->y, allocation->width, allocation->height);
   }
-}
-
-static gboolean
-xtext2_button_press (GtkWidget *widget, GdkEventButton *event)
-{
-  XText2 *xtext = XTEXT2 (widget);
-  int x, y;
-
-  gdk_window_get_pointer (widget->window, &x, &y, 0);
-
-  /* right click */
-  if (event->button == 3)
-  {
-    return FALSE;
-  }
-
-  if (event->button == 2)
-  {
-    return FALSE;
-  }
-
-  /* only want left click at this point */
-  if (event->button != 1)
-    return FALSE;
-
-  /* double click, select word */
-  if (event->type == GDK_2BUTTON_PRESS)
-  {
-    return FALSE;
-  }
-
-  /* triple click, select line */
-  if (event->type == GDK_3BUTTON_PRESS)
-  {
-    return FALSE;
-  }
-
-  /* check if it was a separator bar click */
-  /* FIXME: depends on buffer */
-
-  xtext->priv->button_down = TRUE;
-  xtext->priv->select_start_x = x;
-  xtext->priv->select_start_y = y;
-  xtext->priv->select_start_adj = xtext->adj->value;
-  return FALSE;
-}
-
-static gboolean
-xtext2_button_release (GtkWidget *widget, GdkEventButton *event)
-{
-  XText2 *xtext = XTEXT2 (widget);
-  unsigned char *word;
-
-  if (xtext->priv->moving_separator)
-  {
-    xtext->priv->moving_separator = FALSE;
-    /* FIXME: stuff */
-    return FALSE;
-  }
-
-  if (xtext->priv->word_or_line_select)
-  {
-    xtext->priv->word_or_line_select = FALSE;
-    xtext->priv->button_down = FALSE;
-    return FALSE;
-  }
-
-  if (event->button == 1)
-  {
-    /* FIXME: stuff */
-  }
-
-  return FALSE;
-}
-
-static gboolean
-xtext2_motion_notify (GtkWidget *widget, GdkEventMotion *event)
-{
-  /* FIXME !!! */
-  return FALSE;
-}
-
-static gboolean
-xtext2_selection_clear (GtkWidget *widget, GdkEventSelection *event)
-{
-  /* FIXME !!! */
-  return TRUE;
-}
-
-static void
-xtext2_selection_get (GtkWidget *widget, GtkSelectionData *sel_data, guint info, guint time)
-{
-  XText2 *xtext = XTEXT2 (widget);
-  char *stripped;
-  guchar *new_text;
-  int len;
-  gsize glen;
-
-  stripped = selection_get_text (xtext, &len);
-  if (!stripped)
-    return;
-
-  switch (info)
-  {
-    case TARGET_UTF8_STRING:
-      /* it's already in utf8 */
-      gtk_selection_data_set_text (sel_data, stripped, len);
-      break;
-    case TARGET_TEXT:
-    case TARGET_COMPOUND_TEXT:
-    {
-      GdkAtom encoding;
-      gint format;
-      gint new_length;
-
-#if (GTK_MAJOR_VERSION == 2) && (GTK_MINOR_VERSION == 0)
-      gdk_string_to_compound_text (stripped, &encoding, &format, &new_text, &new_length);
-#else
-      gdk_string_to_compound_text_for_display (gdk_drawable_get_display (widget->window),
-                                   stripped, &encoding, &format, &new_text, &new_length);
-#endif
-      gtk_selection_data_set (sel_data, encoding, format, new_text, new_length);
-      gdk_free_compound_text (new_text);
-      break;
-    }
-    default:
-      new_text = g_locale_from_utf8 (stripped, len, NULL, &glen, NULL);
-      gtk_selection_data_set (sel_data, GDK_SELECTION_TYPE_STRING, 8, new_text, glen);
-      g_free (new_text);
-  };
-  g_free (stripped);
-}
-
-static gboolean
-xtext2_expose (GtkWidget *widget, GdkEventExpose *event)
-{
-  paint (widget, &event->area);
-  return FALSE;
-}
-
-static gboolean
-xtext2_scroll (GtkWidget *widget, GdkEventScroll *event)
-{
-  XText2 *xtext = XTEXT2 (widget);
-  gfloat new_value;
-
-
-  if (event->direction == GDK_SCROLL_UP)
-  {
-    /* mousewheel/pgup */
-    new_value = xtext->adj->value - (xtext->adj->page_increment / 10);
-    if (new_value < xtext->adj->lower)
-      new_value = xtext->adj->lower;
-    gtk_adjustment_set_value (xtext->adj, new_value);
-  }
-  else if (event->direction == GDK_SCROLL_DOWN)
-  {
-    /* mousewheel/pgdn */
-    new_value = xtext->adj->value + (xtext->adj->page_increment / 10);
-    if (new_value > (xtext->adj->upper - xtext->adj->page_size))
-      new_value = xtext->adj->upper -xtext->adj->page_size;
-    gtk_adjustment_set_value (xtext->adj, new_value);
-  }
-  return FALSE;
-}
-
-#ifdef USE_XFT
-/* ========================================= */
-/* ========== XFT 1 and 2 BACKEND ========== */
-/* ========================================= */
-
-static void
-backend_init (XText2 *xtext)
-{
-  if (xtext->priv->xftdraw == NULL)
-  {
-    xtext->priv->xftdraw = XftDrawCreate (
-      GDK_WINDOW_XDISPLAY (xtext->priv->draw_buffer),
-      GDK_WINDOW_XWINDOW (xtext->priv->draw_buffer),
-      GDK_VISUAL_XVISUAL (gdk_drawable_get_visual (xtext->priv->draw_buffer)),
-      GDK_COLORMAP_XCOLORMAP (gdk_drawable_get_colormap (xtext->priv->draw_buffer)));
-    XftDrawSetSubwindowMode (xtext->priv->xftdraw, IncludeInferiors);
-  }
-}
-
-static void
-backend_deinit (XText2 *xtext)
-{
-  if (xtext->priv->xftdraw)
-  {
-    XftDrawDestroy (xtext->priv->xftdraw);
-    xtext->priv->xftdraw = NULL;
-  }
-}
-
-static void
-backend_font_open (XText2 *xtext, char *name)
-{
-}
-
-inline static int
-backend_get_char_width (XText2 *xtext, unsigned char *str, int *mbl_ret)
-{
-}
-
-static int
-backend_get_text_width (XText2 *xtext, char *str, int len, gboolean multibyte)
-{
-}
-
-static int
-backend_draw_text (XText2 *xtext, gboolean fill, GdkGC *gc, int x, int y, char *str, int len, int width, gboolean multibyte)
-{
-}
-#else /* !USE_XFT */
-/* ======================================= */
-/* ============ PANGO BACKEND ============ */
-/* ======================================= */
-
-#define backend_font_close(d,f) pango_font_description_free(f->font)
-static void
-backend_init (XText2 *xtext)
-{
-}
-
-static void
-backend_deinit (XText2 *xtext)
-{
-}
-
-static void
-backend_font_open (XText2 *xtext, char *name)
-{
-}
-
-static int
-backend_get_char_width (XText2 *xtext, unsigned char *str, int *mbl_ret)
-{
-}
-
-static int
-backend_get_text_width (XText2 *xtext, char *str, int len, gboolean multibyte)
-{
-}
-
-static int
-backend_draw_text (XText2 *xtext, gboolean fill, GdkGC *gc, int x, int y, char *str, int len, int width, gboolean multibyte)
-{
-}
-#endif
-
-static void
-paint (GtkWidget *widget, GdkRectangle *area)
-{
-  XText2 *xtext = XTEXT2 (widget);
-  int x, y;
-
-#if defined(USE_XLIB) || defined(WIN32)
-  if (xtext->priv.transparent)
-  {
-    gdk_window_get_origin (widget->window, &x, &y);
-    /* update transparency only if moved */
-    if (xtext->priv->last_win_x != x || xtext->priv->last_win_y != y)
-    {
-      xtext->priv->last_win_x = x;
-      xtext->priv->last_win_y = y;
-      /* FIXME: more stuff */
-    }
-  }
-#endif /* USE_XLIB || WIN32 */
-  if (area->x == 0 && area->y == 0 &&
-      area->height == widget->allocation.height &&
-      area->width == widget->allocation.width)
-  {
-//    dont_scroll ()
-    render_page (xtext);
-  }
-
-  /* FIXME: more stuff */
-  xtext->priv->clip_x  = area->x;
-  xtext->priv->clip_x2 = area->x + area->width;
-  xtext->priv->clip_y  = area->y;
-  xtext->priv->clip_y2 = area->y + area->height;
-
-  /* FIXME: more stuff */
-
-  xtext->priv->clip_x  = 0;
-  xtext->priv->clip_x2 = 1000000;
-  xtext->priv->clip_y  = 0;
-  xtext->priv->clip_y2 = 1000000;
-
-  /* FIXME: more stuff */
-}
-
-static void
-render_page (XText2 *xtext)
-{
-  int width, height;
-
-  if (!GTK_WIDGET_REALIZED (xtext))
-    return;
-
-  /* FIXME: more stuff */
-
-  gdk_drawable_get_size (GTK_WIDGET (xtext)->window, &width, &height);
-}
-
-static char*
-selection_get_text (XText2 *xtext, int *len)
-{
-  /* FIXME !!! */
-}
-
-static void
-set_indent (XText2 *xtext, gboolean indent)
-{
-  xtext->priv->indent = indent;
-}
-
-static void
-set_show_separator (XText2 *xtext, gboolean show)
-{
-  xtext->priv->show_separator = show;
-}
-
-static void
-set_tint_color (XText2 *xtext, GdkColor *color)
-{
-  memcpy (&xtext->priv->tint, color, sizeof (GdkColor));
-}
-
-static void
-set_word_wrap (XText2 *xtext, gboolean wrap)
-{
-  xtext->priv->word_wrap = wrap;
 }
