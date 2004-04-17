d83db5e3ed95c9624744a5975ce86090ae99bc1e
Evan Sheehan <darthmall@users.noreply.github.com>
2004-04-17 00:41:32 +0000

    So far everything compiles. ^^

--- xchat-gnome/fe-gnome/navtree.c
+++ xchat-gnome/fe-gnome/navtree.c
@@ -23,12 +23,16 @@
 
 /***** NavTree *****/
 static void navigation_tree_init       (NavTree *navtree);
-static void navigation_tree_class_init (NavTreeModel *klass);
+static void navigation_tree_class_init (NavTreeClass *klass);
 static void navigation_tree_dispose    (GObject *object);
 static void navigation_tree_finalize   (GObject *object);
 
+void navigation_selection_changed (GtkTreeSelection *treeselection, gpointer user_data);
+static gboolean click             (GtkWidget *widget, GdkEventButton *event, gpointer user_data);
+static gboolean declick           (GtkWidget *widget, GdkEventButton *event, gpointer user_data);
+
 GType
-navigation_tree_get_type (void) G_GNUC_CONST
+navigation_tree_get_type (void)
 {
   static GType navigation_tree_type = 0;
   if (!navigation_tree_type) {
@@ -55,11 +59,11 @@ static void
 navigation_tree_init (NavTree *navtree)
 {
 	navtree->current_path = gtk_tree_path_new();
-	model = NULL;
+	navtree->model = NULL;
 }
 
 static void
-navigation_tree_class_init (NavTreeClas *klass)
+navigation_tree_class_init (NavTreeClass *klass)
 {
 	GObjectClass *object_class = (GObjectClass*) klass;
 
@@ -89,9 +93,29 @@ NavTree*
 navigation_tree_new (NavModel *model)
 {
 	NavTree *new_tree;
+  GtkCellRenderer *icon_renderer, *text_renderer;
+  GtkTreeViewColumn *column;
+  GtkTreeSelection *select;
 
 	new_tree = NAVTREE(g_object_new(navigation_tree_get_type(), NULL));
 	new_tree->model = model;
+  gtk_tree_view_set_model(GTK_TREE_VIEW(new_tree), new_tree->model->sorted);
+
+  column = gtk_tree_view_column_new();
+  icon_renderer = gtk_cell_renderer_pixbuf_new();
+  text_renderer = gtk_cell_renderer_text_new();
+  gtk_tree_view_column_pack_start(column, icon_renderer, FALSE);
+  gtk_tree_view_column_set_attributes(column, icon_renderer, "pixbuf", 0, NULL);
+  gtk_tree_view_column_pack_start(column, text_renderer, TRUE);
+  gtk_tree_view_column_set_attributes(column, text_renderer, "text", 1, "foreground-gdk", 4, NULL);
+  gtk_tree_view_append_column(GTK_TREE_VIEW(new_tree),column);
+
+  select = gtk_tree_view_get_selection(GTK_TREE_VIEW(new_tree));
+  gtk_tree_selection_set_mode(select, GTK_SELECTION_SINGLE);
+/*
+  g_signal_connect(G_OBJECT(select), "changed", G_CALLBACK(navigation_selection_changed), NULL);
+  g_signal_connect(G_OBJECT(new_tree), "button_press_event", G_CALLBACK(click), NULL);
+  g_signal_connect(G_OBJECT(new_tree), "button_release_event", G_CALLBACK(declick), NULL);*/
 
 	return new_tree;
 }
@@ -107,11 +131,11 @@ static void navigation_model_class_init (NavModelClass *klass);
 static void navigation_model_dispose    (GObject *object);
 
 GType
-navigation_model_get_type (void) G_GNUC_CONST
+navigation_model_get_type (void)
 {
   static GType navigation_model_type = 0;
   if (!navigation_model_type) {
-    static const GTypeInfo navigation_mode_info =
+    static const GTypeInfo navigation_model_info =
     {
       sizeof (NavTreeClass),
       NULL,
@@ -133,7 +157,9 @@ navigation_model_get_type (void) G_GNUC_CONST
 static void
 navigation_model_init (NavModel *navmodel)
 {
-	navmodel->model = gtk_tree_model_new();
+  navmodel->store = gtk_tree_store_new(5, GDK_TYPE_PIXBUF, G_TYPE_STRING, G_TYPE_POINTER, G_TYPE_INT, GDK_TYPE_COLOR);
+	navmodel->sorted = gtk_tree_model_sort_new_with_model(GTK_TREE_MODEL(navmodel->store));
+  gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(navmodel->sorted), 1, GTK_SORT_ASCENDING);
 }
 
 static void
@@ -144,9 +170,11 @@ navigation_model_class_init (NavModelClass *klass)
 }
 
 static void
-navigation_model_dispose (NavModel *navmodel)
+navigation_model_dispose (GObject *object)
 {
-	g_object_unref((gpointer)navmodel->model);
+  NavModel *navmodel = NAVMODEL(object);
+  g_object_unref((gpointer)navmodel->store);
+	g_object_unref((gpointer)navmodel->sorted);
 }
 
 /* New NavModel. */
