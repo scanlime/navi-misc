3af83de3ff1dc608d471d9bda1d7af5ec8d11612
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-22 04:41:17 +0000

    bah!

--- trunk/python/cia-patches/hug.py
+++
@@ -1,8 +0,0 @@
-from LibCIA.IRC.Bots import Bot
-def hug_action(bot, user, channel, message):
-    if message.lower().strip() == "hugs %s" % bot.nickname.lower():
-        bot.me(channel, "hugs %s" % user.split("!")[0])
-
-Bot.action = hug_action
-
-
