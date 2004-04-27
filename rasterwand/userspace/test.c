088d1b240fe202173a546b4460afe1430bcc5868
Micah Elizabeth Scott <micah@scanlime.org>
2004-04-27 02:39:00 +0000

    Whee. Read the angle predictor registers periodically, calculating the wand's frequency for all to see displayed in Hz. Also calculate coil phase parameters and upload those. By making the coil pulse wider, I can get it to oscillate full-speed at 5v. Yay! Now I just need a GUI frontend for this so I can tweak the parameters in real-time for optimal optimality.

--- rasterwand/userspace/test.c
+++ rasterwand/userspace/test.c
@@ -33,19 +33,60 @@ void control_write(usb_dev_handle *d, int request, int value, int index) {
   }
 }
 
+void control_read(usb_dev_handle *d, int request, unsigned char* buffer, int bytes) {
+  if (usb_control_msg(d, USB_TYPE_VENDOR | USB_ENDPOINT_IN, request,
+		      0, 0, buffer, bytes, 500) < 0) {
+    perror("usb_control_msg");
+    exit(1);
+  }
+}
+
+void control_read16(usb_dev_handle *d, int request, short unsigned int *buffer, int words) {
+  unsigned char *bytes = (unsigned char*) buffer;
+  int i;
+  control_read(d, request, bytes, words*2);
+  for (i=0; i<words; i++)
+    buffer[i] = bytes[i*2] + (bytes[i*2+1] << 8);
+}
+
 int main(int argc, char **argv) {
-  usb_dev_handle *d = open_rwand();
-  unsigned char c[8];
-  unsigned char x[4];
-  if (!d)
+  usb_dev_handle *d;
+  unsigned short int values[4];
+  int period;
+
+  if (!(d = open_rwand()))
     return 1;
 
   control_write(d, RWAND_CTRL_SET_MODES,
 		RWAND_MODE_ENABLE_SYNC |
 		RWAND_MODE_ENABLE_COIL |
-		RWAND_MODE_ENABLE_DISPLAY,
+		RWAND_MODE_COIL_DEBUG |
+		//		RWAND_MODE_ENABLE_DISPLAY |
+		0,
 		0);
-  control_write(d, RWAND_CTRL_SET_COIL_PHASE, 8000, 15000);
+
+  while (1) {
+    /* Read the current period prediction */
+    control_read16(d, RWAND_CTRL_READ_PREDICTION, values, 2);
+    period = values[0];
+    printf("Wand period: %d (%.02f Hz)\n", period,
+	   1/(period * 16 / 6000000.0));
+
+    /* Update the coil driver phase */
+    {
+      const float pulse_center = 0.25;
+      const float pulse_width  = 0.2;
+      control_write(d, RWAND_CTRL_SET_COIL_PHASE,
+		    period * (pulse_center - pulse_width),
+		    period * (pulse_center + pulse_width));
+    }
+
+    control_write(d, RWAND_CTRL_SET_DISPLAY_PHASE, 100, 0x4FFF);
+    control_write(d, RWAND_CTRL_SET_COLUMN_WIDTH, 100, 0);
+    control_write(d, RWAND_CTRL_FLIP, 0,0);
+
+    usleep(10000);
+  }
 
   return 0;
 }
