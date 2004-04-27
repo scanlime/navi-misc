8829785f688d1447e988fbb336d638771d341100
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-27 03:42:32 +0000

    A little cleanup, and the beginnings of stall detection and unstalling. Currently it detects stalls and unstalls, but the code to zero in on the wand's resonant frequency doesn't do anything useful yet.

--- trunk/rasterwand/userspace/test.c
+++ trunk/rasterwand/userspace/test.c
@@ -1,6 +1,13 @@
 #include <usb.h>
 #include <stdio.h>
 #include <rwand_protocol.h>
+#include <time.h>
+
+struct prediction_status {
+  unsigned short period;
+  unsigned short phase;
+  unsigned char edge_count;
+};
 
 usb_dev_handle *open_rwand(void) {
   struct usb_bus *busses;
@@ -41,18 +48,63 @@ void control_read(usb_dev_handle *d, int request, unsigned char* buffer, int byt
   }
 }
 
-void control_read16(usb_dev_handle *d, int request, short unsigned int *buffer, int words) {
-  unsigned char *bytes = (unsigned char*) buffer;
-  int i;
-  control_read(d, request, bytes, words*2);
-  for (i=0; i<words; i++)
-    buffer[i] = bytes[i*2] + (bytes[i*2+1] << 8);
+void read_prediction_status(usb_dev_handle *d, struct prediction_status *status) {
+  unsigned char packet[5];
+  control_read(d, RWAND_CTRL_READ_PREDICTION, packet, sizeof(packet));
+  status->period = packet[0] + (packet[1] << 8);
+  status->phase = packet[2] + (packet[3] << 8);
+  status->edge_count = packet[4];
+}
+
+void update_coil_driver(usb_dev_handle *d, struct prediction_status *status,
+			float pulse_center, float pulse_width) {
+  control_write(d, RWAND_CTRL_SET_COIL_PHASE,
+		status->period * (pulse_center - pulse_width),
+		status->period * (pulse_center + pulse_width));
+}
+
+void unstall(usb_dev_handle *d) {
+  unsigned period = -1;
+  int unstall_edges = 0;
+  struct prediction_status predicted, last_predicted;
+
+  printf("Stalled. Trying to restart...\n");
+
+  read_prediction_status(d, &last_predicted);
+  predicted = last_predicted;
+
+  while (unstall_edges < 100) {
+    /* This part needs a lot of work...
+     * We need to use our sole source of feedback (whether we get
+     * angle sensor edges or not) to try to zero in on the resonant frequency.
+     */
+    if (unstall_edges == 0) {
+      if (period > 53000)
+	period = 47000;
+      period += 500;
+    }
+    else {
+      period -= 10;
+    }
+
+    printf("period = %d, edges = %d\n", period, unstall_edges);
+    control_write(d, RWAND_CTRL_SET_PREDICTION, 0, period);
+    update_coil_driver(d, &predicted, 0.25, 0.25);
+
+    usleep(1000000);
+
+    last_predicted = predicted;
+    read_prediction_status(d, &predicted);
+    unstall_edges += (predicted.edge_count - last_predicted.edge_count) & 0xFF;
+  }
 }
 
 int main(int argc, char **argv) {
   usb_dev_handle *d;
-  unsigned short int values[4];
-  int period;
+  struct prediction_status predicted, last_predicted;
+  int last_period = 0;
+  time_t last_edge_timestamp = time(NULL);
+  memset(&predicted, 0, sizeof(predicted));
 
   if (!(d = open_rwand()))
     return 1;
@@ -60,32 +112,37 @@ int main(int argc, char **argv) {
   control_write(d, RWAND_CTRL_SET_MODES,
 		RWAND_MODE_ENABLE_SYNC |
 		RWAND_MODE_ENABLE_COIL |
-		RWAND_MODE_COIL_DEBUG |
-		//		RWAND_MODE_ENABLE_DISPLAY |
+		//RWAND_MODE_COIL_DEBUG |
+		//RWAND_MODE_ENABLE_DISPLAY |
 		0,
 		0);
 
   while (1) {
-    /* Read the current period prediction */
-    control_read16(d, RWAND_CTRL_READ_PREDICTION, values, 2);
-    period = values[0];
-    printf("Wand period: %d (%.02f Hz)\n", period,
-	   1/(period * 16 / 6000000.0));
+    /* Read the current period prediction, calculate the frequency.
+     * (the period is in units of 16 CPU cycles on a 6 MIPS processor)
+     */
+    last_predicted = predicted;
+    read_prediction_status(d, &predicted);
+    printf("Wand period: %d (%.02f Hz)\n", predicted.period,
+	   1/(predicted.period * 16 / 6000000.0));
 
-    /* Update the coil driver phase */
-    {
-      const float pulse_center = 0.25;
-      const float pulse_width  = 0.2;
-      control_write(d, RWAND_CTRL_SET_COIL_PHASE,
-		    period * (pulse_center - pulse_width),
-		    period * (pulse_center + pulse_width));
+    /* Have we had any synchronization edges this time? */
+    if (predicted.edge_count != last_predicted.edge_count)
+      last_edge_timestamp = time(NULL);
+
+    /* If it's been a while since we've seen a sync edge, conclude we're stalled */
+    if (time(NULL) > last_edge_timestamp + 1) {
+      unstall(d);
+      last_edge_timestamp = time(NULL);
     }
+
+    update_coil_driver(d, &predicted, 0.25, 0.2);
 
     control_write(d, RWAND_CTRL_SET_DISPLAY_PHASE, 100, 0x4FFF);
     control_write(d, RWAND_CTRL_SET_COLUMN_WIDTH, 100, 0);
     control_write(d, RWAND_CTRL_FLIP, 0,0);
 
-    usleep(10000);
+    usleep(50000);
   }
 
   return 0;
