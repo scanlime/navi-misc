d3ec17acebe5de3bcd8719108a0cb28c49ec6945
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-30 07:14:20 +0000

    utility function to cleaup database of old servers

---
+++ firestarter/listserver/cleanup.php
@@ -0,0 +1,29 @@
+<?php
+
+// get our configs
+include('config.php');
+
+
+function cleanupdb($thetime)
+{
+		/* Connecting, selecting database */
+	$link = mysql_connect($dbhost, $dbuser, $dbpasswd)
+		or die("cleanupdb::Could not connect to $dbhost : " . mysql_error());
+		
+	mysql_select_db($dbname ) or die("cleanupdb::Could not select database : $dbname ");
+
+	// get a list of each field from servers and kill any that have old update times
+	$query = "SELECT * FROM servers";
+	$result = mysql_query($query) or die("Query failed : " . mysql_error());
+
+	while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+	{
+		if ($thetime - line[10] > 5*60 )
+		{
+			$deletequery = "DELETE * FROM servers WHERE 'id' = 'line[0]'";
+			mysql_query($deletequery) or die("cleanupdb::delete failed : " . mysql_error());
+		}
+	}
+}
+
+?>
