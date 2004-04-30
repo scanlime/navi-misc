3588ac8c45453f1c14fc845f956d429b9e8eac64
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-30 21:20:28 +0000

    inital add function

---
+++ firestarter/listserver/add.php
@@ -0,0 +1,30 @@
+<?php
+// list server for firestarter
+// get our configs
+include('config.php');
+
+echo "<html><head></head><body>";
+
+/* Connecting, selecting database */
+$link = mysql_connect($dbhost, $dbuser, $dbpasswd)
+   or die("add failed::Could not connect to $dbhost : " . mysql_error());
+echo "BZFlagFirestarter list server $serverVers : $dbhost : $hostpublicaddr<br>";
+mysql_select_db($dbname )
+	or die("add failed::Could not select database : $dbname ");
+
+$now = date("U");
+$query = "INSERT INTO servers values ( NULL, ".$servername.",".$address.",".$port.",".$game.",".$version.",".$os.",".$maxplayers.",".$currentplayers.",".$now.")";
+$result = mysql_query( $query )
+	or die("add failed ::insert : " . mysql_error());
+
+$lastID = mysql_insert_id();
+
+echo 'add processed<br>servername='.$servername.":".$port;
+echo 'id='.$lastID;
+
+echo "</body></html>";
+
+/* Closing connection */
+mysql_close($link);
+
+?>
