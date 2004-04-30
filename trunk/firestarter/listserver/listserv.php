f75ef6687dc5295f820f9074246060cc6b48bb9f
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-30 06:00:27 +0000

    add a config so we don't have to edit the main file

--- trunk/firestarter/listserver/listserv.php
+++ trunk/firestarter/listserver/listserv.php
@@ -1,16 +1,13 @@
 <?php
-
 // list server for firestarter
 
-$dbhost = 'localhost';
-$dbname = 'fslistdb';
-$dbuser = 'xxxxx';
-$dbpasswd = 'xxxxx';
+// get our configs
+include('config.php');
 
 $serverVers = 0.1;
 
 echo "<html><head></head><body>";
-echo "BZFlagFirestarter list server $serverVers<br>";
+echo "BZFlagFirestarter list server $serverVers : $hostpublicaddr<br>";
 echo "!beginlist<br>";
 echo "#servername #address #port #game #version #os #maxplayers #currentplayers<br>";
 
