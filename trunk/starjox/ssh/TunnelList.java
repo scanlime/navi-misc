8ab84d205b03cde42707cfa48ee923493845db1e
brandon <brandon@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-30 18:58:46 +0000

    This is what I think it is.

--- trunk/starjox/ssh/TunnelList.java
+++ trunk/starjox/ssh/TunnelList.java
@@ -30,61 +30,94 @@ import javax.swing.event.*;
 
 public class TunnelList extends JPanel implements ListSelectionListener
 {
-    private JFrame myFrame;
-    private JList tList;
-    private DefaultListModel tListModel;
-    private JButton ssTunnel;
-    private JButton newTunnel;
-
-    public TunnelList(JFrame frame)
-    {
-	super(new BorderLayout());
-	myFrame = frame;
-	tListModel = new DefaultListModel();
+	private JFrame myFrame;
+	private JList tList;
+	private DefaultListModel tListModel;
+	private JButton ssTunnel;
+	private JButton newTunnel;
+	private JButton editTunnel;
 	
-	tListModel.addElement("Place Holder");
-	
-
-	tList = new JList(tListModel);
-	tList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	tList.setSelectedIndex(0);
-	tList.addListSelectionListener(this);
-	tList.setVisibleRowCount(10);
-	JScrollPane listScrollPane = new JScrollPane(tList);
-	add(listScrollPane, BorderLayout.CENTER);
-
-	
-	newTunnel = new JButton("New Tunnel");
-	newTunnel.addActionListener(new newTunnelListener());
-
-	ssTunnel = new JButton("Start/Stop");
-	ssTunnel.addActionListener(new ssTunnelListener());
-	JPanel buttonPane = new JPanel();
-	buttonPane.setLayout(new BoxLayout(buttonPane,BoxLayout.LINE_AXIS));
-	buttonPane.add(newTunnel);
-	buttonPane.add(ssTunnel);
-	buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-        add(buttonPane, BorderLayout.PAGE_END);
-    }
-
-    public void valueChanged(ListSelectionEvent e)
-    {
-	
-    }
-
-    class newTunnelListener implements ActionListener 
-    {
-	public void actionPerformed(ActionEvent e)
+	public TunnelList(JFrame frame)
 	{
-	    System.out.println("New Tunnel Clicked");
-	}
-    }
+		super(new BorderLayout());
+		myFrame = frame;
+		tListModel = new DefaultListModel();
+	
+		//tListModel.addElement("Place Holder");
+	
 
-    class ssTunnelListener implements ActionListener
-    {
-	public void actionPerformed(ActionEvent e)
-	{
-	    System.out.println("Start/Stop Clicked");
+		tList = new JList(tListModel);
+		tList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tList.setSelectedIndex(0);
+		tList.addListSelectionListener(this);
+		tList.setVisibleRowCount(10);
+		JScrollPane listScrollPane = new JScrollPane(tList);
+		add(listScrollPane, BorderLayout.CENTER);
+
+		newTunnel = new JButton("New Tunnel");
+		newTunnel.addActionListener(new newTunnelListener());
+
+		ssTunnel = new JButton("Start/Stop");
+		ssTunnel.addActionListener(new ssTunnelListener());
+	
+		editTunnel = new JButton("Edit Tunnel");
+		editTunnel.addActionListener(new editTunnelListener());
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new BoxLayout(buttonPane,BoxLayout.LINE_AXIS));
+		buttonPane.add(newTunnel);
+		buttonPane.add(editTunnel);
+		editTunnel.setEnabled(false);
+		buttonPane.add(ssTunnel);
+		ssTunnel.setEnabled(false);
+		buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		add(buttonPane, BorderLayout.PAGE_END);
 	}
-    }
+
+	public void valueChanged(ListSelectionEvent e)
+	{
+		if(e.getValueIsAdjusting() == false);
+		{
+			if(tList.getSelectedIndex() == -1)
+			{
+				ssTunnel.setEnabled(false);
+				editTunnel.setEnabled(false);
+			}
+			else
+			{
+				ssTunnel.setEnabled(true);
+				editTunnel.setEnabled(true);
+			}
+		}
+	}
+
+	class editTunnelListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			//kick up a popup
+			System.out.println("Edit Tunnel clicked");
+		}
+	}
+
+	class newTunnelListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			tListModel.addElement("Place Holder");
+			System.out.println("New Tunnel Clicked");
+		} 
+	}
+
+	class ssTunnelListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			tListModel.remove(0);
+			if(tListModel.getSize() == 0)
+				ssTunnel.setEnabled(false);
+			
+			System.out.println("Start/Stop Clicked");
+		}
+	}
 }
