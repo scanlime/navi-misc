ad4a0532be4d4fe33edf11d2f8c7d0bcf1ee5011
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-30 06:37:05 +0000

    look database access!!!

--- trunk/firestarter/listserver/listserv.php
+++ trunk/firestarter/listserver/listserv.php
@@ -7,11 +7,32 @@ include('config.php');
 $serverVers = 0.1;
 
 echo "<html><head></head><body>";
-echo "BZFlagFirestarter list server $serverVers : $hostpublicaddr<br>";
-echo "!beginlist<br>";
-echo "#servername #address #port #game #version #os #maxplayers #currentplayers<br>";
 
+/* Connecting, selecting database */
+$link = mysql_connect($dbhost, $dbuser, $dbpasswd)
+   or die("Could not connect to $dbhost : " . mysql_error());
+echo "BZFlagFirestarter list server $serverVers : $dbhost : $hostpublicaddr<br>";
+
+mysql_select_db($dbname ) or die("Could not select database : $dbname ");
+
+echo "!beginlist<br><table border=\"1\" cellspacing=\"2\" cellpading=\"5\">";
+echo "<tr><td>#id</td><td>#servername</td><td>#address</td><td>#port</td><td>#game</td><td>#version</td><td>#os</td><td>#maxplayers</td><td>#currentplayers<tr>";
+
+$query = "SELECT * FROM servers";
+$result = mysql_query($query) or die("Query failed : " . mysql_error());
+
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+   echo "<tr>";
+   foreach ($line as $col_value) {
+       echo "<td>$col_value</td> ";
+   }
+   echo "</tr>";
+}
+echo "</table>";
 echo "!endlist<br>";
 echo "</body></html>";
+
+/* Closing connection */
+mysql_close($link);
 
 ?>
