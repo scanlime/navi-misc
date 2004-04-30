06dbab5ae6c354e457622a85611630196be030e9
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-30 22:23:12 +0000

    silly cleanup function, those servers are new, not old

--- trunk/firestarter/listserver/cleanup.php
+++ trunk/firestarter/listserver/cleanup.php
@@ -1,16 +1,17 @@
 <?php
 
 // get our configs
-include('config.php');
+
 
 function cleanupdb($thetime)
 {
+	include('config.php');
 	/* Connecting, selecting database */
 	$link = mysql_connect($dbhost, $dbuser, $dbpasswd)
 		or die("cleanupdb::Could not connect to $dbhost : " . mysql_error());
 	
-	mysql_select_db($dbname )
-		or die("cleanupdb::Could not select database : $dbname ");
+	mysql_select_db($dbname ) or die("Could not select database : $dbname ");
+
 	
 	//get a list of each field from servers and kill any that have old update times
 	$query = "SELECT * FROM servers";
@@ -19,8 +20,10 @@ function cleanupdb($thetime)
 	
 	while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
 	{
-		if ($thetime - $line[10] > 5*60 )	// if it's over 5 min
+		if ($thetime - $line[lastupdate] > 5*60 )	// if it's over 5 min
 		{
+			echo "the time delta was ".$thetime - $line[lastupdate]."<br>";
+			
 			$deletequery = "DELETE FROM servers WHERE id = ".$line[id];
 			mysql_query($deletequery)
 				or die("cleanupdb::delete failed : " . mysql_error());
