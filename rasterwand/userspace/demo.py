d3ebb46eeba0dfef4f37dcc24e42aaf3f2beea4e
Micah Elizabeth Scott <micah@scanlime.org>
2004-05-04 07:02:10 +0000

    Add a program to scroll any input piped to it.. tail -f running on a log file, for example

--- rasterwand/userspace/demo.py
+++ rasterwand/userspace/demo.py
@@ -5,7 +5,7 @@ s = xmlrpclib.ServerProxy("http://navi.picogui.org:4510")
 dev = rasterwand.Device()
 
 dev.params.display_width = 0xFFFF * 0.75
-dev.params.duty_cycle = 0xFFFF * 0.5
+dev.params.duty_cycle = 0xFFFF * 0.2
 
 while 1:
     # Show the time
@@ -14,6 +14,6 @@ while 1:
     # Scroll all temperatures continuously
     desc = s.getDescriptions()
     avg = s.getAverages()
-    text = (" "*10).join(["%s: %0.1f \xb0F" % (name, avg[id]) for id, name in desc.iteritems()])
+    text = (" "*10).join(["%s: %0.1f \xb0F" % (id, avg[id]) for id, name in desc.iteritems()])
     rasterwand.TextScroller(dev, text, width=70).scroll(start=(1, 0), end=(-1, 0), speed=1)
 
