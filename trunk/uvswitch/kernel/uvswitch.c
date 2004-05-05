8a81b31b6b93d541ea718b8376c9e06849c00fb4
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-05-05 09:08:03 +0000

    Blah, silly kernel developers are renaming things. Ported to work on 2.6.5

--- trunk/uvswitch/kernel/uvswitch.c
+++ trunk/uvswitch/kernel/uvswitch.c
@@ -633,7 +633,7 @@ static int uvswitch_probe(struct usb_interface *interface, const struct usb_devi
 {
 	struct usb_device *udev = interface_to_usbdev(interface);
 	struct usb_uvswitch *dev = NULL;
-	struct usb_host_interface *host_interface = &interface->altsetting[interface->act_altsetting];
+	struct usb_host_interface *host_interface = interface->cur_altsetting;
 	int retval, i;
 
 	/* See if the device offered us matches what we can accept */
