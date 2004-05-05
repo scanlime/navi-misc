b75f4f5ff7c97436d4f5d44bb092f9792eed7572
Micah Elizabeth Scott <micah@scanlime.org>
2004-05-05 06:29:21 +0000

    Spread 2.6ity to the fuzzywuzzies

--- uvswitch/kernel/uvswitch.c
+++ uvswitch/kernel/uvswitch.c
@@ -19,23 +19,19 @@
 
 #include <linux/config.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/signal.h>
 #include <linux/errno.h>
-#include <linux/poll.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/fcntl.h>
 #include <linux/module.h>
-#include <linux/spinlock.h>
-#include <linux/list.h>
 #include <linux/smp_lock.h>
-#include <linux/devfs_fs_kernel.h>
+#include <linux/poll.h>
+#include <linux/completion.h>
+#include <asm/uaccess.h>
 #include <linux/usb.h>
 #include <uvswitch_dev.h>
 
-#define DRIVER_VERSION "v0.1"
-#define DRIVER_AUTHOR "Micah Dowty <micah@picogui.org>"
+#define DRIVER_VERSION "v0.2"
+#define DRIVER_AUTHOR "Micah Dowty <micah@navi.cx>"
 #define DRIVER_DESC "USB Video Switch driver"
 
 MODULE_AUTHOR( DRIVER_AUTHOR );
@@ -61,15 +57,15 @@ struct uvswitch_status {
 struct usb_uvswitch {
 	struct usb_device *	udev;			/* save off the usb device pointer */
 	struct usb_interface *	interface;		/* the interface for this device */
-	devfs_handle_t		devfs;			/* main devfs device node */
 	unsigned char		minor;			/* the starting minor number for this device */
 	int			open_count;		/* number of times this port has been opened */
 
 	struct uvswitch_calibration calibration;	/* Tweakable values for the video detector */
 	struct uvswitch_status	status;			/* Status of the device's switches */
 
-	struct urb		adc_urb;		/* URB for the video detector A/D converters */
-       	unsigned char		adc_buffer[UVSWITCH_CHANNELS]; /* Buffer used by adc_urb */
+	struct urb*		adc_urb;		/* URB for the video detector A/D converters */
+       	unsigned char*		adc_buffer;
+	dma_addr_t              adc_dma;
 	int			adc_accumulator[UVSWITCH_CHANNELS]; /* Values being accumulated after each packet received */
 	int			adc_accumulated[UVSWITCH_CHANNELS]; /* Finished ADC values */
 	int			active_inputs[UVSWITCH_CHANNELS]; /* Nonzero values for active video inputs */
@@ -88,42 +84,55 @@ struct uvswitch_fd_private {
 	int			last_change_id;		/* The adc_change_id last time a read() was done */
 };
 
-/* the global devfs handle for the USB subsystem */
-extern devfs_handle_t usb_devfs_handle;
-
-/* we can have up to this number of device plugged in at once */
-#define MAX_DEVICES 16
-
-/* array of pointers to our devices that are currently connected */
-static struct usb_uvswitch *minor_table[MAX_DEVICES];
-
-/* lock to protect the minor_table structure */
-static DECLARE_MUTEX(minor_table_mutex);
+/* prevent races between open() and disconnect() */
+static DECLARE_MUTEX (disconnect_sem);
 
 /* 1 second timeout for control requests */
 #define REQUEST_TIMEOUT HZ
 
 
-/******************************************************************************/
-/************************************************** Function Prototypes *******/
-/******************************************************************************/
+static int          uvswitch_updateStatus      (struct usb_uvswitch *dev);
+static int          uvswitch_updateCalibration (struct usb_uvswitch *dev);
+static int          uvswitch_initCalibration   (struct usb_uvswitch *dev);
+static void         uvswitch_adc_irq           (struct urb *urb, struct pt_regs *regs);
 
-static int uvswitch_updateStatus(struct usb_uvswitch *dev);
-static int uvswitch_updateCalibration(struct usb_uvswitch *dev);
-static int uvswitch_initCalibration(struct usb_uvswitch *dev);
-static void uvswitch_adc_irq(struct urb *urb);
+static int          uvswitch_open              (struct inode *inode, struct file *file);
+static int          uvswitch_release           (struct inode *inode, struct file *file);
+static int          uvswitch_ioctl             (struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg);
+static ssize_t      uvswitch_read              (struct file *file, char *buffer, size_t count, loff_t *ppos);
+static ssize_t      uvswitch_write             (struct file *file, const char *buffer, size_t count, loff_t *ppos);
+static unsigned int uvswitch_poll              (struct file *file, poll_table *wait);
 
-static int uvswitch_open(struct inode *inode, struct file *file);
-static int uvswitch_release(struct inode *inode, struct file *file);
-static int uvswitch_ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg);
-static ssize_t uvswitch_read(struct file *file, char *buffer, size_t count, loff_t *ppos);
-static unsigned int uvswitch_poll(struct file *file, poll_table *wait);
+static inline void  uvswitch_delete            (struct usb_uvswitch *dev);
+static int          uvswitch_probe             (struct usb_interface *interface, const struct usb_device_id *id);
+static void         uvswitch_disconnect        (struct usb_interface *interface);
+static int __init   usb_uvswitch_init          (void);
+static void __exit  usb_uvswitch_exit          (void);
 
-static inline void uvswitch_delete(struct usb_uvswitch *dev);
-static void * uvswitch_probe(struct usb_device *udev, unsigned int ifnum, const struct usb_device_id *id);
-static void uvswitch_disconnect(struct usb_device *udev, void *ptr);
-static int __init usb_uvswitch_init(void);
-static void __exit usb_uvswitch_exit(void);
+
+static struct file_operations uvswitch_dev_fops = {
+	.owner      =  THIS_MODULE,
+	.ioctl      =  uvswitch_ioctl,
+	.open       =  uvswitch_open,
+	.release    =  uvswitch_release,
+	.read       =  uvswitch_read,
+	.write      =  uvswitch_write,
+	.poll       =  uvswitch_poll,
+};
+
+static struct usb_class_driver uvswitch_class = {
+	.name       =  UVSWITCH_DEV_NAMEFORMAT,
+	.fops       =  &uvswitch_dev_fops,
+	.mode       =  S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP,
+	.minor_base =  UVSWITCH_MINOR_BASE,
+};
+
+static struct usb_driver uvswitch_driver = {
+	.name         =	"uvswitch",
+	.probe        = uvswitch_probe,
+	.disconnect   = uvswitch_disconnect,
+	.id_table     = uvswitch_table,
+};
 
 
 /******************************************************************************/
@@ -154,18 +163,6 @@ static int uvswitch_updateCalibration(struct usb_uvswitch *dev)
 	    dev->calibration.integration_packets,
 	    dev->calibration.threshold);
 
-	/* Begin our interrupt transfer polling the video detector ADC */
-	usb_unlink_urb(&dev->adc_urb);
-	FILL_INT_URB(&dev->adc_urb, dev->udev,
-		     usb_rcvintpipe(dev->udev, dev->endpoint->bEndpointAddress),
-		     dev->adc_buffer, UVSWITCH_CHANNELS,
-		     uvswitch_adc_irq, dev, dev->calibration.interval);
-	dbg("Submitting adc_urb, interval %d", dev->calibration.interval);
-	i = usb_submit_urb(&dev->adc_urb);
-	if (i) {
-		dbg("Error submitting URB: %d", i);
-	}
-
 	/* Send a packet to change the device's ADC settings */
 	return usb_control_msg(dev->udev, usb_sndctrlpipe(dev->udev, 0),
 			       UVSWITCH_CTRL_ADC_CYCLES, 0x40,
@@ -189,49 +186,54 @@ static int uvswitch_initCalibration(struct usb_uvswitch *dev)
 /* Callback for handling incoming data from the device's interrupt endpoint.
  * This receives an 8-byte packet about 8 times a second with A/D converter readings.
  */
-static void uvswitch_adc_irq(struct urb *urb)
+static void uvswitch_adc_irq(struct urb *urb, struct pt_regs *regs)
 {
 	int active_inputs[UVSWITCH_CHANNELS];
 	int i;
-
-	/* Callback for processing incoming interrupt transfers from the IR receiver */
 	struct usb_uvswitch *dev = (struct usb_uvswitch*)urb->context;
 
-	if (dev && urb->status == 0 && urb->actual_length == 8) {
-		/* Add this packet to the accumulator */
-		for (i=0; i<UVSWITCH_CHANNELS; i++) {
-			dev->adc_accumulator[i] += dev->adc_buffer[i];
-		}
-		dev->adc_samples++;
-
-		/* Do we have enough samples to get a final analog value yet? */
-		if (dev->adc_samples >= dev->calibration.integration_packets) {
-			/* Copy adc_accumulator to adc_accumulated and zero the accumulator */
-			memcpy(dev->adc_accumulated, dev->adc_accumulator, sizeof(dev->adc_accumulated));
-			memset(dev->adc_accumulator, 0, sizeof(dev->adc_accumulator));
-			dev->adc_samples = 0;
-
-			/* Compare the final accumulated values against our threshold
-			 * to detect active inputs.
-			 */
-			for (i=0; i<UVSWITCH_CHANNELS; i++)
-				active_inputs[i] = dev->adc_accumulated[i] > dev->calibration.threshold;
-
-			/* If our set of active inputs has changed, save it and wake up any sleeping processes */
-			if (memcmp(active_inputs, dev->active_inputs, sizeof(active_inputs)) != 0) {
-				memcpy(dev->active_inputs, active_inputs, sizeof(active_inputs));
-
-				dbg("input status changed: %d %d %d %d %d %d %d %d",
-				    dev->active_inputs[0], dev->active_inputs[1], dev->active_inputs[2], dev->active_inputs[3],
-				    dev->active_inputs[4], dev->active_inputs[5], dev->active_inputs[6], dev->active_inputs[7]);
-
-				dev->adc_change_id++;
-				wake_up_interruptible(&dev->adc_wait);
-			}
-		}
+	switch (urb->status) {
+	case -ECONNRESET:
+	case -ENOENT:
+	case -ESHUTDOWN:
+		return;
 	}
-	else {
+
+	if (urb->status != 0 || urb->actual_length != UVSWITCH_CHANNELS) {
 		dbg("Bad URB status/size: status=%d, length=%d", urb->status, urb->actual_length);
+		return;
+	}
+
+	/* Add this packet to the accumulator */
+	for (i=0; i<UVSWITCH_CHANNELS; i++) {
+		dev->adc_accumulator[i] += dev->adc_buffer[i];
+	}
+	dev->adc_samples++;
+
+	/* Do we have enough samples to get a final analog value yet? */
+	if (dev->adc_samples >= dev->calibration.integration_packets) {
+		/* Copy adc_accumulator to adc_accumulated and zero the accumulator */
+		memcpy(dev->adc_accumulated, dev->adc_accumulator, sizeof(dev->adc_accumulated));
+		memset(dev->adc_accumulator, 0, sizeof(dev->adc_accumulator));
+		dev->adc_samples = 0;
+
+		/* Compare the final accumulated values against our threshold
+		 * to detect active inputs.
+		 */
+		for (i=0; i<UVSWITCH_CHANNELS; i++)
+			active_inputs[i] = dev->adc_accumulated[i] > dev->calibration.threshold;
+
+		/* If our set of active inputs has changed, save it and wake up any sleeping processes */
+		if (memcmp(active_inputs, dev->active_inputs, sizeof(active_inputs)) != 0) {
+			memcpy(dev->active_inputs, active_inputs, sizeof(active_inputs));
+
+			dbg("input status changed: %d %d %d %d %d %d %d %d",
+			    dev->active_inputs[0], dev->active_inputs[1], dev->active_inputs[2], dev->active_inputs[3],
+			    dev->active_inputs[4], dev->active_inputs[5], dev->active_inputs[6], dev->active_inputs[7]);
+
+			dev->adc_change_id++;
+			wake_up_interruptible(&dev->adc_wait);
+		}
 	}
 }
 
@@ -243,28 +245,32 @@ static int uvswitch_open(struct inode *inode, struct file *file)
 {
 	struct usb_uvswitch *dev = NULL;
 	struct uvswitch_fd_private *prv = NULL;
+	struct usb_interface *interface;
 	int subminor;
 	int retval = 0;
 
-	subminor = MINOR(inode->i_rdev) - UVSWITCH_MINOR_BASE;
-	if ((subminor < 0) ||
-	   (subminor >= MAX_DEVICES)) {
-		return -ENODEV;
+	subminor = iminor(inode);
+
+	/* Prevent disconnects */
+	down(&disconnect_sem);
+
+
+	interface = usb_find_interface (&uvswitch_driver, subminor);
+	if (!interface) {
+		err ("%s - error, can't find device for minor %d",
+		     __FUNCTION__, subminor);
+		retval = -ENODEV;
+		goto exit_no_device;
 	}
 
-	/* lock our minor table and get our local data for this minor */
-	down(&minor_table_mutex);
-	dev = minor_table[subminor];
-	if (dev == NULL) {
-		up(&minor_table_mutex);
-		return -ENODEV;
+	dev = usb_get_intfdata(interface);
+	if (!dev) {
+		retval = -ENODEV;
+		goto exit_no_device;
 	}
 
 	/* lock this device */
-	down(&dev->sem);
-
-	/* unlock the minor table */
-	up(&minor_table_mutex);
+	down (&dev->sem);
 
 	/* increment our usage count for the driver */
 	++dev->open_count;
@@ -282,11 +288,12 @@ static int uvswitch_open(struct inode *inode, struct file *file)
 	prv->dev = dev;
 	file->private_data = prv;
 
- exit:
-
+exit:
 	/* unlock this device */
-	up(&dev->sem);
+	up (&dev->sem);
 
+exit_no_device:
+	up (&disconnect_sem);
 	return retval;
 }
 
@@ -303,9 +310,6 @@ static int uvswitch_release(struct inode *inode, struct file *file)
 	}
 	dev = prv->dev;
 
-	/* lock our minor table */
-	down(&minor_table_mutex);
-
 	/* lock our device */
 	down(&dev->sem);
 
@@ -319,7 +323,6 @@ static int uvswitch_release(struct inode *inode, struct file *file)
 		/* the device was unplugged before the file was released */
 		up(&dev->sem);
 		uvswitch_delete(dev);
-		up(&minor_table_mutex);
 		return 0;
 	}
 
@@ -335,8 +338,6 @@ static int uvswitch_release(struct inode *inode, struct file *file)
 
 exit_not_opened:
 	up(&dev->sem);
-	up(&minor_table_mutex);
-
 	return retval;
 }
 
@@ -610,82 +611,81 @@ static unsigned int uvswitch_poll(struct file *file, poll_table *wait)
 }
 
 
-static struct file_operations uvswitch_dev_fops = {
-	owner:		THIS_MODULE,        /* This automates updating the module's use count */
-
-	ioctl:		uvswitch_ioctl,
-	open:		uvswitch_open,
-	release:	uvswitch_release,
-	read:		uvswitch_read,
-	write:		uvswitch_write,
-	poll:		uvswitch_poll,
-};
-
-
 /******************************************************************************/
 /************************************************** USB Housekeeping **********/
 /******************************************************************************/
 
 static inline void uvswitch_delete(struct usb_uvswitch *dev)
 {
-	minor_table[dev->minor] = NULL;
+	if (dev->adc_urb) {
+		usb_unlink_urb(dev->adc_urb);
+		usb_free_urb(dev->adc_urb);
+	}
+	if (dev->adc_buffer) {
+		usb_buffer_free(dev->udev, UVSWITCH_CHANNELS, dev->adc_buffer, dev->adc_dma);
+	}
 	kfree(dev);
 }
 
-static struct usb_driver uvswitch_driver = {
-	name:		"uvswitch",
-	probe:		uvswitch_probe,
-	disconnect:	uvswitch_disconnect,
-	fops:		&uvswitch_dev_fops,
-	minor:		UVSWITCH_MINOR_BASE,
-	id_table:	uvswitch_table,
-};
-
-static void * uvswitch_probe(struct usb_device *udev, unsigned int ifnum, const struct usb_device_id *id)
+static int uvswitch_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
+	struct usb_device *udev = interface_to_usbdev(interface);
 	struct usb_uvswitch *dev = NULL;
-	struct usb_interface *interface;
-	int minor;
-	int buffer_size;
-	int i;
-	char name[10];
-	struct usb_interface_descriptor *iface_desc;
+	struct usb_host_interface *host_interface = &interface->altsetting[interface->act_altsetting];
+	int retval;
 
 	/* See if the device offered us matches what we can accept */
 	if ((udev->descriptor.idVendor != UVSWITCH_VENDOR_ID) ||
 	   (udev->descriptor.idProduct != UVSWITCH_PRODUCT_ID)) {
-		return NULL;
-	}
-
-	/* select a "subminor" number(part of a minor number) */
-	down(&minor_table_mutex);
-	for(minor = 0; minor < MAX_DEVICES; ++minor) {
-		if (minor_table[minor] == NULL)
-			break;
-	}
-	if (minor >= MAX_DEVICES) {
-		info("Too many devices plugged in, can not handle this device.");
-		goto exit;
+		return -ENODEV;
 	}
 
 	/* allocate memory for our device state and intialize it */
 	dev = kmalloc(sizeof(struct usb_uvswitch), GFP_KERNEL);
 	if (dev == NULL) {
 		err("Out of memory");
-		goto exit;
+		retval = -ENOMEM;
+		goto error;
 	}
 	memset(dev, 0x00, sizeof(*dev));
-	minor_table[minor] = dev;
-
-	interface = &udev->actconfig->interface[ifnum];
-	iface_desc = &interface->altsetting[interface->act_altsetting];
-	dev->endpoint = &iface_desc->endpoint[0];
+	init_waitqueue_head(&dev->adc_wait);
 
 	init_MUTEX(&dev->sem);
 	dev->udev = udev;
 	dev->interface = interface;
-	dev->minor = minor;
-	init_waitqueue_head(&dev->adc_wait);
+	dev->endpoint = &interface->altsetting[interface->act_altsetting].endpoint[0].desc;
+
+	usb_set_intfdata(interface, dev);
+	retval = usb_register_dev(interface, &uvswitch_class);
+	if (retval) {
+		/* something prevented us from registering this driver */
+		err ("Not able to get a minor for this device.");
+		goto error;
+	}
+
+	/* Allocate some DMA-friendly memory and a URB used for our interrupt
+	 * transfers carrying actual sensor readings back to us.
+	 */
+	dev->adc_buffer = usb_buffer_alloc(udev, UVSWITCH_CHANNELS, SLAB_ATOMIC, &dev->adc_dma);
+	if (!dev->adc_buffer) {
+		retval = -ENOMEM;
+		goto error;
+	}
+	dev->adc_urb = usb_alloc_urb(0, GFP_KERNEL);
+	if (!dev->adc_urb) {
+		retval = -ENODEV;
+		goto error;
+	}
+
+	/* Begin our interrupt transfer polling the video detector ADC */
+	usb_fill_int_urb(dev->adc_urb, dev->udev,
+			 usb_rcvintpipe(dev->udev, dev->endpoint->bEndpointAddress),
+			 dev->adc_buffer, UVSWITCH_CHANNELS,
+			 uvswitch_adc_irq, dev, dev->calibration.interval);
+
+	dbg("Submitting adc_urb, interval %d", dev->calibration.interval);
+	if (usb_submit_urb(dev->adc_urb, GFP_KERNEL))
+		dbg("Error submitting URB: %d", i);
 
 	/* Put all the switches into a known state (we initialized them
 	 * above in the Big Giant Memset)
@@ -695,41 +695,30 @@ static void * uvswitch_probe(struct usb_device *udev, unsigned int ifnum, const 
 	/* Set up default ADC calibration */
 	uvswitch_initCalibration(dev);
 
-	/* Initialize the devfs node for this device and register it */
-	sprintf(name, UVSWITCH_DEV_NAMEFORMAT, dev->minor);
-	dev->devfs = devfs_register(usb_devfs_handle, name,
-				     DEVFS_FL_DEFAULT, USB_MAJOR,
-				    UVSWITCH_MINOR_BASE + dev->minor,
-				     S_IFCHR | S_IRUSR | S_IWUSR |
-				     S_IRGRP | S_IWGRP | S_IROTH,
-				     &uvswitch_dev_fops, NULL);
-	info("uvswitch device now attached to %s", name);
-
-	goto exit;
+	info("uvswitch device now attached");
+	return 0;
 
 error:
+	usb_set_intfdata(interface, NULL);
 	uvswitch_delete(dev);
-	dev = NULL;
-
-exit:
-	up(&minor_table_mutex);
-	return dev;
+	return retval;
 }
 
-static void uvswitch_disconnect(struct usb_device *udev, void *ptr)
+static void uvswitch_disconnect(struct usb_interface *interface)
 {
 	struct usb_uvswitch *dev;
 	int minor;
 
-	dev =(struct usb_uvswitch *)ptr;
+	/* prevent races with open() */
+	down (&disconnect_sem);
 
-	down(&minor_table_mutex);
+	dev = usb_get_intfdata (interface);
+	usb_set_intfdata (interface, NULL);
+
 	down(&dev->sem);
 
 	minor = dev->minor;
-
-	/* remove our devfs node */
-	devfs_unregister(dev->devfs);
+	usb_deregister_dev(interface, &uvswitch_class);
 
 	/* if the device is not opened, then we clean up right now */
 	if (!dev->open_count) {
@@ -740,11 +729,8 @@ static void uvswitch_disconnect(struct usb_device *udev, void *ptr)
 		up(&dev->sem);
 	}
 
-	dbg("disconnect- unlinking URBs");
-	usb_unlink_urb(&dev->adc_urb);
-
+	up(&disconnect_sem);
 	info("uvswitch #%d now disconnected", minor);
-	up(&minor_table_mutex);
 }
 
 static int __init usb_uvswitch_init(void)
@@ -756,11 +742,11 @@ static int __init usb_uvswitch_init(void)
 	if (result < 0) {
 		err("usb_register failed for the "__FILE__" driver. Error number %d",
 		    result);
-		return -1;
+	} else {
+		info(DRIVER_DESC " " DRIVER_VERSION " - " DRIVER_AUTHOR);
 	}
 
-	info(DRIVER_DESC " " DRIVER_VERSION " - " DRIVER_AUTHOR);
-	return 0;
+	return result;
 }
 
 static void __exit usb_uvswitch_exit(void)
