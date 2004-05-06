c6d7bb8e6fd844401d01274495b2a3fed191dc9b
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-05-06 11:09:42 +0000

    Quit on end-of-file

--- trunk/rasterwand/python/scroller.py
+++ trunk/rasterwand/python/scroller.py
@@ -10,6 +10,8 @@ dev.params.display_width = 0xFFFF * 0.6
 dev.params.duty_cycle = 0xFFFF * 0.2
 
 while 1:
-    line = sys.stdin.readline().strip()
-    rasterwand.TextScroller(dev, line, width=40).scroll(start=(1, 0), end=(-1, 0), speed=1)
+    line = sys.stdin.readline()
+    if not line:
+        break
+    rasterwand.TextScroller(dev, line.strip(), width=40).scroll(start=(1, 0), end=(-1, 0), speed=1)
 
