4b71c16a30d727c3d64ee4dacaf83ad355603d14
Micah Elizabeth Scott <micah@scanlime.org>
2004-05-08 11:41:17 +0000

    Add a small tool for changing the driver's power mode

--- rasterwand/python/rasterwand.py
+++ rasterwand/python/rasterwand.py
@@ -1,5 +1,34 @@
-#!/usr/bin/env python
+""" rasterwand
+
+Pure Python interface to the Raster Wand kernel module. This includes
+simple text and animation support using PIL.
+"""
+#
+# Raster Wand
+# Copyright (C) 2004 Micah Dowty <micah@navi.cx>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 import struct, os, fcntl, time
+
+
+powerModes = {
+    'auto': 0,
+    'off': 1,
+    'on': 2,
+    }
 
 
 class Params:
