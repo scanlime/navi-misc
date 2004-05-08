2b8932d8325266413a5f663816ac33925e603544
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-05-08 11:30:53 +0000

    Specify a protocol

---
+++ trunk/laser/include/laser_protocol.h
@@ -0,0 +1,52 @@
+;//################################################################################
+;//
+;// laser_protocol.h - Definitions describing the host to device protocol used by the
+;//                    USB Laser Projector. This file is both valid C and assembly source, so it can be
+;//                    shared by the firmware and userspace code.
+;//
+;// This file is part of the Raster Wand project.
+;// Micah Dowty <micah@navi.cx>
+;//
+;//###############################################################################
+
+#ifndef __LASER_PROTOCOL_H
+#define __LASER_PROTOCOL_H
+
+
+;//************************************************** Idenfification
+
+;// The protocol version, stored in binary coded decimal.
+;// This is available from the device in the bcdDevice field
+;// of its DEVICE descriptor.
+#define LASER_PROTOCOL_VERSION  0x0100
+
+;// Device vendor and product IDs.
+#define LASER_VENDOR_ID   0xE461
+#define LASER_PRODUCT_ID  0x0006
+
+
+;//************************************************** Control Requests
+
+;// Queue up an event, mapped as follows:
+;//           wValue low: X axis, high byte
+;//          wValue high: Y axis, high byte
+;//           wIndex low: duration
+;//   wIndex high, bit 7: laser enable
+;//   wIndex high, bit 6: tag, bit 2
+;//   wIndex high, bit 5: tag, bit 1
+;//   wIndex high, bit 4: tag, bit 0
+;//   wIndex high, bit 3: Y axis, bit 1
+;//   wIndex high, bit 2: Y axis, bit 0
+;//   wIndex high, bit 1: X axis, bit 1
+;//   wIndex high, bit 0: X axis, bit 0
+;//
+;// Event tags:
+;//   0: Begin loop
+;//   1: End loop, after this event it will jump to the beginning
+;//
+#define RWAND_CTRL_ENQUEUE      0x01
+
+#endif
+
+;//### The End ###
+
