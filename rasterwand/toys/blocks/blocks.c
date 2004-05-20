af2f4ac11b0f665793083a801d708b417ff80514
brandon <gonkulator@gmail.com>
2004-05-20 04:16:51 +0000

    Yay, fixed.  Eat poo jigga!

--- rasterwand/toys/blocks/blocks.c
+++ rasterwand/toys/blocks/blocks.c
@@ -16,38 +16,97 @@
 int main(int argc, char **argv)
 {
   int wand;
-  char blocks[8][80];
+  char blocks[10][80];
   int i, j;
 
   int bs;
+  int top, bottom;
 
   wand = open("/dev/usb/rwand0",O_WRONLY);
  
-  for(i=0;i<8;i++)
+  for(i=0;i<9;i++)
   {
     for(j=0;j<80;j++)
     {
       blocks[i][j] = 0;
-      if((i+j)%2)
+      switch(i)
       {
-	blocks[i][j] ^= ONE;
-	blocks[i][j] ^= THREE;
-	blocks[i][j] ^= FIVE;
-	blocks[i][j] ^= SEVEN;
-      }
-      else
-      {
-	blocks[i][j] ^= TWO;
-	blocks[i][j] ^= FOUR;
-	blocks[i][j] ^= SIX;
-	blocks[i][j]^= EIGHT;
+      case 0: 
+	blocks[i][j] = 0xFF;
+	break;
+      case 1:
+     	if((i+j)%2)
+	{
+	    blocks[i][j] ^= ONE;
+	    blocks[i][j] ^= THREE;
+	    blocks[i][j] ^= FIVE;
+	    blocks[i][j] ^= SEVEN;
+	}
+	else
+	{
+	  blocks[i][j] ^= TWO;
+	  blocks[i][j] ^= FOUR;
+	  blocks[i][j] ^= SIX;
+	  blocks[i][j]^= EIGHT;
+	}
+	break;
+      case 2:
+	bs = i;
+	top = 0 ^ ONE ^ TWO ^ FIVE ^  SIX;
+	bottom = 0 ^ THREE ^ FOUR ^ SEVEN ^ EIGHT;
+	if((j/bs)%2 == 0) blocks[i][j] = top;
+	else blocks[i][j] = bottom;
+	break;
+      case 3:
+	bs = i;
+	top = 0^ ONE ^ TWO ^ THREE ^ SEVEN ^ EIGHT;
+	bottom = 0 ^ FOUR ^ FIVE ^ SIX;
+	if((j/bs)%2 == 0) blocks[i][j] = top;
+	else blocks[i][j] = bottom;
+	break;
+      case 4:
+	bs = i;
+	top = 0 ^ ONE ^ TWO ^ THREE ^ FOUR;
+	bottom = 0 ^ FIVE ^ SIX ^ SEVEN ^ EIGHT;
+	if((j/bs)%2 == 0) blocks[i][j] = top;
+	else blocks[i][j] = bottom;
+	break;
+      case 5:
+	bs = i;
+	top = 0 ^ ONE ^ TWO ^ THREE ^ FOUR ^ FIVE;
+	bottom = 0 ^ SIX ^ SEVEN ^ EIGHT;
+	if((j/bs)%2 == 0) blocks[i][j] = top;
+	else blocks[i][j] = bottom;
+	break;
+      case 6:
+	bs = i;
+	top = 0 ^ ONE ^ TWO ^ THREE ^ FOUR ^ FIVE ^ SIX;
+	bottom = 0 ^ SEVEN ^ EIGHT;
+	if((j/bs)%2 == 0) blocks[i][j] = top;
+	else blocks[i][j] = bottom;
+	break;
+      case 7:
+	bs = i;
+	top = 0 ^ ONE ^ TWO ^ THREE ^ FOUR ^ FIVE ^ SIX ^ SEVEN;
+	bottom = 0 ^ EIGHT;
+	if((j/bs)%2 == 0) blocks[i][j] = top;
+	else blocks[i][j] = bottom;
+	break;
+      case 8:
+	bs = 8;
+	top = 0xFF;
+	bottom = 0;
+	if( (j/bs) % 2 == 0) blocks[8][j] = top;
+	else blocks[8][j] = bottom;
+	break;
       }
     }
   }
   
   for(i=0;;i++)
   {
-    write(wand,blocks[i%8],80);
+    write(wand,blocks[i%9],80);
+    sleep(1);
   }
   close(wand);
 }
