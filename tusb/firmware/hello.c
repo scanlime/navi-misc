2e2c3a6687cae1036bb83456469bb66d5bd00f68
Micah Elizabeth Scott <micah@scanlime.org>
2004-05-22 09:50:13 +0000

    Stop trying to look like an interrupt-driven program when we aren't yet. This just looks at the USBSTA register now, ignoring VECINT. It's now receiving setup packets and dumping them to the serial console, yay.

--- tusb/firmware/hello.c
+++ tusb/firmware/hello.c
@@ -5,6 +5,8 @@
 #define LED          P3_0
 #define RS232_POWER  P3_1
 
+xdata at 0xFF00 unsigned char setupPacket[8];
+
 void watchdog_reset() {
   WDCSR |= WDT;
 }
@@ -13,18 +15,6 @@ void delay(unsigned int i) {
   unsigned int j;
   for (j=0;j<i;j++)
     watchdog_reset();
-}
-
-void usb_init() {
-  FUNADR = 0;       /* We haven't been assigned an address yet */
-
-  USBCTL &= ~CONT;  /* Disconnect from USB then reconnect */
-  delay(60000);
-  USBCTL |= CONT;
-
-  /* Set up endpoint 0 */
-  IEPCNFG_0 = STALL | USBIE | UBME;
-  OEPCNFG_0 = STALL | USBIE | UBME;
 }
 
 void uart_init() {
@@ -50,6 +40,54 @@ void putchar(char c) {
     putchar('\r');
 }
 
+void usb_init() {
+  FUNADR = 0;       /* We haven't been assigned an address yet */
+
+  /* Set up endpoint 0 */
+  IEPCNFG_0 = STALL | USBIE | UBME;
+  OEPCNFG_0 = STALL | USBIE | UBME;
+
+  USBCTL &= ~CONT;  /* Disconnect from USB */
+  USBSTA = 0xFF;    /* Clear USB status bits */
+  delay(60000);     /* Wait a bit and reconnect */
+  USBCTL |= CONT;
+}
+
+void usb_loop() {
+  unsigned char status;
+  int i;
+
+  while (1) {
+    status = USBSTA;     /* Read all status bits and clear them all */
+    USBSTA = 0xFF;
+
+    if (status & RSTR) {
+      puts("Function reset request");
+    }
+
+    if (status & SUSR) {
+      puts("Function suspend request");
+    }
+
+    if (status & RESR) {
+      puts("Function resume request");
+    }
+
+    if (status & SETUP) {
+      puts("SETUP received");
+      for (i=0; i<8; i++)
+	printf(" %02X", setupPacket[i]);
+      printf("\n");
+    }
+
+    if (status & STPOW) {
+      puts("SETUP overwrite");
+    }
+
+    delay(20000);
+  }
+}
+
 void main() {
   EA = 0;           /* Global interrupt disable */
 
@@ -59,9 +97,6 @@ void main() {
   usb_init();
   puts("USB initialized");
 
-  while (1) {
-    printf("VECINT = 0x%02X\n", VECINT);
-    delay(60000);
-  }
+  usb_loop();
 }
 
