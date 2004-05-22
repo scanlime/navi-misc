e5185b588abb48504aaf12ea3186efeeadda3669
Evan Sheehan <darthmall@users.noreply.github.com>
2004-05-22 16:44:10 +0000

    weeble

--- palantir/setup.py
+++ palantir/setup.py
@@ -7,17 +7,20 @@ Installs Palantir.
 from distutils.core import setup
 
 setup(name = 'palantir',
-      version = '0.1',
+      version = '0.1-pre',
       description = 'RPG IRC client',
       author = 'Evan Sheehan',
       author_email = 'evan@navi.cx',
       url = 'http://palantir.navi.cx',
+      license = "LGPL",
       packages = ['Common', 'GtkPalantir'],
+
       scripts = ['palantir-client.py', 'palantir-sheets'],
-      data_files = [('palantir/data',['data/palantirMain.glade']),
-                    ('palantir/pixmaps',['data/pixmaps/dm.png',
-		                             'data/pixmaps/stock_exit.png',
-                                 'data/pixmaps/palantir-icon.png',
-		                             'data/pixmaps/palantir-logo-small.png']),
-		    ('palantir/sheets/layouts',['data/charactersheets/sheets/sheet.xml'])]
+
+      data_files = [Data_Files(
+        preserve_path = 1,
+        base_dir      = 'install_lib',
+        copy_to       = 'Common/data',
+        strip_dirs    = 2,
+        template      = ['graft data',])]
       )
