e0835f4bb00d3e32535a8ac902fcfe86aa74c68e
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-05-23 12:41:28 +0000

    Add a few toys for the I2C bus- a way to read temperatures from a TC74 thermometer, and output values to a DAC8571 D/A converter

--- trunk/tusb/firmware/hello.c
+++ trunk/tusb/firmware/hello.c
@@ -10,46 +10,81 @@
 volatile xdata at 0xFD00 unsigned char ep1_out_x[64];
 unsigned char c;
 
-void io_tester() {
-  while (1) {
-    P3_0 = 1;
-    P3_3 = 1;
-    P3_4 = 0;
-    delay(4000);
-    I2CADR = 0x80;
-    I2CDAO = 0x00;
+void i2c_write_byte(unsigned char b, bit is_last) {
+  I2CADR &= ~1;       /* Write */
 
-    P3_0 = 1;
-    P3_3 = 0;
-    P3_4 = 1;
-    delay(4000);
-    I2CADR = 0x40;
-    I2CDAO = 0x00;
+  if (is_last)
+    I2CSTA |= SWR;    /* Stop condition */
+  else
+    I2CSTA &= ~SWR;   /* No stop condition */
 
-    P3_0 = 0;
-    P3_3 = 1;
-    P3_4 = 1;
-    delay(4000);
-    I2CADR = 0x20;
-    I2CDAO = 0x00;
-
-    P3_0 = 1;
-    P3_3 = 0;
-    P3_4 = 1;
-    delay(4000);
-    I2CADR = 0x10;
-    I2CDAO = 0x00;
+  I2CDAO = b;
+  while ((I2CSTA & TXE) == 0) {
+    /* Waiting for the transmission.. check for errors */
+    if (I2CSTA & ERR) {
+      I2CSTA |= ERR;
+      printf("I2C Error\n");
+      return;
+    }
   }
 }
 
-void main() {
-  //io_tester();
+unsigned char i2c_read_byte(bit is_last) {
+  I2CADR |= 1;        /* Read */
 
+  if (is_last)
+    I2CSTA |= SRD;    /* Stop condition */
+  else
+    I2CSTA &= ~SRD;   /* No stop condition */
+  I2CDAO = 0;         /* Dummy byte starts the read transfer */
+
+  while ((I2CSTA & RXF) == 0) {
+    /* Waiting for the transmission.. check for errors */
+    if (I2CSTA & ERR) {
+      I2CSTA |= ERR;
+      printf("I2C Error\n");
+      return 0;
+    }
+  }
+  return I2CDAI;
+}
+
+signed char therm_read(unsigned char addr) {
+  I2CADR = ((0x48 | addr) << 1);    /* Write to the therm */
+  I2CSTA = ERR;                     /* Clear error flag, 100khz mode, no stop condition */
+  i2c_write_byte(0x01, 0);          /* Write to CONFIG */
+  i2c_write_byte(0x00, 1);
+  i2c_write_byte(0x00, 1);          /* Start reading temperatures */
+  return i2c_read_byte(1);
+}
+
+void dac_write(unsigned char addr, unsigned short value) {
+  I2CADR = ((0x4C | (addr<<1)) << 1);  /* Write to the DAC */
+  I2CSTA = ERR | S1_4;                 /* Clear error flag, 400khz mode, no stop condition */
+  i2c_write_byte(0x10, 0);             /* Control byte, load DAC with data */
+  i2c_write_byte(value >> 8, 0);
+  i2c_write_byte(value & 0xFF, 1);
+}
+
+void main() {
   uart_init();
   puts("\n---- Startup ----");
 
   usb_init();
   puts("USB initialized");
+
+  while (1) {
+    // printf("Temperatures  %d\n", therm_read(5));
+    // delay(10000);
+    static int i;
+    dac_write(0, i);
+    i+=400;
+    watchdog_reset();
+  }
+
+
+  /* Power LED on */
+  LED = 0;
 
   /* Set up the first EP1 OUT transfer */
   usb_dma_unstall(EDB_OEP1);
