688e2ee6e9a448f57481db7f813e7b50bcc3697b
Micah Elizabeth Scott <micah@scanlime.org>
2004-05-23 04:40:08 +0000

    Add a few more library functions to make setting up and polling USB DMA operations easier

--- tusb/firmware/hello.c
+++ tusb/firmware/hello.c
@@ -1,4 +1,3 @@
-#include <tusb.h>
 #include <stdio.h>
 #include <string.h>
 #include "usb_driver.h"
@@ -12,30 +11,24 @@ volatile xdata at 0xFD00 unsigned char ep1_out_x[64];
 unsigned char c;
 
 void main() {
-  EA = 0;     /* Global interrupt disable */
-
   uart_init();
   puts("\n---- Startup ----");
 
   usb_init();
   puts("USB initialized");
 
-  /* Set up extra endpoints */
-  OEPCNF_1 = UBME;
-  OEPBBAX_1 = (0xFD00 >> 3) & 0xFF;
-  OEPSIZXY_1 = sizeof(ep1_out_x);
-  OEPBCTX_1 = 0;
+  /* Set up the first EP1 OUT transfer */
+  usb_dma_read_setup(1, ep1_out_x, sizeof(ep1_out_x));
 
   while (1) {
     watchdog_reset();
     usb_poll();
 
-    c = OEPBCTX_1;
+    c = usb_dma_read_status(1);
     if (c) {
-      c &= ~0x80;
       printf("Received %d bytes on EP1 OUT: %02X %02X %02X %02x...\n", c,
 	     ep1_out_x[0], ep1_out_x[1], ep1_out_x[2], ep1_out_x[3]);
-      OEPBCTX_1 = 0;
+      usb_dma_read_setup(1, ep1_out_x, sizeof(ep1_out_x));
     }
   }
 }
@@ -48,22 +41,6 @@ void usb_handle_vendor_request() {
   printf("Vendor request %d\n", usb_setup_buffer.bRequest);
 
   switch (usb_setup_buffer.bRequest) {
-
-    /* Experimental EP0 OUT */
-  case 42:
-    /* ACK the setup packet */
-    OEPBCNT_0 = 0;
-
-    /* Leave setup */
-    USBCTL &= ~SIR;
-    USBSTA = SETUP;
-
-    /* Wait for our OUT data to arrive */
-    printf("Waiting for OUT data\n");
-    while ((OEPBCNT_0 & 0x80) == 0)
-      watchdog_reset();
-    printf("Received data: '%s'\n", usb_ep0out_buffer);
-    break;
 
     /* EP0 IN test */
   case 123:
--- tusb/firmware/util.h
+++ tusb/firmware/util.h
@@ -1,6 +1,13 @@
+#ifndef __UTIL_H
+#define __UTIL_H
+
+#include <tusb.h>
+
 /* GPIO allocation on the Keyspan USA-19HS board */
 #define LED          P3_0
 #define RS232_POWER  P3_1
 
 void watchdog_reset();
 void delay(unsigned int i);
+
+#endif /* __UTIL_H */
