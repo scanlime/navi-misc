6585c67d08ea047a72a1979f28093a7e87c44de8
jeffm <jeffm@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-05-28 23:17:23 +0000

    move message type defines into commn messages.h

--- trunk/firestarter/client/game/test/src/testGame.cpp
+++ trunk/firestarter/client/game/test/src/testGame.cpp
@@ -19,16 +19,7 @@
 // the drawables for this game
 #include "worldDrawables.h"
 #include "playerDrawables.h"
-
-// messages
-#define	_MESSAGE_SERVER_INFO 0x5349	// SI 
-#define	_MESSAGE_CLIENT_INFO 0x4349	// CI 
-#define	_MESSAGE_USER_PART 0x5550		// UP
-#define	_MESSAGE_USER_ADD 0x5541		// UA
-#define	_MESSAGE_KICK 0x4b4b				// KK
-#define _MESSAGE_UPDATE 0x5544			//UD
-#define _MESSAGE_ACKNOWLEDGE 0x414b	//AK
-#define _MESSAGE_SPAWN 0x5350				//SP
+#include "messages.h"
 
 void CTestGame::registerFactory (const char* name, CBaseDrawableFactory* factory)
 {
