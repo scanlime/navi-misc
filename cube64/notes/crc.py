13a7210b14ad75b209e382f0a38c4d7f557c6b34
Micah Elizabeth Scott <micah@scanlime.org>
2004-06-10 04:41:14 +0000

    Yay, a working small-table version of the reverse-engineered N64 CRC algorithm

--- cube64/notes/crc.py
+++ cube64/notes/crc.py
@@ -20,23 +20,11 @@
 #    The CRC is initialized to 0xFF, then XOR'ed by the table entry for each
 #    bit set in the packet.
 #
+# 4. A few patterns were found in the 256-byte table that lets us reduce
+#    it to a 32-byte table.
+#
 
 import cPickle, sys
-
-
-def bin(i, width=8):
-    return ''.join([ "01"[i & (1<<(width-1-bit)) != 0]
-                     for bit in xrange(width) ])
-
-
-def crc(packet, table):
-    """Table-driven reimplementation of Nintendo's CRC"""
-    crc = 0xFF
-    for byte in xrange(32):
-        for bit in xrange(8):
-            if packet[byte] & 1<<(7-bit):
-                crc ^= table[byte*8 + bit]
-    return crc
 
 
 def extractTable(vectors):
@@ -59,6 +47,8 @@ def verifyAlgorithm(vectors, f, *args, **kwargs):
        """
     print "Testing algorithm"
     passed = 0
+    showFailures = 1
+
     vectorlist = vectors.keys()
     vectorlist.sort()
     for vector in vectorlist:
@@ -66,15 +56,81 @@ def verifyAlgorithm(vectors, f, *args, **kwargs):
         expected = vectors[vector]
         if result == expected:
             passed += 1
-        else:
+        elif showFailures:
             print "Failed: %s => expected %02X, got %02X" % (
                 " ".join(["%02X" % b for b in vector]), expected, result)
+            showFailures = 0
     print "%d/%d tests passed" % (passed, len(vectors))
+
+
+def bin(i, width=8):
+    return ''.join([ "01"[i & (1<<(width-1-bit)) != 0]
+                     for bit in xrange(width) ])
+
+
+def tableCompress(table):
+    """Given the 256-byte table, this returns a smaller 32-byte table
+       with what's left over after decoding a few patterns in the original
+       """
+    b = 0
+    prev = 0xCD
+    smallTable = []
+    for byte in xrange(32):
+        output = 0
+        for bit in xrange(8):
+            e = table[b]
+            b += 1
+
+            e = ((e<<bit) | (e>>(8-bit))) & 0xFF
+            c = e ^ prev
+            prev = e
+            c = ((c>>bit) | (c<<(8-bit))) & 0xFF
+            assert (c == 0x00) or (c == 0x42)
+
+            if c:
+                output |= 1<<(7-bit)
+        smallTable.append(output)
+    return smallTable
+
+
+def largeTableCRC(packet, table):
+    """Table-driven reimplementation of Nintendo's CRC using a 256-byte table"""
+    crc = 0xFF
+    for byte in xrange(32):
+        for bit in xrange(8):
+            if packet[byte] & 1<<(7-bit):
+                crc ^= table[byte*8 + bit]
+    return crc
+
+
+def smallTableCRC(packet, table):
+    """Table-driven reimplementation of Nintendo's CRC using a 32-byte table"""
+    crc = 0xFF
+    y = 0xCD
+    for byte in xrange(32):
+        for bit in xrange(8):
+            if table[byte] & 1<<(7-bit):
+                y ^= ((0x42 << bit) | (0x42 >> (8-bit))) & 0xFF
+            e = ((y >> bit) | (y << (8-bit))) & 0xFF
+
+            if packet[byte] & 1<<(7-bit):
+                crc ^= e
+
+    return crc
 
 
 if __name__ == "__main__":
     testVectors = cPickle.load(open("crc_test_vectors.p", "rb"))
+
+    print "Large-table CRC:"
     table = extractTable(testVectors)
-    verifyAlgorithm(testVectors, crc, table)
+    verifyAlgorithm(testVectors, largeTableCRC, table)
+
+    print "Small-table CRC:"
+    table = tableCompress(table)
+    print table
+    verifyAlgorithm(testVectors, smallTableCRC, table)
+
+
 
 ### The End ###
