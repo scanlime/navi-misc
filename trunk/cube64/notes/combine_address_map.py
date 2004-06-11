d6afb468cc26cd8d8a56c685af6316d5a6d9931d
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-06-11 06:42:52 +0000

    Yay, the address encoding works. The list of secrets still remaining in this hardware grows thin...

--- trunk/cube64/notes/combine_address_map.py
+++ trunk/cube64/notes/combine_address_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from crc import bin
+from crc import bin, cPickle
 
 # Create a mapping from valid addresses to the resulting start address on the bus.
 # We start by mapping each valid address to 0x0000...
@@ -20,8 +20,22 @@ for code, addr in addrmap.iteritems():
 # Print the results- address first, then list of codes, sorted by address
 addrs = codemap.keys()
 addrs.sort()
+vectors = {}
 for addr in addrs:
     # Sort the list of codes
     codes = codemap[addr]
     codes.sort()
-    print "0x%04X :  %s" % (addr, "  ".join(["0x%04X (%s)" % (code, bin(code, 16)) for code in codes]))
+    print "0x%04X :  %s" % (addr, "  ".join(["0x%04X (%s %s)" % (code, bin(code >> 5, 11), bin(code, 5)) for code in codes]))
+
+    # It turns out that each address has exactly two codes, because
+    # there's an extra address bit I haven't found on the bus connector yet.
+    # Generate test vectors using this knowledge.
+    vectors[addr] = codes[0]
+    vectors[addr | 0x8000] = codes[1]
+
+# Save the test vectors
+cPickle.dump(vectors, open("address_test_vectors.p", "wb"), -1)
+
+### The End ###
+
+
