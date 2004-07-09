e656b4a6db2b3c94653612110c55ca0e98428e3e
David Trowbridge <trowbrds@gmail.com>
2004-07-09 20:15:35 +0000

    make it compile cleanly on both gcc and icc

--- src/explorer-tools.c
+++ src/explorer-tools.c
@@ -190,6 +190,7 @@ static gboolean on_button_press(GtkWidget *widget, GdkEvent *event, gpointer use
 static gboolean on_button_release(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
   Explorer *self = EXPLORER(user_data);
   self->tool_active = FALSE;
+  return FALSE;
 }
 
 gboolean explorer_update_tools(Explorer *self) {
