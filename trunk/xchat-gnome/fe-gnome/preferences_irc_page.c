2c452f3584608d8aa2bc06f09bc9f05554359a2f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-07-11 09:24:50 +0000

    start some prefs changes - make nickname instant-apply. for some reason it's not recieving a notification from gconf, but that's less important

--- trunk/xchat-gnome/fe-gnome/preferences_irc_page.c
+++ trunk/xchat-gnome/fe-gnome/preferences_irc_page.c
@@ -19,52 +19,62 @@
  *
  */
 
+#include <gconf/gconf-client.h>
 #include "preferences_irc_page.h"
-#include "preferences.h"
 
-void initialize_preferences_irc_page() {
+static void gconf_nickname_changed (GConfClient *client, guint cnxn_id, const gchar* key, GConfValue* value, gboolean is_default, gpointer data);
+static void entry_nickname_changed (GtkEntry *entry, gpointer data);
+
+void initialize_preferences_irc_page()
+{
 	GtkWidget *widget;
 	GtkSizeGroup *group;
 	char *text;
+	GConfClient *client;
 
-	group = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
+	client = gconf_client_get_default ();
 
-	widget = glade_xml_get_widget(gui.xml, "nick name");
-	text = preferences_nickname();
-	gtk_entry_set_text(GTK_ENTRY(widget), text);
-	g_free(text);
-	gtk_size_group_add_widget(group, widget);
+	group = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
 
-	widget = glade_xml_get_widget(gui.xml, "real name");
-	text = preferences_realname();
-	gtk_entry_set_text(GTK_ENTRY(widget), text);
-	g_free(text);
-	gtk_size_group_add_widget(group, widget);
+	widget = glade_xml_get_widget (gui.xml, "nick name");
+	text = gconf_client_get_string (client, "/apps/xchat/irc/nickname", NULL);
+	gconf_client_notify_add (client, "/apps/xchat/irc/nickname", (GConfClientNotifyFunc) gconf_nickname_changed, NULL, NULL, NULL);
+	g_signal_connect (G_OBJECT (widget), "changed", G_CALLBACK (entry_nickname_changed), NULL);
+	gtk_entry_set_text (GTK_ENTRY (widget), text);
+	g_free (text);
+	gtk_size_group_add_widget (group, widget);
 
-	widget = glade_xml_get_widget(gui.xml, "quit message");
-	text = preferences_quitmsg();
-	gtk_entry_set_text(GTK_ENTRY(widget), text);
-	g_free(text);
-	gtk_size_group_add_widget(group, widget);
+	widget = glade_xml_get_widget (gui.xml, "real name");
+	text = gconf_client_get_string (client, "/apps/xchat/irc/realname", NULL);
+	gtk_entry_set_text (GTK_ENTRY (widget), text);
+	g_free (text);
+	gtk_size_group_add_widget (group, widget);
 
-	widget = glade_xml_get_widget(gui.xml, "part message");
-	text = preferences_partmsg();
-	gtk_entry_set_text(GTK_ENTRY(widget), text);
-	g_free(text);
-	gtk_size_group_add_widget(group, widget);
+	widget = glade_xml_get_widget (gui.xml, "quit message");
+	text = gconf_client_get_string (client, "/apps/xchat/irc/quitmsg", NULL);
+	gtk_entry_set_text (GTK_ENTRY (widget), text);
+	g_free (text);
+	gtk_size_group_add_widget (group, widget);
 
-	widget = glade_xml_get_widget(gui.xml, "away message");
-	text = preferences_awaymsg();
-	gtk_entry_set_text(GTK_ENTRY(widget), text);
-	g_free(text);
-	gtk_size_group_add_widget(group, widget);
+	widget = glade_xml_get_widget (gui.xml, "part message");
+	text = gconf_client_get_string (client, "/apps/xchat/irc/partmsg", NULL);
+	gtk_entry_set_text (GTK_ENTRY (widget), text);
+	g_free (text);
+	gtk_size_group_add_widget (group, widget);
 
-	widget = glade_xml_get_widget(gui.xml, "highlight list container");
-	gtk_size_group_add_widget(group, widget);
+	widget = glade_xml_get_widget (gui.xml, "away message");
+	text = preferences_awaymsg ();
+	text = gconf_client_get_string (client, "/apps/xchat/irc/awaymsg", NULL);
+	gtk_entry_set_text (GTK_ENTRY (widget), text);
+	g_free (text);
+	gtk_size_group_add_widget (group, widget);
+
+	widget = glade_xml_get_widget (gui.xml, "highlight list container");
+	gtk_size_group_add_widget (group, widget);
 
 	g_object_unref (group);
 
-	group = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
+	group = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
 
 	widget = glade_xml_get_widget (gui.xml, "hilight add");
 	gtk_size_group_add_widget (group, widget);
@@ -73,5 +83,33 @@ void initialize_preferences_irc_page() {
 	widget = glade_xml_get_widget (gui.xml, "font selection");
 	gtk_size_group_add_widget (group, widget);
 
-	g_object_unref(group);
+	g_object_unref (group);
+}
+
+static void
+gconf_nickname_changed (GConfClient *client, guint cnxn_id, const gchar* key, GConfValue* value, gboolean is_default, gpointer data)
+{
+	GtkWidget *widget;
+	gchar *text;
+
+	g_print ("hello!\n");
+
+	widget = glade_xml_get_widget (gui.xml, "nick name");
+	g_signal_handlers_block_by_func (widget, "changed", entry_nickname_changed);
+	text = gconf_client_get_string (client, "/apps/xchat/irc/nickname", NULL);
+	gtk_entry_set_text (GTK_ENTRY (widget), text);
+	g_signal_handlers_unblock_by_func (widget, "changed", entry_nickname_changed);
+	g_free (text);
+}
+
+static void
+entry_nickname_changed (GtkEntry *entry, gpointer data)
+{
+	GConfClient *client;
+	const gchar *text;
+
+	client = gconf_client_get_default ();
+	text = gtk_entry_get_text (entry);
+	if (text)
+		gconf_client_set_string (client, "/apps/xchat/irc/nickname", text, NULL);
 }
