fa55b2e9b6a67fd2ebe328bc4fabf87e1bdc8183
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-07-19 09:35:12 +0000

    fix some plugin system niggles, more syntax cleanup

--- trunk/xchat-gnome/fe-gnome/preferences_plugins_page.c
+++ trunk/xchat-gnome/fe-gnome/preferences_plugins_page.c
@@ -21,8 +21,6 @@
 
 #include "preferences_plugins_page.h"
 #include "../common/xchat.h"
-// we have to define PLUGIN_C for no particular reason at all to get the xchat_plugin
-// structure in all its glory. i dunno why, something to ask zed i suppose
 #define PLUGIN_C
 typedef struct session xchat_context;
 #include "../common/xchat-plugin.h"
@@ -30,49 +28,55 @@ typedef struct session xchat_context;
 
 extern GSList *plugin_list;
 
-void initialize_preferences_plugins_page() {
+void
+initialize_preferences_plugins_page ()
+{
 	GtkWidget *treeview;
 	GtkListStore *store;
 	GtkCellRenderer *text_renderer, *load_renderer;
 	GtkTreeViewColumn *text_column, *load_column;
 	GtkTreeSelection *select;
 
-	treeview = glade_xml_get_widget(gui.xml, "plugins list");
+	treeview = glade_xml_get_widget (gui.xml, "plugins list");
 
-	store = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_BOOLEAN);
-	gtk_tree_view_set_model(GTK_TREE_VIEW(treeview), GTK_TREE_MODEL(store));
+	store = gtk_list_store_new (4, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_BOOLEAN);
+	gtk_tree_view_set_model (GTK_TREE_VIEW (treeview), GTK_TREE_MODEL (store));
 
-	load_renderer = gtk_cell_renderer_toggle_new();
-	load_column = gtk_tree_view_column_new_with_attributes("Load", load_renderer, "active", 3, NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), load_column);
-	text_renderer = gtk_cell_renderer_text_new();
-	text_column = gtk_tree_view_column_new_with_attributes("Plugin", text_renderer, "text", 0, NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), text_column);
+	load_renderer = gtk_cell_renderer_toggle_new ();
+	load_column = gtk_tree_view_column_new_with_attributes ("Loaded", load_renderer, "active", 3, NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW (treeview), load_column);
+	text_renderer = gtk_cell_renderer_text_new ();
+	text_column = gtk_tree_view_column_new_with_attributes ("Plugin", text_renderer, "text", 0, NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW (treeview), text_column);
 
-	select = gtk_tree_view_get_selection(GTK_TREE_VIEW(treeview));
-	gtk_tree_selection_set_mode(select, GTK_SELECTION_SINGLE);
+	select = gtk_tree_view_get_selection (GTK_TREE_VIEW (treeview));
+	gtk_tree_selection_set_mode (select, GTK_SELECTION_SINGLE);
 
-	preferences_plugins_page_populate();
+	preferences_plugins_page_populate ();
 }
 
-void preferences_plugins_page_populate() {
+void
+preferences_plugins_page_populate()
+{
 	GtkWidget *treeview;
 	GtkTreeIter iter;
 	GtkListStore *store;
 	GSList *list;
 	xchat_plugin *plugin;
 
-	treeview = glade_xml_get_widget(gui.xml, "plugins list");
-	store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(treeview)));
+	treeview = glade_xml_get_widget (gui.xml, "plugins list");
+	store = GTK_LIST_STORE (gtk_tree_view_get_model (GTK_TREE_VIEW (treeview)));
 
-	gtk_list_store_clear(store);
+	gtk_list_store_clear (store);
 
 	list = plugin_list;
-	while(list) {
+	while (list)
+	{
 		plugin = list->data;
-		if(plugin->version[0] != 0) {
-			gtk_list_store_append(store, &iter);
-			gtk_list_store_set(store, &iter, 0, plugin->name, 1, plugin->version, 2, plugin->desc, -1);
+		if (plugin->version[0] != 0)
+		{
+			gtk_list_store_append (store, &iter);
+			gtk_list_store_set (store, &iter, 0, plugin->name, 1, plugin->version, 2, plugin->desc, -1);
 		}
 		list = list->next;
 	}
