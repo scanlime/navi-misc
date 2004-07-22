72ce9e5b059b1376f13a6f8e0408f75644fc23fe
David Trowbridge <trowbrds@gmail.com>
2004-07-22 07:10:31 +0000

    more cleanup

--- xchat-gnome/fe-gnome/textgui.h
+++ xchat-gnome/fe-gnome/textgui.h
@@ -26,13 +26,13 @@
 #ifndef XCHAT_GNOME_TEXTGUI_H
 #define XCHAT_GNOME_TEXTGUI_H
 
-void initialize_text_gui();
-void text_gui_add_text_buffer(struct session *sess);
-void text_gui_remove_text_buffer(struct session *sess);
-void text_gui_print(xtext_buffer *buf, unsigned char *text, gboolean indent);
-void set_nickname(struct server *serv, char *newnick);
-void set_gui_topic(struct session *sess, char *topic);
-void clear_buffer(struct session *sess);
+void initialize_text_gui ();
+void text_gui_add_text_buffer (struct session *sess);
+void text_gui_remove_text_buffer (struct session *sess);
+void text_gui_print (xtext_buffer *buf, unsigned char *text, gboolean indent);
+void set_nickname (struct server *serv, char *newnick);
+void set_gui_topic (struct session *sess, char *topic);
+void clear_buffer (struct session *sess);
 
 typedef struct {
 	xtext_buffer *buffer;
--- xchat-gnome/fe-gnome/userlist.c
+++ xchat-gnome/fe-gnome/userlist.c
@@ -30,280 +30,280 @@ static void userlist_finalize   (GObject *object);
 
 typedef struct
 {
-  GtkListStore *liststore;
-  GCompletion *completion;
-  GList *completion_items;
+	GtkListStore *liststore;
+	GCompletion *completion;
+	GList *completion_items;
 } Store;
 
 GType
 userlist_get_type (void)
 {
-  static GType userlist_type = 0;
-  if (!userlist_type)
-  {
-    static const GTypeInfo userlist_info =
-    {
-      sizeof (UserlistClass),
-      NULL,               /* base init */
-      NULL,               /* base finalize */
-      (GClassInitFunc)    userlist_class_init,
-      NULL,               /* class finalize */
-      NULL,               /* class data */
-      sizeof (Userlist),
-      0,                  /* n preallocs */
-      (GInstanceInitFunc) userlist_init,
-    };
+	static GType userlist_type = 0;
+	if (!userlist_type)
+	{
+		static const GTypeInfo userlist_info =
+		{
+			sizeof (UserlistClass),
+			NULL,               /* base init */
+			NULL,               /* base finalize */
+			(GClassInitFunc)    userlist_class_init,
+			NULL,               /* class finalize */
+			NULL,               /* class data */
+			sizeof (Userlist),
+			0,                  /* n preallocs */
+			(GInstanceInitFunc) userlist_init,
+		};
 
-    userlist_type = g_type_register_static (G_TYPE_OBJECT, "Userlist", &userlist_info, 0);
-  }
+		userlist_type = g_type_register_static (G_TYPE_OBJECT, "Userlist", &userlist_info, 0);
+	}
 
-  return userlist_type;
+	return userlist_type;
 }
 
 static void
 userlist_class_init (UserlistClass *klass)
 {
-  GObjectClass *object_class = (GObjectClass*) klass;
+	GObjectClass *object_class = (GObjectClass*) klass;
 
-  object_class->dispose = userlist_dispose;
-  object_class->finalize = userlist_finalize;
+	object_class->dispose = userlist_dispose;
+	object_class->finalize = userlist_finalize;
 }
 
 static void
 userlist_init (Userlist *userlist)
 {
-  userlist->stores = g_hash_table_new (g_direct_hash, g_direct_equal);
+	userlist->stores = g_hash_table_new (g_direct_hash, g_direct_equal);
 }
 
 static gboolean
 destroy_store (session *session, Store *store, gpointer data)
 {
-  g_object_unref (store->liststore);
-  g_completion_free (store->completion);
-  g_free (store);
-  return TRUE;
+	g_object_unref (store->liststore);
+	g_completion_free (store->completion);
+	g_free (store);
+	return TRUE;
 }
 
 static void
 userlist_dispose (GObject *object)
 {
-  Userlist *userlist = (Userlist*) object;
-  g_hash_table_foreach_remove (userlist->stores, (GHRFunc) destroy_store, NULL);
+	Userlist *userlist = (Userlist*) object;
+	g_hash_table_foreach_remove (userlist->stores, (GHRFunc) destroy_store, NULL);
 }
 
 static void
 userlist_finalize (GObject *object)
 {
-  Userlist *userlist = (Userlist*) object;
-  g_hash_table_destroy (userlist->stores);
+	Userlist *userlist = (Userlist*) object;
+	g_hash_table_destroy (userlist->stores);
 }
 
 Userlist*
 userlist_new (void)
 {
-  return USERLIST (g_object_new (userlist_get_type (), NULL));
+	return USERLIST (g_object_new (userlist_get_type (), NULL));
 }
 
 static Store*
 create_userlist (Userlist *userlist, session *sess)
 {
-  Store *store = g_new (Store, 1);
+	Store *store = g_new (Store, 1);
 
-  store->liststore = gtk_list_store_new (4, GDK_TYPE_PIXBUF, G_TYPE_STRING, G_TYPE_POINTER, GDK_TYPE_COLOR);
-  store->completion = g_completion_new (NULL);
+	store->liststore = gtk_list_store_new (4, GDK_TYPE_PIXBUF, G_TYPE_STRING, G_TYPE_POINTER, GDK_TYPE_COLOR);
+	store->completion = g_completion_new (NULL);
 	store->completion_items = NULL;
 
-  g_hash_table_insert (userlist->stores, sess, store);
-  return store;
+	g_hash_table_insert (userlist->stores, sess, store);
+	return store;
 }
 
 static GdkPixbuf*
 get_user_icon (struct server *serv, struct User *user)
 {
-  char *pre;
-  int level;
+	char *pre;
+	int level;
 
-  if (!user)
-    return NULL;
+	if (!user)
+		return NULL;
 
-  switch (user->prefix[0])
-  {
-    case '\0': return NULL;
-    case '@':  return pix_op;
-    case '%':  return pix_hop;
-    case '+':  return pix_voice;
-  }
-
-  /* find out how many levels above Operator this user is */
-  pre = strchr (serv->nick_prefixes, '@');
-  if (pre && pre != serv->nick_prefixes)
-  {
-    pre--;
-    level = 0;
-    while (1)
-    {
-      if (pre[0] == user->prefix[0])
-      {
-	switch (level)
+	switch (user->prefix[0])
 	{
-	  case 0: return pix_red;
-	  case 1: return pix_purple;
+		case '\0': return NULL;
+		case '@':  return pix_op;
+		case '%':  return pix_hop;
+		case '+':  return pix_voice;
 	}
-	break; /* 3+, no icons */
-      }
-      level++;
-      if (pre == serv->nick_prefixes)
-	break;
-      pre--;
-    }
-  }
-  return NULL;
+
+	/* find out how many levels above Operator this user is */
+	pre = strchr (serv->nick_prefixes, '@');
+	if (pre && pre != serv->nick_prefixes)
+	{
+		pre--;
+		level = 0;
+		while (1)
+		{
+			if (pre[0] == user->prefix[0])
+			{
+				switch (level)
+				{
+					case 0: return pix_red;
+					case 1: return pix_purple;
+				}
+				break; /* 3+, no icons */
+			}
+			level++;
+			if (pre == serv->nick_prefixes)
+				break;
+			pre--;
+		}
+	}
+	return NULL;
 }
 
 void
 userlist_insert (Userlist *userlist, session *sess, struct User *newuser, int row, gboolean selected)
 {
-  Store *store = (Store*) g_hash_table_lookup (userlist->stores, sess);
-  GtkTreeIter iter;
-  GdkPixbuf *icon;
+	Store *store = (Store*) g_hash_table_lookup (userlist->stores, sess);
+	GtkTreeIter iter;
+	GdkPixbuf *icon;
 	GList *item;
 
-  if (!store)
-    store = create_userlist (userlist, sess);
+	if (!store)
+		store = create_userlist (userlist, sess);
 
-  icon = get_user_icon (sess->server, newuser);
+	icon = get_user_icon (sess->server, newuser);
 
-  gtk_list_store_insert (store->liststore, &iter, row);
-  gtk_list_store_set (store->liststore, &iter, 0, icon, 1, newuser->nick, 2, newuser, 3, newuser->away ? &colors[40] : NULL, -1);
+	gtk_list_store_insert (store->liststore, &iter, row);
+	gtk_list_store_set (store->liststore, &iter, 0, icon, 1, newuser->nick, 2, newuser, 3, newuser->away ? &colors[40] : NULL, -1);
 
-  item = g_list_append (NULL, newuser->nick);
-  g_completion_add_items (store->completion, item);
-  store->completion_items = g_list_concat (store->completion_items, item);
+	item = g_list_append (NULL, newuser->nick);
+	g_completion_add_items (store->completion, item);
+	store->completion_items = g_list_concat (store->completion_items, item);
 }
 
 static GtkTreeIter*
 find_user (Store *store, struct User *user)
 {
-  static GtkTreeIter iter;
-  struct User *row_user;
-  GtkTreeModel *model = GTK_TREE_MODEL (store->liststore);
+	static GtkTreeIter iter;
+	struct User *row_user;
+	GtkTreeModel *model = GTK_TREE_MODEL (store->liststore);
 
-  if (gtk_tree_model_get_iter_first (model, &iter))
-  {
-    do
-    {
-      gtk_tree_model_get (model, &iter, 2, &row_user, -1);
-      if (row_user == user)
-	return &iter;
-    } while (gtk_tree_model_iter_next (model, &iter));
-  }
-  return NULL;
+	if (gtk_tree_model_get_iter_first (model, &iter))
+	{
+		do
+		{
+			gtk_tree_model_get (model, &iter, 2, &row_user, -1);
+			if (row_user == user)
+				return &iter;
+		} while (gtk_tree_model_iter_next (model, &iter));
+	}
+	return NULL;
 }
 
 gboolean
 userlist_remove (Userlist *userlist, session *sess, struct User *user)
 {
-  Store *store = (Store*) g_hash_table_lookup (userlist->stores, sess);
-  GtkTreeIter *iter;
-  GList *item;
+	Store *store = (Store*) g_hash_table_lookup (userlist->stores, sess);
+	GtkTreeIter *iter;
+	GList *item;
 
-  g_return_val_if_fail (store != NULL, FALSE);
+	g_return_val_if_fail (store != NULL, FALSE);
 
-  iter = find_user (store, user);
-  if (!iter)
-    return FALSE;
+	iter = find_user (store, user);
+	if (!iter)
+		return FALSE;
 
-  gtk_list_store_remove (store->liststore, iter);
+	gtk_list_store_remove (store->liststore, iter);
 
-  item = g_list_find_custom (store->completion_items, user->nick, (GCompareFunc) strcmp);
+	item = g_list_find_custom (store->completion_items, user->nick, (GCompareFunc) strcmp);
 	store->completion_items = g_list_remove_link (store->completion_items, item);
-  g_completion_remove_items (store->completion, item);
-  g_list_free (item);
+	g_completion_remove_items (store->completion, item);
+	g_list_free (item);
 
-  return TRUE;
+	return TRUE;
 }
 
 void
 userlist_update (Userlist *userlist, session *sess, struct User *user)
 {
-  Store *store = g_hash_table_lookup (userlist->stores, sess);
-  GtkTreeIter *iter;
-  gchar *nick;
-  GList *item;
+	Store *store = g_hash_table_lookup (userlist->stores, sess);
+	GtkTreeIter *iter;
+	gchar *nick;
+	GList *item;
 
-  g_assert (store != NULL);
+	g_assert (store != NULL);
 
-  iter = find_user (store, user);
-  gtk_tree_model_get (GTK_TREE_MODEL (store->liststore), iter, 1, &nick, -1);
+	iter = find_user (store, user);
+	gtk_tree_model_get (GTK_TREE_MODEL (store->liststore), iter, 1, &nick, -1);
 
-  item = g_list_find_custom (store->completion_items, nick, (GCompareFunc) strcmp);
+	item = g_list_find_custom (store->completion_items, nick, (GCompareFunc) strcmp);
 	store->completion_items = g_list_remove_link (store->completion_items, item);
 	g_completion_remove_items (store->completion, item);
 	g_list_free (item);
 
-  gtk_list_store_set (store->liststore, iter, 1, user->nick, 3, user->away ? &colors[40] : NULL, -1);
+	gtk_list_store_set (store->liststore, iter, 1, user->nick, 3, user->away ? &colors[40] : NULL, -1);
 
 	item = g_list_append (NULL, user->nick);
-  g_completion_add_items (store->completion, item);
-  store->completion_items = g_list_concat (store->completion_items, item);
+	g_completion_add_items (store->completion, item);
+	store->completion_items = g_list_concat (store->completion_items, item);
 }
 
 void
 userlist_move (Userlist *userlist, session *sess, struct User *user, int new_row)
 {
-  Store *store = g_hash_table_lookup (userlist->stores, sess);
-  GtkTreeIter *iter1, iter2;
-  GdkPixbuf *icon;
+	Store *store = g_hash_table_lookup (userlist->stores, sess);
+	GtkTreeIter *iter1, iter2;
+	GdkPixbuf *icon;
 
-  g_assert (store != NULL);
+	g_assert (store != NULL);
 
-  iter1 = find_user (store, user);
-  gtk_list_store_remove (store->liststore, iter1);
+	iter1 = find_user (store, user);
+	gtk_list_store_remove (store->liststore, iter1);
 
-  icon = get_user_icon (sess->server, user);
+	icon = get_user_icon (sess->server, user);
 
-  gtk_list_store_insert (store->liststore, &iter2, new_row);
-  gtk_list_store_set (store->liststore, &iter2, 0, icon, 1, user->nick, 2, user, 3, user->away ? &colors[40] : NULL, -1);
+	gtk_list_store_insert (store->liststore, &iter2, new_row);
+	gtk_list_store_set (store->liststore, &iter2, 0, icon, 1, user->nick, 2, user, 3, user->away ? &colors[40] : NULL, -1);
 }
 
 void
 userlist_clear (Userlist *userlist, session *sess)
 {
-  Store *store = g_hash_table_lookup (userlist->stores, sess);
+	Store *store = g_hash_table_lookup (userlist->stores, sess);
 
-  if (store == NULL)
-    store = create_userlist (userlist, sess);
+	if (store == NULL)
+		store = create_userlist (userlist, sess);
 
-  g_completion_clear_items (store->completion);
-  gtk_list_store_clear (store->liststore);
+	g_completion_clear_items (store->completion);
+	gtk_list_store_clear (store->liststore);
 }
 
 void
 userlist_erase (Userlist *userlist, session *sess)
 {
-  Store *store = g_hash_table_lookup (userlist->stores, sess);
+	Store *store = g_hash_table_lookup (userlist->stores, sess);
 
-  g_assert (store != NULL);
+	g_assert (store != NULL);
 
-  g_object_unref (store->liststore);
-  g_completion_free (store->completion);
-  g_free (store);
-  g_hash_table_remove (userlist->stores, sess);
+	g_object_unref (store->liststore);
+	g_completion_free (store->completion);
+	g_free (store);
+	g_hash_table_remove (userlist->stores, sess);
 }
 
 GtkListStore*
 userlist_get_store (Userlist *userlist, session *sess)
 {
-  Store *store = g_hash_table_lookup (userlist->stores, sess);
-  if (store == NULL)
-    store = create_userlist (userlist, sess);
-  return store->liststore;
+	Store *store = g_hash_table_lookup (userlist->stores, sess);
+	if (store == NULL)
+		store = create_userlist (userlist, sess);
+	return store->liststore;
 }
 
 GCompletion*
 userlist_get_completion (Userlist *userlist, session *sess)
 {
-  Store *store = g_hash_table_lookup (userlist->stores, sess);
-  g_assert (store != NULL);
-  return store->completion;
+	Store *store = g_hash_table_lookup (userlist->stores, sess);
+	g_assert (store != NULL);
+	return store->completion;
 }
