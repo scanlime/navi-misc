6b0a3157127cd5939809c0287fa76c155a860336
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-07-29 09:08:46 +0000

    Improved layout for wide non-scrolling widgets, added vfdwidgets to setup.py

--- trunk/mi6k/userspace/tests/layout.py
+++ trunk/mi6k/userspace/tests/layout.py
@@ -10,7 +10,7 @@ import mi6k, vfdwidgets
 
 vfd = mi6k.Device().vfd
 vfd.powerOn()
-vfd.setBrightness(0)
+vfd.setBrightness(0.1)
 surface = vfdwidgets.Surface(20, 2)
 
 surface.widgets.append(vfdwidgets.LoopingScroller("Wibble wibble wibble wibble splat", priority=10))
@@ -18,7 +18,8 @@ surface.widgets.append(vfdwidgets.LoopingScroller("Whee", (0, 1)))
 surface.widgets.append(vfdwidgets.Text("Boingski", (2, 1)))
 surface.widgets.append(vfdwidgets.Text("*", (1, 1), background='~'))
 
-osd = vfdwidgets.Text('OSD Message', gravity=(0, 10), priority=100)
+osd = vfdwidgets.Text('OSD Message goes here, or something',
+                      gravity=(0, 10), priority=100, ellipses='...')
 surface.widgets.append(osd)
 
 while 1:
