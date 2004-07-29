605e8a62940fe3dd0bfed946c0059b15fbff8864
Micah Elizabeth Scott <micah@scanlime.org>
2004-07-29 09:20:58 +0000

    Add support for widgets with limited lifetimes

--- mi6k/userspace/tests/layout.py
+++ mi6k/userspace/tests/layout.py
@@ -6,26 +6,24 @@
 
 import sys; sys.path[0] += '/..'
 import time
-import mi6k, vfdwidgets
+import mi6k
+from vfdwidgets import *
 
 vfd = mi6k.Device().vfd
 vfd.powerOn()
 vfd.setBrightness(0.1)
-surface = vfdwidgets.Surface(20, 2)
+surface = Surface(20, 2)
 
-surface.widgets.append(vfdwidgets.LoopingScroller("Wibble wibble wibble wibble splat", priority=10))
-surface.widgets.append(vfdwidgets.LoopingScroller("Whee", (0, 1)))
-surface.widgets.append(vfdwidgets.Text("Boingski", (2, 1)))
-surface.widgets.append(vfdwidgets.Text("*", (1, 1), background='~'))
+surface.add(LoopingScroller("Wibble wibble wibble wibble splat", priority=10))
+surface.add(LoopingScroller("Whee", (0, 1)))
+surface.add(Text("Boingski", (2, 1)))
+surface.add(Text("*", (1, 1), background='~'))
 
-osd = vfdwidgets.Text('OSD Message goes here, or something',
-                      gravity=(0, 10), priority=100, ellipses='...')
-surface.widgets.append(osd)
+surface.add(Text('OSD Message goes here, or something',
+                 gravity=(0, 10), priority=100, ellipses='...'),
+            lifetime=10)
 
 while 1:
-    osd.visible = not osd.visible
-    surface.layout()
-    for i in xrange(50):
-        surface.update()
-        vfd.writeLines(surface.draw())
+    surface.update()
+    vfd.writeLines(surface.draw())
 
