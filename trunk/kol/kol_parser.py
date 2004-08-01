95ba7ace773f6218643482bd695932ea94bfbe95
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-08-01 21:40:55 +0000

    All that's left here is to have it out put SQL commands so that we can use it to insert the data into the database.

--- trunk/kol/kol_parser.py
+++ trunk/kol/kol_parser.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-'''
+''' kol_parser.py
+
+Python script that extracts the data from the pages of kol.quantumtunnel.net
+and prepares it for the database for my KoL helper site.
 '''
 # Copyright (C) 2004 W. Evan Sheehan <evan@navi.cx>
 #
@@ -19,21 +22,15 @@
 
 import sys, string
 from HTMLParser import HTMLParser
+from optparse import OptionParser
 
-class Parser(HTMLParser):
-  attrMap = ['name','price','power','muscle_req','description','unk']
-
-  def __init__(self, html):
+class BaseParser(HTMLParser):
+  ''' Base HTML parser for extracting KoL data from kol.quantumtunnel.net '''
+  def __init__(self):
     HTMLParser.__init__(self)
-    self.html = html
-    self.items = []
-    self.tdCount = 0
-    self.currentItem = None
-
-  def handle_starttag(self, tag, attrs):
-    if tag == 'tr':
-      self.currentItem = KoLItem()
-      self.tdCount = 0
+    self.items = []           # List of items created from the file
+    self.tdCount = 0          # Count of table columns so we know where we are
+    self.currentItem = None   # Item we're currently creating
 
   def handle_endtag(self, tag):
     if tag == 'tr' and self.currentItem:
@@ -43,9 +40,44 @@ class Parser(HTMLParser):
       self.tdCount += 1
 
   def handle_data(self, data):
-    if self.currentItem and self.tdCount < 6:
-      attr = getattr(self.currentItem, Parser.attrMap[self.tdCount])
-      setattr(self.currentItem, Parser.attrMap[self.tdCount], string.join([attr, data.strip()]))
+    if self.currentItem and self.tdCount < len(self.__class__.attrMap):
+      attr = getattr(self.currentItem, self.__class__.attrMap[self.tdCount])
+      setattr(self.currentItem, self.__class__.attrMap[self.tdCount], string.join([attr, data.strip()]))
+
+class ItemParser(BaseParser):
+  ''' Item pages parser. '''
+  attrMap = ['name','price','power','muscle_req','description','unk']
+
+  def __init__(self):
+    BaseParser.__init__(self)
+
+  def handle_starttag(self, tag, attrs):
+    if tag == 'tr':
+      self.currentItem = KoLItem()
+      self.tdCount = 0
+
+class ConcParser(BaseParser):
+  ''' Concoction pages parser. '''
+  attrMap = ['name', 'directions', 'options']
+  def __init__(self):
+    BaseParser.__init__(self)
+
+  def handle_starttag(self, tag, attrs):
+    if tag == 'tr':
+      self.currentItem = KoLConcoction()
+      self.tdCount = 0
+
+class EfctParser(BaseParser):
+  ''' Effects pages parser. '''
+  attrMap = ['num', 'effect', 'description']
+
+  def __init__(self):
+    BaseParser.__init__(self)
+
+  def handle_starttag(self, tag, attrs):
+    if tag == 'tr':
+      self.currentItem = KoLEffect()
+      self.tdCount = 0
 
 class KoLItem:
   def __init__(self):
@@ -57,16 +89,50 @@ class KoLItem:
     self.unk = ""
 
   def Print(self):
-    print 'Item:\t\t%s\nDescription:\t\t%s\nPrice:\t\t%s\nPower:\t\t%s\nMuscle Req.:\t\t%s\nUnk:\t\t%s\n' % (self.name, self.description, self.price, self.power, self.muscle_req, self.unk)
+    print 'Item: %s\nDescription: %s\nPrice: %s\nPower: %s\nMuscle Req.: %s\nUnk: %s\n' % (self.name, self.description, self.price, self.power, self.muscle_req, self.unk)
+
+class KoLConcoction:
+  def __init__(self):
+    self.name = ''
+    self.directions = ''
+    self.options = ''
+
+  def Print(self):
+    print 'Item: %s\nDirections: %s\n' % (self.name, self.directions)
+
+class KoLEffect:
+  def __init__(self):
+    self.num = ''
+    self.effect = ''
+    self.description = ''
+
+  def Print(self):
+    print 'Effect: %s\nDescription: %s\n' % (self.effect, self.description)
 
 if __name__ == '__main__':
-  if len(sys.argv) != 2:
-    print 'Usage: kol_parser.py <html file>'
-    sys.exit()
+  ### Set up command line option parser ###
+  usage = '''kol_parser.py parser_type file
+  Parser Types:
+    --item         for KoL items pages
+    --concoctions  for KoL concoctions pages
+    --effects      for KoL effects pages
+  '''
+  options = OptionParser(usage)
+  options.add_option('--item', action='store_const', const='ItemParser', dest='parserType')
+  options.add_option('--concoctions', action='store_const', const='ConcParser', dest='parserType')
+  options.add_option('--effects', action='store_const', const='EfctParser', dest='parserType')
 
-  file = open(sys.argv[1], 'r')
-  html = file.read()
-  parser = Parser(file.readlines())
-  parser.feed(html)
+  if len(sys.argv) < 3:
+    options.error('Too few arguments')
+
+  (opts, args) = options.parse_args()
+
+  # Open the html file.
+  file = open(args[0])
+  # Parse
+  parser = eval(opts.parserType)()
+  parser.feed(file.read())
+
+  # Print
   for item in parser.items:
     item.Print()
