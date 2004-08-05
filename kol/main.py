bfefd6438983616c4ba4a2d677c339aa00dabe34
Evan Sheehan <darthmall@users.noreply.github.com>
2004-08-05 06:47:38 +0000

    shouldn't have made that hard coded anyway... not that it's working any better =P

--- kol/main.py
+++ kol/main.py
@@ -16,8 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import sys
-sys.path.append('/var/www/kol')
+import sys,os
+sys.path.append(os.getcwd())
 
 import ModPython
 from Nouvelle import tag
