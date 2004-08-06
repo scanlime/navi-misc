28cc7a7b7e0a1d7b8e3493069b59efad41d65b62
Evan Sheehan <darthmall@users.noreply.github.com>
2004-08-06 20:33:57 +0000

    Yay! it's actually doing what it's supposed to!

--- cpp_optparse/optparse.cpp
+++ cpp_optparse/optparse.cpp
@@ -36,6 +36,7 @@ Option::operator == (const string & option)
 OptionParser::OptionParser (string usage):
 	use_msg (usage)
 {
+	/* help option is default, it has no destination. */
 	opts.insert(opts.begin(), Option("-h","--help",""));
 }
 
@@ -46,20 +47,41 @@ OptionParser::~OptionParser ()
 void
 OptionParser::add_option (string shrt_flag, string lng_flag, string destination, string dfault)
 {
+	/* Add the option to our list of options. */
 	opts.insert(opts.begin(),Option(shrt_flag, lng_flag, destination));
+
+	/* Set the default value for this option, this not only allows you to
+	 * set default values but insures that every option's destination will
+	 * be in our dictionary, even if the value is only "".
+	 */
 	options[destination] = dfault;
 }
 
 void
 OptionParser::parse_args (int argc, char **argv)
 {
+	/* Walk through the arguments and sort them into options
+	 * or arguments.
+	 */
 	for (int i = 1; i < argc; i++) {
+		/* If we start with a '-' we're probably a <flag><value> pair
+		 * so we need to figure out which option it is. We increment i here
+		 * because we assume that each flag has a value associated with it
+		 * so we're interested in argv[i] and argv[i+1] and we don't want to
+		 * count the value as both an option value and an argument.
+		 * FIXME: this is not gonna work when we start have flags with no
+		 * value (i.e. boolean flags)
+		 */
 		if (argv[i][0] == '-') {
 			find_opt(argv, i);
 			i++;
 		}
+		/* If we're looking at an argument (i.e. a value with no flag who's
+		 * meaning is determined by position) just append it into the
+		 * arguments list.
+		 */
 		else
-			arguments.insert(arguments.begin(), argv[i]);
+			arguments.insert(arguments.end(), argv[i]);
 		//free(argv[i]);
 	}
 }
@@ -81,13 +103,25 @@ OptionParser::help ()
 void
 OptionParser::find_opt(char **argv, int index)
 {
+	/* If it's the built in help option we'll just print the help message
+	 * and quit.
+	 */
+	if ((string)argv[index] == "-h" || (string)argv[index] == "--help")
+		help();
+
+	/* Step through our list of known options. */
 	for (int i = 0; i < opts.size(); i++) {
+		/* Uses the overridden == operator for the Options class
+		 * to compare argv[index] to the flags of each Option.
+		 */
 		if (opts[i] == (string)argv[index]) {
+			/* Set the value and return if we've found a match. */
 			options[opts[i].destination] = argv[index+1];
 			return;
 		}
 	}
 
+	/* If we haven't found a match this is not a known argument. */
 	error("Error: invalid argument");
 }
 /*** The End ***/
--- cpp_optparse/optparse.h
+++ cpp_optparse/optparse.h
@@ -2,7 +2,10 @@
  * optparse.h
  *
  * An option parser for C++ modeled after the optparse Python
- * module.
+ * module. Create an instance of OptionParser in your program
+ * and you can add options to it then call the method parse_args
+ * with argc and argv and it will parse them. The '-h' and '--help'
+ * options are built in, they print the usage message.
  *
  * W. Evan Sheehan <evan_sheehan@nrel.gov>
  *
@@ -18,11 +21,16 @@ class Option
 		Option (std::string shrt, std::string lng, std::string dest);
 		~Option ();
 
+		/* Override the == operator so that when you compare an Option
+		 * to a string it will check the string against shrt_flag and lng_flag
+		 * and return true if either match.
+		 */
 		bool operator == (const std::string & option);
 
-		std::string shrt_flag;
-		std::string lng_flag;
-		std::string destination;
+		std::string shrt_flag;		// short flag, something like -o
+		std::string lng_flag;			// long flag, something like --outfile
+		std::string destination;	// the key used to store this option in the
+															// parser.
 };
 
 class OptionParser
@@ -31,18 +39,23 @@ class OptionParser
 		OptionParser (std::string usage="");
 		~OptionParser ();
 
+		/* Add an option to the parser. */
 		void add_option (std::string shrt_flag, std::string lng_flag, std::string destination, std::string dfault="");
+		/* Parse the commandline args. */
 		void parse_args (int argc, char **argv);
+		/* Print an error message: this->use_msg followed by msg. Exit. */
 		void error (std::string msg);
+		/* Print the help message (use_msg) and exit. */
 		void help ();
 
-		map<std::string, std::string> options;
-		vector<std::string> arguments;
+		map<std::string, std::string> options;	// Dictionary of the options
+		vector<std::string> arguments;			// List of the arguments with no flag
 
 	private:
-		std::string use_msg;
-		vector<Option> opts;
+		std::string use_msg;	// usage message.
+		vector<Option> opts;	// List of the options the parser knows about.
 
+		/* Helper for finding which Option (if any) argv[index] matches. */
 		void find_opt (char **argv, int index);
 };
 
