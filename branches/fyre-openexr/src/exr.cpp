d0c88e1582640e4e0cfe07771d3747e41e38b0c8
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-08-11 05:47:04 +0000

    Hmm, this should make the OpenEXR export generic enough to handle oversampled images too. Note that this doesn't use the oversample gamma set via fyre, it just adds the finished OpenEXR pixels together. This should work, since theoretically those pixels are linear light intensities after the reverse tone mapping.

--- branches/fyre-openexr/src/exr.cpp
+++ branches/fyre-openexr/src/exr.cpp
@@ -44,7 +44,7 @@ extern "C" void exr_save_image_file(HistogramImager *hi, const gchar* filename)
 
   struct {
     float r,g,b,a;
-  } bg, fg, range;
+  } bg, fg, range, bucket, pixel;
 
   bg.r = hi->bgcolor.red / 65535.0;
   bg.g = hi->bgcolor.green / 65535.0;
@@ -61,38 +61,78 @@ extern "C" void exr_save_image_file(HistogramImager *hi, const gchar* filename)
   range.b = fg.b - bg.b;
   range.a = fg.a - bg.a;
 
-  /* FIXME: implement oversampling support */
+  int histogram_stride = oversample * width;
+  float oversample_squared = oversample * oversample;
+  int histogram_block_stride = (oversample-1) * histogram_stride;
 
-  /* A much simpler loop to use when oversampling is disabled */
-  for (int y = height; y; y--) {
-    for (int x = width; x; x--) {
-      /* Linear exposure plus gamma adjustment */
-      float luma = (*cur_bucket) * fscale;
-      luma = pow(luma, one_over_gamma);
+  /* This outer loop iterates over pixels in the resulting image */
+  for (int pix_y = height; pix_y; pix_y--) {
+    for (int pix_x = width; pix_x; pix_x--) {
 
-      /* Optionally clamp before interpolating */
-      if (hi->clamped && luma > 1)
-	luma = 1;
-
-      /* Color interpolation, with no per-component clamping */
-      cur_pixel->r = luma * range.r + bg.r;
-      cur_pixel->g = luma * range.g + bg.g;
-      cur_pixel->b = luma * range.b + bg.b;
-      cur_pixel->a = luma * range.a + bg.a;
-
-      /* Fyre images are generally authored to look good in sRGB,
-       * so they'll already be in the monitor's gamma. This should
-       * perform the inverse of OpenEXR's default monitor gamma
-       * correction. Alpha should always be linear, so leave
-       * that alone.
+      /* Then for each pixel, loop over the corresponding histogram bins.
+       * For oversample==1 this is only one bin, otherwise it's a square
+       * block of oversample^2 bins. For each bin we compute the finished
+       * color values and add them. We don't actually use fyre's oversampling
+       * gamma here, since the OpenEXR pixel values should already be linear.
        */
-      cur_pixel->r = pow(cur_pixel->r * 3.012, 2.2) / 5.55555;
-      cur_pixel->g = pow(cur_pixel->g * 3.012, 2.2) / 5.55555;
-      cur_pixel->b = pow(cur_pixel->b * 3.012, 2.2) / 5.55555;
+      pixel.r = pixel.g = pixel.b = pixel.a = 0;
 
+      guint* cur_bucket_row = cur_bucket;
+      for (int bucket_y = oversample; bucket_y; bucket_y--) {
+	guint* cur_bucket_sample = cur_bucket_row;
+	for (int bucket_x = oversample; bucket_x; bucket_x--) {
+
+	  /* Linear exposure plus gamma adjustment */
+	  float luma = (*cur_bucket_sample) * fscale;
+	  luma = pow(luma, one_over_gamma);
+
+	  /* Optionally clamp before interpolating */
+	  if (hi->clamped && luma > 1)
+	    luma = 1;
+
+	  /* Color interpolation, with no per-component clamping */
+	  bucket.r = luma * range.r + bg.r;
+	  bucket.g = luma * range.g + bg.g;
+	  bucket.b = luma * range.b + bg.b;
+	  bucket.a = luma * range.a + bg.a;
+
+	  /* Fyre images are generally authored to look good in sRGB,
+	   * so they'll already be in the monitor's gamma. This should
+	   * perform the inverse of OpenEXR's default monitor gamma
+	   * correction. Alpha should always be linear, so leave
+	   * that alone.
+	   */
+	  bucket.r = pow(bucket.r * 3.012, 2.2) / 5.55555;
+	  bucket.g = pow(bucket.g * 3.012, 2.2) / 5.55555;
+	  bucket.b = pow(bucket.b * 3.012, 2.2) / 5.55555;
+
+	  /* Accumulate this bucket into our current pixel */
+	  pixel.r += bucket.r;
+	  pixel.g += bucket.g;
+	  pixel.b += bucket.b;
+	  pixel.a += bucket.a;
+
+	  /* Next sample in this oversampling square */
+	  cur_bucket_sample++;
+	}
+
+	/* Next line in the oversampling square */
+	cur_bucket_row += histogram_stride;
+      }
+
+      /* Finish averaging over the oversampling square, and store this pixel */
+      cur_pixel->r = pixel.r / oversample_squared;
+      cur_pixel->g = pixel.g / oversample_squared;
+      cur_pixel->b = pixel.b / oversample_squared;
+      cur_pixel->a = pixel.a / oversample_squared;
+
+      /* Jump to the next pixel, and the beginning of the next block of buckets */
       cur_pixel++;
-      cur_bucket++;
+      cur_bucket += oversample;
     }
+
+    /* Skip over all the histogram lines still part of the same row of buckets */
+    cur_bucket += histogram_block_stride;
   }
 
   file.setFrameBuffer(pixels, 1, width);
