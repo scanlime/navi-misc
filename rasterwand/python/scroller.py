616f169b96a276b3288b22525e5bd8f40f7b2a90
Micah Elizabeth Scott <micah@scanlime.org>
2004-08-31 05:16:20 +0000

    Use a 60x8 display rather than 40x8. Looks like with the latest drivers it's just as fast, and it looks better.

--- rasterwand/python/scroller.py
+++ rasterwand/python/scroller.py
@@ -13,5 +13,5 @@ while 1:
     line = sys.stdin.readline()
     if not line:
         break
-    rasterwand.TextScroller(dev, line.strip(), width=40).scroll(start=(1, 0), end=(-1, 0), speed=1)
+    rasterwand.TextScroller(dev, line.strip(), width=60).scroll(start=(1, 0), end=(-1, 0), speed=1)
 
