88a52108f74b479eac63d4e1ca477ed7c27ff5b3
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-09-09 09:27:41 +0000

    Lots of almost-working code for gchub. The firmware should just need debugging and some extra packet packing goop. The 'daemon' directory currently contains just enough to test the firmware a bit, and requires a CVS version of libusb. It's still unclear whether that will remain a daemon or end up in a kernel module.

--- trunk/wasabi/devices/gchub/firmware/controller.asm
+++ trunk/wasabi/devices/gchub/firmware/controller.asm
@@ -23,8 +23,14 @@
 ;
 ;###############################################################################
 
+	errorlevel -226
+
 #include <p16C745.inc>
 #include "../include/gchub_protocol.h"
+#include "multitasking.inc"
+
+#define N64GC_CLOCK_24
+#include "n64gc_comm.inc"
 
 	errorlevel -302			; Disable register page warnings
 
@@ -37,16 +43,18 @@
 
 bank0	udata
 
-	; Single-packet result buffer
+poll_state	res 2
+
 controller_buffer res 8
-
-	; Controller request and reply buffers
-request_buffer	res 4
-reply_buffer	res 8
-
 rumble_bits	res 1
 
-	code
+gamecube_buffer	res 8
+
+byte_count	res 1
+bit_count	res 1
+parameter_iter	res 1
+
+controller	code
 
 ;************************************************** Macros
 
@@ -66,12 +74,102 @@ clearLoop
 	goto	clearLoop
 	endm
 
+
+	;; Poll one controller, fpsleep'ing for each packet it generates
+poll_controller	macro	port_number
+	local	timeout
+	local	done
+
+	banksel	gamecube_buffer
+	movlw	0x40			; Put 0x400300 in the gamecube_buffer
+	movwf	gamecube_buffer+0
+	movlw	0x03
+	movwf	gamecube_buffer+1
+	movlw	0x00
+	movwf	gamecube_buffer+2
+
+	btfsc	rumble_bits, port_number ; Set the low bit of our gamecube command to turn on rumble
+	bsf	gamecube_buffer+2, 0
+
+	call	controller_io_reset	; Transmit the gamecube_buffer
+	movlw	gamecube_buffer
+	movwf	FSR
+	movlw	3
+	n64gc_tx_buffer	TRISA, port_number, 0
+
+	movlw	gamecube_buffer		; Receive the controller's 8-byte status packet
+	movwf	FSR
+	movlw	8
+	movwf	byte_count
+	n64gc_rx_buffer	PORTA, byte_count, port_number, timeout
+
+	;; Send back analog status
+	movlw	GCHUB_PACKET_ANALOG | port_number
+	movwf	controller_buffer
+	fpsleep	poll_state
+
+	;; Send a timeout packet for this controller
+	pagesel	done
+	goto	done
+timeout
+	movlw	GCHUB_PACKET_DISCONNECT | port_number
+	movwf	controller_buffer
+	fpsleep	poll_state
+
+done
+	endm
+
+
+;************************************************** Utilities
+
+	;; Reset latch state and pin direction on controllers.
+	;; We must do this before transmitting, since read-modify-write
+	;; operations on the same I/O port could put the output latches
+	;; in the wrong state.
+controller_io_reset
+	banksel	PORTA
+	clrf	PORTA
+	banksel TRISA
+	movlw	0x0F
+	movwf	TRISA
+	banksel PORTA
+	return
+
+
 ;************************************************** Frontend
 
 controller_init
+	banksel	PORTA		; All outputs low by default
+	clrf	PORTA
+	clrf	PORTB
+	clrf	PORTC
+	banksel TRISA
+	movlw	0x0F		; Tristate the four controller ports by default
+	movwf	TRISA
+	clrf	TRISB		; Everything else is an output
+	clrf	TRISC
+	banksel PORTA
+
+	clearBuffer	controller_buffer, 8
+
+	;; Start at the beginning of the polling loop
+	fpset	poll_state, controller_poll_loop
 	return
 
+	;; Every time we need another sample for our interrupt endpoint,
+	;; this runs the polling loop until it hits the next 'fpsleep'.
 controller_sample_next
+	fpgoto	poll_state
+
+
+;************************************************** Polling loop
+
+controller_poll_loop
+	poll_controller	0
+	poll_controller	1
+	poll_controller	2
+	poll_controller	3
+	goto	controller_poll_loop
 
 	end
 
