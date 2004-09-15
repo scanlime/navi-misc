125c88c0cba0ab8603d12ec8068b604e3e024a31
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-09-15 23:44:10 +0000

    version bump

--- trunk/xchat-gnome/fe-gnome/setup_dialog.c
+++ trunk/xchat-gnome/fe-gnome/setup_dialog.c
@@ -90,7 +90,7 @@ ok_clicked (GtkButton *button, gpointer data)
 	gconf_client_set_string (client, "/apps/xchat/irc/nickname", nick, NULL);
 	gconf_client_set_string (client, "/apps/xchat/irc/realname", real, NULL);
 
-	gconf_client_set_string (client, "/apps/xchat/version", "0.2", NULL);
+	gconf_client_set_string (client, "/apps/xchat/version", "0.3", NULL);
 
 	done = TRUE;
 }
