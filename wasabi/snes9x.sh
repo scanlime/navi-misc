8c4928cfa85dc379f83f5ee8f83c4111d70a9bc3
Micah Elizabeth Scott <micah@scanlime.org>
2004-09-15 06:21:22 +0000

    Cute little gross shell script to use /proc/bus/input/devices to locate up to the first two attached gamecube controllers and start snes9x with them

--- wasabi/snes9x.sh
+++ wasabi/snes9x.sh
@@ -1,1 +1,24 @@
-/usr/local/bin/osnes9x -joydev1 /dev/js2 -joydev2 /dev/js3 -joymap1 0 1 2 3 5 6 7 4 -joymap2 0 1 2 3 5 6 7 4 /navi/media/games/snes/tetrisattack.smc 
+#
+# Script to run snes9x, autodetecting the first two
+# attached gamecube controllers.
+#
+
+SNES9X_BIN=/usr/local/bin/osnes9x
+DEVICES_FILE=/proc/bus/input/devices
+
+# Get, in order, all handlers attached to gamecube controllers
+HANDLERS=`(for i in 1 2 3 4; do grep -A 4 "Gamecube Controller $i" $DEVICES_FILE; done) | grep ^H | cut -d = -f 2`
+
+# Extract only the joystick devices from that
+JOYSTICKS=`echo $HANDLERS | sed 's/ /\n/g' | grep ^js`
+
+# Separate that into joysticks 1 and 2, using /dev/null for any sticks we can't find
+JOYDEV1=`echo $JOYSTICKS /dev/null /dev/null | cut -d ' ' -f 1`
+JOYDEV2=`echo $JOYSTICKS /dev/null /dev/null | cut -d ' ' -f 2`
+
+# Assume gamecube joysticks for now
+JOYMAP1='0 1 2 3 5 6 7 4'
+JOYMAP2='0 1 2 3 5 6 7 4'
+
+# Yay, run snes9x
+$SNES9X_BIN -joydev1 /dev/input/$JOYDEV1 -joydev2 /dev/input/$JOYDEV2 -joymap1 $JOYMAP1 -joymap2 $JOYMAP2 $*
