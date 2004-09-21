855d9e4d4784f88016e9bf23dd9bd06919d38f2d
Micah Elizabeth Scott <micah@scanlime.org>
2004-09-21 21:06:01 +0000

    Add a Board class for the rcpod-db50, to make DAC and LED access convenient

--- rcpod/pyrcpod/board.py
+++ rcpod/pyrcpod/board.py
@@ -24,11 +24,14 @@ AvailableRcpod's open() method.
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-import device, time
+import time
+import device
+import i2c
+import i2c.ti
 
 # These are the symbols that will be pulled from this module
 # into the 'pyrcpod' package.
-__all__ = ['Rcpod485']
+__all__ = ['Rcpod485', 'RcpodDB50']
 
 
 class Rcpod485(device.OpenedRcpod):
@@ -47,6 +50,35 @@ class Rcpod485(device.OpenedRcpod):
         # Set up transmit enable pin
         self.txe = self.rd4
         self.serialSetTxEnable(self.txe)
+
+
+class RcpodDB50(device.OpenedRcpod):
+    """Implements the special features of an rcpod-db50 board.
+       This board includes one LED, 8 channels of analog output
+       via two quad-DAC chips, and an analog mux with voltage dividers.
+       """
+    def __init__(self, *args, **kwargs):
+        device.OpenedRcpod.__init__(self, *args, **kwargs)
+
+        # Set up LEDs
+        self.led1 = self.rc2
+        self.led1.output().assert_()
+
+        # Set up the I2C bus and DACs
+        self.i2cBus = i2c.Bus(self.rd6, self.rd7)
+        self.dac1 = i2c.ti.DAC8574(self.i2cBus, 2)
+        self.dac2 = i2c.ti.DAC8574(self.i2cBus, 3)
+
+    def analogOutput(self,
+                     a=None, b=None, c=None, d=None,
+                     e=None, f=None, g=None, h=None):
+        """Update up to 8 analog channels. The channels located
+           on the same DAC chip (a-d, e-h) will be updated synchronously.
+           FIXME: The hardware supports updating multiple chips in sync, but
+                  we don't yet.
+           """
+        self.dac1.write(a,b,c,d)
+        self.dac2.write(e,f,g,h)
 
 ### The End ###
 
