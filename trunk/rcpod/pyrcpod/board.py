81ecdf64f7f970fa198a0119e93e3ef00518c7b0
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-09-24 19:07:06 +0000

    Add methods for controlling the rcpod-db50's analog multiplexer, and reading all analog inputs (low voltage and high voltage) in one step

--- trunk/rcpod/pyrcpod/board.py
+++ trunk/rcpod/pyrcpod/board.py
@@ -69,6 +69,69 @@ class RcpodDB50(device.OpenedRcpod):
         self.dac1 = i2c.ti.DAC8574(self.i2cBus, 2)
         self.dac2 = i2c.ti.DAC8574(self.i2cBus, 3)
 
+        # Set up the analog mux
+        self.analogMuxPins = [self.rd0, self.rd1, self.rd2, self.rd3]
+        for pin in self.analogMuxPins:
+            pin.output().assert_()
+
+        # Store gains for each analog channel. This is the
+        # number our 0-255 reading is multiplied by to get
+        # voltage. The defaults assume ideal resistors and
+        # voltage reference, but this may be calibrated
+        # by a separate tool.
+        self.analogGains = {}
+        for pin in (3,4,5,7,8,9,10):
+            # Low-voltage internal inputs
+            self.analogGains[pin] = 5.0 / 255
+        for pin in xrange(27,43):
+            # High-voltage multiplexed inputs (voltage divider,
+            # ideally with 1.0k and 4.7k resistors)
+            self.analogGains[pin] = (5.0 / 255) * ((1.0 + 4.7) / 1.0)
+
+    def setAnalogMuxChannel(self, ch):
+        """Set the current channel on the external analog multiplexer
+           to the indicated value between 0 and 15.
+           """
+        pins = list(self.analogMuxPins)
+        for i in range(4):
+            if not (ch & (1<<i)):
+                pins[i] = pins[i].negate()
+        self.assertPins(pins)
+
+    def analogScan(self, muxChannels=range(16)):
+        """Scan through all multiplexer channels in the given list,
+           taking analog readings for each. With the default settings,
+           this reads all high voltage analog inputs once, and reads each
+           low-voltage analog input 16 times. All readings are averaged
+           together, and the result is a dictionary mapping DB-50 pin
+           number to voltage in volts.
+           """
+        totals = {}
+        nSamples = {}
+        for muxChannel in muxChannels:
+            self.setAnalogMuxChannel(muxChannel)
+
+            # analogReadAll is faster usually than just reading
+            # one channel, since this is implemented in firmware.
+            # The first result is our current multiplexed input,
+            # the rest are samples for the low-voltage inputs.
+            v = self.analogReadAll()
+
+            # Store the external mux results
+            pin = 27 + muxChannel
+            totals[pin] = totals.get(pin, 0) + v[0]
+            nSamples[pin] = nSamples.get(pin, 0) + 1
+
+            # Store the internal mux results
+            for pin, ichannel in ( (3, 1), (4, 2), (5, 3), (7, 4),
+                                   (8, 5), (9, 6), (10, 7) ):
+                totals[pin] = totals.get(pin, 0) + v[ichannel]
+                nSamples[pin] = nSamples.get(pin, 0) + 1
+
+        for pin, total in totals.items():
+            totals[pin] = float(total) / nSamples[pin] * self.analogGains[pin]
+        return totals
+
     def analogOutput(self,
                      a=None, b=None, c=None, d=None,
                      e=None, f=None, g=None, h=None):
