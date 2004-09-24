7349c5a15e293145662b2db7f05533d3ded4dc61
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-09-24 19:25:54 +0000

    Add a utility to graph the rcpod-db50's low and high voltage analog inputs using rtgraph

---
+++ trunk/rcpod/db50_adc_graph.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# A gtk UI for graphing ADC readings. This version
+# is for the rcpod-db50 board, and supports its
+# multiplexed high-voltage analog inputs.
+#
+# Requires pygtk 2 and the rtgraph package from the
+# Subversion repository at:
+#
+#  http://navi.picogui.org/svn/misc/trunk/rtgraph/
+#
+# -- Micah Dowty <micah@picogui.org>
+#
+
+from __future__ import division
+import gtk, rtgraph, threading, pyrcpod, time
+
+
+class AdcChannel(rtgraph.Channel):
+    def strValue(self):
+        """Represent this channel's value as a voltage"""
+        if self.value is not None:
+            return "%.02f V" % self.value
+
+
+class AnalogUpdaterThread(threading.Thread):
+    def __init__(self, channels):
+        threading.Thread.__init__(self)
+        self.channels = channels
+        self.pinChannelMap = {}
+        self.rcpod = pyrcpod.devices[0].open(pyrcpod.RcpodDB50)
+        self.running = True
+        self.start()
+
+    def run(self):
+        while self.running:
+            for pin, value in self.rcpod.analogScan().iteritems():
+                try:
+                    c = self.pinChannelMap[pin]
+                except KeyError:
+                    c = AdcChannel(name="Pin %d" % pin)
+                    self.pinChannelMap[pin] = c
+                    self.channels.append(c)
+                c.value = value
+
+            # Very tiny delay, just so the scheduler doesn't
+            # let this thread hog all the CPU.
+            time.sleep(0.0001)
+
+
+def main():
+    channels = []
+    analogThread = AnalogUpdaterThread(channels)
+
+    # Wait for our channel list to be populated
+    while not channels:
+        time.sleep(0.1)
+
+    graph = rtgraph.HScrollLineGraph(range=(0.0, 30.0), scrollRate=10)
+    win = rtgraph.GraphUIWindow(channels, graph, valueUpdateInterval=500)
+    win.connect("destroy", gtk.mainquit)
+
+    try:
+        gtk.threads_init()
+        gtk.main()
+    finally:
+        analogThread.running = False
+
+
+if __name__ == "__main__":
+    main()
