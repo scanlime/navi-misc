ff4b7a5275a6fa83d6d5c30e5e6fd565adf96644
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-09-26 10:07:53 +0000

    increase the clock division to 2^24 so we can see what's going on more easily, and remove what appear to be all the autogenerated files.

--- trunk/wasabi/devices/unicone/fpga/test/test.v
+++ trunk/wasabi/devices/unicone/fpga/test/test.v
@@ -3,11 +3,11 @@ module test (clk, foo);
 input clk;
 output foo;
 
-reg [10:0] div;
+reg [24:0] div;
 
 always @(posedge clk)
 	div <= div+1;
 
-assign foo = div[10];
+assign foo = div[24];
 
 endmodule
