e6c6ba716fcdf674fc9ed6311bd06dee575f22cd
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-09-26 01:25:03 +0000

    Added a firmare abstraction for the FPGA device, implemented most of what should be necessary to configure an FPGA over the USB port.

--- trunk/wasabi/devices/unicone/firmware/main.c
+++ trunk/wasabi/devices/unicone/firmware/main.c
@@ -28,16 +28,18 @@
 #include <util.h>
 
 #include <unicone.h>
+#include "hardware.h"
+#include "fpga.h"
 
 
-/* We give SDCC from 0xF800 to 0xFCFF as general-purpose xdata,
- * the rest is available for buffers. Start at 0xFD00. Note that
- * DMA buffers must be aligned on an 8-byte boundary.
+/* We reserve space starting at 0xFD00 for endpoint buffers.
+ * EP1 OUT is used for configuring the FPGA.
  */
 volatile xdata at 0xFD00 unsigned char ep1_out_x[64];
 
 
-void main() {
+void main()
+{
   int c;
 
   uart_init();
@@ -48,9 +50,6 @@ void main() {
   usb_init();
   puts("USB initialized");
 
-  /* Turn on the (active-low) LED */
-  LED = 0;
-
   /* Set up the first EP1 OUT transfer */
   usb_dma_unstall(EDB_OEP1);
   usb_dma_setup(EDB_OEP1, ep1_out_x, sizeof(ep1_out_x));
@@ -59,44 +58,39 @@ void main() {
     watchdog_reset();
     usb_poll();
 
-    MCR &= ~MCR_DTR;
+    /* Use the LED brightness as a rough indicator of CPU activity */
+    led_off();
     delay(100);
-    MCR |= MCR_DTR;
+    led_on();
 
-    /* Poll for completed DMA transfers from the EP1 OUT endpoint */
+    /* If we have data on EP1 OUT, configure the FPGA with it and look for more */
     c = usb_dma_status(EDB_OEP1);
     if (c) {
-      printf("Received %d bytes on EP1 OUT: %02X %02X %02X %02x...\n", c,
-	     ep1_out_x[0], ep1_out_x[1], ep1_out_x[2], ep1_out_x[3]);
+      fpga_config_write(ep1_out_x, c);
       usb_dma_setup(EDB_OEP1, ep1_out_x, sizeof(ep1_out_x));
     }
   }
 }
 
 
-void usb_handle_vendor_request() {
-  static unsigned char v = 0;
-  v++;
-
-  printf("Vendor request %d\n", usb_setup_buffer.bRequest);
+void usb_handle_vendor_request()
+{
+  static unsigned char retval = 0;
 
   switch (usb_setup_buffer.bRequest) {
 
-    /* EP0 IN test */
-  case 123:
-    printf("Sending %d\n", v);
-    usb_write_ep0_buffer(&v, 1);
-    break;
-
-    /* LED test, uses wValue but no data stage */
-  case 13:
-    if (usb_setup_buffer.wValue)
-      LED = 0;
-    else
-      LED = 1;
+  case UNICONE_REQ_FPGA_CONFIG_BEGIN:
+    fpga_config_begin();
     usb_write_ack();
     break;
 
+  case UNICONE_REQ_FPGA_CONFIG_END:
+    retval = fpga_config_end();
+    usb_write_ep0_buffer(&retval, 1);
+    break;
+
+  default:
+    printf("Unknown vendor request %d\n", usb_setup_buffer.bRequest);
   }
 }
 
--- trunk/wasabi/devices/unicone/src/test.c
+++ trunk/wasabi/devices/unicone/src/test.c
@@ -1,11 +1,12 @@
 #include <usb.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#include <unicone.h>
 #include "bit_file.h"
 
-#define VENDOR_ID   0xE461
-#define PRODUCT_ID  0x0007
-
 usb_dev_handle *open_dev(void);
+int write_bitstream(usb_dev_handle *d, struct bitfile *bf);
 
 int main()
 {
@@ -31,10 +32,9 @@ int main()
     return 1;
   }
 
-  /* Send the bitstream over EP1 OUT */
   d = open_dev();
-  if (usb_bulk_write(d, 1, bf->data, bf->length, 5000) < 0) {
-    perror("usb_bulk_write");
+  if (write_bitstream(d, bf) < 0) {
+    perror("write_bitstream");
     return 1;
   }
 
@@ -42,7 +42,49 @@ int main()
   return 0;
 }
 
-usb_dev_handle *open_dev(void) {
+int write_bitstream(usb_dev_handle *d, struct bitfile *bf)
+{
+  unsigned char *p;
+  int remaining, retval, write_size;
+  unsigned char retbyte;
+
+  /* Begin configuration */
+  if (usb_control_msg(d, USB_TYPE_VENDOR, UNICONE_REQ_FPGA_CONFIG_BEGIN,
+		      0, 0, NULL, 0, 1000) < 0)
+    return 1;
+
+  /* Wait while the FPGA clears itself */
+  usleep(100000);
+
+  /* Send the bitstream */
+  p = bf->data;
+  remaining = bf->length;
+
+  while (remaining > 0) {
+    /* Write in small chunks so we can use a smaller timeout */
+    write_size = remaining;
+    if (write_size > 256)
+      write_size = 256;
+
+    retval = usb_bulk_write(d, 1, p, write_size, 500);
+    if (retval < 0)
+      return retval;
+
+    remaining -= retval;
+    p += retval;
+  }
+
+  /* End configuration and show the status code */
+  if (usb_control_msg(d, USB_TYPE_VENDOR | 0x80, UNICONE_REQ_FPGA_CONFIG_END,
+		      0, 0, &retbyte, 1, 1000) < 0)
+    return -1;
+  printf("configuration returned: 0x%02X\n", retbyte);
+
+  return 0;
+}
+
+usb_dev_handle *open_dev(void)
+{
   struct usb_bus *busses;
   struct usb_bus *bus;
   struct usb_device *dev;
@@ -55,8 +97,8 @@ usb_dev_handle *open_dev(void) {
 
   for (bus=busses; bus; bus=bus->next) {
     for (dev=bus->devices; dev; dev=dev->next) {
-      if (dev->descriptor.idVendor == VENDOR_ID &&
-	  dev->descriptor.idProduct == PRODUCT_ID) {
+      if (dev->descriptor.idVendor == UNICONE_VENDOR_ID &&
+	  dev->descriptor.idProduct == UNICONE_PRODUCT_ID) {
 	return usb_open(dev);
       }
     }
