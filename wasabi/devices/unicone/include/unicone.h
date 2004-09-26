558be69f25218afd84b2db1ccc52a2bece84d7b4
Micah Elizabeth Scott <micah@scanlime.org>
2004-09-26 00:08:35 +0000

    Start the unicone firmware. Currently it's mostly just a copy of the example included in my tusb library, but it has a hopefully-correct USB descriptor, and a build system that fits wasabi's directory tree.

---
+++ wasabi/devices/unicone/include/unicone.h
@@ -0,0 +1,32 @@
+/*
+ * unicone.h - Shared definitions for the Unicone firmware and userspace code
+ *
+ * Universal Controller Emulator project
+ * Copyright (C) 2004 Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#ifndef _H_UNICONE
+#define _H_UNICONE
+
+#define UNICONE_VENDOR_ID   0xE461
+#define UNICONE_PRODUCT_ID  0x000C
+#define UNICONE_VERSION     0x0100
+
+#endif /* _H_UNICONE */
+
+/* The End */
