06e6960adddc503458a2f2ec3ca1df1a7b9f59b6
Micah Elizabeth Scott <micah@scanlime.org>
2004-10-01 07:54:54 +0000

    Lots of random cleanup. Separated i2c and pwm code into separate verilog files, removed some probably-unnecessary files from the FPGA projects, added a 'gamecube' project, added FPGA pin numbers to the description of the GC cable, and fixed a small wraparound bug in the test program

---
+++ wasabi/devices/unicone/fpga/lib/pwm.v
@@ -0,0 +1,42 @@
+/*
+ * pwm.v - Implementation of an I2C-controlled pulse width
+ *         modulation peripheral.
+ *
+ * Universal Controller Emulator project
+ * Copyright (C) 2004 Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+// A simple 16-bit pulse width modulation module
+module pwm16 (clk, duty_cycle, out);
+	input clk;
+	input [15:0] duty_cycle;
+	output out;
+	reg out;
+	reg [16:0] pwmreg;
+
+	always @(posedge clk)
+		if (pwmreg < 17'h10000)
+			pwmreg <= pwmreg + 1;
+		else
+			pwmreg <= 0;
+
+	always @(posedge clk)
+		out <= ( {1'b0, duty_cycle} >= pwmreg );
+endmodule
+
+/* The End */
