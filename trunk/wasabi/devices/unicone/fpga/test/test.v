1b0e451e05195e24cf7d710778bde001f0a23046
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-10-08 05:37:16 +0000

    whitespace

--- trunk/wasabi/devices/unicone/fpga/test/test.v
+++ trunk/wasabi/devices/unicone/fpga/test/test.v
@@ -8,37 +8,37 @@ module test (clk, reset,
 	input scl;
 	inout sda;
 	inout serial_out;
-	
+
 	/* Buffer between external 2-wire I2C and on-chip 3-wire I2C */
 	wire int_scl, int_sda_in;
 	wire [1:0] int_sda_out;
 	i2c_io_buffer i2cbuf(clk, reset,
 	                     scl, sda,
 	                     int_scl, int_sda_in, int_sda_out == 2'b11);
-	
+
 	/* Add an I2C-addressable LED brightness control */
 	pwm16_i2c #(7'h21) led_controller(
-		clk, reset, 
+		clk, reset,
 		int_scl, int_sda_in, int_sda_out[0],
 		led);
-		
+
 	/************** Nintendo serial transmitter, sending 1-byte packets supplied via I2C */
 
 	wire tx, rx;
 	n_serial_io_buffer iobuffer(clk, reset, serial_out, tx, rx);
-	
+
 	wire busy;
 	reg tx_stopbit;
 	wire [7:0] tx_data;
 	reg tx_strobe;
 	n_serial_tx #(5) transmitter(clk, reset, busy, tx_stopbit, tx_data, tx_strobe, tx);
-		
+
 	wire reg_strobe;
 	i2c_slave_reg #(7'h30, 1) n_i2c_reg(
 		clk, reset,
 		int_scl, int_sda_in, int_sda_out[1],
 		tx_data, reg_strobe);
-	
+
 	reg [1:0] state;
 	parameter
 		S_WAIT_FOR_I2C = 0,
@@ -66,13 +66,13 @@ module test (clk, reset,
 					tx_strobe <= 0;
 				end
 			end
-			
+
 			S_WAIT_BEFORE_STOP: begin
 				state <= S_STOP;
 				tx_stopbit <= 0;
 				tx_strobe <= 0;
 			end
-			
+
 			S_STOP: begin
 				state <= S_WAIT_FOR_I2C;
 				tx_stopbit <= 1;
