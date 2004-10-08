dcfed975c1fdde077382f6226752dbcb86d8c4ee
David Trowbridge <trowbrds@gmail.com>
2004-10-08 08:02:07 +0000

    add a refresh field to the weather plugin as well

--- evo/e-weather-plugin/e-calendar-weather.c
+++ evo/e-weather-plugin/e-calendar-weather.c
@@ -19,11 +19,7 @@
  *
  */
 
-#include <gtk/gtklabel.h>
-#include <gtk/gtktable.h>
-#include <gtk/gtkscrolledwindow.h>
-#include <gtk/gtktreeview.h>
-#include <gtk/gtkvbox.h>
+#include <gtk/gtk.h>
 #include <e-util/e-config.h>
 #include <calendar/ec-config.h>
 #include <libedataserver/e-source.h>
@@ -74,4 +70,62 @@ e_calendar_weather_location (EPlugin *epl, EConfigHookItemFactoryData *data)
 	gtk_table_attach (GTK_TABLE (parent), vbox, 0, 2, row, row+1, GTK_EXPAND | GTK_FILL, 0, 0, 0);
 
 	return treeview;
+}
+
+GtkWidget *
+e_calendar_weather_refresh (EPlugin *epl, EConfigHookItemFactoryData *data)
+{
+	GtkWidget *label, *spin, *option, *menu, *hbox, *parent;
+	GtkWidget *times[4];
+	int row, i;
+	ECConfigTargetSource *t = (ECConfigTargetSource *) data->target;
+	ESource *source = t->source;
+	EUri *uri;
+	static GtkWidget *hidden = NULL;
+
+	if (!hidden)
+		hidden = gtk_label_new ("");
+
+	uri = e_uri_new (e_source_get_uri (source));
+	if (strcmp (uri->protocol, "weather"))
+		return hidden;
+	e_uri_free (uri);
+
+	if (data->old)
+		return data->old;
+
+	parent = data->parent;
+
+	row = ((GtkTable*)parent)->nrows;
+
+	label = gtk_label_new (_("Refresh:"));
+	gtk_widget_show (label);
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (parent), label, 0, 1, row, row+1, GTK_FILL, 0, 0, 0);
+
+	hbox = gtk_hbox_new (FALSE, 6);
+	gtk_widget_show (hbox);
+
+	spin = gtk_spin_button_new_with_range (0, 100, 1);
+	gtk_widget_show (spin);
+	gtk_box_pack_start (GTK_BOX (hbox), spin, FALSE, TRUE, 0);
+
+	option = gtk_option_menu_new ();
+	gtk_widget_show (option);
+	times[0] = gtk_menu_item_new_with_label (_("minutes"));
+	times[1] = gtk_menu_item_new_with_label (_("hours"));
+	times[2] = gtk_menu_item_new_with_label (_("days"));
+	times[3] = gtk_menu_item_new_with_label (_("weeks"));
+	menu = gtk_menu_new ();
+	gtk_widget_show (menu);
+	for (i = 0; i < 4; i++) {
+		gtk_widget_show (times[i]);
+		gtk_menu_shell_append (GTK_MENU_SHELL (menu), times[i]);
+	}
+	gtk_option_menu_set_menu (GTK_OPTION_MENU (option), menu);
+	gtk_box_pack_start (GTK_BOX (hbox), option, FALSE, TRUE, 0);
+
+	gtk_table_attach (GTK_TABLE (parent), hbox, 1, 2, row, row+1, GTK_EXPAND | GTK_FILL, 0, 0, 0);
+
+	return hbox;
 }
