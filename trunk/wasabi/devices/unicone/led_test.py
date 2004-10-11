b4962e1db5cd4c7db9021452528e6d7ca1b0be27
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-10-11 02:35:11 +0000

    Make the example progress reporter actually work

--- trunk/wasabi/devices/unicone/led_test.py
+++ trunk/wasabi/devices/unicone/led_test.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-import pyunicone, time
+import pyunicone, time, sys
 
 
 class ProgressOperation:
     def __init__(self, name):
-        print "Starting %s:" % name,
+        sys.stderr.write("Starting %s: " % name)
 
-    def report(self, completion):
-        print "%.02f" % completion,
+    def update(self, completion):
+        sys.stderr.write("%.02f " % completion)
 
-    def finish(self):
-        print "Done."
-
+    def finish(self, message):
+        if message:
+	    sys.stderr.write("[%s]\n" % message)
+	else:
+            sys.stderr.write("Done.\n")
 
 class MyProgressReporter:
     def start(self, op):
