6beeeff918908db2f58d172b3e0e9fca806eb921
Micah Elizabeth Scott <micah@scanlime.org>
2004-10-30 05:34:27 +0000

    In fact, greatly reduced CPU usage all around!

--- fieldsensor/userspace/graph.py
+++ fieldsensor/userspace/graph.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
-import gtk, rtgraph, sys, fcntl, os
+import gtk, rtgraph, sys, threading
+
+class InputThread(threading.Thread):
+    def run(self):
+        self.running = True
+        while self.running:
+            values = sys.stdin.readline().strip().split(' ')
+
+            for i, value in enumerate(values):
+                self.channels[i].value = float(value)
+
 
 def main():
     channels = [rtgraph.Channel (name="Parameter block %d" % i) for i in xrange (8)]
 
-    graph = rtgraph.GraphUI (channels,
-                             rtgraph.HScrollLineGraph(range=(0,255)))
+#    horizontal = rtgraph.Channel(name="Horizontal")
+#    channels.append(horizontal)
 
-    win = gtk.Window (gtk.WINDOW_TOPLEVEL)
-    vbox = gtk.VBox ()
-    win.add (vbox)
-    win.set_border_width (5)
-    vbox.pack_start (graph, gtk.TRUE)
-    win.set_default_size (400,400)
-    win.show_all ()
-    win.connect ("destroy", gtk.mainquit)
+    graph = rtgraph.HScrollLineGraph(range=(0.0, 255.0))
+    win = rtgraph.GraphUIWindow(channels, graph, valueUpdateInterval=500)
+    win.connect("destroy", gtk.mainquit)
 
-    stdin = sys.stdin.fileno()
-
-    flags = fcntl.fcntl (stdin, fcntl.F_GETFL)
-    fcntl.fcntl (stdin, fcntl.F_SETFL, flags | os.O_NONBLOCK)
-
-    while True:
-        try:
-            values = os.read(stdin, 4096).split(' ')
-            for i in xrange(8):
-                channels[i].value = float(values[i])
-        except:
-            pass
-        while gtk.events_pending():
-            gtk.main_iteration()
+    inputThread = InputThread()
+    inputThread.channels = channels
+    inputThread.start()
+    try:
+        gtk.threads_init()
+        gtk.main()
+    finally:
+        inputThread.running = False
 
 if __name__ == "__main__":
     main()
