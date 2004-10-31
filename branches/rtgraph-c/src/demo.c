745678abafe711e340f918f6f18de14e91b15b12
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-10-31 09:19:29 +0000

    Define most of the interface for our B+-Tree

--- branches/rtgraph-c/src/demo.c
+++ branches/rtgraph-c/src/demo.c
@@ -3,17 +3,36 @@
 #include <stdio.h>
 #include <string.h>
 #include "pagestorage.h"
+#include "bptree.h"
+
+
+gint compare_int_func(gconstpointer _a, gconstpointer _b, gpointer)
+{
+  int a = *(int*) _a;
+  int b = *(int*) _b;
+  if (a<b)
+    return -1;
+  else if (a>b)
+    return 1;
+  else
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
   RtgPageStorage* storage;
-  RtgPageAddress foo;
+  RtgBPTree* tree;
+  int key;
+  gdouble value;
 
   gtk_init(&argc, &argv);
 
   storage = rtg_page_storage_mapped_new("foo.rtg", 0);
-  printf("Created new storage %p, page size %d\n", storage, storage->page_size);
 
+  tree = rtg_bptree_new(storage, "My Tree", sizeof(key), sizeof(value),
+			compare_int_func, NULL);
+
+  foo = rtg_page_storage_find_name("MyFooWidget Plus!"
   foo = rtg_page_storage_alloc(storage);
   printf("Allocated page %d\n", foo);
 
