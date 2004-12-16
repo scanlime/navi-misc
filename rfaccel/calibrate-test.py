c507344677ebed42f4060c2f8f277e4fbf429643
Micah Elizabeth Scott <micah@scanlime.org>
2004-12-16 10:10:47 +0000

    Implement the statistical calibration, and add a cute little 3D-ish demo using rtgraph's isometric widget

--- rfaccel/calibrate-test.py
+++ rfaccel/calibrate-test.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-import sys, time, math
+import gtk, rtgraph, sys, math
 import rfaccel
 
-calibration = [ 0.0001814,
-                0.00490136,
-                -0.00018937,
-                0.00246719,
-                -0.00015337,
-                0.00315704]
+calibration = (0.0028319861431870671, -94.271154734411084, 0.0025761554621848743, -78.892184873949589, 0.0025870253164556965, -79.845949367088622)
 
-class Foo(rfaccel.PacketReceiver):
-    def __init__(self, device):
+class PacketGrapher(rfaccel.PacketReceiver):
+    def __init__(self, channels, device):
+        self.channels = channels
         rfaccel.PacketReceiver.__init__(self, device)
 
     def receivedRawAccel(self, vect):
@@ -18,12 +14,29 @@ class Foo(rfaccel.PacketReceiver):
                   vect['y'] * calibration[2] + calibration[3],
                   vect['z'] * calibration[4] + calibration[5] )
         mag = math.sqrt(accel[0]**2 + accel[1]**2 + accel[2]**2)
-        print mag, accel
+
+        self.channels[0].value = (accel[0] / 20.0,
+                                  accel[1] / 20.0,
+                                  accel[2] / 20.0)
+        self.channels[1].value = (0,0, mag / 20.0)
+
+def main():
+    channels = [
+        rtgraph.Channel(name="Vector"),
+        rtgraph.Channel(name="Magnitude"),
+        ]
+
+    graph = rtgraph.IsometricVectorGraph()
+    win = rtgraph.GraphUIWindow(channels, graph, valueUpdateInterval=500)
+    win.connect("destroy", gtk.main_quit)
+
+    device = sys.argv[1]
+    thread = rfaccel.ReceiverThread(PacketGrapher(channels, device))
+    try:
+        gtk.threads_init()
+        gtk.main()
+    finally:
+        thread.running = False
 
 if __name__ == "__main__":
-    x = Foo(sys.argv[1])
-    try:
-        while 1:
-            x.waitForData()
-    except KeyboardInterrupt:
-        pass
+    main()
--- rfaccel/calibrate.py
+++ rfaccel/calibrate.py
@@ -7,7 +7,7 @@ psyco.full()
 
 class DataCollector(rfaccel.PacketReceiver):
     """Collects calibration data periodically, storing (x,y,z) tuples"""
-    def __init__(self, device, interval=0.05):
+    def __init__(self, device, interval=0.01):
         self.vectors = []
         self.lastTimestamp = 0
         self.interval = interval
@@ -22,29 +22,29 @@ class DataCollector(rfaccel.PacketReceiver):
         print "%d samples. Latest: %r" % (
             len(self.vectors), self.vectors[-1])
 
+def calibrate(vectors, gravity=9.81):
+    vectors = asarray(vectors)
+    return (calibrateAxis(vectors[:,0], gravity) +
+            calibrateAxis(vectors[:,1], gravity) +
+            calibrateAxis(vectors[:,2], gravity))
 
-def errorMetric(params, vectors, gravity=9.81):
-    #
-    # Calibration:
-    #  (x * Xgain + Xbias)^2 + (y * Ygain + Ybias)^2 + (z * Zgain + Zbias)^2 = g^2
-    #
-    e = sum((((vectors[:,0] * params[0] + params[1])**2 +
-              (vectors[:,1] * params[2] + params[3])**2 + 
-              (vectors[:,2] * params[4] + params[5])**2) -
-             gravity**2)**2)
-    print params, e
-    return e
+def calibrateAxis(samples, gravity, percentile=5.0):
+    samples = list(samples)
+    samples.sort()
 
-def calibrate(vectors):
-    x0 = (1.0, 0.0,
-          1.0, 0.0,
-          1.0, 0.0)
-    return optimize.fmin(errorMetric, x0, args=(asarray(vectors),),
-                         maxiter = 1000000,
-                         maxfun = 100000,
-                         xtol=1e-14,
-                         ftol=1e-14,
-                         )
+    # Discard upper and lower extremes
+    p = int(percentile / 100.0 * len(samples))
+    minimum = samples[p]
+    maximum = samples[-(1+p)]
+
+    # Solve for:
+    #  maximum * gain + bias =  gravity
+    #  minimum * gain + bias = -gravity
+
+    gain = 2.0 * gravity / (maximum - minimum)
+    bias = -0.5 * (maximum + minimum) * gain
+    return (gain, bias)
+
 
 if __name__ == "__main__":
     collector = DataCollector(sys.argv[1])
