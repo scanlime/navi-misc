eed327997b9a1dc28319f2e2f8db82292b53d359
Micah Elizabeth Scott <micah@scanlime.org>
2004-12-18 13:55:34 +0000

    Simplify the protocol, implement an I2C master, and add debuggative cruft here and there. This returns temperature values now, but they're fairly bogus- the TC74 probably isn't getting enough time to power up. Anyway, this should be enough to test power management with a realistic load.

--- therm/rx-test.py
+++ therm/rx-test.py
@@ -3,6 +3,8 @@
 # Receiver prototype, implemented in python using a PC serial port
 #
 
+import time, sys
+
 def crc8(bits):
     reg = 0x00
     for bit in bits + "00000000":
@@ -42,12 +44,10 @@ def createBitCountTable():
         table.append(count)
     return table
 
-def receiveBits():
+def receiveBits(device):
+    """Receive bits from the device, yielding (bit, noise) tuples"""
     bitCounts = createBitCountTable()
-    port = open("/dev/usb/tts/0", "rb")
-    bits = ''
-    noise = 0
-    flag = "011110"
+    port = open(device, "rb")
 
     while 1:
         # Each byte read from the serial port is actually one bit
@@ -55,16 +55,26 @@ def receiveBits():
         # matches the ideal bit patterns.
         b = port.read(1)
         if bitCounts[ord(b)] < 4:
-            noise += bitCounts[ord(b) ^ 0xC0]
-            bits += "1"
+            yield ("1", bitCounts[ord(b) ^ 0xC0])
         else:
-            noise += bitCounts[ord(b) ^ 0xFC]
-            bits += "0"
+            yield ("0", bitCounts[ord(b) ^ 0xFC])
+
+
+def receiveFrames(device, flag="011110"):
+    """Receive sequences of bits bounded by two 'flag' sequences. Yields
+       (bitstring, signalStrength) tuples.
+       """
+    bits = ''
+    noise = 0
+
+    for bit, bitNoise in receiveBits(device):
+        noise += bitNoise
+        bits += bit
 
         # Put an upper limit on the buffer size
         if len(bits) > 1024:
-            bits = ''
             noise = 0
+            bits = ''
 
         # If we've received a flag, examine the contents. It might be a valid packet
         if bits.endswith(flag):
@@ -76,24 +86,56 @@ def receiveBits():
             # that 1/3 noise is a signal strength of 0, and no noise is a signal strength of 100%
             signalStrength = (1 - min(noiseRatio, 1.0/3.0) * 3) * 100.0
 
-            receivedRawPacket(bits[:-len(flag)], signalStrength)
+            yield (bits[:-len(flag)], signalStrength)
             bits = ''
 
-def receivedRawPacket(bits, signalStrength):
-    """We received a bit sequence between two flags. It might be a valid packet"""
-    # Unstuff it. Is it too short?
-    bits = unstuff(bits)
-    if len(bits) < 8:
-        return
 
-    # Check the CRC
-    content = bits[:-8]
-    crc = unpack(bits[-8:])
-    if crc8(content) != crc:
-        return
+def receiveContent(device):
+    """Receive validated packet content. Yields (content, signalStrength) tuples"""
+    for bits, signalStrength in receiveFrames(device):
+        # Unstuff it. Is it too short?
+        bits = unstuff(bits)
+        if len(bits) < 8:
+            continue
 
-    # Yay, it's valid
-    print content, signalStrength
+        # Check the CRC
+        content = bits[:-8]
+        crc = unpack(bits[-8:])
+        if crc8(content) != crc:
+            continue
+
+        # Yay, it's valid
+        yield (content, signalStrength)
+
+logFile = None
+def log(*values):
+    """Log a tuple of values in CSV format to stdout and a log file"""
+    global logFile
+    if logFile is None:
+        logFile = open("rx-log.csv", "w")
+    line = ", ".join([str(value) for value in values])
+    print line
+    logFile.write(line + "\n")
+    logFile.flush()
+
 
 if __name__ == "__main__":
-    receiveBits()
+    log("time", "signalStrength", "protocol", "station", "sequence", "voltage", "temperature")
+
+    for content, signalStrength in receiveContent("/dev/usb/tts/0"):
+        if len(content) != 31:
+            continue
+
+        try:
+            protocol = unpack(content[:2])
+            station = unpack(content[2:8])
+            sequence = unpack(content[8:13])
+            voltage = unpack(content[13:23])
+
+            temperature = int(content[23:31], 2)
+            if temperature & 0x80:
+                temperature -= 0x100
+
+            log(time.time(), signalStrength, protocol, station, sequence, voltage, temperature)
+        except:
+            print sys.exc_info()[1]
--- therm/transmitter/therm-tx.asm
+++ therm/transmitter/therm-tx.asm
@@ -2,17 +2,17 @@
 ; Wireless temperature sensors, transmitter side
 ; Copyright 2004, Micah Dowty <micah@navi.cx>
 ;
-; This samples one or more I2C temperature sensors
-; (Microchip TC74) connected to the same bus, and sends back
-; all results over a Laipac 315MHz ASK transmitter.
+; This samples an I2C temperature sensor (Microchip TC74) and sends back
+; all results over a Laipac 315MHz ASK transmitter. Currently it only supports
+; a single temperature sensor, but the protocol is extensible.
 ;
 ; Hardware description:
 ;      GP0: Battery voltage sensor (470Kohms to battery+, 470K to ground)
 ;      GP1: Transmitter data
-;      GP2: Transmitter power
-;      GP3: I2C data (4.7k pullup resistor)
-;      GP4: I2C clock
-;      GP5: I2C power
+;      GP2: Transmitter and I2C power
+;      GP3: Unused input-only pin, external 4.7k pullup resistor to I2C power
+;      GP4: I2C data, 4.7k pullup resistor to VCC
+;      GP5: I2C clock
 ;
 ; The protocol is designed to be relatively radio-friendly, but still
 ; possible to receive using a UART designed for RS-232. Ideally, ones
@@ -32,7 +32,7 @@
 ; sequence never occurs. After any run of three "1" bits, a "0" is explicitly
 ; inserted. On the receiving side, after receiving a run of three "1" bits
 ; another "1" will indicate a flag but a "0" will be removed. All values
-; are sent LSB-first.
+; are sent LSB-first unless otherwise indicated.
 ;
 ; Packets are of the following form:
 ;
@@ -42,9 +42,7 @@
 ;         - 6-bit station ID
 ;         - 5-bit packet sequence number
 ;         - 10-bit battery voltage
-;         - For each attached TC74 sensor:
-;	    - 3-bit sensor address
-;           - 8-bit sensor value
+;         - 8-bit sensor value(s), MSB first (streamed directly from I2C)
 ;         - 8-bit frame check sequence (CRC-8 of above)
 ;      - 6-bit Flag sequence
 ;
@@ -58,34 +56,38 @@
 
 ;----------------------------------------------------- Device configuration
 
-STATION_ID	equ	1
+STATION_ID	equ	1	; A unique ID between 0 and 63
+TC74_ADDR	equ	7	; The address marked on this station's TC74
+SLEEP_TIME	equ	4	; Time to sleep between readings, in 2.3 second units
 
 
 ;----------------------------------------------------- Constants
 
 BATT_VOLT_PIN	equ 0
 TX_PIN		equ 1
-TX_POWER_PIN	equ 2
-SDA_PIN		equ 3
-SCL_PIN		equ 4
-I2C_POWER_PIN	equ 5
+POWER_PIN	equ 2
+UNUSED_PIN	equ 3
+SDA_PIN		equ 4
+SCL_PIN		equ 5
 
 BATT_VOLT_MASK	equ 0x01
 TX_MASK		equ 0x02
-TX_POWER_MASK	equ 0x04
-SDA_MASK	equ 0x08
-SCL_MASK	equ 0x10
-I2C_POWER_MASK	equ 0x20
+POWER_MASK	equ 0x04
+UNUSED_MASK	equ 0x08
+SDA_MASK	equ 0x10
+SCL_MASK	equ 0x20
 
-TX_SHORT_DELAY	equ .178		; 156.3us
-TX_LONG_DELAY	equ .74		; 364.6us
+TC74_ADDR_READ	equ (0x91 | (TC74_ADDR << 1))
+TC74_ADDR_WRITE	equ (0x90 | (TC74_ADDR << 1))
+
+TX_SHORT_DELAY	equ .180	; 156.3us
+TX_LONG_DELAY	equ .76		; 364.6us
 
 
 ;----------------------------------------------------- Variables
 
 	cblock 0x20
-		temp, consecutive_ones, crc_reg, temp2
-		packet_seq, i2c_address, i2c_data
+		temp, consecutive_ones, crc_reg, temp2, packet_seq
 	endc
 
 
@@ -94,9 +96,9 @@ TX_LONG_DELAY	equ .74		; 364.6us
 start
 
 	; Initialize I/O
-	movlw	I2C_POWER_MASK | SDA_MASK | SCL_MASK | TX_POWER_MASK
+	movlw	POWER_MASK | SCL_MASK
 	movwf	GPIO
-	movlw	BATT_VOLT_MASK		; Set pin directions (also turns on peripherals)
+	movlw	BATT_VOLT_MASK | UNUSED_MASK | SDA_MASK
 	bsf	STATUS, RP0
 	movwf	TRISIO
 	bcf	STATUS, RP0
@@ -131,6 +133,32 @@ start
 	clrf	crc_reg
 	clrf	consecutive_ones
 
+	;; Turn on the TC74
+	call	i2c_start
+	movlw	TC74_ADDR_WRITE	; Address it
+	movwf	temp
+	call	i2c_send_byte
+	movlw	0x01		; Select the CONFIG register
+	movwf	temp
+	call	i2c_send_byte
+	movlw	0x80		; Out of standby mode
+	movwf	temp
+	call	i2c_send_byte
+	call	i2c_stop
+	call	i2c_start
+	movlw	TC74_ADDR_WRITE	; Address it
+	movwf	temp
+	call	i2c_send_byte
+	movlw	0x00		; Select the TEMP register
+	movwf	temp
+	call	i2c_send_byte
+	call	i2c_stop
+
+	call	i2c_start
+	movlw	TC74_ADDR_READ	; Address the TC74 for reading
+	movwf	temp
+	call	i2c_send_byte
+
 
 ;----------------------------------------------------- Main Program
 
@@ -160,20 +188,8 @@ start
 	movwf	temp
 	call	tx_content_2bit
 
-	clrf	i2c_address	; Loop over all I2C sensors (0-7)
-sensors_loop
-
-	movf	i2c_address, w	; The sensor was valid. Send 3-bit address, 8-bit data
-	movwf	temp
-	call	tx_content_3bit
-	movf	i2c_data, w
-	movwf	temp
-	call	tx_content_8bit
-
-invalid_sensor
-	incf	i2c_address, f	; Next sensor...
-	btfss	i2c_address, 3	; Quit when we hit 8
-	goto	sensors_loop
+	call	tx_i2c_byte	; 8-bit temperature, read directly from I2C
+	call	i2c_stop
 
 	call	tx_end_content
 
@@ -183,16 +199,135 @@ invalid_sensor
 	clrf	GPIO		; Turn off external peripherals
 	clrf	ADCON0		; Turn off the A/D converter
 
-	;; Assign a 1:128 prescaler to the WDT, and hit the sack
+	;; Assign a 1:128 prescaler to the WDT. Each 'sleep' we do now
+	;; will last as long as possible, about 2.3 seconds.
 	clrwdt			; Clear the watchdog
 	clrf	TMR0		; Clear the prescaler
 	movlw	0xDF		; Reassign prescaler
 	bsf	STATUS, RP0
 	movwf	OPTION_REG
 	bcf	STATUS, RP0
-	sleep			; Clear the watchdog and sleep until it resets us
+
+	movlw	SLEEP_TIME	; Sleep for a configurable amount of time
+	movwf	temp
+sleep_loop
+	sleep
+	decfsz	temp, f
+	goto	sleep_loop
 
 	goto	start		; Reinitialize and send another packet
+
+
+;----------------------------------------------------- I2C Master
+
+i2c_sda_high macro
+	movlw	BATT_VOLT_MASK | UNUSED_MASK | SDA_MASK
+	bsf	STATUS, RP0
+	movwf	TRISIO
+	bcf	STATUS, RP0
+	endm
+
+i2c_sda_low macro
+	movlw	BATT_VOLT_MASK | UNUSED_MASK
+	bsf	STATUS, RP0
+	movwf	TRISIO
+	bcf	STATUS, RP0
+	endm
+
+i2c_scl_high macro
+	movlw	POWER_MASK | SCL_MASK
+	movwf	GPIO
+	endm
+
+i2c_scl_low macro
+	movlw	POWER_MASK
+	movwf	GPIO
+	endm
+
+i2c_delay
+	nop
+	nop
+	nop
+	nop
+	nop
+	return
+
+i2c_start
+	i2c_sda_low
+	call	i2c_delay
+	i2c_scl_low
+	call	i2c_delay
+	return
+
+i2c_stop
+	i2c_sda_low
+	call	i2c_delay
+	i2c_scl_high
+	call	i2c_delay
+	i2c_sda_high
+	call	i2c_delay
+	return
+
+i2c_send_zero
+	i2c_sda_low
+	call	i2c_delay
+	i2c_scl_high
+	call	i2c_delay
+	i2c_scl_low
+	call	i2c_delay
+	return
+
+i2c_send_one
+	i2c_sda_high
+	call	i2c_delay
+	i2c_scl_high
+	call	i2c_delay
+	i2c_scl_low
+	call	i2c_delay
+	return
+
+	;; Shift one bit out of 'temp'
+i2c_send_bit
+	rlf	temp, f
+	btfss	STATUS, C
+	goto	i2c_send_zero
+	goto	i2c_send_one
+
+	;; Shift one byte out of 'temp'
+i2c_send_byte
+	call	i2c_send_bit
+	call	i2c_send_bit
+	call	i2c_send_bit
+	call	i2c_send_bit
+	call	i2c_send_bit
+	call	i2c_send_bit
+	call	i2c_send_bit
+	call	i2c_send_bit
+	i2c_scl_high		; Clock in and ignore the ACK
+	i2c_scl_low
+
+	;; Read a bit from I2C and transmit it as packet content
+tx_i2c_bit
+	i2c_scl_high
+	call	i2c_delay
+	call	tx_i2c_bit_test
+	i2c_scl_low
+	call	i2c_delay
+	return
+tx_i2c_bit_test
+	btfsc	GPIO, SDA_PIN
+	goto	tx_content_one
+	goto	tx_content_zero
+
+tx_i2c_byte
+	call	tx_i2c_bit
+	call	tx_i2c_bit
+	call	tx_i2c_bit
+	call	tx_i2c_bit
+	call	tx_i2c_bit
+	call	tx_i2c_bit
+	call	tx_i2c_bit
+	goto	tx_i2c_bit
 
 
 ;----------------------------------------------------- Protocol: Frame check sequence
@@ -310,6 +445,13 @@ tx_set_timer macro value
 	bcf	INTCON, T0IF
 	endm
 
+	;; Add to TMR0 and reset its overflow flag
+tx_add_timer macro value
+	movlw	value
+	addwf	TMR0, f
+	bcf	INTCON, T0IF
+	endm
+
 	;; Wait for TMR0 to overflow
 tx_wait_timer macro
 	local keep_waiting
@@ -321,13 +463,13 @@ keep_waiting
 
 	;; Bring the transmit pin high
 tx_pin_high macro
-	movlw	I2C_POWER_MASK | SDA_MASK | SCL_MASK | TX_POWER_MASK | TX_MASK
+	movlw	POWER_MASK | SCL_MASK | TX_MASK
 	movwf	GPIO
 	endm
 
 	;; Bring the transmit pin low
 tx_pin_low macro
-	movlw	I2C_POWER_MASK | SDA_MASK | SCL_MASK | TX_POWER_MASK
+	movlw	POWER_MASK | SCL_MASK
 	movwf	GPIO
 	endm
 
@@ -355,6 +497,7 @@ tx_send_start
 	call	sync_delay
 	tx_pin_low
 	call	sync_delay
+	tx_set_timer 0
 	return
 
 	;; End a transmission (wait for the last bit to finish)
@@ -366,20 +509,20 @@ tx_send_end
 	;; Send a '1' bit
 tx_send_one
 	tx_wait_timer		; Let the previous bit end
-	tx_set_timer	TX_LONG_DELAY
+	tx_add_timer	TX_LONG_DELAY
 	tx_pin_high
 	tx_wait_timer
-	tx_set_timer	TX_SHORT_DELAY
+	tx_add_timer	TX_SHORT_DELAY
 	tx_pin_low
 	return
 
 	;; Send a '0' bit
 tx_send_zero
 	tx_wait_timer		; Let the previous bit end
-	tx_set_timer	TX_SHORT_DELAY
+	tx_add_timer	TX_SHORT_DELAY
 	tx_pin_high
 	tx_wait_timer
-	tx_set_timer	TX_LONG_DELAY
+	tx_add_timer	TX_LONG_DELAY
 	tx_pin_low
 	return
 
