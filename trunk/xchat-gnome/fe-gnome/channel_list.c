e9fffbd4bca9c8d49d00093e2609110fd99e458e
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-01-01 05:40:28 +0000

    update status on just about everything that it's my responsibility to do

--- trunk/xchat-gnome/fe-gnome/channel_list.c
+++ trunk/xchat-gnome/fe-gnome/channel_list.c
@@ -1,7 +1,7 @@
 /*
  * channel_list.c - channel list
  *
- * Copyright (C) 2004 xchat-gnome team
+ * Copyright (C) 2004-2005 xchat-gnome team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- trunk/xchat-gnome/fe-gnome/main_window.h
+++ trunk/xchat-gnome/fe-gnome/main_window.h
@@ -1,7 +1,7 @@
 /*
  * main_window.h - main GUI window functions
  *
- * Copyright (C) 2004 xchat-gnome team
+ * Copyright (C) 2004-2005 xchat-gnome team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
