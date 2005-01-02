a2949d853685951042b814fba1b3383765e8b393
Micah Elizabeth Scott <micah@scanlime.org>
2005-01-02 09:47:01 +0000

    Bump copyright dates

--- tusb/include/uart_driver.h
+++ tusb/include/uart_driver.h
@@ -2,7 +2,7 @@
  * uart_driver.h - A driver for the TUSB's UART, compatible with
  *                 SDCC's stdio implementation.
  *
- * Copyright (C) 2004 Micah Dowty
+ * Copyright (C) 2004-2005 Micah Dowty
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
