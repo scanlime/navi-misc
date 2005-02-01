c8464224ebc7752f172891fd37aa3fc501578d2c
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-02-01 21:08:43 +0000

    add a bunch of setter functions

--- trunk/bzedit/scripts/BZFlag/Box.py
+++ trunk/bzedit/scripts/BZFlag/Box.py
@@ -42,13 +42,41 @@ class Box(Object):
              (1, 5, 7, 3), # Z-
             ]
 
-    def __init__(self):
+    def __init__(self, list=None):
         # Load defaults
         self.drivethrough = 0
         self.shootthrough = 0
-        self.position = [0, 0, 0]
-        self.size = [10, 10, 9.42]
-        self.rotation = 0
-        self.scale = [1, 1, 1]
-        self.shear = [0, 0, 0]
-        self.spin = [0, 0, 0, 0]
+        self.set_position()
+        self.set_size()
+        self.set_rotation()
+        self.set_scale()
+        self.set_shear()
+        self.set_spin()
+        if list is not None:
+            for property in list[1:]:
+                f = getattr(self, "set_%s" % property[0])
+
+
+    def set_drivethrough(self):
+        self.drivethrough = 1
+
+    def set_shootthrough(self):
+        self.shootthrough = 1
+
+    def set_position(self, pos=[0, 0, 0]):
+        self.position = [float(n) for n in pos]
+
+    def set_size(self, size=[10, 10, 9.42]):
+        self.size = [float(n) for n in size]
+
+    def set_rotation(self, *r):
+        self.rotation = float(r[0])
+
+    def set_scale(self, scale=[1, 1, 1]):
+        self.scale = [float(n) for n in scale]
+
+    def set_shear(self, shear=[0, 0, 0]):
+        self.shear = [float(n) for n in shear]
+
+    def set_spin(self, spin=[0, 0, 0, 0]):
+        self.spin = [float(n) for n in spin]
