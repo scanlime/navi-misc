0b4a509b0d245a7db5ae73287fa248c40cc93f34
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-02-11 05:24:36 +0000

    stub out the beginnings of a zone object

---
+++ trunk/bzedit/scripts/BZFlag/Zone.py
@@ -0,0 +1,45 @@
+""" BZFlag.Zone
+
+BZFlag.Object implementing zones.
+"""
+#
+# Copyright (C) 2005 David Trowbridge
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+from Box import Box
+import Object
+import Blender
+
+class Zone(Box):
+    type = 'zone'
+
+    def __init__(self, list=None):
+        self.set_mode()
+        self.set_flag()
+        self.set_team()
+        self.set_safety()
+
+    def set_mode(self, mode='flag'):
+        pass
+
+    def set_flag(self, flag=['']):
+        pass
+
+    def set_team(self, team=['']):
+        pass
+
+    def set_safety(self, safety=['']):
+        pass
