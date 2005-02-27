a1fe8999a443ceee4b87fffb5e6f5fb5de6fef1c
Micah Elizabeth Scott <micah@scanlime.org>
2005-02-27 18:59:14 +0000

    Implemented the rest of the planned requests- those for reading and writing actual file contents. Haven't tested the code for writing yet, but reading seems to be mostly working

--- rio-karma/python/demo.py
+++ rio-karma/python/demo.py
@@ -3,6 +3,7 @@
 from twisted.internet import protocol, reactor
 from twisted.python import log
 import RioKarma
+import sys
 
 
 def connected(protocol):
@@ -16,9 +17,43 @@ def show(result):
 
 def synchronized(retval):
     print "Synchronized database, %d entries" % len(fs.cache.fileDetails)
-    i = fs.cache.fileIdAllocator.next()
-    print "new id: %d" % i
-    print "details: %r" % fs.cache.fileDetails.get(str(i))
+
+    def nextFile(i):
+        f = RioKarma.File(fs, i.next())
+        length = int(f.details.get('length'))
+        destName = f.pickFilename()
+        dest = open(destName, "wb")
+
+        print
+        print
+        print "         Title: %s" % f.details.get('title')
+        print "         Album: %s" % f.details.get('source')
+        print "        Artist: %s" % f.details.get('artist')
+        print "Downloading to: %s" % destName
+        print "        Length: %s" % length
+
+        nextChunk(0, f, 0, length, dest, i)
+
+    def nextChunk(readSize, f, offset, length, dest, i):
+        sys.stderr.write(".")
+
+        offset += readSize
+        length -= readSize
+        blockSize = min(length, 64*1024)
+
+        if length > 0:
+            fs.protocol.sendRequest(RioKarma.Request_ReadFileChunk(f.fileID, offset, blockSize, dest)).addCallback(
+                nextChunk, f, offset, length, dest, i).addErrback(log.err)
+        else:
+            nextFile(i)
+
+    #for filename in sys.argv[1:]:
+        #f = RioKarma.File(fs)
+        #f.loadMetadataFrom(filename)
+        #print f.details
+
+    nextFile(fs.cache.fileDetails.iterkeys())
+
 
 
 # Oh no, the mighty h4><0rz will break into my rio!
