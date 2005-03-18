e576993e58c0d4415362a5273622d9efd98c7c60
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-03-18 21:41:15 +0000

    add a StochasticBlur element, tweak some things and start in on a specialized ElementTooltip window

---
+++ trunk/fyre/pipeline-editor/plugins/basic/StochasticBlur.cs
@@ -0,0 +1,63 @@
+/*
+ * StochasticBlur.cs - An Element which takes a blur radius and ratio
+ *	and perturbs it's input point randomly according to the 
+ *	probability given in the ratio.
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004-2005 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using Gdk;
+
+
+class StochasticBlur : Element
+{
+	private static Gdk.Pixbuf icon;
+
+	public override string Name ()
+	{
+		return "Stochastic Blur";
+	}
+
+	public override string Category()
+	{
+		return "Arithmetic";
+	}
+
+	public override Gdk.Pixbuf Icon ()
+	{
+		if (icon == null)
+			icon = new Gdk.Pixbuf ("/usr/share/fyre/2.0/StochasticBlur.png");
+		return icon;
+	}
+
+	public override string Description ()
+	{
+		return "Creates a blur by\nstochastically perturbing\na point within the\ngiven radius\n";
+	}
+
+	public override string InputDesc ()
+	{
+		return "<i>v<sub>0</sub></i>:  point\n" + "<i>ratio</i>\n" + "<i>radius</i>";
+	}
+
+	public override string OutputDesc ()
+	{
+		return "<i>v<sub>1</sub></i>:  new point";
+	}
+}
