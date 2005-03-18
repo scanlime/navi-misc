fc14a1f148a40579c262644f0c36ce5d8a09a609
David Trowbridge <trowbrds@gmail.com>
2005-03-18 21:41:15 +0000

    add a StochasticBlur element, tweak some things and start in on a specialized ElementTooltip window

---
+++ fyre/pipeline-editor/ElementTooltip.cs
@@ -0,0 +1,34 @@
+/*
+ * ElementTooltip.cs - a widget which displays a pretty tooltip
+ *	with information about an Element.
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004-2005 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using System;
+using Gtk;
+using GtkSharp;
+
+public class ElementTooltip : Window
+{
+	public ElementTooltip (Element e) : base (WindowType.Popup)
+	{
+		this.Resizable = false;
+	}
+}
