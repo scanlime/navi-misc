4e1246c79e962e2f23bfdc82b0deaf4fb9cc1560
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-03-18 23:47:22 +0000

    less code duplication

--- trunk/fyre/pipeline-editor/ElementTooltip.cs
+++ trunk/fyre/pipeline-editor/ElementTooltip.cs
@@ -49,42 +49,31 @@ public class ElementTooltip
 			e.Description () +
 			"</span>";
 
-		string[,] inputs = e.InputDesc ();
-		Gtk.Table input = new Gtk.Table ((uint) inputs.Length / 2, 2, false);
-		for (uint i = 0; i < inputs.Length / 2; i++) {
-			Gtk.Label variable = new Gtk.Label ();
-			Gtk.Label desc = new Gtk.Label ();
-			
-			variable.Markup =
-				"<span size=\"small\">" +
-				inputs[i,0] +
-				"</span>";
-			desc.Markup =
-				"<span size=\"small\">" +
-				inputs[i,1] +
-				"</span>";
-			input.Attach (variable, 0, 1, i, i+1);
-			input.Attach (desc, 1, 2, i, i+1);
-		}
-		this.inputs.PackStart (input, false, true, 0);
+		this.inputs.PackStart  (CreateDescTable (e.InputDesc  ()), false, true, 0);
+		this.outputs.PackStart (CreateDescTable (e.OutputDesc ()), false, true, 0);
+	}
 
-		string[,] outputs = e.OutputDesc ();
-		Gtk.Table output = new Gtk.Table ((uint) outputs.Length / 2, 2, false);
-		for (uint i = 0; i < outputs.Length / 2; i++) {
+	/* Create a 2xn table from a list of strings */
+	private Gtk.Table CreateDescTable (string[,] s)
+	{
+		uint len = (uint) s.Length / 2;
+		Gtk.Table t = new Gtk.Table (len, 2, false);
+		for (uint i = 0; i < len; i++) {
 			Gtk.Label variable = new Gtk.Label ();
-			Gtk.Label desc = new Gtk.Label ();
+			Gtk.Label desc     = new Gtk.Label ();
 			
 			variable.Markup =
 				"<span size=\"small\">" +
-				outputs[i,0] +
+				s[i,0] +
 				"</span>";
 			desc.Markup =
 				"<span size=\"small\">" +
-				inputs[i,1] +
+				s[i,1] +
 				"</span>";
-			output.Attach (variable, 0, 1, i, i+1);
-			output.Attach (desc, 1, 2, i, i+1);
+		
+			t.Attach (variable, 0, 1, i, i+1);
+			t.Attach (desc,     1, 2, i, i+1);
 		}
-		this.outputs.PackStart (output, false, true, 0);
+		return t;
 	}
 }
