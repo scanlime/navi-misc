17d2116871dd87e419a976d7834115aae3906e43
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-03-19 06:59:34 +0000

    add a new plugin for the HistogramImager, tweak sizes

---
+++ trunk/fyre/pipeline-editor/plugins/histogram-imager/HistogramImager.cs
@@ -0,0 +1,74 @@
+/*
+ * HistogramImager.cs - An Element which implements the histogram
+ *	imager algorithm.
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004-2005 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using Gdk;
+
+
+class HistogramImager : Element
+{
+	static Gdk.Pixbuf icon;
+	static string[,] inputs;
+	static string[,] outputs;
+
+	public override string Name ()
+	{
+		return "Histogram Imager";
+	}
+
+	public override string Category()
+	{
+		return "Renderers";
+	}
+
+	public override Gdk.Pixbuf Icon ()
+	{
+		if (icon == null)
+			icon = new Gdk.Pixbuf ("/usr/share/fyre/2.0/HistogramImager.png");
+		return icon;
+	}
+
+	public override string Description ()
+	{
+		return "Maintains a histogram\nof the points given to\nit, suitable for\ntone-mapping.\n";
+	}
+
+	public override string[,] InputDesc ()
+	{
+		if (inputs == null) {
+			inputs = new string[4,2];
+			inputs[0,0] = "<i>v</i>";	inputs[0,1] = "point";
+			inputs[1,0] = "<i>w</i>";	inputs[1,1] = "width";
+			inputs[2,0] = "<i>h</i>";	inputs[2,1] = "height";
+		}
+		return inputs;
+	}
+
+	public override string[,] OutputDesc ()
+	{
+		if (outputs == null) {
+			outputs = new string[1,2];
+			outputs[0,0] = "<b>M</b>";	outputs[0,1] = "image";
+		}
+		return outputs;
+	}
+}
