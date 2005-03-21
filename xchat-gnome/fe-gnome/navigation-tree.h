ef72a60b0abbec2c17b2ca7b68456296ae4d81f6
Evan Sheehan <darthmall@users.noreply.github.com>
2005-03-21 21:04:36 +0000

    rename navigation_tree_server_connected

--- xchat-gnome/fe-gnome/navigation-tree.h
+++ xchat-gnome/fe-gnome/navigation-tree.h
@@ -76,7 +76,7 @@ void navigation_tree_select_prev_network (NavTree *navtree);
 
 /* Misc. functions. */
 void navigation_tree_set_channel_name (NavTree *navtree, struct session *sess);
-gboolean navigation_tree_server_connected (NavTree *navtree, gchar *name);
+gboolean navigation_tree_server_is_connected (NavTree *navtree, gchar *name);
 NavTree* get_navigation_tree ();
 
 
