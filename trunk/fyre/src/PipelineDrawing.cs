0958482c163563d70d204f865b697d1827e82a39
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-03-26 05:27:38 +0000

    more scrolley stuff

--- trunk/fyre/src/PipelineDrawing.cs
+++ trunk/fyre/src/PipelineDrawing.cs
@@ -50,8 +50,19 @@ class PipelineDrawing : Gtk.DrawingArea
 			drawing_y = size_y;
 
 		if (hadj != null) {
-			hadj.Upper = (float) ev.Width;
-			vadj.Upper = (float) ev.Height;
+			float w = (float) ev.Width;
+			float h = (float) ev.Height;
+
+			// temporarily set a big region. this will need to be replaced by
+			// tracking the drawing on the page
+			hadj.Upper = w * 8;
+			vadj.Upper = h * 8;
+
+			// set our page size to be wxh
+			hadj.PageSize = w;
+			vadj.PageSize = h;
+			hadj.PageIncrement = w;
+			vadj.PageIncrement = h;
 		}
 
 		return base.OnConfigureEvent (ev);
@@ -59,17 +70,18 @@ class PipelineDrawing : Gtk.DrawingArea
 
 	protected override void OnSetScrollAdjustments (Adjustment hadj, Adjustment vadj)
 	{
+		// set step size to 10px, everything else to 0
 		hadj.Value         = 0.0f;
 		hadj.Upper         = 0.0f;
 		hadj.Lower         = 0.0f;
-		hadj.StepIncrement = 0.0f;
+		hadj.StepIncrement = 10.0f;
 		hadj.PageIncrement = 0.0f;
 		hadj.PageSize      = 0.0f;
 
 		vadj.Value         = 0.0f;
 		vadj.Upper         = 0.0f;
 		vadj.Lower         = 0.0f;
-		vadj.StepIncrement = 0.0f;
+		vadj.StepIncrement = 10.0f;
 		vadj.PageIncrement = 0.0f;
 		vadj.PageSize      = 0.0f;
 
