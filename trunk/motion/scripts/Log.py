00da4c13bbba26f0e3fda5590f84e48d5c589c39
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-04-06 07:40:56 +0000

    hmm, maybe on to something...

--- trunk/motion/scripts/Log.py
+++ trunk/motion/scripts/Log.py
@@ -19,6 +19,8 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
+import Blender
+
 class Logger:
     """This is a singleton that collects error and warning messages, presenting
        them to the user in batches.
