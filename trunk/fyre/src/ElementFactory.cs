96be9465de694e1d18bffff35fe2917c1b3e3743
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-04-08 00:24:58 +0000

    add a factory for Elements, which will make it easier to manage creation of Elements

---
+++ trunk/fyre/src/ElementFactory.cs
@@ -0,0 +1,66 @@
+/*
+ * ElementFactory.cs - factory method which creates Elements based on
+ *	a string or Type representation.
+ *
+ * Fyre - a generic framework for computational art
+ * Copyright (C) 2004-2005 Fyre Team (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using System.Collections;
+using System.Reflection;
+
+namespace Fyre
+{
+
+	class ElementFactory
+	{
+		// This maps name->Type, so we can create Elements when deserializing
+		// XML or dragging from the ElementList
+		Hashtable	elements;
+
+		public
+		ElementFactory ()
+		{
+			elements = new Hashtable ();
+		}
+
+		public void
+		AddType (System.Type t)
+		{
+			Element e = Create (t);
+			string name = e.Name ();
+			elements.Add (name, t);
+		}
+
+		public Element
+		Create (string name)
+		{
+			System.Type t = (System.Type) elements[name];
+			return Create (t);
+		}
+
+		public Element
+		Create (System.Type t)
+		{
+			object[] i = {};
+			Element e = (Element) t.GetConstructor(System.Type.EmptyTypes).Invoke(i);
+
+			return e;
+		}
+	}
+}
