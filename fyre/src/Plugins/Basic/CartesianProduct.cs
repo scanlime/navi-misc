542637f3682ba476dad773a14be93155851a3135
David Trowbridge <trowbrds@gmail.com>
2005-04-08 01:31:22 +0000

    add a Cartesian Product element

---
+++ fyre/src/Plugins/Basic/CartesianProduct.cs
@@ -0,0 +1,99 @@
+/*
+ * CartesianProduct.cs - An Element which takes two ranges of integers
+ *	and outputs a matrix of the cartesian product.
+ *	FIXME - we may want to have this take other "generators" as inputs,
+ *	rather than just restricting it to integers
+ *
+ * Fyre - a generic framework for computational art
+ * Copyright (C) 2004-2005 Fyre Team (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using System.Xml;
+
+class CartesianProduct : Fyre.Element
+{
+	static Gdk.Pixbuf icon;
+	static string[,] inputs;
+	static string[,] outputs;
+
+	public
+	CartesianProduct ()
+	{
+		NewCanvasElement ();
+		NewID ();
+	}
+
+	public override string
+	Name ()
+	{
+		return "Cartesian Product";
+	}
+
+	public override string
+	Category()
+	{
+		return "Arithmetic";
+	}
+
+	public override Gdk.Pixbuf
+	Icon ()
+	{
+		if (icon == null)
+			icon = new Gdk.Pixbuf (null, "CartesianProduct.png");
+		return icon;
+	}
+
+	public override string
+	Description ()
+	{
+		return "Computes the cartesian product\nof two integer ranges";
+	}
+
+	public override string[,]
+	InputDesc ()
+	{
+		if (inputs == null) {
+			inputs = new string[2,2];
+			inputs[0,0] = "<i>(x<sub>0</sub>,x<sub>1</sub>)</i>";	inputs[0,1] = "X range";
+			inputs[1,0] = "<i>(y<sub>0</sub>,y<sub>1</sub>)</i>";	inputs[1,1] = "Y range";
+		}
+		return inputs;
+	}
+
+	public override string[,]
+	OutputDesc ()
+	{
+		if (outputs == null) {
+			outputs = new string[1,2];
+			outputs[0,0] = "<b>M</b>";	outputs[0,1] = "matrix";
+		}
+		return outputs;
+	}
+
+	public override void
+	Serialize (XmlWriter writer)
+	{
+		base.Serialize (writer);
+	}
+
+	public override void
+	DeSerialize (XmlReader reader)
+	{
+		base.DeSerialize (reader);
+	}
+}
