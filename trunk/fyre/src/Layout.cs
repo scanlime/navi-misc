ff059734728f54f98ea8f8b00137ed88e02657df
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-04-18 22:32:11 +0000

    start adding some API

--- trunk/fyre/src/Layout.cs
+++ trunk/fyre/src/Layout.cs
@@ -31,10 +31,41 @@ namespace Fyre
 	{
 		Hashtable		elements;
 
+		public Gdk.Rectangle	Extents
+		{
+			get {
+				// Compute the layout extents. We do this by taking a union of all
+				// the rectangles of each element.
+				IDictionaryEnumerator e = elements.GetEnumerator ();
+				e.Reset ();
+				Gdk.Rectangle extents;
+
+				// If we don't have anything on that layout, just return 0x0+0+0
+				extents.Width = extents.Height = 0;
+				extents.X = extents.Y = 0;
+				if (e.MoveNext ())
+					extents = (Gdk.Rectangle) e.Current;
+				else
+					return extents;
+
+				while (e.MoveNext ()) {
+					Gdk.Rectangle r = (Gdk.Rectangle) e.Current;
+					extents = extents.Union (r);
+				}
+
+				return extents;
+			}
+		}
+
 		public
 		Layout ()
 		{
 			elements = new Hashtable ();
+		}
+
+		public void
+		Draw (System.Drawing.Graphics context)
+		{
 		}
 	}
 }
