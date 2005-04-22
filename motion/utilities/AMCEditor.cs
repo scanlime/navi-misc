4831fcdaa3339137c50101660fc58120c238687f
David Trowbridge <trowbrds@gmail.com>
2005-04-22 23:16:04 +0000

    progress on implementation

--- motion/utilities/AMCEditor.cs
+++ motion/utilities/AMCEditor.cs
@@ -20,9 +20,80 @@
  *
  */
 
+class CellRendererColor : Gtk.CellRenderer
+{
+}
+
 class AMCEditor
 {
 	[Glade.Widget] Gtk.Window		toplevel;
 	[Glade.Widget] Gtk.ScrolledWindow	editor_swin;
 	[Glade.Widget] Gtk.TreeView		bone_list;
+
+	Gtk.TreeStore				bone_store;
+
+	public static void
+	Main (string[] args)
+	{
+		Gtk.Application.Init ();
+		new AMCEditor ();
+		Gtk.Application.Run ();
+	}
+
+	private
+	AMCEditor ()
+	{
+		Glade.XML.SetCustomHandler (new Glade.XMLCustomWidgetHandler (GladeCustomHandler));
+		Glade.XML gxml = new Glade.XML (null, "amc-editor.glade", "toplevel", null);
+		gxml.Autoconnect (this);
+
+		// Create the tree store
+		bone_store = new Gtk.TreeStore (
+				typeof (string),	// name
+				typeof (Gdk.Color),	// color
+				typeof (bool));		// shown
+		bone_list.Model = bone_store;
+
+		// Create our text column
+		Gtk.TreeViewColumn text_column  = new Gtk.TreeViewColumn ();
+		Gtk.CellRenderer text_renderer = new Gtk.CellRendererText ();
+		text_column.PackStart (text_renderer, true);
+		text_column.AddAttribute (text_renderer, "text", 0);
+		bone_list.AppendColumn (text_column);
+
+		// Create our color column
+		Gtk.TreeViewColumn color_column = new Gtk.TreeViewColumn ();
+		Gtk.CellRenderer color_renderer = new CellRendererColor ();
+		color_column.PackStart (color_renderer, false);
+		color_column.AddAttribute (color_renderer, "color",   1);
+		color_column.AddAttribute (color_renderer, "enabled", 2);
+	}
+
+	static Gtk.Widget
+	GladeCustomHandler (Glade.XML xml, string func_name, string name, string str1, string str2, int int1, int int2)
+	{
+		return null;
+	}
+
+	// Signal handlers
+	public void
+	OnOpen (object o, System.EventArgs args)
+	{
+	}
+
+	public void
+	OnSave (object o, System.EventArgs args)
+	{
+	}
+
+	public void
+	OnSaveAs (object o, System.EventArgs args)
+	{
+	}
+
+	public void
+	OnQuit (object o, System.EventArgs args)
+	{
+		Gtk.Application.Quit ();
+	}
 }
