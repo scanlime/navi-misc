7d423f3eba33bcce23fdf878ef424806478e1b6f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-06-14 06:28:07 +0000

    add the beginnings of a python extension module for dealing with this data. i suppose this will eventually involve porting my amc editor to python and making the blender stuff use this to load

---
+++ trunk/motion/Motion/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup, Extension
+
+load_amc = Extension('load_amc', sources=['load_amc.c'])
+
+setup (name='Motion', version='1.0', description='Modules for dealing with motion-capture data', ext_modules=[load_amc])
