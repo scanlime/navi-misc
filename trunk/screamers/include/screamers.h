5b9dc03fc8614d351fa4f6c11c0559c2e8b36f79
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-06-17 01:50:01 +0000

    no need for a separate win32 header just for a couple includes (at least for now), ws

--- trunk/screamers/include/screamers.h
+++ trunk/screamers/include/screamers.h
@@ -1,18 +1,22 @@
 /* screamers
-* Copyright (c) 2005 ScreamersGroup
-*
-* This package is free software;  you can redistribute it and/or
-* modify it under the terms of the license found in the file
-* named COPYING that should have accompanied this file.
-*
-* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) 2005 ScreamersGroup
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #ifndef _SCREAMERS_H_
 #define _SCREAMERS_H_
 
-#include "win32.h"
+// windows-specific includes
+#ifdef _WIN32
+#include "resource.h"
+#include <windows.h>
+#endif
 
 #endif //_SCREAMERS_H_
--- trunk/screamers/include/version.h
+++ trunk/screamers/include/version.h
@@ -1,14 +1,14 @@
 /* screamers
-* Copyright (c) 2005 ScreamersGroup
-*
-* This package is free software;  you can redistribute it and/or
-* modify it under the terms of the license found in the file
-* named COPYING that should have accompanied this file.
-*
-* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-* WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * Copyright (c) 2005 ScreamersGroup
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #ifndef _VERSION_H_
 #define _VERSION_H_
