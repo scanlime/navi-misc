e0b0996b63154183a4610b613803e988a3e7de3a
David Trowbridge <trowbrds@gmail.com>
2005-06-20 20:40:57 +0000

    pull in glib, 'cuz C without it is crummy

--- motion/Motion/setup.py
+++ motion/Motion/setup.py
@@ -1,5 +1,15 @@
 from distutils.core import setup, Extension
+import os
 
-load_amc = Extension('motion_c', sources=['motion.c'])
+# get compile info for glib from pkg-config
+pkgconfig = os.popen ('pkg-config --cflags glib-2.0')
+glib_cflags = pkgconfig.readline ().strip ().split(' ')
+pkgconfig.close ()
+pkgconfig = os.popen ('pkg-config --libs glib-2.0')
+glib_libs = pkgconfig.readline ().strip ().split(' ')
+pkgconfig.close ()
 
-setup (name='Motion', version='1.0', description='Modules for dealing with motion-capture data', ext_modules=[load_amc])
+# build!
+motion_c = Extension('motion_c', sources=['motion.c'], extra_compile_args=glib_cflags, extra_link_args=glib_libs)
+
+setup (name='Motion', version='1.0', description='Modules for dealing with motion-capture data', ext_modules=[motion_c])
