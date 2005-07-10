2f3cc519ac789ea1ffd285896968f26024ab089f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-07-10 21:49:42 +0000

    some work towards implementing the scrollable interface, but apparently that's not possible in pygtk just yet. will have to replace this with individual scrollbars in a table

--- trunk/motion/py/amc_editor/CurveEditor.py
+++ trunk/motion/py/amc_editor/CurveEditor.py
@@ -18,12 +18,19 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
+import gobject
 import gtk
 import gtk.gdk
 import pango
 
 class CurveEditor (gtk.DrawingArea):
+    __gsignals__ = {
+        'set-scroll-adjustments' : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gtk.Adjustment, gtk.Adjustment))
+    }
+
     def __init__ (self):
+        gobject.GObject.__init__ (self)
+
         self._enabled_bones = {}
         self._pad_positions = []
         self._selected_pads = []
@@ -68,5 +75,20 @@ class CurveEditor (gtk.DrawingArea):
     def _create_back_buffer (self):
         self._back_buffer = gtk.gdk.Pixmap (self.window, self.allocation.width, self.allocation.height)
 
-    def _set_scroll_adjustments (self, hadjustment, vadjustment, data=None):
-        pass
+    def set_scroll_adjustments (self, hadjustment, vadjustment, data=None):
+        self.hadj = hadjustment
+        self.vadj = vadjustment
+
+        self.hadj.lower          = 0;
+        self.hadj.upper          = 1;
+        self.hadj.step_increment = 0;
+        self.hadj.page_size      = 1;
+        self.hadj.page_increment = 0;
+        self.hadj.value          = 0;
+
+        self.vadj.lower          = 0;
+        self.vadj.upper          = 1;
+        self.vadj.step_increment = 0;
+        self.vadj.page_size      = 1;
+        self.vadj.page_increment = 0;
+        self.vadj.value          = 0;
