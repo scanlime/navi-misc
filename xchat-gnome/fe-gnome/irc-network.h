680582a4550e87399b89a70d076385613aa662a8
David Trowbridge <trowbrds@gmail.com>
2005-07-22 00:21:21 +0000

    fix copyright lines

--- xchat-gnome/fe-gnome/irc-network.h
+++ xchat-gnome/fe-gnome/irc-network.h
@@ -1,7 +1,7 @@
 /*
  * irc-network.h - an object which interfaces around the core 'ircnet'
  *
- * Copyright (C) 2005 David Trowbridge
+ * Copyright (C) 2004-2005 xchat-gnome team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- xchat-gnome/fe-gnome/util.h
+++ xchat-gnome/fe-gnome/util.h
@@ -1,7 +1,7 @@
 /*
  * util.h: Helper functions for miscellaneous tasks
  *
- * Copyright (C) 2005 David Trowbridge
+ * Copyright (C) 2004-2005 xchat-gnome team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
