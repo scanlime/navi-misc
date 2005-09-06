4fda4ada63b0898dbe72b517aea589866b258614
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-09-06 21:41:33 +0000

    Beginnings of the shuffle nonsense. Just stubs at the moment.

---
+++ trunk/motion/py/Dance/Sequence.py
@@ -0,0 +1,21 @@
+""" Sequence
+
+Map a sequence to a chaotic attractor and shuffle it.
+"""
+
+class Sequence:
+    """ This class represents a dance sequence mapped to an attractor.
+        """
+    def __init__ (self, func, mcd):
+        """ Each Sequence object requires a chaotic function (func) and some
+            motion capture data (mcd).
+            """
+        pass
+
+    def shuffle (self, ics):
+        """ shuffle takes a set of initial conditions (ics) and returns a new
+            sequence object containing the shuffled dance.
+            """
+        pass
+
+# vim:ts=4:sw=4:et:tw=80
