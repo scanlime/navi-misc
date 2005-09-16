01438715f7b575cf705dfd6a6895b9ab8e6bbc58
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-09-16 02:13:52 +0000

    Some of the test stuff I was working on earlier and forgot to commit

--- trunk/motion/py/Test/test_dance.py
+++ trunk/motion/py/Test/test_dance.py
@@ -3,7 +3,21 @@
 Test the Dance module.
 """
 
-import unittest
-from Dance import Sequence, ODE, Systems
+from unittest import TestCase
+from Dance import ODE, Systems
+
+
+class TestSystem (TestCase):
+    def testCalc (self):
+        self.fail ("Unimplemented")
+
+
+class TestODE (TestCase):
+    def testStep (self):
+        self.fail ("Unimplemented")
+
+    def testSolve (self):
+        self.fail ("Unimplemented")
+
 
 # vim:ts=4:sw=4:et:tw=80
