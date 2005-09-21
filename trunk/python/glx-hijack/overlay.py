93ac687a11375bbf0f9341f21718e20c136f96b3
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-09-21 03:11:07 +0000

    Back out the hyper-experimental and mostly completely-broken changes to overlay.py

--- trunk/python/glx-hijack/overlay.py
+++ trunk/python/glx-hijack/overlay.py
@@ -1,100 +1,32 @@
-import sys, os, os.path, soya, math
-from random import random
-import soya.pudding as pudding
 from soya import opengl as gl
+import soya
 
-class BlueSmokeEmitter(soya.Smoke):
-    spark_material = soya.Material()
-    spark_material.additive_blending = 1
-    spark_material.texture = soya.open_image("blue_spark.png")
-    t1 = 0.0
-    t2 = 0.0
+a = 0
 
-    def __init__(self,parent):
-        soya.Particles.__init__(self,parent,nb_max_particles=150)
-
-        self.material = self.spark_material
-
-        self.set_colors((1.0, 1.0, 1.0, 1.0),
-                        (0.0, 0.0, 0.0, 0.0))
-        self.set_sizes((0.3, 0.3),
-                       (0.0, 0.0))
-        self.auto_generate_particle = 1
-
-    def generate(self, index):
-        sx = (random() - 0.5) * 0.05
-        sy = random() * 0.1
-        sz = (random() - 0.5) * 0.05
-        l = 2.0 * random()
-        self.set_particle(index,
-                          l,           # life
-                          sx, sy, sz,  # speed
-                          0.,0.,0.)    # acceleration
-
-    def advance_time(self, proportion):
-        soya.Smoke.advance_time(self, proportion)
-
-        self.t1 += 0.4 * proportion
-        r = 1.0
-
-        self.set_xyz(math.sin(self.t1)*r,
-                     math.cos(self.t1)*r - r,
-                     0)
-
-soya.init(create_surface=0)
-soya.path.append(os.getcwd())
-
-scene = soya.World()
-scene.atmosphere = soya.NoBackgroundAtmosphere()
-
-pudding.init()
-w = pudding.core.RootWidget()
-soya.set_root_widget(w)
-w.resize(0, 0, 640, 480)
-
-l = pudding.control.SimpleLabel(w)
-l.font = soya.Font("geodesic.ttf", 100, 100)
-l.label = "wasabi"
-l.update()
-l.left = (w.width - l.width) / 2
-l.top = w.height / 4 - l.height / 2
-print l.top, l.left
-
-particles = BlueSmokeEmitter(scene)
-
-light = soya.Light(scene)
-light.set_xyz(0.5, 0.0, 2.0)
-
-camera = soya.Camera(scene)
-camera.z = 10.0
-camera.resize(0, 0, 640, 480)
-
-idler = soya.Idler(scene)
 def _frame():
-    idler.advance_time(1.0)
+    global a
 
-    gl.glEnable(gl.GL_BLEND)
     gl.glDisable(gl.GL_TEXTURE_2D)
+    gl.glEnable(gl.GL_BLEND)
 
-    if 0:
-        gl.glPushMatrix()
-        gl.glLoadIdentity()
+    gl.glPushMatrix()
+    gl.glLoadIdentity()
 
-        gl.glColor4f(0.2, 0.2, 0.2, 1.0)
-        gl.glBegin(gl.GL_QUADS)
-        s = 0.25
-        gl.glVertex3f(-s,-s,0)
-        gl.glVertex3f(s,-s,0)
-        gl.glVertex3f(s,s,0)
-        gl.glVertex3f(-s,s,0)
-        gl.glEnd()
-        gl.glPopMatrix()
+    gl.glRotatef(a, 1.0, 0.2, 0.0)
+    a += 2
 
-    camera.render()
+    gl.glColor4f(0.2, 0.2, 0.2, 1.0)
+    gl.glBegin(gl.GL_QUADS)
+    s = 0.25
+    gl.glVertex3f(-s,-s,0)
+    gl.glVertex3f(s,-s,0)
+    gl.glVertex3f(s,s,0)
+    gl.glVertex3f(-s,s,0)
+    gl.glEnd()
 
-    gl.glEnable(gl.GL_TEXTURE_2D)
+    gl.glPopMatrix()
     gl.glDisable(gl.GL_BLEND)
-
+    gl.glEnable(gl.GL_TEXTURE_2D)
 
 def frame():
     try:
@@ -102,3 +34,4 @@ def frame():
     except:
         import traceback
         traceback.print_exc()
+
