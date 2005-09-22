91b55b75b15c33e897adc92f6b5de08912c275f6
David Trowbridge <trowbrds@gmail.com>
2005-09-22 06:08:06 +0000

    neato override on glViewport to automatically set soya's screen size to whatever the player is using

--- python/glx-hijack/glx-hijack.c
+++ python/glx-hijack/glx-hijack.c
@@ -4,17 +4,24 @@
 #include <dlfcn.h>
 #include <Python.h>
 
+static PyObject *module = NULL;
+
+void init() {
+  Py_Initialize ();
+  module = PyImport_ImportModule(getenv("PY_MODULE"));
+  if (!module) {
+    PyErr_Print();
+    exit(1);
+  }
+}
+
 void glXSwapBuffers(void *display, void *drawable) {
   static void (*orig)(void*, void*) = NULL;
-  static PyObject *module, *func, *result;
+  static PyObject *func, *result;
 
   if (!orig) {
-    Py_Initialize();
-    module = PyImport_ImportModule(getenv("PY_MODULE"));
-    if (!module) {
-      PyErr_Print();
-      exit(1);
-    }
+    if (!module)
+      init ();
     func = PyObject_GetAttrString(module, "frame");
     if (!func) {
       PyErr_Print();
@@ -33,4 +40,28 @@ void glXSwapBuffers(void *display, void *drawable) {
 
   orig(display, drawable);
 }
-  
+
+void glViewport(int x, int y, int width, int height) {
+  static void (*orig)(int, int, int, int) = NULL;
+  static PyObject *func, *result;
+
+  if (!orig) {
+    if (!module)
+      init ();
+    func = PyObject_GetAttrString(module, "viewport");
+    if (!func) {
+      PyErr_Print();
+      exit(1);
+    }
+    orig = dlsym(RTLD_NEXT, "glViewport");
+  }
+
+  orig(x, y, width, height);
+
+  result = PyObject_CallFunction(func, "iiii", x, y, width, height);
+  if (!result) {
+    PyErr_Print();
+    exit(1);
+  }
+  Py_DECREF(result);
+}
