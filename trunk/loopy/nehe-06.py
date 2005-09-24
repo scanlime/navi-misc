b08ddca9332054598f78ef4a0f2f223e75facbe4
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-09-24 13:26:06 +0000

    Ported NeHe example 6 to a loopy.Overlay object. I left in the (now unnecessary) pygame initialization to test the fake SDL support present in Loopy.

--- trunk/loopy/nehe-06.py
+++ trunk/loopy/nehe-06.py
@@ -1,127 +1,112 @@
 #!/usr/bin/env python
 # pygame + PyOpenGL version of Nehe's OpenGL lesson06
 # Paul Furber 2001 - m@verick.co.za
+# Loopy version - Micah Dowty
 
-import os
+import os, loopy
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import pygame, pygame.image
 from pygame.locals import *
 
-xrot = yrot = zrot = 0.0
-textures = [0,0]
+class NeheOverlay(loopy.Overlay):
+    def __init__(self):
+        loopy.Overlay.__init__(self)
+        self.xrot = 0.0
+        self.yrot = 0.0
+        self.zrot = 0.0
+        self.textures = [0]
 
-def resize((width, height)):
-    if height==0:
-        height=1
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45, 1.0*width/height, 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
+    def resize(self):
+        width, height = self.resolution
 
-def init():
-    glEnable(GL_TEXTURE_2D)
-    load_textures()
-    glShadeModel(GL_SMOOTH)
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-    glClearDepth(1.0)
-    glEnable(GL_DEPTH_TEST)
-    glDepthFunc(GL_LEQUAL)
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45, 1.0*width/height, 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
 
-def load_textures():
-    texturefile = os.path.join('data','nehe.bmp')
-    textureSurface = pygame.image.load(texturefile)
-    textureData = pygame.image.tostring(textureSurface, "RGBX", 1)
+    def setup(self):
+        video_flags = OPENGL|DOUBLEBUF
 
-    glBindTexture(GL_TEXTURE_2D, textures[0])
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, textureSurface.get_width(), textureSurface.get_height(), 0,
-                  GL_RGBA, GL_UNSIGNED_BYTE, textureData );
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        pygame.init()
+        surface = pygame.display.set_mode((640,480), video_flags)
 
+        glEnable(GL_TEXTURE_2D)
+        self.load_textures()
+        glShadeModel(GL_SMOOTH)
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClearDepth(1.0)
+        glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LEQUAL)
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
 
-def draw():
-    global xrot, yrot, zrot
+        self.resize()
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-    glTranslatef(0.0,0.0,-5.0)
+    def load_textures(self):
+        texturefile = os.path.join('data','nehe.bmp')
+        textureSurface = pygame.image.load(texturefile)
+        textureData = pygame.image.tostring(textureSurface, "RGBX", 1)
 
-    glRotatef(xrot,1.0,0.0,0.0)
-    glRotatef(yrot,0.0,1.0,0.0)
-    glRotatef(zrot,0.0,0.0,1.0)
-    
-    
-    glBegin(GL_QUADS)
-	
-    # Front Face (note that the texture's corners have to match the quad's corners)
-    glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)	# Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)	# Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)	# Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)	# Top Left Of The Texture and Quad
-	
-    # Back Face
-    glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0)	# Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)	# Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)	# Top Left Of The Texture and Quad
-    glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0)	# Bottom Left Of The Texture and Quad
-	
-    # Top Face
-    glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)	# Top Left Of The Texture and Quad
-    glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0)	# Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0)	# Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)	# Top Right Of The Texture and Quad
-	
-    # Bottom Face       
-    glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0)	# Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0)	# Top Left Of The Texture and Quad
-    glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)	# Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)	# Bottom Right Of The Texture and Quad
-	
-    # Right face
-    glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0)	# Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)	# Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)	# Top Left Of The Texture and Quad
-    glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)	# Bottom Left Of The Texture and Quad
-	
-    # Left Face
-    glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0)	# Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)	# Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)	# Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)	# Top Left Of The Texture and Quad
-	
-    glEnd();				
-    
-    xrot = xrot + 0.2
-    yrot = yrot + 0.2
-    zrot = zrot + 0.2
-        
+        glBindTexture(GL_TEXTURE_2D, self.textures[0])
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA,
+                      textureSurface.get_width(), textureSurface.get_height(), 0,
+                      GL_RGBA, GL_UNSIGNED_BYTE, textureData );
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
-def main():
+    def render(self):
+        glClear(GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslatef(0.0,0.0,-5.0)
 
-    video_flags = OPENGL|DOUBLEBUF
-    
-    pygame.init()
-    surface = pygame.display.set_mode((640,480), video_flags)
+        glRotatef(self.xrot, 1.0,0.0,0.0)
+        glRotatef(self.yrot, 0.0,1.0,0.0)
+        glRotatef(self.zrot, 0.0,0.0,1.0)
 
-    resize((640,480))
-    init()
+        glBegin(GL_QUADS)
 
-    frames = 0
-    ticks = pygame.time.get_ticks()
-    while 1:
-        event = pygame.event.poll()
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            break
-        
-        draw()
-        pygame.display.flip()
-        frames = frames+1
+        # Front Face (note that the texture's corners have to match the quad's corners)
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)	# Bottom Left Of The Texture and Quad
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)	# Bottom Right Of The Texture and Quad
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)	# Top Right Of The Texture and Quad
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)	# Top Left Of The Texture and Quad
 
-    print "fps:  %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks))
+        # Back Face
+        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0)	# Bottom Right Of The Texture and Quad
+        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)	# Top Right Of The Texture and Quad
+        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)	# Top Left Of The Texture and Quad
+        glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0)	# Bottom Left Of The Texture and Quad
 
+        # Top Face
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)	# Top Left Of The Texture and Quad
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0)	# Bottom Left Of The Texture and Quad
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0)	# Bottom Right Of The Texture and Quad
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)	# Top Right Of The Texture and Quad
 
-if __name__ == '__main__': main()
+        # Bottom Face
+        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0)	# Top Right Of The Texture and Quad
+        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0)	# Top Left Of The Texture and Quad
+        glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)	# Bottom Left Of The Texture and Quad
+        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)	# Bottom Right Of The Texture and Quad
+
+        # Right face
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0)	# Bottom Right Of The Texture and Quad
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)	# Top Right Of The Texture and Quad
+        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)	# Top Left Of The Texture and Quad
+        glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)	# Bottom Left Of The Texture and Quad
+
+        # Left Face
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0)	# Bottom Left Of The Texture and Quad
+        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)	# Bottom Right Of The Texture and Quad
+        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)	# Top Right Of The Texture and Quad
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)	# Top Left Of The Texture and Quad
+
+        glEnd();
+
+        self.xrot += 0.2
+        self.yrot += 0.2
+        self.zrot += 0.2
+
+loopy.overlays.append(NeheOverlay())
