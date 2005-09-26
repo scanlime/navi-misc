73952dc1eea5f1a137f659eb9b18fb74f775b2ac
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-09-26 00:43:31 +0000

    Use a function to create a TestSuite for the entire module. TestRK4 and TestSequence fail right now because they're not done.

--- trunk/motion/py/Dance/tests.py
+++ trunk/motion/py/Dance/tests.py
@@ -20,37 +20,48 @@ Unit tests for the Dance module.
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-import unittest, sys, Numeric
+import sys, Numeric
+from unittest import makeSuite, TestCase, TestSuite
 from ODE import RK4, Time
 from Systems import Lorenz
 from Sequence import Sequence
 
 
 def suite ():
-    return unittest.TestSuite ([TestRK4, TestLorenz])
+    tests = (makeSuite (TestRK4),
+            makeSuite (TestLorenz),
+            makeSuite (TestSequence))
+    return TestSuite (tests)
 
 
-class TestRK4 (unittest.TestCase):
+class TestRK4 (TestCase):
     def setUp (self):
         self.solver = RK4 (Lorenz (1, 1, 1))
 
     def testStep (self):
-        """Test an RK4 step."""
+        """Test an RK4 step"""
         self.fail ("Not implemented.")
 
 
-class TestLorenz (unittest.TestCase):
+class TestLorenz (TestCase):
     def setUp (self):
         self.system = Lorenz (1, 1, 1)
 
     def testEquation (self):
-        """Test the Lorenz system."""
+        """Test the Lorenz system"""
         self.assertEqual (Numeric.array ([1, -9, 2]), self.system ([2, 3, 4],
             None))
 
 
+class TestSequence (TestCase):
+    def testShuffle (self):
+        """Test the chaotic shuffle"""
+        self.fail ("Not implemented")
+
+
 if __name__ == "__main__":
-    unittest.main ()
+    from unittest import TextTestRunner
+    TextTestRunner (verbosity=2).run (suite ())
 
 
 # vim: ts=4:sw=4:et
