7de0b45a1fa9390912660159bdc8c569bb9fbcae
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-10-08 09:07:49 +0000

    Playing with a new blurring algorithm- Multiple repetitions of a separable box convolution. I've heard this is the same one photoshop uses. The demo looks quite good now, but it's a little pokey even on a fast video card. As it's configured now, this requires about 23 rendering passes.

--- trunk/loopy/pbuffer-blur.py
+++ trunk/loopy/pbuffer-blur.py
@@ -25,13 +25,18 @@ class Overlay(loopy.Overlay):
         GL.glMatrixMode(GL.GL_MODELVIEW)
         GL.glLoadIdentity()
 
+    def genTempBuffer(self, size=(256,256)):
+        b = loopy.RenderTarget(*size)
+        b.bind(GL.glGenTextures(1))
+        GL.glTexParameterf(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP)
+        GL.glTexParameterf(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP)
+        return b
+
     def setup(self):
         self.src = loadTexture("images/icon_ps2.png")
 
-        self.t1 = loopy.RenderTarget(256, 256)
-        self.t1.bind(GL.glGenTextures(1))
-        self.t2 = loopy.RenderTarget(256, 256)
-        self.t2.bind(GL.glGenTextures(1))
+        self.t1 = self.genTempBuffer()
+        self.t2 = self.genTempBuffer()
 
         self.square = GL.glGenLists(1)
         GL.glNewList(self.square, GL.GL_COMPILE)
@@ -50,57 +55,98 @@ class Overlay(loopy.Overlay):
 
         self.resized()
 
+    def drawIcons(self, offset=(0,0), scale=(0.3, 0.4), shadow=False):
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.src)
+        GL.glColor(1,1,1,1)
+        if shadow:
+            GL.glBlendFunc(GL.GL_ZERO, GL.GL_ONE_MINUS_SRC_ALPHA)
+        else:
+            GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
+
+        GL.glLoadIdentity()
+        GL.glTranslatef(offset[0], offset[1], 0)
+        GL.glScalef(scale[0], scale[1], 1)
+        GL.glCallList(self.square)
+
+    def drawShadowMask(self, buffer):
+        """In the given buffer, draw a mask which will eventually
+           be multiplied by the image to generate a shadow.
+           """
+        GL.glViewport(0, 0, buffer.w, buffer.h)
+        buffer.lock()
+
+        GL.glClearColor(1,1,1,1)
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+
+        self.drawIcons(shadow=True)
+
+        buffer.unlock()
+        GL.glViewport(0, 0, *self.resolution)
+
+    def drawBlurredShadowMask(self, buffer, tempBuffer,
+                              boxSize=0.005, boxSteps=5, iters=2, opacity=0.6):
+        self.drawShadowMask(buffer)
+
+        # Set up accumulation
+        a = 1.0 / boxSteps
+        GL.glViewport(0, 0, buffer.w, buffer.h)
+        GL.glColor(a,a,a,a)
+        GL.glClearColor(0,0,0,0)
+        GL.glBlendFunc(GL.GL_ONE, GL.GL_ONE)
+        GL.glLoadIdentity()
+        GL.glMatrixMode(GL.GL_TEXTURE)
+
+        for j in xrange(iters):
+            # Vertical
+            tempBuffer.lock()
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+            GL.glBindTexture(GL.GL_TEXTURE_2D, buffer.texture)
+            for i in xrange(boxSteps):
+                GL.glLoadIdentity()
+                GL.glTranslatef(0, (i - boxSteps / 2.0) * boxSize, 0)
+                GL.glCallList(self.square)
+            tempBuffer.unlock()
+
+            # Horizontal
+            buffer.lock()
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+            GL.glBindTexture(GL.GL_TEXTURE_2D, tempBuffer.texture)
+            for i in xrange(boxSteps):
+                GL.glLoadIdentity()
+                GL.glTranslatef((i - boxSteps / 2.0) * boxSize, 0, 0)
+                GL.glCallList(self.square)
+            buffer.unlock()
+
+        # Blend a white square onto the shadow mask to limit its overall
+        # opacity. FIXME: Is there a way to do this while blending the
+        # completed shadow mask onto the scene below?
+        buffer.lock()
+        GL.glColor(1,1,1,1-opacity)
+        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
+        GL.glDisable(GL.GL_TEXTURE_2D)
+        GL.glCallList(self.square)
+        GL.glEnable(GL.GL_TEXTURE_2D)
+        buffer.unlock()
+
+        GL.glLoadIdentity()
+        GL.glMatrixMode(GL.GL_MODELVIEW)
+        GL.glViewport(0, 0, *self.resolution)
 
     t = 0
     def render(self):
-        self.t += 0.02
-        blurAmount = abs(math.sin(self.t)) * 0.2
-        nSteps = 16
-        a = 0.01
+        self.t += 1
+        h = (math.sin(self.t * 0.1) + 1) * 0.05
 
-        # Draw to buffer t1
-        # The first stage masks off the source's RGB (using glColor)
-        # alpha-blending blackness onto a white buffer.
-        self.t1.lock()
-        GL.glViewport(0, 0, self.t1.w, self.t1.h)
-        GL.glClearColor(1,1,1,1)
-        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-        GL.glBindTexture(GL.GL_TEXTURE_2D, self.src)
-        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
-        GL.glColor(0,0,0,a)
+        self.drawBlurredShadowMask(self.t1, self.t2, boxSize= h * 0.1)
 
-        # Vertical box filter
-        for i in xrange(nSteps):
-            GL.glLoadIdentity()
-            GL.glTranslatef(0, 2.0 * blurAmount / (nSteps-1) * i - blurAmount, 0)
-            GL.glScalef(1, 1-blurAmount, 1)
-            GL.glCallList(self.square)
-
-        # Draw to t2 buffer
-        self.t1.unlock()
-        self.t2.lock()
-        GL.glClearColor(1,1,1,1)
-        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+        # Blend the completed shadow mask
         GL.glBindTexture(GL.GL_TEXTURE_2D, self.t1.texture)
         GL.glBlendFunc(GL.GL_DST_COLOR, GL.GL_ZERO)
         GL.glColor(1,1,1,1)
-
-        # Horizontal box filter
-        for i in xrange(nSteps):
-            GL.glLoadIdentity()
-            GL.glTranslatef(2.0 * blurAmount / (nSteps-1) * i - blurAmount, 0, 0)
-            GL.glScalef(1-blurAmount, 1, 1)
-            GL.glCallList(self.square)
-
-        # Back to main buffer
-        self.t2.unlock()
-        GL.glViewport(0, 0, *self.resolution)
-
-        GL.glBlendFunc(GL.GL_ZERO, GL.GL_SRC_COLOR)
         GL.glLoadIdentity()
-        GL.glScalef(0.75, 0.75, 0.75)
-        GL.glBindTexture(GL.GL_TEXTURE_2D, self.t2.texture)
         GL.glCallList(self.square)
+
+        self.drawIcons(offset=(-h, h))
 
 if __name__ == "__main__":
     loopy.overlays.append(Overlay())
