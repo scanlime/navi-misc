d7edf44d0698139294ec044fc8ce0aa76c075f08
David Trowbridge <trowbrds@gmail.com>
2005-10-15 23:16:26 +0000

    more implementation

--- motion/py/Graph/ExtraAlgorithms.py
+++ motion/py/Graph/ExtraAlgorithms.py
@@ -16,3 +16,20 @@ class ParallelBFS:
     def addGraph (graph, startNode, endNode):
         self.starts[graph] = startNode
         self.ends[graph]   = endNode
+
+    def search ():
+        bfsObjects = []
+        for key in self.starts.keys ():
+            start = self.starts[key]
+            end   = self.ends[key]
+            bfsObjects.append (ParallelBFSSearch (key, start, end))
+
+class ParallelBFSSearch ():
+    """A class which runs a BFS on a graph, but without any specific stopping
+       point.
+       """
+
+    def __init__ (self, graph, start, end):
+        self.graph = graph
+        self.start = start
+        self.end   = end
