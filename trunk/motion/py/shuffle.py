120777e002e7a2d08d0bf46b360d048fb983b680
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-02 06:39:44 +0000

    Increase the number of iterations for this to get a better shuffle.

--- trunk/motion/py/shuffle.py
+++ trunk/motion/py/shuffle.py
@@ -28,8 +28,8 @@ if len (sys.argv) != 3:
 else:
     amc = AMC.from_file (sys.argv[1])
     lorenz = Systems.Lorenz (16.0, 45.0, 4.0)
-    sequence = Sequence.Sequence (amc, lorenz, Numeric.array ([1, 2, 3]), n=1000)
-    sequence.shuffle (Numeric.array ([17.0, 2.0, -1.0]), n=1000)
+    sequence = Sequence.Sequence (amc, lorenz, Numeric.array ([1, 2, 3]), n=5000)
+    sequence.shuffle (Numeric.array ([17.0, 2.0, -1.0]), n=5000)
 
     sequence.save (sys.argv[2], amc.format)
 
