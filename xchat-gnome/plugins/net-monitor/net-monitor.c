be21afbcab10e8e8dcccb3c6ec8dd5671953e5bf
David Trowbridge <trowbrds@gmail.com>
2005-11-05 05:20:23 +0000

    lots of progress, but now it's telling me that getActiveDevice doesn't exist

--- xchat-gnome/plugins/net-monitor/net-monitor.c
+++ xchat-gnome/plugins/net-monitor/net-monitor.c
@@ -24,6 +24,7 @@
 
 #define DBUS_API_SUBJECT_TO_CHANGE
 #include <dbus/dbus.h>
+#include <dbus/dbus-glib.h>
 #include <glib.h>
 #include <glib/gi18n.h>
 #include "xchat-plugin.h"
@@ -35,20 +36,73 @@
 #define NM_OBJECT_PATH          "/org/freedesktop/NetworkManager"
 #define NM_INTERFACE            "org.freedesktop.NetworkManager"
 
+#define NM_NO_ACTIVE_DEVICE     "org.freedesktop.NetworkManager.NoActiveDevice"
+
+typedef enum
+{
+	NETWORK_UP,
+	NETWORK_DOWN,
+} NetworkStatus;
+
 static xchat_plugin    *ph;
 static DBusConnection  *bus;
+
+static void
+set_network_mode (NetworkStatus status)
+{
+	if (status == NETWORK_UP)
+		g_print ("network up!\n");
+	else
+		g_print ("network down!\n");
+}
+
+static void
+determine_network_status ()
+{
+	DBusMessage *message, *reply;
+	DBusError error;
+
+	message = dbus_message_new_method_call (NM_SERVICE, NM_OBJECT_PATH, NM_INTERFACE, "getActiveDevice");
+	if (message == NULL) {
+		g_warning (_("Net Monitor: Couldn't allocate dbus message : %s: %s\n"), error.name, error.message);
+		/* just guess that we're up */
+		set_network_mode (NETWORK_UP);
+		return;
+	}
+
+	dbus_error_init (&error);
+	reply = dbus_connection_send_with_reply_and_block (bus, message, -1, &error);
+
+	if (dbus_error_is_set (&error)) {
+		if (dbus_error_has_name (&error, NM_NO_ACTIVE_DEVICE)) {
+			set_network_mode (NETWORK_DOWN);
+		} else {
+			g_warning (_("Net Monitor: Can't talk to Network Manager: %s: %s\n"), error.name, error.message);
+			set_network_mode (NETWORK_UP);
+		}
+	} else {
+		if (reply == NULL)
+			g_warning ("Network Monitor plugin got NULL reply from NetworkManager");
+		set_network_mode (NETWORK_UP);
+	}
+
+	if (reply)
+		dbus_message_unref (reply);
+
+	if (message)
+		dbus_message_unref (message);
+}
 
 static DBusHandlerResult
 filter_func (DBusConnection *connection, DBusMessage *message, void *data)
 {
-	xchat_printf (ph, "filter_func\n");
 	if (dbus_message_is_signal (message, NM_INTERFACE, "DeviceNoLongerActive")) {
-		xchat_printf (ph, "network down\n");
+		determine_network_status ();
 		return DBUS_HANDLER_RESULT_HANDLED;
 	}
 
 	if (dbus_message_is_signal (message, NM_INTERFACE, "DeviceNowActive")) {
-		xchat_printf (ph, "network up\n");
+		set_network_mode (NETWORK_UP);
 		return DBUS_HANDLER_RESULT_HANDLED;
 	}
 
@@ -66,7 +120,7 @@ init_dbus ()
 	dbus_connection_setup_with_g_main (bus, NULL);
 
 	if (dbus_error_is_set (&error)) {
-		xchat_printf (ph, _("Couldn't connect to system bus : %s: %s\n"), error.name, error.message);
+		xchat_printf (ph, _("Net Monitor: Couldn't connect to system bus : %s: %s\n"), error.name, error.message);
 		return FALSE;
 	}
 
@@ -74,7 +128,7 @@ init_dbus ()
 	dbus_bus_add_match (bus, "type='signal',interface='" NM_INTERFACE "'", &error);
 
 	if (dbus_error_is_set (&error)) {
-		xchat_printf (ph, _("Could not register signal handler: %s: %s\n"), error.name, error.message);
+		xchat_printf (ph, _("Net Monitor: Could not register signal handler: %s: %s\n"), error.name, error.message);
 		return FALSE;
 	}
 
@@ -102,6 +156,8 @@ xchat_plugin_init (xchat_plugin * plugin_handle, char **plugin_name, char **plug
 	*plugin_version = NET_MONITOR_VERSION;
 
 	ph = plugin_handle;
+
+	xchat_set_context (ph, xchat_find_context (ph, NULL, NULL));
 
 	success = init_dbus ();
 	if (success)
