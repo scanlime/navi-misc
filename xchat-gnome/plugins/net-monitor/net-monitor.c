3dade91b93068c7ba5d7bc71fd076670a2a5d250
David Trowbridge <trowbrds@gmail.com>
2005-11-05 04:32:16 +0000

    commit latest

--- xchat-gnome/plugins/net-monitor/net-monitor.c
+++ xchat-gnome/plugins/net-monitor/net-monitor.c
@@ -30,10 +30,10 @@
 
 #define NET_MONITOR_VERSION     "0.1"
 #define NET_MONITOR_NAME        "Network Monitor"
-#define NET_MONITOR_DESCRIPTION "NetworkManager connection monitor";
-#define NM_SERVICE              "org.freedesktop.NetworkManager";
-#define NM_OBJECT_PATH          "/org/freedesktop/NetworkManager";
-#define NM_INTERFACE            "org.freedesktop.NetworkManager";
+#define NET_MONITOR_DESCRIPTION "NetworkManager connection monitor"
+#define NM_SERVICE              "org.freedesktop.NetworkManager"
+#define NM_OBJECT_PATH          "/org/freedesktop/NetworkManager"
+#define NM_INTERFACE            "org.freedesktop.NetworkManager"
 
 static xchat_plugin    *ph;
 static DBusConnection  *bus;
@@ -41,6 +41,17 @@ static DBusConnection  *bus;
 static DBusHandlerResult
 filter_func (DBusConnection *connection, DBusMessage *message, void *data)
 {
+	xchat_printf (ph, "filter_func\n");
+	if (dbus_message_is_signal (message, NM_INTERFACE, "DeviceNoLongerActive")) {
+		xchat_printf (ph, "network down\n");
+		return DBUS_HANDLER_RESULT_HANDLED;
+	}
+
+	if (dbus_message_is_signal (message, NM_INTERFACE, "DeviceNowActive")) {
+		xchat_printf (ph, "network up\n");
+		return DBUS_HANDLER_RESULT_HANDLED;
+	}
+
 	return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 }
 
@@ -58,10 +69,10 @@ init_dbus ()
 	}
 
 	dbus_connection_add_filter (bus, filter_func, NULL, NULL);
-	dbus_bus_add_match (bus, "type='signal',interface='org.freedesktop.NetworkManager'", &error);
+	dbus_bus_add_match (bus, "type='signal',interface='" NM_INTERFACE "'", &error);
 
 	if (dbus_error_is_set (&error)) {
-		xchat_printf (ph, _("Could not register signal handler: %s: %s"), error.name, error.message);
+		xchat_printf (ph, _("Could not register signal handler: %s: %s\n"), error.name, error.message);
 		return FALSE;
 	}
 
