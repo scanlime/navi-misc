393455c4d097c4ece4cef64a109e75e9adf588e1
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-06 22:34:49 +0000

    Seek operations on L0 and L1 pages are now implemented, as well as a basic L0 page append. Still plenty buggy.

--- trunk/fidtool/test.py
+++ trunk/fidtool/test.py
@@ -2,8 +2,9 @@
 import _fidtool
 import os
 
-fd = os.open("foo", os.O_CREAT | os.O_TRUNC | os.O_RDWR, 0666)
-dataset = map(int, open("cia-commits.dataset"))
+fd = os.open("foo", os.O_CREAT | os.O_RDWR, 0666)
+#dataset = map(int, open("cia-commits.dataset"))
+dataset = [2] * 1024
 
 _fidtool.append_samples(fd, dataset)
 
