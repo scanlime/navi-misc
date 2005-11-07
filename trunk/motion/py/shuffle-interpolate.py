a781de37b67b13e2e7086232c5f10e6dbb4c7d7b
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-07 20:12:42 +0000

    use correct slice

--- trunk/motion/py/shuffle-interpolate.py
+++ trunk/motion/py/shuffle-interpolate.py
@@ -55,7 +55,7 @@ sequence = Sequence.Sequence (samc, lorenz, Numeric.array ([1, 2, 3]), n=5000)
 sequence.shuffle (Numeric.array ([17.0, 2.0, -1.0]), n=5000)
 
 amcs = []
-for filename in sys.argv[1:]:
+for filename in sys.argv[2:]:
     print 'loading',filename
     amcs.append (AMC.from_file (filename))
 
