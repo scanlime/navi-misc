ac4d4ad2e992afe7a8a858a58ee6b5b080ae5fbb
Micah Elizabeth Scott <micah@scanlime.org>
2005-11-07 05:17:23 +0000

    Implement basic interval queries, fix a few subtle bugs in indexing. This now seems to mostly-work, but still has some subtle bugs that test.py is reporting.

--- fidtool/test.py
+++ fidtool/test.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
 import _fidtool
-import os
+import os, time, random
 
-fd = os.open("foo", os.O_CREAT | os.O_RDWR, 0666)
-dataset = map(int, open("cia-commits.dataset"))
-_fidtool.append_samples(fd, dataset)
+fd = os.open("foo", os.O_CREAT | os.O_TRUNC | os.O_RDWR, 0666)
+
+r = range(10)
+_fidtool.append_samples(fd, r)
+
+while 1:
+    r2 = _fidtool.query_samples(fd, r)
+    if r2 == r:
+        print "Success"
+    else:
+        print "Failure: %r -> %r" % (r, r2)
+        break
+    random.shuffle(r)
+
+#dataset = map(int, open("cia-commits.dataset"))
+#_fidtool.append_samples(fd, dataset)
+
+#start = 1124400000
+#hour_length = 60 * 60
+#times = [start + hour_length * hour for hour in xrange(24*7*4)]
+
+##indices = _fidtool.query_samples(fd, times)
+#indices = [_fidtool.query_samples(fd, (t,))[0] for t in times]
+
+#for i in xrange(len(times)-1):
+#    print times[i], indices[i+1] - indices[i]
+
 os.close(fd)
