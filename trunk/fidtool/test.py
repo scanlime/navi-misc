c23a2da94aabed4c07f16765f723a9e3d4e120d4
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-10 07:55:56 +0000

    A little more cleanup and commenting on the L2 through L1 seek process

--- trunk/fidtool/test.py
+++ trunk/fidtool/test.py
@@ -4,12 +4,9 @@ import os, time, random
 import psyco
 psyco.full()
 
-fd = os.open("foo", os.O_CREAT | os.O_TRUNC | os.O_RDWR, 0666)
-
-dataset = map(int, open("cia-commits.dataset"))[2000:3000]
-#dataset.sort()
-#dataset = range(1000)
-_fidtool.append_samples(fd, dataset)
+def build_fid(dataset, fd):
+    dataset.sort()
+    _fidtool.append_samples(fd, dataset)
 
 def reference_query(stamps, seek_to):
     i = 0
@@ -17,26 +14,36 @@ def reference_query(stamps, seek_to):
         i += 1
     return i
 
-query = [int(random.uniform(dataset[0], dataset[-1])) for i in xrange(1000)]
-query.sort()
+def test_fid(dataset, fd):
+    query = [int(random.uniform(dataset[0], dataset[-1]) + 0.5)
+             for i in xrange(1000)]
+    query.sort()
 
-while 1:
-    r1 = _fidtool.query_samples(fd, query)
-    r2 = [reference_query(dataset, q) for q in query]
-    if r1 == r2:
-        print "Success"
-    elif len(r1) != len(r2):
-        print "Failure, len(r1)=%d, len(r2)=%d" % (len(r1), len(r2))
+    while 1:
+        r1 = _fidtool.query_samples(fd, query)
+        r2 = [reference_query(dataset, q) for q in query]
+        if r1 == r2:
+            print "Success"
+        elif len(r1) != len(r2):
+            print "Failure, len(r1)=%d, len(r2)=%d" % (len(r1), len(r2))
+            break
+        else:
+            for i, (j, k) in enumerate(zip(r1, r2)):
+                if j != k:
+                    print "%d: %d != %d" % (query[i], j, k)
+                else:
+                    print "%d: %d" % (query[i], j)
+            break
+
         break
-    else:
-        for i, (j, k) in enumerate(zip(r1, r2)):
-            if j != k:
-                print "%d: %d != %d" % (query[i], j, k)
-            else:
-                print "%d: %d" % (query[i], j)
-        break
+        random.shuffle(query)
 
-    break
-    random.shuffle(query)
 
-os.close(fd)
+if __name__ == "__main__":
+    #dataset = map(int, open("cia-commits.dataset"))[2000:3000]
+    dataset = range(20)
+
+    fd = os.open("foo", os.O_CREAT | os.O_TRUNC | os.O_RDWR, 0666)
+    build_fid(dataset, fd)
+    test_fid(dataset, fd)
+    os.close(fd)
