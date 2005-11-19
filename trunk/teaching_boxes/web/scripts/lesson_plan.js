e712cd9278dfcb9d646b6d36b2ceb687f126664e
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-19 18:32:54 +0000

    Oops... Missed a conflict

--- trunk/teaching_boxes/web/scripts/lesson_plan.js
+++ trunk/teaching_boxes/web/scripts/lesson_plan.js
@@ -149,13 +149,9 @@ function NewBox ()
 	this.div.style.visibility = "visible"
 }
 
->>>>>>> .r9421
 // Selection changed handler
 function selectionChanged (event)
 {
-<<<<<<< .mine
-	return new Array ()
-=======
 	parentDiv = newBox.div
 	select = newBox.select
 	div = top.document.getElementById ('description')
@@ -223,28 +219,14 @@ function selectionChanged (event)
 	div.innerHTML = ""
 	div.appendChild (boldText)
 	div.appendChild (descriptText)
->>>>>>> .r9421
 }
 
-<<<<<<< .mine
-function createField (name)
-=======
 // Create button clicked
 function createButtonClicked ()
->>>>>>> .r9421
 {
-<<<<<<< .mine
-	document.write ("<br/>")
-	title = document.createElement ("div")
-	text  = document.createTextNode (name)
-=======
 	div = newBox.div
 	div.style.visibility = "hidden"
->>>>>>> .r9421
 
-<<<<<<< .mine
-	title.setAttribute ("class","fieldName")
-=======
 	// Figure out which fields were selected and add them to the view
 	options = div.options
 	for (i in options)
@@ -255,25 +237,14 @@ function createButtonClicked ()
 		}
 	}
 }
->>>>>>> .r9421
 
-<<<<<<< .mine
-	title.appendChild (text)
-=======
 // Cancel button clicked
 function cancelButtonClicked ()
 {
 	div = newBox.div
 	div.style.visibility = "hidden"
 }
->>>>>>> .r9421
 
-<<<<<<< .mine
-	entry = document.createElement ("textarea")
-
-	document.body.appendChild (title)
-	document.body.appendChild (entry)
-=======
 //-----------------------------
 // Main functions
 //-----------------------------
