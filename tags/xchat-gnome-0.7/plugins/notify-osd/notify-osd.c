8a199ce94dbd6fc3e35efc66508f87ff8f93da3c
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-20 23:15:26 +0000

    tag

---
+++ tags/xchat-gnome-0.7/plugins/notify-osd/notify-osd.c
@@ -0,0 +1,192 @@
+/*
+ * notify-osd.c - xchat plugin which provides "OSD" like functionality through
+ *                libnotify
+ *
+ * Copyright (C) 2005 David Trowbridge
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#include <libnotify/notify.h>
+#include "xchat-plugin.h"
+#include "xg-plugin.h"
+
+#define NOTIFY_OSD_NAME        "OSD"
+#define NOTIFY_OSD_VERSION     "0.1"
+#define NOTIFY_OSD_DESCRIPTION "Pops up notification of important messages when xchat doesn't have the focus"
+
+enum
+{
+	STRIP_COLORS = 1 << 0,
+	STRIP_ATTRS  = 1 << 1,
+};
+
+static xchat_plugin       *ph;
+static xchat_gnome_plugin *xgph;
+static GtkWidget          *main_window;
+static gboolean            focused = TRUE;
+static NotifyIcon         *icon;
+static GSList             *notifications = NULL;
+
+static void
+hide_notifications (NotifyHandle *handle)
+{
+	notify_close (handle);
+}
+
+static gboolean
+got_focus_cb (GtkWidget *wigdet, GdkEventFocus *event, gpointer data)
+{
+	g_slist_foreach (notifications, hide_notifications, NULL);
+	g_slist_free (notifications);
+	notifications = NULL;
+	focused = TRUE;
+	return FALSE;
+}
+
+static gboolean
+lost_focus_cb (GtkWidget *wigdet, GdkEventFocus *event, gpointer data)
+{
+	focused = FALSE;
+	return FALSE;
+}
+
+static int
+new_msg_cb (char *word[], gpointer data)
+{
+	const char *channel;
+	gchar *stripped, *message, *summary;
+	NotifyHandle *handle;
+
+	if (focused)
+		return XCHAT_EAT_NONE;
+
+	channel = xchat_get_info (ph, "channel");
+	stripped = xchat_strip (ph, word[2], -1, STRIP_COLORS | STRIP_ATTRS);
+
+	message = g_strdup_printf ("&lt;%s&gt; %s", word[1], stripped);
+	if (channel[0] == '#')
+		summary = g_strdup_printf ("Message in %s", channel);
+	else
+		summary = g_strdup_printf ("Message from %s", channel);
+
+	handle = notify_send_notification (NULL, NULL, NOTIFY_URGENCY_NORMAL, summary, message, icon, TRUE, 0, NULL, NULL, 0);
+	notifications = g_slist_prepend (notifications, handle);
+
+	xchat_free (ph, stripped);
+	g_free (message);
+	g_free (summary);
+	return XCHAT_EAT_NONE;
+}
+
+static int
+new_action_cb (char *word[], gpointer data)
+{
+	const char *channel;
+	gchar *stripped, *message, *summary;
+	NotifyHandle *handle;
+
+	if (focused)
+		return XCHAT_EAT_NONE;
+
+	channel = xchat_get_info (ph, "channel");
+	stripped = xchat_strip (ph, word[2], -1, STRIP_COLORS | STRIP_ATTRS);
+
+	message = g_strdup_printf ("* %s %s", word[1], stripped);
+	if (channel[0] == '#')
+		summary = g_strdup_printf ("Message in %s", channel);
+	else
+		summary = g_strdup_printf ("Message from %s", channel);
+
+	handle = notify_send_notification (NULL, NULL, NOTIFY_URGENCY_NORMAL, summary, message, icon, TRUE, 0, NULL, NULL, 0);
+	notifications = g_slist_prepend (notifications, handle);
+
+	xchat_free (ph, stripped);
+	g_free (message);
+	g_free (summary);
+	return XCHAT_EAT_NONE;
+}
+
+static int
+private_msg_cb (char *word[], gpointer data)
+{
+	gchar *message, *summary;
+	NotifyHandle *handle;
+
+	if (focused)
+		return XCHAT_EAT_NONE;
+
+	message = xchat_strip (ph, word[2], -1, STRIP_COLORS | STRIP_ATTRS);
+	summary = g_strdup_printf ("Private Message from %s", word[1]);
+
+	handle = notify_send_notification (NULL, NULL, NOTIFY_URGENCY_NORMAL, summary, message, icon, TRUE, 0, NULL, NULL, 0);
+	notifications = g_slist_prepend (notifications, handle);
+
+	xchat_free (ph, message);
+	g_free (summary);
+	return XCHAT_EAT_NONE;
+}
+
+int
+xchat_gnome_plugin_init (xchat_gnome_plugin *xg_plugin)
+{
+	xgph = xg_plugin;
+
+	/* Hook up focus callbacks */
+	main_window = xg_get_main_window ();
+	g_signal_connect (main_window, "focus-in-event",  G_CALLBACK (got_focus_cb),  NULL);
+        g_signal_connect (main_window, "focus-out-event", G_CALLBACK (lost_focus_cb), NULL);
+
+	return TRUE;
+}
+
+void
+xchat_plugin_get_info (char **plugin_name, char **plugin_desc, char **plugin_version, void **reserved)
+{
+	*plugin_name    = NOTIFY_OSD_NAME;
+	*plugin_desc    = NOTIFY_OSD_DESCRIPTION;
+	*plugin_version = NOTIFY_OSD_VERSION;
+}
+
+int
+xchat_plugin_init (xchat_plugin *plugin_handle, char **plugin_name, char **plugin_desc, char **plugin_version, char *arg)
+{
+	xchat_plugin_get_info (plugin_name, plugin_desc, plugin_version, NULL);
+
+	ph = plugin_handle;
+
+	if (notify_init ("Xchat OSD")) {
+		icon = notify_icon_new_from_uri (XCHATSHAREDIR "/xchat-gnome.png");
+
+		xchat_hook_print (ph, "Channel Msg Hilight",       XCHAT_PRI_NORM, new_msg_cb,     NULL);
+		xchat_hook_print (ph, "Channel Action Hilight",    XCHAT_PRI_NORM, new_action_cb,  NULL);
+		xchat_hook_print (ph, "Private Message",           XCHAT_PRI_NORM, private_msg_cb, NULL);
+		xchat_hook_print (ph, "Private Message to Dialog", XCHAT_PRI_NORM, private_msg_cb, NULL);
+
+		xchat_print (ph, "OSD loaded\n");
+
+		return TRUE;
+	} else {
+		xchat_print (ph, "OSD initialization failed\n");
+	}
+	return FALSE;
+}
+
+int
+xchat_plugin_deinit ()
+{
+	notify_icon_destroy (icon);
+}
---
+++ tags/xchat-gnome-0.7/plugins/url_scraper/UrlScraper.py
@@ -0,0 +1,165 @@
+''' UrlScraper.py
+
+xchat-gnome script for pulling URLs out of channels
+and displaying them in a separate window.
+'''
+# Copyright (C) 2004 W. Evan Sheehan <evan@navi.cx>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import xchat, string, re, gconf, gnome
+import pygtk
+import gtk
+import gobject
+
+__module_name__ = 'URL Scraper'
+__module_version__ = '0.1-pre'
+__module_description__ = 'Collect URLs said and display them in a separate window'
+
+
+class URLScraper:
+
+    def __init__( self ):
+        self.urls = 0
+        self.new_urls = 0
+        self.loaded = True
+
+        # Set up gconf stuff.
+        self.client = gconf.client_get_default()
+        self.get_prefs( self.client )
+
+        # Create the window.
+        self.window = gtk.Window( gtk.WINDOW_TOPLEVEL )
+        self.window.set_default_size( 300, 350 )
+        self.window.set_title( 'URL Scraper' )
+
+        scrolled = gtk.ScrolledWindow()
+        scrolled.set_policy( gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC )
+        self.store = gtk.ListStore( gobject.TYPE_STRING,
+                                    gobject.TYPE_STRING,
+                                    gobject.TYPE_STRING )
+
+        view = gtk.TreeView( self.store )
+        nick_col =  gtk.TreeViewColumn( 'Nick', gtk.CellRendererText(), text=0 )
+        nick_col.set_sort_column_id( 0 )
+        nick_col.set_resizable( gtk.TRUE )
+        chan_col = gtk.TreeViewColumn( 'Channel', gtk.CellRendererText(), text=1)
+        chan_col.set_sort_column_id( 1 )
+        chan_col.set_resizable( gtk.TRUE )
+        url_col =  gtk.TreeViewColumn( 'URL', gtk.CellRendererText(), text=2 )
+        url_col.set_resizable( gtk.TRUE )
+
+        view.append_column( nick_col )
+        view.append_column( chan_col )
+        view.append_column( url_col )
+
+        # FIXME: This also causes crashes... probably due to the python plugin
+        #        itself.
+        #view.connect( 'row-activated', self.urlClicked )
+
+        scrolled.add_with_viewport( view )
+        self.window.add( scrolled )
+        self.window.connect( 'destroy', self.close )
+        # FIXME: this causes xchat-gnome to crash.
+        #self.window.connect( 'focus-in-event', self.windowFocused )
+        self.window.show_all()
+
+    def get_prefs( self, client ):
+        # If our gconf keys don't exist...
+        if not client.dir_exists( '/apps/xchat/url_scraper' ):
+            client.add_dir( '/apps/xchat/url_scraper', gconf.CLIENT_PRELOAD_NONE )
+            self.history = 10
+            self.scrape_email = False
+            client.set_int( '/apps/xchat/url_scraper/history', self.history )
+            client.set_bool( '/apps/xchat/url_scraper/scrape_email', self.scrape_email )
+        # If our gconf keys exist...
+        else:
+            self.history = client.get_int( '/apps/xchat/url_scraper/history' )
+            self.scrape_email = client.get_bool( '/apps/xchat/url_scraper/scrape_email' )
+
+
+    def urlClicked( self, view, path, col, user_data ):
+        print 'double clicked'
+        iter = self.store.get_iter( path )
+        url = self.store.get_value( iter, 2 )
+        print 'url = %s' % url
+        gnome.url_show( url )
+
+    def windowFocused( self, widget, direction, user_data=None ):
+        self.window.set_title( 'URL Scraper' )
+        self.new_urls = 0
+
+        return gtk.FALSE
+
+    def grabbedURL( self, nick, match ):
+        ''' Handler for a URL. Append the URL to the store. '''
+        if self.urls >= self.history:
+            iter = self.store.get_iter_first()
+            self.store.remove( iter )
+        else:
+            self.urls += 1
+
+        # FIXME: Commented out because removing the change to the title causes
+        #        a crash.
+        #if not self.window.has_focus:
+        #    self.new_urls += 1
+        #    self.window.set_title( '[' + str(self.new_urls) + '] URL Scraper' )
+
+        self.store.set( self.store.append(), 0, nick, 1,
+                xchat.get_info('channel'), 2, match.group())
+
+    def grabbedEmail( self, nick, match ):
+        ''' Handler for email addresses. Since the behaviour is the same
+            as the URL handler we might as well just pass this on to it.
+            The only difference is you can disable this one, so we check
+            first.
+            '''
+        if self.scrape_email:
+            self.grabbedURL( nick, match )
+
+    regexes = {
+        re.compile('(ht|f)tps?://[^\s\>\]\)]+'):                 grabbedURL,
+        re.compile('[\w\.\-\+]+@([0-9a-zA-Z\-]+\.)+[a-zA-Z]+'):  grabbedEmail,
+        }
+
+    def grabURL( self, word, word_eol, user_data ):
+        ''' Check a message for a URL. '''
+        nick = word[0]
+        message = word[1]
+        for regex, handler in URLScraper.regexes.iteritems():
+            for match in regex.finditer(message):
+                handler( self, nick, match )
+        return xchat.EAT_NONE
+
+    def unload( self, user_data ):
+        ''' Destroy the window and stuff before we go away. '''
+        self.loaded = False
+        self.window.destroy()
+        print 'URL Scraper unloaded.'
+
+    def close( self, window, event=None, user_data=None ):
+        ''' Unload the module when you close the window. '''
+        # FIXME: In the future this might just hide the window if we can add a
+        #        item to the main window that would allow us to show the window,
+        #        in which case we wouldn't unload the plugin here.
+        if self.loaded:
+            xchat.command( 'py unload '+__module_name__ )
+
+scraper = URLScraper()
+xchat.hook_print( 'Channel Message', scraper.grabURL )
+xchat.hook_unload( scraper.unload )
+
+print 'URL Scraper loaded.'
+### The End ###
