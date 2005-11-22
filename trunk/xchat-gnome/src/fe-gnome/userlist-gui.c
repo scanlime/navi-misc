506f69962e8ad85fd202d445df09be7a161c5d5f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-11-22 23:32:37 +0000

    remove some cruft, make it close the userlist on key press rather than release, so you can escape out of a context menu and the userlist separately

--- trunk/xchat-gnome/src/fe-gnome/userlist-gui.c
+++ trunk/xchat-gnome/src/fe-gnome/userlist-gui.c
@@ -275,9 +275,8 @@ userlist_gui_hide ()
 static gboolean
 userlist_window_event (GtkWidget *window, GdkEvent *event, gpointer data)
 {
-	gboolean handled = FALSE;
 	switch (event->type) {
-	case GDK_KEY_RELEASE:
+	case GDK_KEY_PRESS:
 		if (((GdkEventKey *)event)->keyval == GDK_Escape) {
 			userlist_gui_hide ();
 			break;
@@ -285,7 +284,7 @@ userlist_window_event (GtkWidget *window, GdkEvent *event, gpointer data)
 	default:
 		break;
 	}
-	return handled;
+	return FALSE;
 }
 
 static gboolean
@@ -305,17 +304,6 @@ userlist_grab ()
 	                               GDK_POINTER_MOTION_MASK | GDK_BUTTON_PRESS_MASK |
 				       GDK_BUTTON_RELEASE_MASK | GDK_ENTER_NOTIFY_MASK |
 				       GDK_LEAVE_NOTIFY_MASK,
-				/*
-	                               GDK_POINTER_MOTION_MASK  | GDK_POINTER_MOTION_HINT_MASK |
-	                               GDK_BUTTON_MOTION_MASK   | GDK_BUTTON1_MOTION_MASK      |
-	                               GDK_BUTTON2_MOTION_MASK  | GDK_BUTTON3_MOTION_MASK      |
-	                               GDK_BUTTON_PRESS_MASK    | GDK_BUTTON_RELEASE_MASK      |
-	                               GDK_ENTER_NOTIFY_MASK    | GDK_LEAVE_NOTIFY_MASK        |
-	                               GDK_FOCUS_CHANGE_MASK    | GDK_STRUCTURE_MASK           |
-	                               GDK_PROPERTY_CHANGE_MASK | GDK_VISIBILITY_NOTIFY_MASK   |
-	                               GDK_PROXIMITY_IN_MASK    | GDK_PROXIMITY_OUT_MASK       |
-	                               GDK_SCROLL_MASK          | GDK_SUBSTRUCTURE_MASK,
-				       */
 	                               NULL, NULL, GDK_CURRENT_TIME) == GDK_GRAB_SUCCESS);
 	if (have_grab) {
 		have_grab = (gdk_keyboard_grab (gui.userlist_window->window, TRUE, GDK_CURRENT_TIME) == GDK_GRAB_SUCCESS);
