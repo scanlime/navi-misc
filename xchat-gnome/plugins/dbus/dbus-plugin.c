5e0f12de743527ae2c2b577011fdb37332b66008
David Trowbridge <trowbrds@gmail.com>
2005-12-04 06:44:21 +0000

    i'm not sure if this is the right way to fix this problem with dbus 0.6, but it compiles.

--- xchat-gnome/plugins/dbus/dbus-plugin.c
+++ xchat-gnome/plugins/dbus/dbus-plugin.c
@@ -279,7 +279,7 @@ init_dbus(void)
 
   if (!dbus_g_proxy_call (bus_proxy, "RequestName", &error,
                           G_TYPE_STRING, DBUS_SERVICE,
-                          G_TYPE_UINT, DBUS_NAME_FLAG_PROHIBIT_REPLACEMENT,
+                          G_TYPE_UINT, 0,
                           G_TYPE_INVALID,
                           G_TYPE_UINT, &request_name_result,
                           G_TYPE_INVALID))
