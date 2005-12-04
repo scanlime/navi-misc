6ff4222afe87aa637e8c1bd1607e78e3f9e955e7
Evan Sheehan <darthmall@users.noreply.github.com>
2005-12-04 06:05:00 +0000

    Implemented a cliche walk that follows highest probability path. In strong need of some debugging.

--- motion/py/graph_walk.py
+++ motion/py/graph_walk.py
@@ -2,12 +2,32 @@
 
 from optparse import OptionParser
 from Dance import Sequence, MotionGraph
+from Graph.Data import AdjacencyList
+import random, pickle
 
 def clicheWalk (graph, len):
     """Find a path in graph of length len by following the edges with the
        highest probabilities.
        """
-    pass
+    weight = 0
+    path = []
+    adjacency = graph.representations[AdjacencyList]
+
+    v = None
+    u = random.choice ([u for u in adjacency.iterU ()])
+    path.append (u.center ())
+
+    for i in range (len):
+        for edge in adjacency.query (u):
+            if edge.weight >= weight: 
+                v = edge.v
+                weight = edge.weight
+
+        path.append (v.center ())
+        u = v
+        weight = 0
+
+    return path
 
 
 def randomWalk (graph, len):
@@ -16,7 +36,7 @@ def randomWalk (graph, len):
 
 
 parser = OptionParser ("usage: %prog <graph pickle> <output amc>")
-parser.add_option ("--cliche", dest="cliche", default=false,
+parser.add_option ("--cliche", dest="cliche", default=False,
         action="store_true", help="Choose edges based on probability")
 parser.add_option ("-l", "--len", dest="len", default=1000,
         help="Set length of paths")
@@ -25,6 +45,13 @@ opts, args = parser.parse_args ()
 
 if len (args) != 2: parser.error ("graph and output file required")
 
-graphs = pickle.load (open (args[1]))
+graphs = pickle.load (open (args[0]))
+bones  = {}
+
+for bone in graphs.keys ():
+    if opts.cliche: bones[bone] = clicheWalk (graphs[bone], opts.len)
+    else: bones[bone] = randomWalk (graphs[bone], opts.len)
+
+print bones
 
 # vim:ts=4:sw=4:et
