8dff11be57002b34a529a83427c50ac4dd62f0f7
Evan Sheehan <darthmall@users.noreply.github.com>
2006-01-31 18:28:29 +0000

    Commented up, yo.

--- motion/py/Motion/Interpolate.py
+++ motion/py/Motion/Interpolate.py
@@ -27,6 +27,8 @@ def spline(data, quality):
        original data is. quality is the number of interpolated points to insert
        between each point in the initial data.
        """
+    # Special case: The input data is an AMC object. For each bone in the AMC
+    # object create a spline. Return an AMC object.
     if data.__class__ == AMC:
         interpolated = AMC()
         for bone, motion in data.bones.iteritems():
@@ -38,23 +40,36 @@ def spline(data, quality):
     length, dof  = Numeric.shape(data)
     interpolated = Numeric.empty((length * quality, dof))
 
+    # This is the range of times we'll be using for the vast majority of the
+    # splining process.
     times = Numeric.arange(1, 2, 1. / quality)
-    f     = lambda c: lambda t: c[0] + c[1] * t + c[2] * t**2 + c[3] * t**3
+
+    # This function is used to generate the intermediate points from the
+    # constants and the time. 
+    f = lambda c: lambda t: c[0] + c[1] * t + c[2] * t**2 + c[3] * t**3
 
     for frame in range(length - 3):
+        # Generate matrices and solve for the constants for this section of the
+        # data.
+        # FIXME - I am wholly unconvinced that the nonsense about mapping arrays
+        # to that lambda function actually works.
         A, b = __getMatrix(data[frame:frame + 4], dof)
         z    = Numeric.matrixmultiply(inverse(A), b)
 
+        # Special case: At the beginning of the data we need to use the first
+        # set of constants for the interpolation.
         if frame == 0:
             eqs = map(f, z[:4])
             for degree in range(dof):
                 interpolated[:quality, degree] = map(eqs, Numeric.arange(0, 1, 1. / quality))
 
+        # Interpolate the middle two points of this section of data.
         eqs = map(f, z[4:8])
         sFrame = (frame + 1) * quality
         for degree in range(dof):
             interpolated[sFrame:sFrame + quality, degree] = map(eqs, times)
 
+    # When all is said and done, interpolate the last two pointsin the data.
     eqs = map(f, z[-4:])
     for degree in range(dof):
         interpolated[-quality:, degree] = map(eqs, Numeric.arange(2, 3, 1. / quality))
@@ -64,6 +79,7 @@ def spline(data, quality):
 
 def __getMatrix(data, dof):
     """Create the matrices A and b for a spline with 4 data points."""    
+    # We need to be sure that we've got exactly 4 data points. 
     assert(Numeric.shape(data)[0] == 4)
 
     A   = Numeric.zeros((12, 12, dof))
