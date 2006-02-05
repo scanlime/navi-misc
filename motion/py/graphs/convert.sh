7a96599018e4fe1234dc62f32f3e0f538e0979a2
Micah Elizabeth Scott <micah@scanlime.org>
2006-02-05 05:31:44 +0000

    Release pycaptcha 0.4 The only change is relicensing from LGPL to MIT.

--- motion/py/graphs/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# convert .dot files to .png
-for file in *.dot; do
-	echo "Converting $file..."
-	dot -Tpng -o "`echo $file |cut -d'.' -f1`.png" $file
-	dot -Tps -o "`echo $file |cut -d'.' -f1`.ps" $file
-done
