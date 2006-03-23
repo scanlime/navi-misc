31caedd121aa2b168768562eba58add756e361ed
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-03-23 17:54:18 +0000

    Going to make some unit tests to verify our searches.

---
+++ trunk/motion/py/Graph/tests.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""Unit tests for the Graph module."""
+
+# Copyright (C) 2006 W. Evan Sheehan
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from unittest import makeSuite, TestCase, TestSuite
+
+
+def suite ():
+    """Return a TestSuite consisting of all the tests in this module."""
+    tests = (makeSuite (TestDijkstra),
+             makeSuite (TestAStar),
+             makeSuite (TestIterativeDeepening))
+    return TestSuite (tests)
+
+
+class TestDijkstra (TestCase):
+    """Test Dijkstra's shortest path algorithm."""
+    def setUp (self):
+        pass
+
+
+class TestAStar (TestCase):
+    """Test the A* search."""
+    def setUp (self):
+        pass
+
+
+class TestIterativeDeepening (TestCase):
+    """Test the iterative deepening depth first search."""
+    def setUp (self):
+        pass
+
+
+if __name__ == "__main__":
+    from unittest import TextTestRunner
+    TextTestRunner (verbosity=2).run (suite ())
+
+
+# vim: ts=4:sw=4:et
