e43bd0ca4ecf5c2293a89341e2ab31444208fa72
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-03-24 07:20:05 +0000

    er... no. I must be getting tired

--- trunk/motion/py/Graph/Algorithms.py
+++ trunk/motion/py/Graph/Algorithms.py
@@ -373,7 +373,7 @@ class CombinHeuristic (Algorithm):
 
             # Add the successors of this node to the agenda and record the node
             # that generated these successors.
-            for s in self.successors (node.keys (), node.items ()):
+            for s in self.successors (node.keys (), node.values ()):
                 if s not in self.visited:
                     self.predecessors[s] = node
                     agenda.append (s)
