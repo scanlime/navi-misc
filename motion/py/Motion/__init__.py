3eccaf320a8d1ded85d87b961eea6cc9604ac1b7
David Trowbridge <trowbrds@gmail.com>
2006-04-05 02:59:25 +0000

    tag 0.11

--- motion/py/Motion/__init__.py
+++
@@ -1,9 +0,0 @@
-""" Motion
-
-This is a Python package for dealing with motion capture data,
-mostly in ASF-AMC form
-"""
-
-from motion_c import AMC
-from Bone import Bone
-from ASFReader import ASFReader
