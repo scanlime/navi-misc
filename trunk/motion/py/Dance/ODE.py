07ddf3e9df67816d49fa31fd9eea1473b5a822d3
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-05-16 18:51:31 +0000

    ws

--- trunk/motion/py/Dance/ODE.py
+++ trunk/motion/py/Dance/ODE.py
@@ -1,4 +1,4 @@
-"""A collection of Ordinary Differential Equation (ODE) solvers.
+"""A collection of Ordinary Differential Equation(ODE) solvers.
 
 Provides a variety of ODE solvers all based on a generic class so that they may
 be used interchangeably.
@@ -25,7 +25,7 @@ class ODE:
                         system
     """
 
-    def __init__ (self, func):
+    def __init__(self, func):
         """Create a solver with an ODE to solve.
 
         Arguments:
@@ -33,11 +33,11 @@ class ODE:
         """
         self.f = func
 
-    def __call__ (self):
+    def __call__(self):
         pass
 
 
-class RK4 (ODE):
+class RK4(ODE):
     """Runge-Kutte 4 ODE solver.
 
     Methods:
@@ -45,19 +45,19 @@ class RK4 (ODE):
                         Runge-Kutte 4 solver
     """
 
-    def __init__ (self, func):
-        ODE.__init__ (self, func)
+    def __init__(self, func):
+        ODE.__init__(self, func)
 
-    def _step (self, last, h, t):
+    def _step(self, last, h, t):
         """Execute a single RK4 step."""
-        k1 = self.f (last, t, h)
-        k2 = self.f (last + h/2. * k1, t + h/2., h)
-        k3 = self.f (last + h/2. * k2, t + h/2., h)
-        k4 = self.f (last + h * k3, t + h, h)
+        k1 = self.f(last, t, h)
+        k2 = self.f(last + h/2. * k1, t + h/2., h)
+        k3 = self.f(last + h/2. * k2, t + h/2., h)
+        k4 = self.f(last + h * k3, t + h, h)
 
         return last + h/6.*(k1 + 2.*k2 + 2.*k3 + k4)
 
-    def __call__ (self, ic, n, h):
+    def __call__(self, ic, n, h):
         """Solve the system and return the result.
 
         Arguments:
@@ -65,19 +65,19 @@ class RK4 (ODE):
             - n         The number of iterations
             - h         The step size
         """
-        result = [Numeric.array (ic)]
+        result = [Numeric.array(ic)]
 
-        for i in range (n):
-            step = self._step (result[i], h, i*h)
-            result.append (step)
+        for i in range(n):
+            step = self._step(result[i], h, i*h)
+            result.append(step)
 
         return result
 
 
-class ARK4 (RK4):
+class ARK4(RK4):
     """An Adaptive Runge-Kutte 4 ODE solver."""
 
-    def __init__ (self, func, eps=.001):
+    def __init__(self, func, eps=.001):
         """Create an ARK4 object with an error tolerance.
 
         Arguments:
@@ -85,34 +85,34 @@ class ARK4 (RK4):
             - eps       The error tolerance used to adjust the step size
         """
 
-        RK4.__init__ (self, func)
+        RK4.__init__(self, func)
         # Error tolerance.
         self.epsilon = eps
 
-    def _step (self, last, i):
+    def _step(self, last, i):
         """Overridden from RK4."""
         # Take one step.
-        single = RK4._step (self, last, i)
+        single = RK4._step(self, last, i)
 
         # Take two half steps.
-        checker = RK4 (self.f, last, i.time, 2, .5*i.h)
+        checker = RK4(self.f, last, i.time, 2, .5*i.h)
         checker()
 
         # Find the difference.
         diff = checker.results[2] - single
 
         # If the difference is too large, decrease the time step.
-        delta = math.sqrt (Numeric.dot (diff, diff))
+        delta = math.sqrt(Numeric.dot(diff, diff))
         if delta > self.epsilon:
             i.h *= .5
-            return self._step (last, i)
+            return self._step(last, i)
         # If the difference is less then our tolerance try doubling h.
         elif delta < self.epsilon:
-            checker = RK4 (self.f, last, i.time, 2, 2*i.h)
+            checker = RK4(self.f, last, i.time, 2, 2*i.h)
             checker()
 
             diff = checker.results[2] - single
-            delta = math.sqrt (Numeric.dot (diff, diff))
+            delta = math.sqrt(Numeric.dot(diff, diff))
             # Is it ok to double h?
             if delta < self.epsilon:
                 i.h *= 2
