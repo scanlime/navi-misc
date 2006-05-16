44a844f9a604ee5a4513eab61d6b60227d1371cb
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-05-16 19:30:39 +0000

    Clean up the Sequence module. Removed the _Coordinate class, streamlined the _findNearest function.

--- trunk/motion/py/Dance/Sequence.py
+++ trunk/motion/py/Dance/Sequence.py
@@ -11,8 +11,6 @@ Classes:
 import Motion, Numeric, math
 from ODE import RK4
 
-__all__ = ["Sequence", "Frame"]
-
 
 class Sequence:
     """Represent a dance sequence mapped to a trajectory.
@@ -29,38 +27,38 @@ class Sequence:
         - __getitem__       Access individual frames using the [] operator
     """
 
-    def __init__ (self, data, system, ic, n=10000, h=.001):
+    def __init__(self, data, system, ic, n=10000, h=.001):
         """Create a new sequence from a chaotic system and a dance sequence.
 
         Arguments:
             - data      The motion data of the sequence
             - system    The chaotic system to map the sequence to
             - ic        The initial conditions for the chaotic system
-            - n         The number of iterations for the trajectory (default 10000)
-            - h         The step size for the trajectory (default .001)
+            - n         The number of iterations for the trajectory(default 10000)
+            - h         The step size for the trajectory(default .001)
         """
         # If we got passed an AMC object, create a list from it. It's unlikely
         # that anything other than an AMC object will be useful here, but for
         # the purpose of testing, allow this class to work with any list of
         # data.
-        if isinstance (data, Motion.AMC): data = self._dataFromAMC (data)
+        if isinstance(data, Motion.AMC): data = self._dataFromAMC(data)
 
-        self.ode = RK4 (system)
+        self.ode = RK4(system)
 
-        traj = self.ode (ic, n, h)
+        traj = self.ode(ic, n, h)
 
-        frames = len (data)
-        step = len (traj) / frames
+        frames = len(data)
+        step = len(traj) / frames
         length = frames * step
 
         # Map each frame evenly over the trajectory.
         self.mapping = {}
-        for i in range (frames):
-            self.mapping[_Coordinate (traj[i * step])] = data[i]
+        for i in range(frames):
+            self.mapping[tuple(traj[i * step])] = data[i]
 
         self.original = data
 
-    def shuffle (self, ic, n=10000, h=.001):
+    def shuffle(self, ic, n=10000, h=.001):
         """Create a shuffled sequence from this sequence.
 
         Generate a new trajectory from the chaotic system used in the initial
@@ -72,23 +70,23 @@ class Sequence:
 
         Arguments:
             - ic        The initial conditions of the new chaotic trajectory
-            - n         The number of iterations of the trajectory (default 10000)
-            - h         The step size of the trajectory (default .001)
+            - n         The number of iterations of the trajectory(default 10000)
+            - h         The step size of the trajectory(default .001)
         """
         shuffled = []
-        frames = len (self.mapping.keys ())
-        traj = self.ode (ic, n, h)
-        step = len (traj) / frames
+        frames = len(self.mapping.keys())
+        traj = self.ode(ic, n, h)
+        step = len(traj) / frames
 
-        for i in range (frames):
-            shuffled.append (self._findNearest (traj[i*step]))
+        for i in range(frames):
+            shuffled.append(self._findNearest(traj[i*step]))
 
         self.shuffled = shuffled
 
         # Parse the shuffled sequence looking for chunk boundaries.
-        self._markChunks ()
+        self._markChunks()
 
-    def insert (self, data, index):
+    def insert(self, data, index):
         """Insert a frame into the sequence at index.
        
         Arguments:
@@ -98,63 +96,62 @@ class Sequence:
         # FIXME - should probably do some error checking to ensure that the
         # Frame or dictionary passed in is compatible with the existing Frames
         # in the sequence.
-        if isinstance (data, dict): data = Frame (data)
-        assert (isinstance (data, Frame))
-        self.shuffled.insert (index, data)
+        if isinstance(data, dict): data = Frame(data)
+        assert(isinstance(data, Frame))
+        self.shuffled.insert(index, data)
 
-    def save (self, filename, format):
+    def save(self, filename, format):
         """Write the sequence to a file.
         
         Arguments:
             - filename      The filename to write to
             - format        The format of the AMC file
         """
-        amc = Motion.AMC ()
+        amc = Motion.AMC()
         amc.format = format
 
         bones = {}
 
-        for bone in self.shuffled[0].bones.iterkeys ():
+        for bone in self.shuffled[0].bones.iterkeys():
             bones[bone] = [frame[bone] for frame in self.shuffled]
 
-        amc.bones = dict ([(bone, Numeric.array (data)) for bone,data in bones.iteritems ()])
+        amc.bones = dict([(bone, Numeric.array(data)) for bone,data in bones.iteritems()])
 
-        amc.save (filename)
+        amc.save(filename)
 
-    def __getitem__ (self, frame):
+    def __getitem__(self, frame):
         """Use [] on a Sequence object to get a frame."""
-        return self.mapping.items () [frame]
+        return self.mapping.items() [frame]
 
-    def _dataFromAMC (self, amc):
+    def _dataFromAMC(self, amc):
         """Create a list of Frames from an AMC object."""
         data = []
-        n = len (amc.bones.values ()[0])
+        n = len(amc.bones.values()[0])
 
-        for i in range (n):
-            data.append (Frame (amc.bones, i))
+        for i in range(n):
+            data.append(Frame(amc.bones, i))
 
         return data
 
-    def _distance (self, x, y):
+    def _distance(self, x, y):
         """Calculate the Euclidean distance between the points x and y."""
-        return math.sqrt (Numeric.sum ((y - x)**2))
+        return math.sqrt(Numeric.sum((y - x)**2))
 
-    def _findNearest (self, point):
+    def _findNearest(self, point):
         """Find a point in the trajectory nearest to the coordinate given."""
-        traj = [Numeric.array (key.coordinates) for key in self.mapping.keys ()]
-        frames = self.mapping.values ()
-        dist = self._distance (traj[0], point)
-        winner = frames[0]
+        # Initialize the distance to infinity
+        dist = 1e300000
+        winner = None
 
-        for i in range (1, len (traj)):
-            delta = self._distance (traj[i], point)
+        for coord, frame in self.mapping.iteritems():
+            delta = self._distance(coord, point)
             if delta < dist:
                 dist = delta
-                winner = frames[i]
+                winner = frame
 
         return winner
 
-    def _markChunks (self):
+    def _markChunks(self):
         """Go over the shuffled sequence looking for chunk boundaries that
            require interpolation. The boundaries list contains indices that mark
            the beginning of a new chunk in the shuffled sequence. So if
@@ -164,44 +161,20 @@ class Sequence:
            shuffled[y].
            """
         self.boundaries = []
-        i = self.original.index (self.shuffled[0])
+        i = self.original.index(self.shuffled[0])
 
-        for j in range (len (self.shuffled)):
+        for j in range(len(self.shuffled)):
             # If we've run off the end of the original sequence, or the frame
             # in shuffled doesn't match the frame in original, we've got the
             # first index of a new chunk.
-           if i >= len (self.original) or self.shuffled[j] != self.original[i]:
-               self.boundaries.append (j)
+           if i >= len(self.original) or self.shuffled[j] != self.original[i]:
+               self.boundaries.append(j)
                # Add one to i because j will be incremented the next time
                # around. At this point we know that shuffled[j] == original[i]
                # because we set i to a value that makes that statement true.
-               i = self.original.index (self.shuffled[j]) + 1
+               i = self.original.index(self.shuffled[j]) + 1
            else:
                i += 1
-
-
-class _Coordinate:
-    """A hashable coordinate object."""
-    def __init__ (self, coords):
-        self.coordinates = Numeric.array (coords)
-
-    def __cmp__ (self, other):
-        if math.sqrt (Numeric.sum (self.coordinates**2)) < \
-                math.sqrt (Numeric.sum (other.coordinates**2)):
-            return -1
-        else:
-            i = iter (other.coordinates)
-            for coord in self.coordinates:
-                if coord != i.next():
-                    return 1
-
-        return 0
-
-    def __hash__ (self):
-        h = 0
-        for coord in self.coordinates:
-            h = h ^ hash (coord)
-        return h
 
 
 class Frame:
@@ -219,7 +192,7 @@ class Frame:
         - __getitem__       Access a single bone in the frame using the []
                             operator
     """
-    def __init__ (self, data, index=None):
+    def __init__(self, data, index=None):
         """Create a Frame object from data.
 
         Arguments:
@@ -235,16 +208,12 @@ class Frame:
         if index != None:
             self.bones = {}
 
-            for bone, values in data.iteritems ():
+            for bone, values in data.iteritems():
                 self.bones[bone] = values[index]
         else:
             self.bones = data
 
-    #def __cmp__ (self, other):
-    #   """Compare dictionaries when comparing frames."""
-    #   return self.bones.__cmp__ (other.bones)
-
-    def __getitem__ (self, bone):
+    def __getitem__(self, bone):
         """Return the position for bone in this frame.
 
         Arguments:
