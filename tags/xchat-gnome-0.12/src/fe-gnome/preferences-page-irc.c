525d6c051f7d421b411005a3b29d662e4f670b4c
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-06-20 23:30:59 +0000

    Tag 0.12

---
+++ tags/xchat-gnome-0.12/src/fe-gnome/preferences-page-irc.c
@@ -0,0 +1,379 @@
+/*
+ * preferences-page-irc.c - helpers for the irc preferences page
+ *
+ * Copyright (C) 2004-2006 xchat-gnome team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#include <config.h>
+#include <glib/gi18n.h>
+#include <string.h>
+#include <gconf/gconf-client.h>
+#include "preferences-page-irc.h"
+#include "preferences-dialog.h"
+
+extern struct xchatprefs prefs;
+
+static void
+entry_changed (GtkEntry *entry, const gchar *key)
+{
+	GConfClient *client;
+	const gchar *text;
+
+	client = gconf_client_get_default ();
+	text = gtk_entry_get_text (entry);
+	if (text)
+		gconf_client_set_string (client, key, text, NULL);
+	g_object_unref (client);
+}
+
+static void
+bool_changed (GtkToggleButton *button, const gchar *key)
+{
+	GConfClient *client;
+	gboolean value;
+
+	client = gconf_client_get_default ();
+	value = gtk_toggle_button_get_active (button);
+	gconf_client_set_bool (client, key, value, NULL);
+	g_object_unref (client);
+}
+
+static void
+font_changed (GtkFontButton *button, const gchar *key)
+{
+	GConfClient *client;
+	const gchar *text;
+
+	client = gconf_client_get_default ();
+	text = gtk_font_button_get_font_name (button);
+	gconf_client_set_string (client, key, text, NULL);
+	g_object_unref (client);
+}
+
+static void
+gconf_entry_changed (GConfClient *client, guint cnxn_id,  GConfEntry *entry, GtkEntry *gtkentry)
+{
+	gchar *text;
+
+	g_signal_handlers_block_by_func (gtkentry, "changed", entry_changed);
+	text = gconf_client_get_string (client, entry->key, NULL);
+	gtk_entry_set_text (gtkentry, text);
+	g_free (text);
+	g_signal_handlers_unblock_by_func (gtkentry, "changed", entry_changed);
+}
+
+static void
+gconf_bool_changed (GConfClient *client, guint cnxn_id, GConfEntry *entry, GtkToggleButton *button)
+{
+	gboolean toggle;
+
+	g_signal_handlers_block_by_func (button, "toggled", bool_changed);
+	toggle = gconf_client_get_bool (client, entry->key, NULL);
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), toggle);
+	g_signal_handlers_unblock_by_func (button, "toggled", bool_changed);
+}
+
+static void
+gconf_font_changed (GConfClient *client, guint cnxn_id, GConfEntry *entry, GtkFontButton *button)
+{
+	gchar *text;
+
+	g_signal_handlers_block_by_func (button, "font-set", font_changed);
+	text = gconf_client_get_string (client, entry->key, NULL);
+	gtk_font_button_set_font_name (button, text);
+	g_free (text);
+	g_signal_handlers_unblock_by_func (button, "font-set", font_changed);
+}
+
+static void
+sysfonts_changed (GtkToggleButton *toggle, PreferencesIrcPage *page)
+{
+	gtk_widget_set_sensitive (page->font_selection, !gtk_toggle_button_get_active (toggle));
+}
+
+static void
+highlight_selection_changed (GtkTreeSelection *select, PreferencesIrcPage *page)
+{
+	if (gtk_tree_selection_get_selected (select, NULL, NULL)) {
+		gtk_widget_set_sensitive (page->highlight_edit, TRUE);
+		gtk_widget_set_sensitive (page->highlight_remove, TRUE);
+	} else {
+		gtk_widget_set_sensitive (page->highlight_edit, FALSE);
+		gtk_widget_set_sensitive (page->highlight_remove, FALSE);
+	}
+}
+
+static void
+save_highlight (PreferencesIrcPage *page)
+{
+	GtkTreeIter iter;
+	gchar *highlight, *tmp1, *tmp2;
+	if (gtk_tree_model_get_iter_first (GTK_TREE_MODEL (page->highlight_store), &iter)) {
+		gtk_tree_model_get (GTK_TREE_MODEL (page->highlight_store), &iter, 0, &tmp1, -1);
+		highlight = g_strdup (tmp1);
+	} else {
+		prefs.irc_extra_hilight[0] = '\0';
+		return;
+	}
+	while (gtk_tree_model_iter_next (GTK_TREE_MODEL (page->highlight_store), &iter)) {
+		tmp2 = highlight;
+		gtk_tree_model_get (GTK_TREE_MODEL (page->highlight_store), &iter, 0, &tmp1, -1);
+		highlight = g_strdup_printf ("%s,%s", tmp2, tmp1);
+		g_free (tmp2);
+	}
+	strncpy (prefs.irc_extra_hilight, highlight, 300);
+	g_free (highlight);
+}
+
+static void
+highlight_add (GtkButton *button, PreferencesIrcPage *page)
+{
+	GtkTreeIter iter;
+	GtkTreePath *path;
+
+	gtk_list_store_append (page->highlight_store, &iter);
+	path = gtk_tree_model_get_path (GTK_TREE_MODEL (page->highlight_store), &iter);
+	gtk_tree_view_set_cursor (GTK_TREE_VIEW (page->highlight_list), path, page->highlight_column, TRUE);
+	gtk_tree_path_free (path);
+}
+
+static void
+highlight_edit (GtkButton *button, PreferencesIrcPage *page)
+{
+	GtkTreeSelection *select;
+	GtkTreeIter iter;
+	GtkTreeModel *model;
+	GtkTreePath *path;
+
+	select = gtk_tree_view_get_selection (GTK_TREE_VIEW (page->highlight_list));
+	if (gtk_tree_selection_get_selected (select, &model, &iter)) {
+		path = gtk_tree_model_get_path (GTK_TREE_MODEL (page->highlight_store), &iter);
+		gtk_tree_view_set_cursor (GTK_TREE_VIEW (page->highlight_list), path, page->highlight_column, TRUE);
+		gtk_tree_path_free (path);
+	}
+}
+
+static void
+highlight_remove (GtkButton *button, PreferencesIrcPage *page)
+{
+	GtkTreeSelection *select;
+	GtkTreeIter iter;
+	GtkTreeModel *model;
+
+	select = gtk_tree_view_get_selection (GTK_TREE_VIEW (page->highlight_list));
+	if (gtk_tree_selection_get_selected (select, &model, &iter)) {
+		gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
+		save_highlight (page);
+	}
+}
+
+static void
+highlight_edited (GtkCellRendererText *renderer, gchar *arg1, gchar *newtext, PreferencesIrcPage *page)
+{
+	GtkTreeSelection *selection;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+
+	selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (page->highlight_list));
+	if (gtk_tree_selection_get_selected (selection, &model, &iter)) {
+		if (strlen (newtext))
+			gtk_list_store_set (GTK_LIST_STORE (model), &iter, 0, newtext, -1);
+		else
+			gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
+		save_highlight (page);
+	}
+}
+
+static void
+highlight_canceled (GtkCellRendererText *renderer, PreferencesIrcPage *page)
+{
+	GtkTreeSelection *selection;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+	gchar *text;
+
+	selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (page->highlight_list));
+	if (gtk_tree_selection_get_selected (selection, &model, &iter)) {
+		gtk_tree_model_get (GTK_TREE_MODEL (page->highlight_store), &iter, 0, &text, -1);
+		if (text == NULL)
+			gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
+	}
+}
+
+PreferencesIrcPage *
+preferences_page_irc_new (gpointer prefs_dialog, GladeXML *xml)
+{
+	PreferencesIrcPage *page = g_new0 (PreferencesIrcPage, 1);
+	PreferencesDialog *p = (PreferencesDialog *) prefs_dialog;
+	GtkTreeIter iter;
+	GtkCellRenderer *renderer;
+	gchar *text;
+	gboolean toggle;
+	GtkTreeSelection *select;
+	gchar **highlight_entries;
+	gint i;
+
+#define GW(name) ((page->name) = glade_xml_get_widget (xml, #name))
+	GW(nick_name);
+	GW(real_name);
+	GW(quit_message);
+	GW(part_message);
+	GW(away_message);
+
+	GW(highlight_list);
+	GW(highlight_add);
+	GW(highlight_edit);
+	GW(highlight_remove);
+
+	GW(usesysfonts);
+	GW(usethisfont);
+	GW(font_selection);
+
+	GW(show_colors);
+	GW(show_timestamps);
+#undef GW
+
+	if (g_file_test ("../../data/irc.png", G_FILE_TEST_EXISTS))
+		page->icon = gdk_pixbuf_new_from_file ("../../data/irc.png", NULL);
+	else
+		page->icon = gdk_pixbuf_new_from_file (XCHATSHAREDIR "/irc.png", NULL);
+	gtk_list_store_append (p->page_store, &iter);
+	gtk_list_store_set (p->page_store, &iter, 0, page->icon, 1, _("IRC Preferences"), 2, 0, -1);
+
+	g_signal_connect (G_OBJECT (page->nick_name),        "changed",  G_CALLBACK (entry_changed),    "/apps/xchat/irc/nickname");
+	g_signal_connect (G_OBJECT (page->real_name),        "changed",  G_CALLBACK (entry_changed),    "/apps/xchat/irc/realname");
+	g_signal_connect (G_OBJECT (page->quit_message),     "changed",  G_CALLBACK (entry_changed),    "/apps/xchat/irc/quitmsg");
+	g_signal_connect (G_OBJECT (page->part_message),     "changed",  G_CALLBACK (entry_changed),    "/apps/xchat/irc/partmsg");
+	g_signal_connect (G_OBJECT (page->away_message),     "changed",  G_CALLBACK (entry_changed),    "/apps/xchat/irc/awaymsg");
+	g_signal_connect (G_OBJECT (page->usesysfonts),      "toggled",  G_CALLBACK (bool_changed),     "/apps/xchat/main_window/use_sys_fonts");
+	g_signal_connect (G_OBJECT (page->usesysfonts),      "toggled",  G_CALLBACK (sysfonts_changed), page);
+	g_signal_connect (G_OBJECT (page->font_selection),   "font-set", G_CALLBACK (font_changed),     "/apps/xchat/main_window/font");
+	g_signal_connect (G_OBJECT (page->show_colors),      "toggled",  G_CALLBACK (bool_changed),     "/apps/xchat/irc/showcolors");
+	g_signal_connect (G_OBJECT (page->show_timestamps),  "toggled",  G_CALLBACK (bool_changed),     "/apps/xchat/irc/showtimestamps");
+	g_signal_connect (G_OBJECT (page->highlight_add),    "clicked",  G_CALLBACK (highlight_add),    page);
+	g_signal_connect (G_OBJECT (page->highlight_edit),   "clicked",  G_CALLBACK (highlight_edit),   page);
+	g_signal_connect (G_OBJECT (page->highlight_remove), "clicked",  G_CALLBACK (highlight_remove), page);
+
+	page->nh[0] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/nickname",              (GConfClientNotifyFunc) gconf_entry_changed, page->nick_name,       NULL, NULL);
+	page->nh[1] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/realname",              (GConfClientNotifyFunc) gconf_entry_changed, page->real_name,       NULL, NULL);
+	page->nh[2] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/quitmsg",               (GConfClientNotifyFunc) gconf_entry_changed, page->quit_message,    NULL, NULL);
+	page->nh[3] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/partmsg",               (GConfClientNotifyFunc) gconf_entry_changed, page->part_message,    NULL, NULL);
+	page->nh[4] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/awaymsg",               (GConfClientNotifyFunc) gconf_entry_changed, page->away_message,    NULL, NULL);
+	page->nh[5] = gconf_client_notify_add (p->gconf, "/apps/xchat/main_window/use_sys_fonts", (GConfClientNotifyFunc) gconf_bool_changed,  page->usesysfonts,     NULL, NULL);
+	page->nh[6] = gconf_client_notify_add (p->gconf, "/apps/xchat/main-window/font",          (GConfClientNotifyFunc) gconf_font_changed,  page->font_selection,  NULL, NULL);
+	page->nh[7] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/showcolors",            (GConfClientNotifyFunc) gconf_bool_changed,  page->show_colors,     NULL, NULL);
+	page->nh[8] = gconf_client_notify_add (p->gconf, "/apps/xchat/irc/showtimestamps",        (GConfClientNotifyFunc) gconf_bool_changed,  page->show_timestamps, NULL, NULL);
+
+	text = gconf_client_get_string (p->gconf, "/apps/xchat/irc/nickname", NULL);
+	if (text) {
+		gtk_entry_set_text (GTK_ENTRY (page->nick_name), text);
+		g_free (text);
+	} else {
+		gtk_entry_set_text (GTK_ENTRY (page->nick_name), "");
+	}
+
+	text = gconf_client_get_string (p->gconf, "/apps/xchat/irc/realname", NULL);
+	if (text) {
+		gtk_entry_set_text (GTK_ENTRY (page->real_name), text);
+		g_free (text);
+	} else {
+		gtk_entry_set_text (GTK_ENTRY (page->real_name), "");
+	}
+
+	text = gconf_client_get_string (p->gconf, "/apps/xchat/irc/quitmsg", NULL);
+	if (text) {
+		gtk_entry_set_text (GTK_ENTRY (page->quit_message), text);
+		g_free (text);
+	} else {
+		gtk_entry_set_text (GTK_ENTRY (page->quit_message), "");
+	}
+
+	text = gconf_client_get_string (p->gconf, "/apps/xchat/irc/partmsg", NULL);
+	if (text) {
+		gtk_entry_set_text (GTK_ENTRY (page->part_message), text);
+		g_free (text);
+	} else {
+		gtk_entry_set_text (GTK_ENTRY (page->part_message), "");
+	}
+
+	text = gconf_client_get_string (p->gconf, "/apps/xchat/irc/awaymsg", NULL);
+	if (text) {
+		gtk_entry_set_text (GTK_ENTRY (page->away_message), text);
+		g_free (text);
+	} else {
+		gtk_entry_set_text (GTK_ENTRY (page->away_message), "");
+	}
+
+	toggle = gconf_client_get_bool (p->gconf, "/apps/xchat/main_window/use_sys_fonts", NULL);
+	if (toggle)
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (page->usesysfonts), TRUE);
+	else
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (page->usethisfont), TRUE);
+	gtk_widget_set_sensitive (page->font_selection, !toggle);
+
+	text = gconf_client_get_string (p->gconf, "/apps/xchat/main_window/font", NULL);
+	if (text) {
+		gtk_font_button_set_font_name (GTK_FONT_BUTTON (page->font_selection), text);
+		g_free (text);
+	}
+
+	toggle = gconf_client_get_bool (p->gconf, "/apps/xchat/irc/showcolors", NULL);
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (page->show_colors), toggle);
+
+	toggle = gconf_client_get_bool (p->gconf, "/apps/xchat/irc/showtimestamps", NULL);
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (page->show_timestamps), toggle);
+
+	/* highlight list */
+	page->highlight_store = gtk_list_store_new (1, G_TYPE_STRING);
+	gtk_tree_view_set_model (GTK_TREE_VIEW (page->highlight_list), GTK_TREE_MODEL (page->highlight_store));
+	renderer = gtk_cell_renderer_text_new ();
+	g_object_set (G_OBJECT (renderer), "editable", TRUE, NULL);
+	page->highlight_column = gtk_tree_view_column_new_with_attributes ("highlight", renderer, "text", 0, NULL);
+	gtk_tree_view_insert_column (GTK_TREE_VIEW (page->highlight_list), page->highlight_column, 0);
+	gtk_widget_set_sensitive (page->highlight_edit, FALSE);
+	gtk_widget_set_sensitive (page->highlight_remove, FALSE);
+	select = gtk_tree_view_get_selection (GTK_TREE_VIEW (page->highlight_list));
+	g_signal_connect (G_OBJECT (select), "changed", G_CALLBACK (highlight_selection_changed), page);
+	g_signal_connect (G_OBJECT (renderer), "edited", G_CALLBACK (highlight_edited), page);
+	g_signal_connect (G_OBJECT (renderer), "editing-canceled", G_CALLBACK (highlight_canceled), page);
+
+	highlight_entries = g_strsplit (prefs.irc_extra_hilight, ",", 0);
+	for (i = 0; highlight_entries[i]; i++) {
+		gtk_list_store_append (page->highlight_store, &iter);
+		gtk_list_store_set (page->highlight_store, &iter, 0, highlight_entries[i], -1);
+	}
+	g_strfreev (highlight_entries);
+
+	return page;
+}
+
+void
+preferences_page_irc_free (PreferencesIrcPage *page)
+{
+	gint i;
+	GConfClient *client;
+
+	client = gconf_client_get_default ();
+	for (i = 0; i < 9; i++)
+		gconf_client_notify_remove (client, page->nh[i]);
+	g_object_unref (client);
+
+	g_object_unref (page->highlight_store);
+	g_object_unref (page->icon);
+	g_free (page);
+}
---
+++ tags/xchat-gnome-0.12/src/fe-gnome/topic-label.c
@@ -0,0 +1,316 @@
+/*
+ * topic-label.c - Widget encapsulating the topic label
+ *
+ * Copyright (C) 2004-2006 xchat-gnome team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+#include <config.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+#ifdef HAVE_LIBSEXY
+#include <libsexy/sexy-url-label.h>
+#endif
+#include "gui.h"
+#include "text-entry.h"
+#include "topic-label.h"
+#include "../common/fe.h"
+#include "../common/url.h"
+#include "../common/outbound.h"
+
+static void  topic_label_class_init       (TopicLabelClass *klass);
+static void  topic_label_init             (TopicLabel      *label);
+static void  topic_label_finalize         (GObject         *object);
+static void  topic_label_expand_activate  (GtkExpander     *expander,
+                                           TopicLabel      *label);
+static char *topic_label_get_topic_string (const char      *topic);
+static void  topic_label_size_allocate    (GtkWidget       *widget,
+                                           GtkAllocation   *allocation);
+#ifdef HAVE_LIBSEXY
+static void  topic_label_url_activated    (GtkWidget       *url_label,
+                                           const char      *url,
+                                           TopicLabel      *label);
+#endif
+static void  topic_entry_activate         (GtkTextBuffer   *textbuffer,
+                                           GtkTextIter     *arg1,
+                                           gchar           *text,
+                                           gint             len,
+                                           GtkDialog       *dialog);
+
+struct _TopicLabelPriv
+{
+	GtkWidget      *expander;
+	GtkWidget      *label;
+	GtkWidget      *sizing_label;
+
+	GHashTable     *topics;
+	struct session *current;
+};
+
+static GtkHBoxClass *parent_class;
+
+G_DEFINE_TYPE (TopicLabel, topic_label, GTK_TYPE_HBOX);
+
+static void
+topic_label_class_init (TopicLabelClass *klass)
+{
+	GObjectClass   *gobject_class;
+	GtkWidgetClass *widget_class;
+
+	parent_class = g_type_class_peek_parent (klass);
+
+	gobject_class = G_OBJECT_CLASS (klass);
+	gobject_class->finalize = topic_label_finalize;
+
+	widget_class = GTK_WIDGET_CLASS (klass);
+	widget_class->size_allocate = topic_label_size_allocate;
+}
+
+static void
+topic_label_init (TopicLabel *label)
+{
+	label->priv = g_new0 (TopicLabelPriv, 1);
+	label->priv->expander = gtk_expander_new (NULL);
+#ifdef HAVE_LIBSEXY
+	label->priv->label = sexy_url_label_new ();
+	label->priv->sizing_label = sexy_url_label_new ();
+#else
+	label->priv->label = gtk_label_new (NULL);
+	label->priv->sizing_label = gtk_label_new (NULL);
+#endif
+
+	gtk_expander_set_expanded (GTK_EXPANDER (label->priv->expander), FALSE);
+	gtk_expander_set_use_markup (GTK_EXPANDER (label->priv->expander), TRUE);
+	gtk_label_set_ellipsize (GTK_LABEL (label->priv->label), PANGO_ELLIPSIZE_END);
+
+	gtk_widget_show (label->priv->expander);
+	gtk_widget_show (label->priv->label);
+	gtk_widget_show (GTK_WIDGET (label));
+
+	gtk_box_pack_start (GTK_BOX (label), label->priv->expander, FALSE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (label), label->priv->label,    TRUE,  TRUE, 0);
+	gtk_box_set_spacing (GTK_BOX (label), 6);
+
+	g_signal_connect (G_OBJECT (label->priv->expander), "activate",      G_CALLBACK (topic_label_expand_activate), (gpointer) label);
+#ifdef HAVE_LIBSEXY
+	g_signal_connect (G_OBJECT (label->priv->label),    "url_activated", G_CALLBACK (topic_label_url_activated),   (gpointer) label);
+#endif
+
+	label->priv->topics = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, g_free);
+}
+
+static void
+topic_label_finalize (GObject *object)
+{
+	if (G_OBJECT_CLASS (parent_class)->finalize)
+		G_OBJECT_CLASS (parent_class)->finalize (object);
+}
+
+static void
+topic_label_expand_activate (GtkExpander *expander, TopicLabel *label)
+{
+	if (gtk_expander_get_expanded (expander)) {
+		gtk_label_set_ellipsize (GTK_LABEL (label->priv->label), PANGO_ELLIPSIZE_END);
+		gtk_label_set_line_wrap (GTK_LABEL (label->priv->label), FALSE);
+		gtk_misc_set_alignment (GTK_MISC (label->priv->label), 0.5, 0.5);
+	} else {
+		gtk_label_set_ellipsize (GTK_LABEL (label->priv->label), PANGO_ELLIPSIZE_NONE);
+		gtk_label_set_line_wrap (GTK_LABEL (label->priv->label), TRUE);
+		gtk_misc_set_alignment (GTK_MISC (label->priv->label), 0.0, 0.5);
+	}
+}
+
+#ifdef HAVE_LIBSEXY
+static void
+topic_label_url_activated (GtkWidget *url_label, const char *url, TopicLabel *label)
+{
+	char *command;
+	command = g_strdup_printf ("URL %s", url);
+	handle_command (label->priv->current, command, 1);
+	g_free (command);
+}
+#endif
+
+GtkWidget *
+topic_label_new (void)
+{
+	TopicLabel *label = g_object_new (topic_label_get_type (), NULL);
+	gtk_label_set_selectable (GTK_LABEL (label->priv->label), TRUE);
+	return GTK_WIDGET (label);
+}
+
+void
+topic_label_set_topic (TopicLabel *label, struct session *sess, const char *topic)
+{
+	gchar *escaped;
+
+	escaped = topic_label_get_topic_string (topic);
+
+	g_hash_table_insert (label->priv->topics, sess, escaped);
+
+	if (sess == label->priv->current)
+		topic_label_set_current (label, sess);
+}
+
+void
+topic_label_remove_session (TopicLabel *label, struct session *sess)
+{
+	g_hash_table_remove (label->priv->topics, sess);
+	if (sess == label->priv->current)
+		gtk_label_set_text (GTK_LABEL (label->priv->label), "");
+}
+
+void
+topic_label_set_current (TopicLabel *label, struct session *sess)
+{
+	gchar *topic;
+
+	topic = g_hash_table_lookup (label->priv->topics, sess);
+#ifdef HAVE_LIBSEXY
+	if (topic) sexy_url_label_set_markup (SEXY_URL_LABEL (label->priv->label), topic);
+	else       gtk_label_set_text (GTK_LABEL (label->priv->label), "");
+
+	if (topic) sexy_url_label_set_markup (SEXY_URL_LABEL (label->priv->sizing_label), topic);
+	else       gtk_label_set_text (GTK_LABEL (label->priv->sizing_label), "");
+#else
+	if (topic) gtk_label_set_text (GTK_LABEL (label->priv->label), topic);
+	else       gtk_label_set_text (GTK_LABEL (label->priv->label), "");
+
+	if (topic) gtk_label_set_text (GTK_LABEL (label->priv->sizing_label), topic);
+	else       gtk_label_set_text (GTK_LABEL (label->priv->sizing_label), "");
+#endif
+
+	if (sess->type == SESS_SERVER)
+		gtk_widget_hide (gui.topic_hbox);
+	else
+		gtk_widget_show (gui.topic_hbox);
+
+	label->priv->current = sess;
+}
+
+static char *
+topic_label_get_topic_string (const char *topic)
+{
+#ifdef HAVE_LIBSEXY
+	/* FIXME: this probably isn't unicode-clean */
+	gchar **tokens;
+	gchar *escaped, *result, *temp;
+	int i;
+
+	if ((topic == NULL) || (strlen (topic) == 0))
+		return NULL;
+
+	/* escape out <>&"' so that pango markup doesn't get confused */
+	escaped = g_markup_escape_text (topic, strlen (topic));
+
+	/* surround urls with <a> markup so that sexy-url-label can link it */
+	tokens = g_strsplit_set (escaped, " \t\n", 0);
+	if (url_check_word (tokens[0], strlen (tokens[0])) == WORD_URL)
+		result = g_strdup_printf ("<a href=\"%s\">%s</a>", tokens[0], tokens[0]);
+	else
+		result = g_strdup (tokens[0]);
+	for (i = 1; tokens[i]; i++) {
+		if (url_check_word (tokens[i], strlen (tokens[i])) == WORD_URL) {
+			temp = g_strdup_printf ("%s <a href=\"%s\">%s</a>", result, tokens[i], tokens[i]);
+			g_free (result);
+			result = temp;
+		} else {
+			temp = g_strdup_printf ("%s %s", result, tokens[i]);
+			g_free (result);
+			result = temp;
+		}
+	}
+	g_strfreev (tokens);
+	g_free (escaped);
+	return result;
+#else
+	return g_strdup (topic);
+#endif
+}
+
+void
+topic_label_change_current (TopicLabel *label)
+{
+	GladeXML *xml = NULL;
+	GtkWidget *dialog, *entry;
+	GtkTextBuffer *buffer;
+	gchar *title, *topic;
+
+	if ((label->priv->current == NULL) || (label->priv->current->type != SESS_CHANNEL))
+		return;
+
+	if (g_file_test ("../../data/topic-change.glade", G_FILE_TEST_EXISTS))
+		xml = glade_xml_new ("../../data/topic-change.glade", NULL, NULL);
+	else
+		xml = glade_xml_new (XCHATSHAREDIR "/topic-change.glade", NULL, NULL);
+
+	dialog = glade_xml_get_widget (xml, "topic change");
+        entry  = glade_xml_get_widget (xml, "topic entry box");
+
+	title = g_strdup_printf (_("Changing topic for %s"), label->priv->current->channel);
+	gtk_window_set_title (GTK_WINDOW (dialog), title);
+	g_free (title);
+
+	topic = label->priv->current->topic;
+	buffer = gtk_text_buffer_new (NULL);
+        gtk_text_view_set_buffer (GTK_TEXT_VIEW (entry), buffer);
+	g_signal_connect (G_OBJECT (buffer), "insert-text", G_CALLBACK (topic_entry_activate), dialog);
+	if (topic)
+        	gtk_text_buffer_set_text (buffer, topic, -1);
+
+	if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_OK) {
+		GtkTextIter start, end;
+		gchar *newtopic;
+
+		gtk_text_buffer_get_start_iter (buffer, &start);
+		gtk_text_buffer_get_end_iter (buffer, &end);
+		gtk_widget_hide (dialog);
+		gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (entry), GTK_WRAP_NONE);
+		newtopic = gtk_text_buffer_get_text (buffer, &start, &end, FALSE);
+		label->priv->current->server->p_topic (label->priv->current->server, label->priv->current->channel, newtopic);
+		g_free (newtopic);
+	}
+
+	gtk_widget_destroy (dialog);
+        g_object_unref (xml);
+}
+
+static void
+topic_label_size_allocate (GtkWidget *widget, GtkAllocation *allocation)
+{
+	TopicLabel     *label;
+	GtkRequisition  request;
+
+	if (GTK_WIDGET_CLASS (parent_class)->size_allocate)
+		GTK_WIDGET_CLASS (parent_class)->size_allocate (widget, allocation);
+
+	label = TOPIC_LABEL (widget);
+	gtk_widget_size_request (label->priv->sizing_label, &request);
+
+	if (request.width < allocation->width - 6) {
+		gtk_widget_hide (label->priv->expander);
+	} else {
+		gtk_widget_show (label->priv->expander);
+	}
+}
+
+static void
+topic_entry_activate (GtkTextBuffer *textbuffer, GtkTextIter *arg1, gchar *text, gint len, GtkDialog *dialog)
+{
+	if (strncmp (text, "\n", len) == 0)
+		gtk_dialog_response (dialog, GTK_RESPONSE_OK);
+}
