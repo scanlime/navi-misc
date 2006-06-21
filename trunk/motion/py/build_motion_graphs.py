f724a263ac7390a97b6d295521d62b41032674dc
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-06-21 17:50:53 +0000

    Fix a bug in the cost function: add the length of the path returned by dijkstraSearch instead of the path itself to the cost. Fix the import statements for MotionGraph. Remove the succ() function since it is obsolete and update the call to HeuristicPrint.

--- trunk/motion/py/build_motion_graphs.py
+++ trunk/motion/py/build_motion_graphs.py
@@ -21,7 +21,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
-from Dance import MotionGraph
+from Graph import MotionGraph
 from Motion import AMC
 import sys, pickle
 
