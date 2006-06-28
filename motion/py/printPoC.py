7ddbf2fdb1fb6f859683fb457f121d79b5cf0761
Evan Sheehan <darthmall@users.noreply.github.com>
2006-06-28 17:23:13 +0000

    Comment up the printing script. Make Combinatoric.Node a flyweight to work around the problem of generating duplicate nodes.

--- motion/py/printPoC.py
+++ motion/py/printPoC.py
@@ -21,27 +21,41 @@ from optparse import OptionParser
 import random, re
 
 class Node (object, Dot.Node):
+    """Basic node class for printing faked data.
+   
+    The class uses the flyweight pattern because algorithms_c.dijkstraSearch
+    uses a direct hash of the memory address of the node for comparison, so we
+    don't want to equal nodes having different memory addresses.
+    """
+
     _cached = {}
 
     def __new__ (cls, data, **kwargs):
+        """Return a Node. If one doesn't already exist for this piece of data,
+        create a new one.
+        """
         if data not in Node._cached:
             Node._cached[data] = object.__new__ (cls)
 
         return Node._cached[data]
 
     def __init__ (self, data, **kwargs):
+        """Initialize the node."""
         Dot.Node.__init__ (self, **kwargs)
         if 'label' not in self.dotAttrs:
             self.dotAttrs['label'] = str (data)
         self.data = data
 
     def __eq__ (self, other):
+        """Nodes are equal when their data is equal."""
         return self.data == other.data
 
     def __ne__ (self, other):
+        """Nodes are unequal when their data is unequal."""
         return self.data != other.data
 
     def __hash__ (self):
+        """The only identifying attribute of the node is its data."""
         return hash (self.data)
 
     def __str__ (self):
@@ -52,6 +66,11 @@ class Node (object, Dot.Node):
 
 
 def cost (path, goal):
+    """Return the cost of ``path``.
+    
+    Cost of a given path is the length of the path, plus the sum of each
+    shortest path from the end of it to the goal.
+    """
     g = len (path)
     h = 0
     for name, x in graphs.iteritems ():
@@ -61,6 +80,9 @@ def cost (path, goal):
     return (g + h)
 
 def startGraph (match):
+    """Create a new graph and add it to the dictionary of graphs using its name
+    as the key.
+    """
     graph = Graph ()
     adj = AdjacencyList (graph)
     vMap = VertexMap (graph)
@@ -68,21 +90,26 @@ def startGraph (match):
     graphs[match.group(1)] = graph
 
 def startEdge (match):
+    """Create an edge."""
     u, v = match.groups ()
     edges.add (Edge (Node (u), Node (v)))
 
 def endGraph (match):
+    """Add the ``edges`` set to the current graph and clear ``edges``."""
     global edges
     graphs[match.group (1)].addList (edges)
     edges = set ([])
 
 
+# Option parser in case this script gets more complicated
 options = OptionParser (usage="%prog <graphs>")
 opts, args = options.parse_args ()
 
+# Check the arguments
 if len (args) != 1:
     options.error ("Incorrect number of arguments")
 
+# Set up the regexs and callbacks used for parsing the file of graphs
 callbacks = {re.compile (r'^\[(\w*)\]$'): startGraph, \
         re.compile (r'^(\w*) -> (\w*)$'): startEdge, \
         re.compile (r'^\[/(\w*)\]$'): endGraph}
@@ -92,6 +119,7 @@ graphs = {}
 edges = set ([])
 f = file (args[0], 'r')
 for line in f:
+    # When we find a matching regex, call the corresponding function
     for regex, callback in callbacks.iteritems ():
         m = regex.search (line)
         if m:
@@ -99,6 +127,7 @@ for line in f:
             break
 f.close ()
 
+# Build a combinatoric graph from all the graphs found in the input file.
 cgraph = Graph ()
 #adj = Combinatoric.AdjacencyList (graph)
 cmap = Combinatoric.VertexMap (cgraph)
@@ -106,28 +135,46 @@ cedges = Combinatoric.EdgeList (cgraph)
 
 cgraph.addList (graphs.items ())
 
+# Create a non-combinatoric graph from the combinatoric graph.
+# FIXME: this is a hack because the combinatoric representations do not work
+# with the printing search.
 graph = Graph ()
 adj = AdjacencyList (graph)
 map = VertexMap (graph)
 edges = EdgeList (graph)
 
+print "building single graph from combined"
+#graph.addList ([Edge (Node (repr (e.u)), Node (repr (e.v))) for e in cedges])
 graph.addList ([e for e in cedges])
+print "done"
 
+# Save a copy of each individual graph
 for name, g in graphs.iteritems ():
     print "saving", name
     f = file ('graphs/%s.dot' % name, 'w')
     DotPrint (g, f)
     f.close ()
 
+# Save a copy of the combinatoric graph
 print "saving combined"
 f = file ('graphs/all.dot', 'w')
 DotPrint (graph, f)
 f.close ()
 
-source = random.choice ([v for v in cmap])
+# Choose a random start and build a path from it
+source = random.choice ([v for v in map])
 p = [source]
-p.append (random.choice ([v for v in adj.query (p[-1])]).v)
+for i in range (3):
+    q = random.choice ([v for v in adj.query (p[-1])]).v
+    p.append (q)
 
+for v in map:
+    if v in p and id (v) != id (p[p.index (v)]):
+        print "2 copies of", repr (v)
+
+print p
+
+# Run the search to find a path from source to the end of the path
 HeuristicPrint (graph, cost, source, p[-1])
             
 # vim: ts=4:sw=4:et
