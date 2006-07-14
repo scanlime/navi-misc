a0406295f9e4103976601c73584ca536f1a37df2
Evan Sheehan <darthmall@users.noreply.github.com>
2006-07-14 17:59:51 +0000

    Import MotionGraph and rename the script---apparently pickle uses a module called inspect, so I can't name this script that.

--- motion/py/graph_inspect.py
+++ motion/py/graph_inspect.py
@@ -16,6 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+from Graph import MotionGraph
 from Graph.Data import VertexMap, AdjacencyList
 from optparse import OptionParser
 import pickle
