9318f327af5176efbb211df553debe22035c056f
cassidy <cassidy@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-07-27 19:40:31 +0000

    Apply Xavier Claessens's patch: - Change org.xchat. to org.xchatgnome. - Set PVERSION to "" like that the dbus plugin doesn't appear in plugin list anymore. - DBus plugin depends on dbus and dbus-plugin >=0.60

--- xchat-gnome/src/common/dbus/example.py
+++ xchat-gnome/src/common/dbus/example.py
@@ -3,8 +3,8 @@
 import dbus
 
 bus = dbus.SessionBus()
-proxy_obj = bus.get_object('org.xchat.service', '/org/xchat/RemoteObject')
-xchat = dbus.Interface(proxy_obj, 'org.xchat.interface')
+proxy_obj = bus.get_object('org.xchatgnome.service', '/org/xchatgnome/RemoteObject')
+xchat = dbus.Interface(proxy_obj, 'org.xchatgnome.interface')
 
 lst = xchat.ListGet ("users")
 while xchat.ListNext (lst):
