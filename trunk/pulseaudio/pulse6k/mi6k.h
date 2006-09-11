7a18f65f6e3d1d6d4b75987aa91d85f3b18cad75
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-09-11 00:21:11 +0000

    Grayscale mode! Since the display doesn't have enough bandwidth to update every column's brightness at every frame, we prioritize the columns by generating a probability distribution based on how much each column has changed since the last update.

--- trunk/pulseaudio/pulse6k/mi6k.h
+++ trunk/pulseaudio/pulse6k/mi6k.h
@@ -15,7 +15,21 @@
 #define MI6K_WIDTH        20
 #define MI6K_HEIGHT       2
 
-void mi6k_frame_new(pa_memchunk *out, unsigned char **chars);
+#define MI6K_BRIGHTNESS_MIN    0x07
+#define MI6K_BRIGHTNESS_MAX    0x00
+
+#define MI6K_BRIGHTNESS(x) ((int) (MI6K_BRIGHTNESS_MIN + \
+                           (MI6K_BRIGHTNESS_MAX - MI6K_BRIGHTNESS_MIN) * (x) + 0.5))
+
+struct mi6k_dim_command {
+  unsigned char cmd[2];
+  unsigned char column;
+  unsigned char level;
+};
+
+void mi6k_frame_new(pa_memchunk *out,
+		    unsigned char **chars_out,
+		    struct mi6k_dim_command **dim_command_out);
 void mi6k_frame_bargraph(pa_memchunk *out, float *columns);
 int mi6k_needs_frame(void);
 void mi6k_commit_frame(pa_memchunk *frame);
