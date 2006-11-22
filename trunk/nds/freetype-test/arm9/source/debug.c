59ac1130390f0c39ef2678f20d01a27cce8b5daa
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-11-22 03:09:46 +0000

    A working libfreetype2 demo for the Nintendo DS, finally. Found what may be a Thumb interwork bug in GCC. Disabled Thumb temporarily, and wrote a new CPU exception handler to help diagnose the problem. Next up: Unicode, better font scaling, and higher performance.

--- trunk/nds/freetype-test/arm9/source/debug.c
+++ trunk/nds/freetype-test/arm9/source/debug.c
@@ -1,4 +1,5 @@
 #include <nds.h>
+#include <nds/arm9/exceptions.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <loader-exit.h>
@@ -8,53 +9,90 @@
 #include FT_FREETYPE_H
 #include FT_INTERNAL_DEBUG_H
 
+#ifdef FT_DEBUG_LEVEL_TRACE
 int ft_trace_levels[trace_count];
+#endif
 
 void Panic(void)
 {
-  iprintf("\nPanic!\nPress any key for loader exit.\n");
+    iprintf("\nPanic! Press any key for loader");
 
-  irqInit();
-  irqEnable(IRQ_VBLANK);
+    REG_IME = 0;
+    do {
+	scanKeys();
+    } while (keysHeld());
+    do {
+	scanKeys();
+    } while (!keysHeld());
 
-  do {
-    swiWaitForVBlank();
-    scanKeys();
-  } while (keysHeld());
-  do {
-    swiWaitForVBlank();
-    scanKeys();
-  } while (!keysHeld());
-
-  Loader_Exit9();
-  iprintf("No loader found!\n");
-  while (1);
+    Loader_Exit9();
+    iprintf("\nNo loader found! Halting.\n");
+    while (1);
 }
  
 void FT_Message(const char*  fmt, ...)
 {
-  va_list ap;
+    va_list ap;
 
-  va_start(ap, fmt);
-  viprintf(fmt, ap);
-  va_end(ap);
+    va_start(ap, fmt);
+    viprintf(fmt, ap);
+    va_end(ap);
 }
 
 void FT_Panic(const char*  fmt, ...)
 {
-  va_list ap;
+    va_list ap;
 
-  va_start(ap, fmt);
-  viprintf(fmt, ap);
-  va_end(ap);
+    va_start(ap, fmt);
+    viprintf(fmt, ap);
+    va_end(ap);
 
-  Panic();
+    Panic();
 }
 
 void ft_debug_init(void)
 {
-  int i;
-  for (i=0; i<trace_count; i++) {
-    ft_trace_levels[i] = 10;
-  }
+#ifdef FT_DEBUG_LEVEL_TRACE
+    int i;
+    for (i=0; i<trace_count; i++) {
+	ft_trace_levels[i] = 10;
+    }
+#endif
+}
+
+void exception_handler() {
+    int i;
+    u32 currentMode = getCPSR() & 0x1f;
+    u32 thumbState = ((*(u32*)0x027FFD90) & 0x20);    
+    u32 savedPC = *(u32*)0x027FFD98;
+    u32 exceptionAddress;
+
+    static const char *registerNames[] = {
+	"r0","r1","r2","r3","r4","r5","r6","r7",
+	"r8","r9","r10","r11","r12","sp","lr","pc",
+    };
+
+    exceptionRegisters[15] = savedPC;
+
+    if (currentMode == 0x17) {
+	/* Data abort- actual faulting instruction was 8 bytes earlier */
+	exceptionAddress = savedPC - 8;
+    } else {
+	/*
+	 * XXX: Assuming invalid instruction error?
+	 * Place the fault at the previous instruction.
+	 */
+	exceptionAddress = savedPC - (thumbState ? 2 : 4);
+    }
+
+    iprintf("\nException %02x @ %08x (%s)\n",
+	    currentMode, exceptionAddress,
+	    thumbState ? "Thumb" : "ARM");
+
+    for (i = 0; i < 8; i++) {
+	iprintf(" %-03s %08x ", registerNames[i], exceptionRegisters[i]);
+	iprintf(" %-03s %08x \n", registerNames[i+8], exceptionRegisters[i+8]);
+    }
+
+    Panic();
 }
