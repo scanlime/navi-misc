8ac3ba9d1cdd9cfb3ce4de5eaea08cfe935cd7c9
lurgyman <lurgyman@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-12-30 08:15:07 +0000

    yay, nearly doin stuff

--- trunk/simple-irc/simple-irc.py
+++ trunk/simple-irc/simple-irc.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 import gtk
-from sirc import ui, connection
+from sirc import ui
+from sirc.common import server
+from telepathy.interfaces import *
 
-factory = connection.ConnectionFactory()
-connection = factory()
-
+factory = server.ServerFactory()
+server = factory('irc')
+server.Connect()
 win = ui.Window()
 win.show_all()
 gtk.main()
+print server.GetStatus()
+print server.conn.get_valid_interfaces()
+server.Disconnect()
