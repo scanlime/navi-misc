a7b864fb7a70784354fa7fab213fe498acef51ab
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-08 11:06:47 +0000

    Convert registration to newforms

--- trunk/bonvivant/apps/accounts/views.py
+++ trunk/bonvivant/apps/accounts/views.py
@@ -1,7 +1,8 @@
-from bonvivant.apps.ext.forms import DojoTextField, DojoPasswordField
+#from bonvivant.apps.ext.forms import DojoTextField, DojoPasswordField, CaptchaField
 from bonvivant.apps.recipes.models import Recipe
 
-from django import forms
+from django import oldforms
+from django import newforms as forms
 from django.contrib.auth import authenticate, login, logout, LOGIN_URL, REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -10,76 +11,71 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-class RegistrationForm(forms.Manipulator):
-    def __init__(self):
-        self.fields = (
-            DojoTextField(field_name='username',
-                          length=30, maxlength=30,
-                          is_required=True,
-                          validator_list=[validators.isAlphaNumeric, self.isValidUsername]),
+def user_exists(username):
+    return User.objects.filter(username=username).count()
 
-            forms.EmailField(field_name='email',
-                             length=30, maxlength=70,
-                             is_required=False),
+class RegistrationForm(forms.Form):
+    username = forms.RegexField(r"^[a-zA-Z0-9_\-\.]*$", max_length=30, error_message='Only A-Z, 0-9, "_", "-", and "." allowed.')
+    password1 = forms.CharField(min_length=5, max_length=30, widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
 
-            DojoPasswordField(field_name='password1',
-                              length=30, maxlength=60,
-                              is_required=True),
+    def clean_username(self):
+        if user_exists(self.clean_data['username']):
+            raise forms.ValidationError("Sorry, this username is taken.")
+        return self.clean_data['username']
 
-            DojoPasswordField(field_name='password2',
-                              length=30, maxlength=60,
-                              is_required=True,
-                              validator_list=[validators.AlwaysMatchesOtherField('password1', 'Passwords must match.')]),
-        )
+    def clean_password2(self):
+        if 'password1' in self.clean_data:
+            if self.clean_data['password1'] != self.clean_data['password2']:
+                raise forms.ValidationError('Passwords must match')
+        return self.clean_data['password2']
 
-    def isValidUsername(self, field_data, all_data):
-        try:
-            User.objects.get(username=field_data)
-        except User.DoesNotExist:
-            return;
-        raise validators.ValidationError('The username "%s" is already taken.' % field_data)
-
-    def save(self, new_data):
-        u = User.objects.create_user(new_data['username'],
-                                     '', # email address can be optionally filled in later
-                                     new_data['password1'])
-        u.save()
-        return u
+    def save(self):
+        if not self.errors:
+            d = dict((k, v.encode("utf8")) for k, v in self.clean_data.iteritems())
+            try:
+                # I hate web sites that require email addresses, so I'm not
+                # going to.  The email address may be optionally filled in later
+                # if a user wants to be able to retrieve their password.
+                user = User.objects.create_user(d['username'],
+                                                '',
+                                                d['password1'])
+            except:
+                # We also check for duplicate users here, since it's possible
+                # that two users could race for a name.
+                if user_exists(username=d['username']):
+                    self.errors['username'] = forms.util.ErrorList(["Sorry, this username is taken."])
+                else:
+                    raise
+            return user
 
 def register(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect('/accounts/profile/')
-
-    manipulator = RegistrationForm()
     redirect_to = request.REQUEST.get(REDIRECT_FIELD_NAME, '')
 
     if request.POST:
-        new_data = request.POST.copy()
-        errors = manipulator.get_validation_errors(new_data)
+        form = RegistrationForm(request.POST)
 
-        if not errors:
-            manipulator.do_html2python(new_data)
-            new_user = manipulator.save(new_data)
+        if not request.session.test_cookie_worked():
+            form.errors['submit'] = forms.util.ErrorList(["Cookies must be enabled."])
 
-            if not redirect_to or '://' in redirect_to or ' ' in redirect_to:
-                redirect_to = '/accounts/profile/'
-            from django.contrib.auth import login
-
-            user = authenticate(username=new_data['username'], password=new_data['password1'])
+        if form.is_valid():
+            user = form.save()
             if user:
+                from django.contrib.auth import login
+                user = authenticate(username=form.clean_data['username'],
+                                    password=form.clean_data['password1'])
+                assert user
                 login(request, user)
                 request.session.delete_test_cookie()
+
+                if not redirect_to or '://' in redirect_to or ' ' in redirect_to:
+                    redirect_to = '/accounts/profile/'
                 return HttpResponseRedirect(redirect_to)
-            else:
-                raise Exception("database integrity problem: newly created user can't authenticate")
     else:
-        errors = {}
-        new_data = {}
+        form = RegistrationForm()
+
     request.session.set_test_cookie()
-    form = forms.FormWrapper(manipulator, new_data, errors)
-    return render_to_response('registration/register.html',
-                              {'form': form},
-                              context_instance=RequestContext(request))
+    return render_to_response('registration/register2.html', RequestContext(request, {'form': form}))
 
 @login_required
 def edit(request):
@@ -102,7 +98,7 @@ def edit(request):
         errors = {}
         new_data = manipulator.flatten_data()
 
-    form = forms.FormWrapper(manipulator, new_data, errors)
+    form = oldforms.FormWrapper(manipulator, new_data, errors)
     return render_to_response('accounts/edit.html',
                               {'form' : form},
                               context_instance=RequestContext(request))
