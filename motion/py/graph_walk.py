75601234e53b2ea17e3ab5636d59cb56fa15f0f5
Evan Sheehan <darthmall@users.noreply.github.com>
2007-01-08 17:17:37 +0000

    Refactor the walks so they take a starting node instead of choosing a starting node; the starting node is now chosen before the walk function is called. Fix the weightedWalk function so that nodes are chosen based on probabilities correctly, so that there is no unmapped range of probabilities. Add comments to the weightedWalk function.

--- motion/py/graph_walk.py
+++ motion/py/graph_walk.py
@@ -13,25 +13,22 @@ from Graph.Data import AdjacencyList, VertexMap
 from Motion.Interpolate import spline
 import Motion, Numeric, pickle, random
 
-def clicheWalk (graph, len):
+def clicheWalk (start, adjList, len):
     """Find a path in graph of length len by following the edges with the
        highest probabilities.
        """
-    vertexMap = graph.representations[VertexMap]
-
-    # Choose a random starting point.
-    u = random.choice ([v for v in vertexMap])
+    u = start
     path = [u.center]
 
     for i in range (len):
         choice = None
         weight = 0
         # Check all the edges associated with this vertex.
-        for edge in vertexMap.query (u):
+        for edge in adjList.query (u):
             # If this vertex is the start of the edge (not the end) and the
             # edge is more likely than any other we've seen, it becomes our next
             # edge.
-            if edge.u is u and edge.weight >= weight:
+            if edge.weight >= weight:
                 weight = edge.weight
                 choice = edge.v
 
@@ -41,28 +38,48 @@ def clicheWalk (graph, len):
 
     return path
 
-def weightedWalk(graph, length):
-    vertexMap = graph.representations[VertexMap]
+def weightedWalk(start, adjList, length):
+    """Perform a weighted graph walk.
 
-    u = random.choice ([v for v in vertexMap])
+    Unlike the cliche walk, this walk does not choose the node with the highest
+    probability. Instead, it assigns a probability range to each node based on
+    its probability and then chooses the node whose probability range contains
+    a randomly generated probability.
+    """
+    # Get the necessary graph representations
+
+    # Choose a random starting point and initialize the path with it
+    u = start
     path = [u.center]
+
     for i in range (length):
-        edges = []
-        weight = random()
-        for edge in vertexMap.query (u):
-            if edge.u is u and weight < edge.weight:
-                edges.append (edge)
+        # Choose a random weight
+        weight = random.random()
+        # Keep a running sum of probabilities
+        p = 0.0
+        # Generate a list of edges coming out of u and sort according to weight
+        edges = [edge for edge in adjList.query(u)]
         edges.sort(cmp=lambda x,y: cmp(x.weight, y.weight))
-        path.append(edges[0].v.center)
+
+        # By iterating over the edges in order of ascending weights and summing
+        # the seen probabilities, we can just compare the randomly generated
+        # weight to the summed probability. If the weight is less than the summed
+        # probability, we know that this is the edge whose probability range
+        # contains the randomly generated weight.
+        for edge in edges:
+            p += edge.weight
+            if weight <= p:
+                path.append(edge.v.center)
+                continue
 
     return path
 
-def randomWalk (graph, len):
+def randomWalk (start, adjList, len):
     """Find a path in graph of langth len by following a random edge."""
     adjacency = graph.representations[AdjacencyList]
 
     # Choose a random starting place.
-    u = random.choice ([v for v in adjacency.iterU ()])
+    u = start
     path = [u.center]
 
     for i in range (len):
@@ -92,18 +109,19 @@ opts, args = parser.parse_args()
 if len (args) < 1:
     parser.error("graph file required")
 
-# print 'Reading graph'
 graphs = pickle.load(open(args[-1]))
 bones = {}
 smoothed = {}
-
-for bone in graphs.keys ():
+adjacencyLists = graphs.representations[AdjacencyList]
+for bone, adjList in adjacencyLists.data.items ():
+    vMap = graphs.representations[VertexMap].data[bone]
+    start = random.choice([v for v in vMap])
     if opts.type == 'cliche':
-        bones[bone] = clicheWalk (graphs[bone], opts.len)
+        bones[bone] = clicheWalk (start, adjList, opts.len)
     elif opts.type == 'random':
-        bones[bone] = randomWalk (graphs[bone], opts.len)
+        bones[bone] = randomWalk (start, adjList, opts.len)
     elif opts.type == 'weighted':
-        bones[bone] = weightedWalk (graphs[bone], opts.len)
+        bones[bone] = weightedWalk (start, adjList, opts.len)
 
     if hasattr(opts, "interpolate"):
         smoothed[bone] = spline(bones[bone], opts.quality)
