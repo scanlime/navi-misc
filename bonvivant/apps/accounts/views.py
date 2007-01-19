abcde4e63bffe6e457d2da8bb1ac8316ab2153de
David Trowbridge <trowbrds@gmail.com>
2007-01-19 10:02:12 +0000

    trim the fat

--- bonvivant/apps/accounts/views.py
+++ bonvivant/apps/accounts/views.py
@@ -1,49 +1,35 @@
-from django import newforms as forms
-from django.conf import settings
-from django.contrib import auth
-from django.http import HttpResponseRedirect
+#
+# bonvivant.apps.accounts.views
+#
+# Copyright (C) 2007 David Trowbridge
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
+from djblets.auth.util import login_required
 
 from bonvivant.apps.recipes.models import Recipe
 
 import datetime
 
-def login_required(view_func):
-    """Simplified version of auth.decorators.login_required,
-       which works with our LOGIN_URL and removes the 'next'
-       parameter which we don't need yet.
-       """
-    def _checklogin(request, *args, **kwargs):
-        if request.user.is_authenticated():
-            return view_func(request, *args, **kwargs)
-        else:
-            return HttpResponseRedirect(settings.LOGIN_URL)
-    _checklogin.__doc__ = view_func.__doc__
-    _checklogin.__dict__ = view_func.__dict__
-    return _checklogin
-
-def get_user(username):
-    try:
-        return auth.models.User.objects.get(username=username)
-    except auth.models.User.DoesNotExist:
-        return None
-
-def internal_login(request, username, password):
-    user = auth.authenticate(username=username, password=password)
-    if user is None:
-        return "Incorrect username or password."
-    elif not user.is_active:
-        return "This account is inactive."
-    elif not request.session.test_cookie_worked():
-        return "Cookies must be enabled."
-
-    auth.login(request, user)
-    request.session.delete_test_cookie()
-    user.last_login = datetime.datetime.now()
-    user.save()
-
-##### User Profile #####
+###########################
+#       User Profile      #
+###########################
 
 @login_required
 def profile(request):
