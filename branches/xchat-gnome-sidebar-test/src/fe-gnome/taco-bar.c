bb95af11487534c7f56165c9f5d758b172dcadd2
lurgyman <lurgyman@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-21 21:57:34 +0000

    fix an irritating focus bug with the taco bar

--- branches/xchat-gnome-sidebar-test/src/fe-gnome/taco-bar.c
+++ branches/xchat-gnome-sidebar-test/src/fe-gnome/taco-bar.c
@@ -356,6 +356,7 @@ taco_bar_pack_page (TacoBar		*taco_bar,
 	gtk_box_pack_start (GTK_BOX (hbox), label, FALSE, FALSE, 0);
 	gtk_box_pack_start (GTK_BOX (hbox), pad2, TRUE, FALSE, 0);
 	gtk_container_add (GTK_CONTAINER (toggle), hbox);
+	GTK_WIDGET_UNSET_FLAGS (toggle, GTK_CAN_FOCUS);
 	gtk_button_set_relief (GTK_BUTTON (toggle), GTK_RELIEF_NONE);
 
 	switch (page_type)
