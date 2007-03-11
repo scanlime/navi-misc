f315900ec7e98266aedf9be4e6d2320f97d22df9
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-03-11 19:54:40 +0000

    Add some CSS and HTML. Added a login page.

--- trunk/character_vitae/urls.py
+++ trunk/character_vitae/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+        (r'^$', 'django.contrib.auth.views.login',
+            {'template_name': 'login.html'}),
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': 'site_media'}),
         (r'^admin/', include('django.contrib.admin.urls')),
