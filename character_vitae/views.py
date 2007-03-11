b03802de8fcf89329e8de26b58ba70961618f4cf
Evan Sheehan <darthmall@users.noreply.github.com>
2007-03-11 21:54:28 +0000

    Display the list of available characters for a user or a link to the new character form.

--- character_vitae/views.py
+++ character_vitae/views.py
@@ -14,13 +14,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+from character_vitae.Character.models import Character
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def profile(request):
     user = User.objects.get(username=request.META["USERNAME"])
-    context = {"name": "%s %s" % (user.first_name, user.last_name)}
+    context = {
+            "name": "%s %s" % (user.first_name, user.last_name),
+            "character_list": Character.objects.filter(player=user).order_by("name")
+            }
     response = render_to_response("profile.html", context)
     return response
 
