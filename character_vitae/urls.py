b0ab562d1ba676ba253e0a5b5ef900b8f8b43acb
Evan Sheehan <darthmall@users.noreply.github.com>
2007-03-11 21:09:28 +0000

    Beginnings of a profile page. This will make more sense when you can actually store characters here.

--- character_vitae/urls.py
+++ character_vitae/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
         (r'^$', 'django.contrib.auth.views.login',
             {'template_name': 'login.html'}),
+        (r'^accounts/profile/$', 'character_vitae.views.profile'),
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': 'site_media'}),
         (r'^admin/', include('django.contrib.admin.urls')),
---
+++ character_vitae/views.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2006 W. Evan Sheehan
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def profile(request):
+    user = User.objects.get(username=request.META["USERNAME"])
+    context = {"name": "%s %s" % (user.first_name, user.last_name)}
+    response = render_to_response("profile.html", context)
+    return response
+
+# vim: ts=4:sw=4:et
