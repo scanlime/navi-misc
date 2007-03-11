5e726e4d36bed1235d853a918db746eeb68d4ffc
Evan Sheehan <darthmall@users.noreply.github.com>
2007-03-11 22:48:46 +0000

    Tried to use form_for_model to get a quick form for new characters, but that doesn't seem to be working all that well. Gonna have to build my own Form object.

--- character_vitae/Character/views.py
+++ character_vitae/Character/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django import newforms as forms
+from django.shortcuts import render_to_response
+from models import Character
+
+def character_form(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = forms.form_for_model(Character)
+        context = {"form": form}
+        response = render_to_response("Character/character_form.html", context)
+
+    response["Content-Type"] = "application/xhtml+xml"
+    return response
+
+# vim: ts=4:sw=4:et
