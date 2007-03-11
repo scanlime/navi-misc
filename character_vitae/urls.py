22c28cf8f49beed54003b0f167d7e15c9f436789
Evan Sheehan <darthmall@users.noreply.github.com>
2007-03-11 22:36:05 +0000

    I prefer double quotes to single these days.

--- character_vitae/urls.py
+++ character_vitae/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-        (r'^$', 'django.contrib.auth.views.login',
-            {'template_name': 'login.html'}),
-        (r'^accounts/profile/$', 'character_vitae.views.profile'),
-        (r'^character/$', include('character_vitae.Character.urls')),
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': 'site_media'}),
-        (r'^admin/', include('django.contrib.admin.urls')),
+urlpatterns = patterns("",
+        (r"^$", "django.contrib.auth.views.login",
+            {"template_name": "login.html"}),
+        (r"^accounts/profile/$", "character_vitae.views.profile"),
+        (r"^character/$", include("character_vitae.Character.urls")),
+        (r"^site_media/(?P<path>.*)$", "django.views.static.serve",
+            {"document_root": "site_media"}),
+        (r"^admin/", include("django.contrib.admin.urls")),
 )
 
 # vim: ts=4:sw=4:et
