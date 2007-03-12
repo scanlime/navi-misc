07e44e3c4d05a796c17c9579c0205b0f252404a9
evan <evan@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-03-12 03:41:45 +0000

    The beginning of a character form. Added a background image, too.

--- trunk/character_vitae/Character/views.py
+++ trunk/character_vitae/Character/views.py
@@ -1,14 +1,20 @@
 from django import newforms as forms
+from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from models import Character
 
 def character_form(request):
+    user = User.objects.get(username=request.META["USER"])
     character_form = forms.form_for_model(Character)
+
     if request.method == "POST":
         form = character_form(request.POST)
     else:
         form = forms.form_for_model(Character)
-        context = {"form": form()}
+        context = {
+                "form": form(),
+                "user": "%s %s" % (user.first_name, user.last_name)
+        }
         response = render_to_response("Character/character_form.html", context)
 
     response["Content-Type"] = "application/xhtml+xml"
