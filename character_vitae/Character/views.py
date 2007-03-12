22127a953cde7624a6f232f86a5e39160470f943
Evan Sheehan <darthmall@users.noreply.github.com>
2007-03-12 02:33:21 +0000

    So the form_for_model returns a class object that you instantiate to get the form. That makes a lot of sense! Django, ftw.

--- character_vitae/Character/views.py
+++ character_vitae/Character/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render_to_response
 from models import Character
 
 def character_form(request):
+    character_form = forms.form_for_model(Character)
     if request.method == "POST":
-        pass
+        form = character_form(request.POST)
     else:
         form = forms.form_for_model(Character)
-        context = {"form": form}
+        context = {"form": form()}
         response = render_to_response("Character/character_form.html", context)
 
     response["Content-Type"] = "application/xhtml+xml"
