07f7d1160d66a2d280fbe47edf46e62286f31d72
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-01-14 04:21:34 +0000

    Replace the old mysql-based daemon with a new version which writes to flat XML and rrdtool files.

--- trunk/therm/daemon/main.c
+++ trunk/therm/daemon/main.c
@@ -1,7 +1,7 @@
 /*
  * main.c - Main loop for the therm daemon. This daemon receives data
- *          from the therm base station and stores it in a remote MySQL
- *          database.
+ *          from the therm base station, and stores each received
+ *          sample by calling a provided shell script.
  *
  * Wireless therm system
  * Copyright (C) 2004-2005 Micah Dowty
@@ -22,59 +22,55 @@
  *
  */
 
-#include <mysql.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <assert.h>
 #include "therm-daemon.h"
 
 #define LOCAL_MS_PER_SAMPLE      4000    /* Approximate milliseconds per local temperature sample */
 #define LOCAL_SAMPLES_PER_STORE  15      /* Number of local samples to average before storing */
 
-void   store_packet              (MYSQL* mysql, struct rx_packet* packet);
-void   store_packet_therm0       (MYSQL* mysql, struct rx_packet* packet);
-void   store_local_temperature   (MYSQL* mysql, struct fraction* temperature);
-void   average_local_temperature (MYSQL* mysql, int temperature);
+struct temperature_info {
+  const char*        sensor_type;
+  int                sensor_index;
+  int                sequence;
+  float              battery_voltage;
+  float              temperature;
+  float              signal_strength;
+};
 
+void   store_packet              (const char *dataDir, struct rx_packet* packet);
+void   store_packet_therm0       (const char *dataDir, struct rx_packet* packet);
+void   average_local_temperature (const char *dataDir, int temperature);
+void   store_temperature_info    (const char *dataDir, struct temperature_info *info);
 
 int main(int argc, char **argv) {
   int fd;
   struct rx_packet* packet;
-  MYSQL mysql;
+  const char *dataDir;
   int t;
 
-  if (argc != 5) {
-    printf("usage: %s <hostname> <username> <password> <database>\n",
-	   argv[0]);
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <data directory>\n", argv[0]);
     return 1;
   }
-
-  mysql_init(&mysql);
-  if (!mysql_real_connect(&mysql, argv[1], argv[2], argv[3],
-			  argv[4], 0, NULL, 0)) {
-    printf("MySQL connection failed: %s\n", mysql_error(&mysql));
-    return 1;
-  }
+  dataDir = argv[1];
 
   /* Loop between connecting and reading... */
   while (1) {
 
     fd = receiver_open();
     if (fd >= 0)
-      printf("Connected to base station.\n");
+      fprintf(stderr, "Connected to base station.\n");
     else
-      printf("Can't open device. Trying again...\n");
+      fprintf(stderr, "Can't open device. Trying again...\n");
 
     /* Do something useful as long as we have a connection */
     while (fd >= 0) {
       packet = receiver_read(fd, LOCAL_MS_PER_SAMPLE);
       if (packet) {
-	/* We have a packet from the radio. Decode it and
-	 * store it somewhere if we can
-	 */
-	db_begin_transaction(&mysql);
-	store_packet(&mysql, packet);
-	db_commit_transaction(&mysql);
+	store_packet(dataDir, packet);
 	packet_free(packet);
       }
       else {
@@ -82,9 +78,7 @@ int main(int argc, char **argv) {
 	 * to sample the local temperature
 	 */
 	if (receiver_get_local_temp(fd, &t) >= 0) {
-	  db_begin_transaction(&mysql);
-	  average_local_temperature(&mysql, t);
-	  db_commit_transaction(&mysql);
+	  average_local_temperature(dataDir, t);
 	}
 	else {
 	  /* This shouldn't fail- reconnect, there could be something up with our device. */
@@ -101,121 +95,145 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-void store_packet(MYSQL* mysql, struct rx_packet* packet)
+void store_packet(const char *dataDir, struct rx_packet* packet)
 {
   int protocol = packet_read_int(packet, 2);
 
   switch (protocol) {
 
   case 0:
-    store_packet_therm0(mysql, packet);
+    store_packet_therm0(dataDir, packet);
     break;
 
   default:
-    printf("Received a packet with unknown protocol %d\n", protocol);
+    fprintf(stderr, "Received a packet with unknown protocol %d\n", protocol);
 
   }
 }
 
-void store_packet_therm0(MYSQL* mysql, struct rx_packet* packet)
+void store_packet_therm0(const char *dataDir, struct rx_packet* packet)
 {
   /* Store a packet using temperature protocol 0 */
-  int packet_id, source_id;
-  int station_id, sequence, temp_total, temp_count;
-  struct fraction voltage;
+  struct temperature_info info;
+  int source_id, temp_total, temp_count;
   static struct rx_packet* previous_packet = NULL;
-  static int previous_packet_id = -1;
 
   /* Is this a duplicate of the last packet? */
   if (!packet_cmp(previous_packet, packet)) {
-    db_packet_mark_duplicate(mysql, previous_packet_id);
+    /* Ignore it */
     return;
   }
 
   /* Verify length */
   if (packet->buffer_bits != 47) {
-    printf("Received a protocol 0 temperature packet with incorrect length (%d bits)\n",
+    fprintf(stderr, "Received a protocol 0 temperature packet with incorrect length (%d bits)\n",
 	   packet->buffer_bits);
     return;
   }
 
   /* Unpack the packet */
-  station_id = packet_read_int(packet, 6);
-  sequence = packet_read_int(packet, 5);
-  voltage.numerator = packet_read_int(packet, 10) * 10;
-  voltage.denominator = 1024;
+  info.sensor_type = "rf";
+  info.sensor_index = packet_read_int(packet, 6);
+  info.sequence = packet_read_int(packet, 5);
+  info.battery_voltage = packet_read_int(packet, 10) / 102.4f;
   temp_total = packet_read_signed_int(packet, 16);
   temp_count = packet_read_int(packet, 8);
 
-  /* Look up a source ID for this packet */
-  source_id = db_find_source(mysql, "rf", 0, station_id);
-  if (source_id < 0)
-    return;
-
-  /* Allocate a new packet ID */
-  packet_id = db_packet_new_full(mysql, source_id,
-				 sequence, &packet->signal_strength);
-  if (packet_id < 0) {
-    printf("Error allocating a therm0 packet\n");
-    return;
-  }
-
-  /* We always have a battery voltage, store that */
-  db_packet_add_batt_volts(mysql, packet_id, &voltage);
-
-  /* Only store temperatures if we actually have any- if the sensor is having
-   * problems or the battery has just been inserted, there won't be any samples.
-   */
   if (temp_count > 0) {
-    struct fraction f;
-    f.numerator = temp_total;
-    f.denominator = temp_count;
-    db_packet_add_temperature(mysql, packet_id, &f, temp_count);
+    info.temperature = temp_total / (float)temp_count;
+    info.signal_strength = packet->signal_strength;
+
+    store_temperature_info(dataDir, &info);
   }
 
   /* This is now the previous packet */
   packet_free(previous_packet);
   previous_packet = packet_dup(packet);
-  previous_packet_id = packet_id;
 }
 
-void store_local_temperature(MYSQL* mysql, struct fraction* temperature)
-{
-  int source_id, packet_id;
-
-  /* Our source is usb:0:0 */
-  source_id = db_find_source(mysql, "usb", 0, 0);
-  if (source_id < 0)
-    return;
-
-  /* Create a new empty packet. We have no sequence number
-   * or signal strength to report
-   */
-  packet_id = db_packet_new(mysql, source_id);
-  if (packet_id < 0)
-    return;
-
-  /* Store our lone temperature average */
-  db_packet_add_temperature(mysql, packet_id, temperature,
-			    LOCAL_SAMPLES_PER_STORE);
-}
-
-void average_local_temperature(MYSQL* mysql, int temperature)
+void average_local_temperature(const char *dataDir, int temperature)
 {
   static int total = 0;
   static int n_samples = 0;
+  static int sequence = 0;
 
   total += temperature;
   n_samples++;
 
   if (n_samples >= LOCAL_SAMPLES_PER_STORE) {
-    struct fraction f;
-    f.numerator = total;
-    f.denominator = n_samples;
-    store_local_temperature(mysql, &f);
+    struct temperature_info info;
+
+    info.sensor_type = "local";
+    info.sensor_index = 0;
+    info.sequence = sequence++;
+    info.battery_voltage = 0;
+    info.temperature = total / (float)n_samples;
+    info.signal_strength = 1.0f;
+
+    store_temperature_info(dataDir, &info);
+
     total = 0;
     n_samples = 0;
   }
+}
+
+void store_temperature_info(const char *dataDir, struct temperature_info *info)
+{
+  char tmpFile[1024];
+  char latestFile[1024];
+  char rrdCommand[1024];
+  FILE *f;
+  time_t now = time(NULL);
+
+  /*
+   * Write the latest temperature data to a per-sensor file.  We write
+   * to a separate file and move it into place, to ensure that the
+   * data is updated atomically.
+   */
+
+  sprintf(tmpFile, "%s/%s%d.tmp", dataDir, info->sensor_type, info->sensor_index);
+  sprintf(latestFile, "%s/%s%d.xml", dataDir, info->sensor_type, info->sensor_index);
+
+  f = fopen(tmpFile, "w");
+  if (!f) {
+    perror(tmpFile);
+    return;
+  }
+
+  fprintf(f, "<temperature-info>\n");
+  fprintf(f, "    <timestamp>%d</timestamp>\n", now);
+  fprintf(f, "    <sensor-type>%s</sensor-type>\n", info->sensor_type);
+  fprintf(f, "    <sensor-index>%d</sensor-index>\n", info->sensor_index);
+  fprintf(f, "    <sequence>%d</sequence>\n", info->sequence);
+  fprintf(f, "    <battery-voltage>%f</battery-voltage>\n", info->battery_voltage);
+  fprintf(f, "    <temperature>%f</temperature>\n", info->temperature);
+  fprintf(f, "    <signal-strength>%f</signal-strength>\n", info->signal_strength);
+  fprintf(f, "</temperature-info>\n");
+  
+  fclose(f);
+  if (rename(tmpFile, latestFile) < 0) {
+    perror(latestFile);
+  }
+
+  /*
+   * Update this sensor's RRD files, using rrdtool.
+   */
+  
+  sprintf(rrdCommand, "rrdtool update %s/%s%d-temperature.rrd %d:%f",
+	  dataDir, info->sensor_type, info->sensor_index, now, info->temperature);
+  system(rrdCommand);
+
+  sprintf(rrdCommand, "rrdtool update %s/%s%d-voltage.rrd %d:%f",
+	  dataDir, info->sensor_type, info->sensor_index, now, info->battery_voltage);
+  system(rrdCommand);
+
+  sprintf(rrdCommand, "rrdtool update %s/%s%d-signal.rrd %d:%f",
+	  dataDir, info->sensor_type, info->sensor_index, now, info->signal_strength);
+  system(rrdCommand);
+
+  sprintf(rrdCommand, "rrdtool update %s/%s%d-sequence.rrd %d:%d",
+	  dataDir, info->sensor_type, info->sensor_index, now, info->sequence);
+  system(rrdCommand);
 }
 
 /* The End */
