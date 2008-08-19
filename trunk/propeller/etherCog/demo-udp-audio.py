c7ffc0acc84f85734d429eb3068505b1e8f22656
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-08-19 10:07:44 +0000

    Implemented a much nicer digital PLL for synchronizing the frequency and phase of demo-udp-audio's output to the stream source. It's kind of high jitter, but it still sounds fine to my untrained ears :P

--- trunk/propeller/etherCog/demo-udp-audio.py
+++ trunk/propeller/etherCog/demo-udp-audio.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 #
-# Client for UDP to Audio example for etherCog
+# Client for UDP to Audio example for etherCog.
+#
+# Usage:
+#   python demo-udp-audio.py <filename> [<filename> ...]
+#
+#   Where <filename> must be a 16-bit uncompressed 44.1 kHz WAV file.
+#   It will play on loop, to the etherCog device at IP_ADDR below.
+#
+#   This program infinitely loops one or more WAV files. If you specify
+#   more than one file, they will play in sequence.
 #
 # Micah Dowty <micah@navi.cx>
 #
 
-UDP_PORT    = 1234
+UDP_PORT    = 6502
 IP_ADDR     = "192.168.1.32"
-BUFFER_SIZE = 512
+BUFFER_SIZE = 1024
 SAMPLE_HZ   = 44100.0
 BYTE_HZ     = SAMPLE_HZ * 4
 
 import socket, time, sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("", 12345))
-s.connect((IP_ADDR, UDP_PORT))
-
-while True:
-    f = open(sys.argv[1], "rb")
+def play(filename):
+    f = open(filename, "rb")
 
     startTime = time.time()
     bytesTotal = 0
 
+    print "Playing %r" % filename
     while True:
         block = f.read(BUFFER_SIZE)
         if not block:
@@ -32,12 +38,14 @@ while True:
         bytesTotal += BUFFER_SIZE
 
         elapsed = bytesTotal / BYTE_HZ
-        print elapsed
         deadline = startTime + elapsed
         delay = deadline - time.time()
         if delay > 0:
             time.sleep(delay)
-        
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((IP_ADDR, UDP_PORT))
 
-    
+while True:
+    for filename in sys.argv[1:]:
+        play(filename)
