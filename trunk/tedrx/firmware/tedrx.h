ff607c33fa5588be6fe48448589963fa0e18b5bc
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-09-28 08:37:57 +0000

    Indent and untabify. This is what I get for writing in C...

--- trunk/tedrx/firmware/tedrx.h
+++ trunk/tedrx/firmware/tedrx.h
@@ -6,7 +6,13 @@
 #define SERIAL_OUT_PIN   PINB4         /* RS-232 output */
 #define ASK_PIN          PINB0         /* Filtered and amplified ASK modulated data. */
 #define MAINS_PIN        PINB5         /* Mains signal, for phase detection */
-//#define DEBUG_PIN	     PINB4         /* Optional debug pin */
+//#define DEBUG_PIN      PINB4         /* Optional debug pin */
+
+/**** Optional debug features: Only used if DEBUG_PIN is set ****/
+
+//#define DEBUG_TMR0
+//#define DEBUG_BASEBAND
+#define DEBUG_RX_BITS
 
 /**** serial.c ****/
 
