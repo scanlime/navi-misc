8642554284b9197c60decc8c3f80d01fc22366fc
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-10-02 08:42:09 +0000

    This could use a bit of polish, but it works. My homebrew receiver is now decoding actual power and voltage readings from the TED transmitter. Also checking in the test harness and spreadsheet I used to determine the linear relationship between the raw data and actual watts/volts.

--- trunk/tedrx/firmware/main.c
+++ trunk/tedrx/firmware/main.c
@@ -1,6 +1,7 @@
 /* -*- Mode: C; c-basic-offset: 4 -*-
  *
- * Work-in-progress: Open source receiver for The Energy Detective
+ * Main program for an open source receiver unit for The Energy
+ * Detective.
  *
  * Copyright (c) 2008 Micah Dowty <micah@navi.cx>
  *
@@ -39,9 +40,20 @@ FUSES = {
     .extended = 0xFF,  /* Defaults */
 };
 
+uint8_t checksum(uint8_t *buf, uint8_t len)
+{
+    uint8_t sum = 0;
+
+    while (len) {
+	sum += *buf;
+	buf++;
+	len--;
+    }
+}
+
 int main(void)
 {
-    uint8_t buf[32];
+    uint8_t buf[16];
 
     filter_init();
     serial_init();
@@ -49,6 +61,7 @@ int main(void)
 
     while (1) {
         uint8_t i, size;
+	uint32_t power, voltage;
 
         size = filter_rx(buf, sizeof buf);
 
@@ -60,15 +73,49 @@ int main(void)
 	 *  0.     Header (0x55)
 	 *  1.     House code
 	 *  2.     Packet counter
-	 *  3-5.   Raw power reading (complement)
-	 *  6-8.   Raw voltage?
-	 *  9-10.  16-bit checksum?
+	 *  3-5.   Raw power reading
+	 *  6-8.   Raw voltage reading
+	 *  9.     Unknown
+	 *  10.    Checksum
+	 *
+	 * The raw voltage and power readings can be converted to
+	 * kilowatts and volts using these experimentally determined
+	 * formulae:
+	 *
+	 *   kW = (raw_power - 16777177) / -62048
+	 *   V  = (raw_voltage - 170350) / 55822
+	 *
+	 * Byte 9 seems to always hover around 250 in my
+	 * measurements. This could be a flag byte, some kind of
+	 * parity signal, or maybe even a measurement of the AC line
+	 * frequency.
+	 *
+	 * Byte 10 is a simple checksum. Add all the bytes in the packet,
+	 * modulo 256, and this byte ensures that the result is zero.
 	 */
 
-        printf("[%02d]", size);
-        for (i = 0; i < size; i++) {
-            printf(" %02x", buf[i]);
-        }
-        printf("\r\n");
+	if (size != 11 || buf[0] != 0x55 || checksum(buf, size) != 0) {
+	    /* Bad packet */
+	    continue;
+	}
+
+	power = (uint32_t)buf[3] | (((uint32_t)buf[4]) << 8) | (((uint32_t)buf[5]) << 16);
+	voltage = (uint32_t)buf[6] | (((uint32_t)buf[7]) << 8) | (((uint32_t)buf[8]) << 16);
+
+	power = -(power - 16777177L);
+	voltage -= 170350L;
+
+	/*
+	 * Print a representation of this packet which is designed
+	 * to be both human- and machine-readable.
+	 */
+
+	printf("HC=%03u KW=%03u.%03u V=%03u.%03u CNT=%03u\r\n",
+	       buf[1],
+	       (int)(power / 62048L),
+	       (int)((power * 100L / 6204L) % 1000),
+	       (int)(voltage / 55822L),
+	       (int)((voltage * 100L / 5582L) % 1000),
+	       buf[2]);
     }
 }
