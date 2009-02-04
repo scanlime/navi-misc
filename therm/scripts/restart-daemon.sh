926bd8584c2ad41f2d363d72ffc0a0ceaaa9d498
Christian Hammond <chipx86@users.noreply.github.com>
2009-02-04 02:21:06 +0000

    Tag Djblets 0.5 alpha 2 for release (r11962)

--- therm/scripts/restart-daemon.sh
+++
@@ -1,5 +0,0 @@
-cd /home/micah/therm
-datadir=/home/httpd/html/therm
-(ps ax | grep therm-daemon | grep -v -q grep) || nohup ./therm-daemon $datadir > $datadir/daemon.stdout 2> $datadir/daemon.stderr
-
-
