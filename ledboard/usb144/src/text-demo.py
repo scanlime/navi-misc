33c274644b8e6e84388d8826f6ded326f5c01f10
Micah Elizabeth Scott <micah@scanlime.org>
2009-03-27 07:26:41 +0000

    A few more LED demos, including the mandatory game of life.

--- ledboard/usb144/src/text-demo.py
+++ ledboard/usb144/src/text-demo.py
@@ -1,3 +1,4 @@
+
 import pygame.font
 
 pygame.font.init()
@@ -6,14 +7,14 @@ fontSize = 9
 yOffset = 2
 f = pygame.font.Font("fonts/Vera.ttf", fontSize)
 
-text = "Hello World, in 12 point Bitstream Vera with antialiasing."
+text = "Quack!"
 surf = f.render(text, True, (15, 15, 15, 255), (0, 0, 0, 255))
 
 while True:
     for xOffset in range(-16, surf.get_width()):
         pixels = []
         for y in range(9):
-            for x in reversed(range(16)):
+            for x in range(16):
                 try:
                     pixels.append("%x" % surf.get_at((
                         x + xOffset, y + yOffset))[0])
