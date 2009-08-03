944a81e920adcf63cb639eb0ba0bc3d32af60002
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-08-03 08:59:32 +0000

    Add the SASC UART core from OpenCores. Receive is working, transmit not so much.

--- trunk/nds/spi-mem-emulator/main.v
+++ trunk/nds/spi-mem-emulator/main.v
@@ -26,6 +26,7 @@
 
 module main(mclk, switch, button,
             ledseg_c, ledseg_a, led,
+            serial_txd, serial_rxd,
             spi_clk, spi_mosi, spi_miso, spi_cs,
             sram_a, sram_oe, sram_we,
             sram1_io, sram1_ce, sram1_ub, sram1_lb,
@@ -38,6 +39,9 @@ module main(mclk, switch, button,
    output [7:0] ledseg_c;
    output [3:0] ledseg_a;
    output [7:0] led;
+
+   output       serial_txd;
+   input        serial_rxd;
 
    input        spi_clk, spi_mosi, spi_cs;
    inout        spi_miso;
@@ -56,7 +60,7 @@ module main(mclk, switch, button,
 
    wire         reset = button[0];
 
-   reg [35:0]   clkdiv;
+   reg [14:0]   clkdiv;
    always @(posedge mclk or posedge reset)
      if (reset)
        clkdiv <= 0;
@@ -77,6 +81,74 @@ module main(mclk, switch, button,
                    spi_clk, spi_mosi, spi_miso, spi_cs,
                    spi_mem_begin_wr, spi_mem_begin_rd, spi_mem_finish,
                    spi_mem_addr, spi_mem_data_wr, spi_mem_data_rd);
+
+
+   /************************************************
+    * RS-232 Serial Port
+    */
+
+   wire         sio_ce, sio_ce_x4;
+   wire         sio_rts_o;
+   wire         sio_cts_i = 0;
+   wire         sio_txfull, sio_rxempty;
+   wire [7:0]   sio_rxd;
+   reg [7:0]    sio_txd;
+   reg          sio_rxe, sio_txe;
+
+   /*
+    * 115200 baud with a 50 MHz master clock:
+    *    Baud x4 clock is 460.8 kHz.
+    *    50 MHz / 460.8 kHz = 108.507 ~ 4*27
+    */
+   sasc_brg sio_brg(mclk, !reset, 8'd2, 8'd27, sio_ce, sio_ce_x4);
+
+   sasc_top sio_uart(mclk, !reset, serial_rxd, serial_txd,
+                     sio_cts_i, sio_rts_o, sio_ce, sio_ce_x4,
+                     sio_txd, sio_rxd, sio_rxe, sio_txe,
+                     sio_txfull, sio_rxempty);
+
+   parameter S_IDLE = 0;
+   parameter S_RX_BYTE = 1;
+   reg       state;
+
+   /*
+    * XXX TODO:
+    *   - Protocol for reading/writing memory
+    *   - Move to separate module
+    */
+
+   always @(posedge mclk or posedge reset)
+     if (reset) begin
+        state <= S_IDLE;
+        sio_txd <= 0;
+        sio_rxe <= 0;
+        sio_txe <= 0;
+     end
+     else case (state)
+
+            S_IDLE: begin
+               if (!sio_rxempty) begin
+                  state <= S_RX_BYTE;
+                  sio_txd <= 0;
+                  sio_rxe <= 1;
+                  sio_txe <= 0;
+               end
+               else begin
+                  state <= S_IDLE;
+                  sio_txd <= 0;
+                  sio_rxe <= 0;
+                  sio_txe <= 0;
+               end
+            end
+
+            S_RX_BYTE: begin
+               state <= S_IDLE;
+               sio_txd <= sio_rxd + 1;
+               sio_rxe <= 0;
+               sio_txe <= 1;
+            end
+
+          endcase
 
 
    /************************************************
@@ -106,13 +178,15 @@ module main(mclk, switch, button,
     * Debug
     */
 
-   wire         pulse_cs, pulse_clk, pulse_wr, pulse_rd;
+   wire         pulse_clk, pulse_wr, pulse_rd;
    pulse_stretcher s1(mclk, reset, !spi_cs, pulse_cs);
    pulse_stretcher s2(mclk, reset, !spi_clk, pulse_clk);
    pulse_stretcher s3(mclk, reset, spi_mem_begin_rd, pulse_rd);
    pulse_stretcher s4(mclk, reset, spi_mem_begin_wr, pulse_wr);
 
-   assign led = { pulse_clk, pulse_cs, 4'b000, pulse_wr, pulse_rd };
+   assign led = { pulse_clk, pulse_cs,
+                  sio_rxe, sio_txe, sio_txfull, sio_rxempty,
+                  pulse_wr, pulse_rd };
    led_hex display(clkdiv[14], reset, spi_mem_addr, ledseg_c, ledseg_a);
 
 endmodule
