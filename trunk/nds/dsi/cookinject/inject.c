82cf13107ab5faddea5ca15ccf8f20cb5b18e883
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-08-06 06:58:40 +0000

    The original exploit code is now a bootloader that pulls in an arbitrarily large binary from the SPI emulator FPGA, then stores it in unused RAM and executes it. That binary can be normal C code, using a subset of newlib and libnds. To show off all the extra RAM we have now, added a logo that is decompressed onto the top screen to replace the game's welcome message.

---
+++ trunk/nds/dsi/cookinject/inject.c
@@ -0,0 +1,54 @@
+/*
+ * Payload for the 'cookinject' exploit.
+ *
+ * This code is loaded from SPI memory by the bootstrapper in
+ * 'exploit.s'.  Our main() is called. When it returns, we return
+ * control to the game.
+ *
+ * Copyright (C) 2009 Micah Dowty <micah@navi.cx>
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ *    with this program; if not, write to the Free Software Foundation, Inc.,
+ *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <string.h>
+#include <nds.h>
+#include "logo.h"
+
+static void setupLogo();
+
+
+void main()
+{
+   setupLogo();
+}
+
+
+void setupLogo()
+{
+   /*
+    * The game already drew a background image on the top screen.
+    * Replace it with our logo. It hasn't drawn the bottom screen
+    * yet, so we can't do much about that right now.
+    */
+   decompress(logoPal, BG_PALETTE, RLEVram);
+   decompress(logoBitmap, BG_GFX, RLEVram);
+
+   /*
+    * Make the logo visible immediately. We don't have to do this,
+    * since the game will fade both screens in.  but it helps with
+    * debugging.
+    */
+   REG_MASTER_BRIGHT = 0;
+}
