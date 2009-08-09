eb640c3d74ea3ba8bd0d545eae5bb65df1a795ff
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-08-09 10:45:03 +0000

    Add a 'cont' command, to continue execution after a hook.

--- trunk/nds/dsi/cookinject/arm7mon.py
+++ trunk/nds/dsi/cookinject/arm7mon.py
@@ -13,18 +13,31 @@ def command(cmd, arg0=0, arg1=0, arg2=0):
         pass
 
 def readBytes(addr, size=0x40):
-    command('R', 0x10, addr, size)
-    return spime.memRead(port, 0x10, size)
+    data = []
+    while size > 0:
+        chunkSize = min(size, 0xF0000)
+        command('R', 0x10, addr, chunkSize)
+        data.append(spime.memRead(port, 0x10, chunkSize))
+        addr += chunkSize
+        size -= chunkSize
+    return ''.join(data)
 
 def writeBytes(addr, data):
-    spime.memWrite(port, 0x10, data)
-    command('W', 0x10, addr, len(data))
+    while data:
+        chunkSize = min(len(data), 0xF0000)
+        spime.memWrite(port, 0x10, data[:chunkSize])
+        command('W', 0x10, addr, chunkSize)
+        addr += chunkSize
+        data = data[chunkSize:]
 
 def write16(addr, word):
     command('H', addr, word)
 
 def write32(addr, word):
     command('L', addr, word)
+
+def cont():
+    command('C')
 
 hd = spime.hexDump
 hdl = lambda s: hd(s, wordLE=True)
