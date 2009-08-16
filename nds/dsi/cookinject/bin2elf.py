b6bd4ad5850694c81bdb239968ca34c068b26e1d
Micah Elizabeth Scott <micah@scanlime.org>
2009-08-16 01:56:09 +0000

    Objcopy was never meant to do crazy things like this. Instead of using objcopy and ld, just write the ELF header manually. Now it works, and the resulting ELF file can be loaded into IDA Pro. Yay!

--- nds/dsi/cookinject/bin2elf.py
+++ nds/dsi/cookinject/bin2elf.py
@@ -4,31 +4,92 @@
 # their load address (%x.bin), into a single .ELF file. The ELF file
 # is written as "memory.elf".
 #
+# This ELF file will have one segment for each input file. It has no
+# section header. This ELF can be loaded into memory by any standard
+# ELF loader, plus it's a convenient way to load many segments into
+# IDA Pro at once.
+#
+# I couldn't get the results I wanted with normal GNU Binutils ld and
+# objcopy, so this just writes an ELF header directly.
+#
 # -- Micah Dowty
 #
 
 import sys
 import os
+import struct
+import binascii
 
-# Parse args
+# XXX: These really should be command line arguments
+outputFile = "memory.elf"
+entryAddr  = 0x2380000
+
+# Parse args, and come up with a name, address, and size for each file.
 files = []
 for file in sys.argv[1:]:
     addr = int(file.split('.')[0], 16)
-    files.append((file, addr))
+    size = os.stat(file).st_size
+    files.append((file, addr, size))
 
 # Sort files by address
 files.sort()
 
-objcopy = 'arm-eabi-objcopy -S -O elf32-littlearm'
+# Output file
+out = open(outputFile, 'wb')
 
-# Make an empty ELF file by adding a dummy Binary section, then removing it.
-os.system('%s -B arm -I binary %s memory.elf --remove-section .data'
-          % (objcopy, files[0][0]))
+# Header sizes
+ehSize    = 0x34
+phentSize = 0x20
+phnum     = len(files)
+phSize    = phentSize * phnum
 
-# Add sections one at a time- objcopy chokes on long lists of added sections.
+def pageAlign(p):
+    return (p + 0xfff) & ~0xfff
 
-for i, (file, addr) in enumerate(files):
-    os.system(('%s -I elf32-littlearm memory.elf memory.elf ' +
-               '--add-section .data.%d=%s --change-section-address .data.%d=0x%x ' +
-               '--set-section-flags .data.%d=CONTENTS,ALLOC,LOAD,DATA')
-              % (objcopy, i, file, i, addr, i))
+# Calculate a file offsets for each input binary
+offsets = [pageAlign(ehSize + phSize)]
+for file, addr, size in files:
+    offsets.append(pageAlign(offsets[-1] + size))
+
+# File header
+out.write(binascii.a2b_hex("7f454c46010101610000000000000000"))  # e_ident
+out.write(struct.pack("<H", 2))           # e_type
+out.write(struct.pack("<H", 0x28))        # e_machine
+out.write(struct.pack("<I", 1))           # e_version
+out.write(struct.pack("<I", entryAddr))   # e_entry
+out.write(struct.pack("<I", ehSize))      # e_phoff
+out.write(struct.pack("<I", 0))           # e_shoff
+out.write(struct.pack("<I", 0))           # e_flags
+out.write(struct.pack("<H", ehSize))      # e_ehsize
+out.write(struct.pack("<H", phentSize))   # e_phentsize
+out.write(struct.pack("<H", phnum))       # e_phnum
+out.write(struct.pack("<H", 0x28))        # e_shentsize
+out.write(struct.pack("<H", 0))           # e_shnum
+out.write(struct.pack("<H", 0))           # e_shstrndx
+
+assert(out.tell() == ehSize)
+
+# Program header
+for i, (file, addr, size) in enumerate(files):
+    offset = offsets[i]
+
+    # Program header entry
+    out.write(struct.pack("<I", 1))       # p_type = PT_LOAD
+    out.write(struct.pack("<I", offset))  # p_offset
+    out.write(struct.pack("<I", addr))    # p_vaddr
+    out.write(struct.pack("<I", addr))    # p_paddr
+    out.write(struct.pack("<I", size))    # p_filesz
+    out.write(struct.pack("<I", size))    # p_memsz
+    out.write(struct.pack("<I", 0x7))     # p_flags = read/write/exec
+    out.write(struct.pack("<I", 1))       # p_align
+
+# Write file contents
+for i, (file, addr, size) in enumerate(files):
+    out.seek(offsets[i])
+
+    infile = open(file, 'rb')
+    while True:
+        block = infile.read(0x10000)
+        if not block:
+            break
+        out.write(block)
