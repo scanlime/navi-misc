57f0961e41e76b6ba96e62ae49b903b508b1b8cd
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-09-01 04:09:41 +0000

    LB/UB support, a hack for burst writes

--- trunk/nds/dsi/ram-tracer/decoder/decoder.c
+++ trunk/nds/dsi/ram-tracer/decoder/decoder.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <ctype.h>
 
 int main(void)
 {
    uint32_t addr = 0;
-   uint16_t data = 0;
-   uint32_t sync_offset = 0;
+   int burst_cnt = 0;
 
    FILE *memcontent = fopen("mem-content.bin", "wb");
    if (!memcontent) {
       perror("open");
       return 1;
    }
-
-   /*
-    * Look for the first sync byte.
-    * XXX: Huge hack!
-    */
-
-   while (!feof(stdin) && fgetc(stdin) != 0xAA)
-      sync_offset++;
-   fgetc(stdin); fgetc(stdin);
-   fprintf(stderr, "Sync at offset %d\n", sync_offset);
 
    while (!feof(stdin)) {
       uint8_t packet[3];
@@ -34,23 +24,59 @@ int main(void)
       if (packet[0] & 0x80) {
          /* Data packet */
 
-         if (packet[0] != 0xaa) {
-            fprintf(stderr, "Data sync byte incorrect!\n");
-            return 1;
+         uint16_t data = (packet[1] << 8) | packet[2];
+         const char *label;
+         int width;
+         uint32_t va = addr << 1;
+
+         switch (packet[0]) {
+
+         case 0xA0:
+            label = "w_word";
+            width = 2;
+            break;
+
+         case 0xA1:
+            label = "w_upper";
+            va++;
+            width = 1;
+            break;
+
+         case 0xA2:
+            label = "w_lower";
+            width = 1;
+            break;
+
+         case 0xA3:
+            label = "w_none";
+            width = 0;
+            break;
+
+         default:
+            label = "w_ERROR";
+            width = 0;
+            break;
          }
 
-         data = (packet[1] << 8) | packet[2];
+         printf("0x%08x = 0x%04x %c%c %s b=%d\n", addr, data,
+                isprint(data >> 8) ? data >> 8 : '.',
+                isprint(data & 0xFF) ? data & 0xFF : '.',
+                label, burst_cnt);
 
-         printf("0x%08x = 0x%04x\n", addr, data);
-
-         fseek(memcontent, addr << 1, SEEK_SET);
-         fwrite(&data, sizeof data, 1, memcontent);
+         // XXX Huge hack for burst writes!
+         if (burst_cnt >= 1 && burst_cnt <= 16) {
+            fseek(memcontent, va, SEEK_SET);
+            fwrite(&data, width, 1, memcontent);
+         }
 
          addr++;
+         burst_cnt++;
 
       } else {
          /* Address packet */
          addr = (packet[0] << 16) | (packet[1] << 8) | packet[2];
+
+         burst_cnt = 0;
       }
     }
 
