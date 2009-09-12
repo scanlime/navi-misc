da0067f736d2f40618d63e0c367870d07bbbcaaf
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-09-12 09:59:45 +0000

    Fix two more hardware bugs: CE1 was being ignored due to a misnamed wire, and ram_d[2] was stuck high due to sharing a pin with HSWAP.

--- trunk/nds/dsi/ram-tracer/hdl/main.v
+++ trunk/nds/dsi/ram-tracer/hdl/main.v
@@ -93,7 +93,7 @@ module main(mclk,
    wire        nfilter_strobe;
 
    ram_sampler ramsam(mclk, reset,
-                      ram_a, ram_d, ram_oe, ram_we, ram_ce1, ram_ce2,
+                      ram_a, ram_d, ram_oe, ram_we, ram_ce1_in, ram_ce2,
                       ram_ub, ram_lb, ram_adv, ram_clk,
                       filter_a, filter_d, filter_ublb, filter_read,
                       filter_write, filter_addr_latch, filter_strobe,
