b7670dc288316a961f250b57864aaa639d62b700
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-09-12 21:21:30 +0000

    The tiniest bit of polish- a usage message, and a copyright header

--- trunk/nds/dsi/ram-tracer/host/main.c
+++ trunk/nds/dsi/ram-tracer/host/main.c
@@ -1,10 +1,35 @@
-#include "fastftdi.h"
+/*
+ * Main file for 'memhost', the host-side component of the RAM Tracer.
+ * This tool configures the hardware and streams captured data to disk.
+ *
+ * Copyright (C) 2009 Micah Dowty
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <signal.h>
+#include "fastftdi.h"
 
-FILE *outputFile;
-bool exitRequested;
+static FILE *outputFile;
+static bool exitRequested;
 
 
 static bool
@@ -68,6 +93,11 @@ int main(int argc, char **argv)
 {
   FTDIDevice dev;
   int err;
+
+  if (argc != 3) {
+     fprintf(stderr, "usage: %s <fpga bitstream> <output file>\n", argv[0]);
+     return 1;
+  }
 
   err = FTDIDevice_Open(&dev);
   if (err) {
