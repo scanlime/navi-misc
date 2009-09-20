25d51aeba8d37909b180979b72d1438018e66710
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-09-20 18:33:22 +0000

    The VT100 codes aren't working so well. Just print a bunch of spaces to erase the status line when displaying log lines.

--- trunk/nds/dsi/ram-tracer/host/iohook_svc.c
+++ trunk/nds/dsi/ram-tracer/host/iohook_svc.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2009 Micah Dowty
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ * of this software and associated documentation files (the "Software"), to
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -18,7 +18,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* OUT OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
 
@@ -51,7 +51,9 @@ static FILE *currentFile;
 static void
 eraseLine(void)
 {
-   fprintf(stderr, "\e[12K\r");
+   char spaces[110];
+   memset(spaces, ' ', sizeof spaces);
+   fprintf(stderr, "%s\r", spaces);
 }
 
 
