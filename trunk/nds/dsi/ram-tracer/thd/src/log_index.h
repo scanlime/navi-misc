568f266c3c3ba2fe059f99d254a6d852a7dfd57e
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-10-04 00:12:24 +0000

    First crude but kinda-functional timeline rendering. Rendered timeline slices are stored in a huge LRU cache, copied to the display via wxPixelData and wxBitmap, and they're rendered by querying the index for LogInstants. Rendering is a total hack right now, so the rendered data is totally meaningless, but is unique for each slice.

--- trunk/nds/dsi/ram-tracer/thd/src/log_index.h
+++ trunk/nds/dsi/ram-tracer/thd/src/log_index.h
@@ -125,7 +125,7 @@ private:
     static const int BLOCK_SIZE = 1 << BLOCK_SHIFT;
     static const int BLOCK_MASK = BLOCK_SIZE - 1;
 
-    static const int STRATUM_SHIFT = 14;             // 16 kB (4096 strata per 16MB)
+    static const int STRATUM_SHIFT = 16;             // 64 kB (256 strata per 16MB)
     static const int STRATUM_SIZE = 1 << STRATUM_SHIFT;
     static const int STRATUM_MASK = STRATUM_SIZE - 1;
 
