cd32b34365feabb39953127bbc1129e16b2ce324
Micah Elizabeth Scott <micah@scanlime.org>
2009-10-18 07:37:36 +0000

    Use wxColour instead of ColorRGB, for compatibility with bushing's Mac gcc.

--- nds/dsi/ram-tracer/thd/src/thd_timeline.cpp
+++ nds/dsi/ram-tracer/thd/src/thd_timeline.cpp
@@ -747,12 +747,12 @@ THDTimelineOverlay::Paint(wxDC &dc)
     if (!visible)
         return;
 
-    wxBrush labelBrush(ColorRGB(0xddffdd));
-    wxPen labelPen(ColorRGB(0x88dd88));
+    wxBrush labelBrush(wxColour(0xdd, 0xff, 0xdd));
+    wxPen labelPen(wxColour(0x88, 0xdd, 0x88));
 
-    wxBrush outlineBrush(ColorRGB(0xdddddd));
-    wxBrush vBrush(ColorRGB(0xff4444));
-    wxBrush hBrush(ColorRGB(0xaaaaff));
+    wxBrush outlineBrush(wxColour(0xdd, 0xdd, 0xdd));
+    wxBrush vBrush(wxColour(0xff, 0x44, 0x44));
+    wxBrush hBrush(wxColour(0xaa, 0xaa, 0xff));
 
     bool inTopHalf = (pos.y >= THDTimeline::SLICE_STRATA_TOP &&
                       pos.y < THDTimeline::SLICE_STRATA_BOTTOM);
